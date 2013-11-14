23e283694a324767733e2471247eb62fea8231cb
Woland2k <sasha@virtoway.com>
2013-11-13 19:28:25 -0800

    - added missing files

---
+++ src/Presentation/Admin/Presentation.Main/Infrastructure/NewsTileItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using VirtoCommerce.ManagementClient.Core.Infrastructure.Tiles;
+
+namespace VirtoCommerce.ManagementClient.Main.Infrastructure
+{
+	public class NewsTileItem : TileItem, INewsTileItem
+	{
+		private IEnumerable<SyndicationItem> newsList;
+		public IEnumerable<SyndicationItem> NewsList
+		{
+			get { return newsList; }
+			set { newsList = value; OnPropertyChanged(); }
+		}
+	}
+}
