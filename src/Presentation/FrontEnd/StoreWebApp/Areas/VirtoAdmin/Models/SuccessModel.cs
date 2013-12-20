2f0c1b09cfddf028bc9b8667282597d5e9fd7ada
Woland2k <sasha@virtoway.com>
2013-12-19 22:13:51 -0800

    updated to include admin download page

---
+++ src/Presentation/FrontEnd/StoreWebApp/Areas/VirtoAdmin/Models/SuccessModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.Web.Areas.VirtoAdmin.Models
+{
+    public class SuccessModel
+    {
+        public string Website { get; set; }
+    }
+}
