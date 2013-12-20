99c01658bdc4ebaafa9d2570e1efa28cb94be64b
aartiomov <andrej.artiomov@gmail.com>
2013-12-20 10:28:09 +0200

    - FIX. Added missing file.

---
+++ src/Presentation/Admin/Presentation.Import/Model/ImportProgress.cs
@@ -0,0 +1,13 @@
+﻿using VirtoCommerce.Foundation.Importing.Model;
+using VirtoCommerce.ManagementClient.Import.Model;
+
+namespace VirtoCommerce.ManagementClient.Import.ViewModel.Implementations
+{
+	public class ImportProgress
+	{
+		public ImportResult ImportResult { get; set; }
+		public ImportEntity ImportEntity { get; set; }
+		public string StatusId { get; set; }
+		public int Processed { get; set; }
+	}
+}
