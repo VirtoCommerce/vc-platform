db4e56d8fb0538dee0985805a8ad172d686e289e
Woland2k <sasha@virtoway.com>
2013-12-23 17:47:53 -0800

    - updated connectionstrings - re-added error handling under install controller

--- src/Presentation/FrontEnd/StoreWebApp/Areas/VirtoAdmin/Controllers/InstallController.cs
+++ src/Presentation/FrontEnd/StoreWebApp/Areas/VirtoAdmin/Controllers/InstallController.cs
@@ -68,7 +68,7 @@ namespace VirtoCommerce.Web.Areas.VirtoAdmin.Controllers
             {
                 return View(model);
             }
-            //try
+            try
             {
                 if (CreateDb(model))
                 {
@@ -76,12 +76,10 @@ namespace VirtoCommerce.Web.Areas.VirtoAdmin.Controllers
                     return Restart();
                 }
             }
-            /*
             catch (Exception err)
             {
                 model.ErrorMessage = err.Message;
             }
-             * */
             return View(model);
         }
 
@@ -141,14 +139,13 @@ namespace VirtoCommerce.Web.Areas.VirtoAdmin.Controllers
             model.ClearMessages();
             Trace.Listeners.Add(traceListener);
             
-            //try
+            try
             {
                 SetupDb(model);
                 model.StatusMessage = "Database successfully created.";
                 AppConfigConfiguration.Instance.Setup.IsCompleted = true;
                 result = true;
             }
-            /*
             catch (AutomaticMigrationsDisabledException err)
             {
                 log.Append(err.Message);
@@ -159,7 +156,6 @@ namespace VirtoCommerce.Web.Areas.VirtoAdmin.Controllers
                 log.Append(err.Message);
                 model.ErrorMessage = err.Message;
             }
-             * */
             
             Session["log"] = log.ToString();
             Trace.Listeners.Remove(traceListener);
