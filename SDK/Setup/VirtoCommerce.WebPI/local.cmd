ef0d17ad1dd116a0a257806019c36c699499571b
Igorisb <ibe@virtoway.com>
2014-01-06 19:57:46 +0200

    WebPI package setup files

---
+++ SDK/Setup/VirtoCommerce.WebPI/local.cmd
@@ -0,0 +1,10 @@
+﻿ECHO delete temp folders
+rmdir /s /q "%TEMP%\WebPI"
+rmdir /s /q "../../../SDK\Setup\VirtoCommerce.WebPI\bin"
+
+ECHO create frontend temp
+robocopy "%TEMP%\FrontEnd" "%TEMP%\WebPI\VirtoCommerce" /mir /NFL /NDL
+robocopy "../../../SDK/Setup/VirtoCommerce.WebPI/Package" "%TEMP%\WebPI" /E /NFL /NDL
+robocopy "..\..\..\src\Extensions\Setup\VirtoCommerce.PowerShell" "%TEMP%\WebPI\VirtoCommerce\App_Data\Virto\SampleData\Database" *.sql /mir /NFL /NDL
+
+"../../../SDK\Setup\VirtoCommerce.WebPI\Tools\7z.exe" a -r ../../../SDK\Setup\VirtoCommerce.WebPI\bin\VirtoCommerce.WebPI.zip %temp%/webpi/*.*
