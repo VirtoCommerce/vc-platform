2b371bdb52811cbe93dd78c51c8887f7f6194a8c
Igorisb <ibe@virtoway.com>
2014-01-27 08:32:03 +0200

    Presentation.Asset missing file fix

---
+++ src/Presentation/Admin/Presentation.Asset/Dialogs/View/InputNameDialogView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VirtoCommerce.ManagementClient.Asset.Dialogs.View
+{
+    /// <summary>
+    /// Interaction logic for InputNameDialogView.xaml
+    /// </summary>
+    public partial class InputNameDialogView : UserControl
+    {
+        public InputNameDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
