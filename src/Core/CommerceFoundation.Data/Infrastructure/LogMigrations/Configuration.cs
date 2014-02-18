0858da619716cee64917f0bc7297580c04ad295e
Woland2k <sasha.la@gmail.com>
2014-02-18 15:24:36 -0800

    - updated config for operationlogs

--- src/Core/CommerceFoundation.Data/Infrastructure/LogMigrations/Configuration.cs
+++ src/Core/CommerceFoundation.Data/Infrastructure/LogMigrations/Configuration.cs
@@ -7,22 +7,11 @@ namespace VirtoCommerce.Foundation.Data.Infrastructure.LogMigrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            ContextKey = "VCF.OperationLogs";
         }
 
         protected override void Seed(VirtoCommerce.Foundation.Data.Infrastructure.OperationLogContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
