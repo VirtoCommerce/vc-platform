c7fcb3991c74218cce7fcb7e70b5fe5a3acd0431
adomass <asu@virtoway.com>
2014-02-27 15:26:28 +0200

    Sitemap improvements

---
+++ src/Presentation/FrontEnd/StoreWebApp/Virto/Helpers/MVC/CatalogNodeProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MvcSiteMapProvider;
+using VirtoCommerce.Web.Client.Extensions;
+using VirtoCommerce.Web.Client.Extensions.Routing;
+
+namespace VirtoCommerce.Web.Virto.Helpers.MVC
+{
+    public class CatalogNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            return new List<DynamicNode>
+            {
+                new DynamicNode
+                {
+                    Action = "Display",
+                    Controller = "Catalog",
+                    PreservedRouteParameters = new[] {Constants.Category},
+                    ChangeFrequency = ChangeFrequency.Always,
+                    UpdatePriority = UpdatePriority.High
+                },
+                new DynamicNode
+                {
+                    Action = "DisplayItem",
+                    Controller = "Catalog",
+                    PreservedRouteParameters = new[] {Constants.Item},
+                    ChangeFrequency = ChangeFrequency.Always,
+                    UpdatePriority = UpdatePriority.High
+                }
+            };
+        }
+    }
+}
