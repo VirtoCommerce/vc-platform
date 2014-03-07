ccce0f46d2a08fb0899af5566b7f775a6ed23cda
adomass <asu@virtoway.com>
2014-03-07 17:12:36 +0200

    cokkie names

--- src/Presentation/FrontEnd/StoreWebApp/Scripts/responsive/main.js
+++ src/Presentation/FrontEnd/StoreWebApp/Scripts/responsive/main.js
@@ -98,7 +98,7 @@
 
 			$('.view-mode a.' + name).addClass('current').siblings().removeClass('current');
 			$('div.' + name).addClass('selected').siblings().removeClass('selected');
-			VirtoCommerce.setCookie("categoryListView",name, 7);
+			VirtoCommerce.setCookie("vcf.categoryListView",name, 7);
 		});
 	};
 
