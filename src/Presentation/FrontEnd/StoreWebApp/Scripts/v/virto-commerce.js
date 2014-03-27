fff942b0bd6cd842ce4c41d8815d3e47a6724bea
adomass <asu@virtoway.com>
2014-03-27 17:36:40 +0200

    #1157 IE 8 (can´t change the Store neither Language or Currency)

--- src/Presentation/FrontEnd/StoreWebApp/Scripts/v/virto-commerce.js
+++ src/Presentation/FrontEnd/StoreWebApp/Scripts/v/virto-commerce.js
@@ -32,6 +32,12 @@
 
 };
 
+if (typeof String.prototype.trim !== 'function') {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
+}
+
 
 VirtoCommerce = function ()
 {
