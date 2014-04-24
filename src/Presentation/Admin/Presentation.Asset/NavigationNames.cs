6f5a7a087f28fb27c043bc03bb2253110c36a039
aartiomov <andrej.artiomov@gmail.com>
2014-04-24 11:38:01 +0300

    - FIX. Asset build fix.

---
+++ src/Presentation/Admin/Presentation.Asset/NavigationNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtoCommerce.Client.Globalization;
+
+namespace VirtoCommerce.ManagementClient.Asset
+{
+	public static class NavigationNames
+	{
+		public const string ModuleName = "Asset";
+
+		public static string Localize(this string source)
+		{
+			return source.Localize(null, ModuleName);
+		}
+	}
+}
