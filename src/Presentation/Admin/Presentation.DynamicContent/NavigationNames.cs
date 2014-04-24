a1fc5b83404644a8746a2624b2d6f16d42924884
aartiomov <andrej.artiomov@gmail.com>
2014-04-24 12:18:44 +0300

    - ADD. Started Dynamic content localization.

--- src/Presentation/Admin/Presentation.DynamicContent/NavigationNames.cs
+++ src/Presentation/Admin/Presentation.DynamicContent/NavigationNames.cs
@@ -1,11 +1,19 @@
-﻿
+﻿using VirtoCommerce.Client.Globalization;
+
 namespace VirtoCommerce.ManagementClient.DynamicContent
 {
-	public class NavigationNames
+	public static class NavigationNames
 	{
 		public const string MenuName = "MarketingMenu"; //"DynamicContentMenu";
 		public const string HomeName = "MarketingHome", //"DynamicContentHome";
             HomeNameDynamicContent = "DynamicContentHome",
                 HomeNameContentPublishing = "ContentPublishingHome";
+
+		public const string ModuleName = "DynamicContent";
+
+		public static string Localize(this string source)
+		{
+			return source.Localize(null, ModuleName);
+		}
     }
 }
