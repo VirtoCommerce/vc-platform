bc3f4a2aa4d7eadf0ba194687e24f653391016dd
aartiomov <andrej.artiomov@gmail.com>
2014-05-01 22:14:43 +0300

    - UPD. Localized Reviews, Reporting modules.

--- src/Presentation/Admin/Presentation.Reporting/NavigationNames.cs
+++ src/Presentation/Admin/Presentation.Reporting/NavigationNames.cs
@@ -1,8 +1,17 @@
-﻿namespace VirtoCommerce.ManagementClient.Reporting
+﻿using VirtoCommerce.Client.Globalization;
+
+namespace VirtoCommerce.ManagementClient.Reporting
 {
-    public class NavigationNames
+    public static class NavigationNames
     {
         public const string MenuName = "ReportingMenu";
         public const string HomeName = "ReportingHome";
+
+		public const string ModuleName = "Reporting";
+
+		public static string Localize(this string source)
+		{
+			return source.Localize(null, ModuleName);
+		}
     }
 }
--- src/Presentation/Admin/Presentation.Reviews/NavigationNames.cs
+++ src/Presentation/Admin/Presentation.Reviews/NavigationNames.cs
@@ -1,10 +1,19 @@
-﻿namespace VirtoCommerce.ManagementClient.Reviews
+﻿using VirtoCommerce.Client.Globalization;
+
+namespace VirtoCommerce.ManagementClient.Reviews
 {
 	public static class NavigationNames
 	{
 		public const string MenuName = "ReviewsMenu",
 			HomeName = "ReviewsHome",
 			CatalogHome = "CatalogHome",
-			CatalogMenu = "CatalogMenu";		 
+			CatalogMenu = "CatalogMenu";
+
+		public const string ModuleName = "Reviews";
+
+		public static string Localize(this string source)
+		{
+			return source.Localize(null, ModuleName);
+		}
 	}
 }
