98557948a932b7469e8e207a966e256f054adcbd
Woland2k <sasha@virtoway.com>
2014-05-07 00:07:54 -0700

    - updated version from 1.9 to 1.10

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -10,11 +10,11 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("VirtoCommerce")]
-[assembly: AssemblyProduct("Virto Commerce 1.9")]
+[assembly: AssemblyProduct("Virto Commerce 1.10")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2014")]
-[assembly: AssemblyFileVersion("1.9.780.0")]
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyInformationalVersion("1.9")]
+[assembly: AssemblyFileVersion("1.10.780.0")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyInformationalVersion("1.10")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
