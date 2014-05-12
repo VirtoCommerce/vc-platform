e92af1efb2c841dd5990bd40b1cf99d53c982d75
Woland2k <sasha@virtoway.com>
2014-05-12 03:11:42 -0700

    - added missing files

---
+++ src/Core/CommerceFoundation/Catalogs/Search/CategoryFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace VirtoCommerce.Foundation.Catalogs.Search
+{
+    using System.Runtime.Serialization;
+    using VirtoCommerce.Foundation.Search;
+
+    [DataContract]
+    public class CategoryFilter : ISearchFilter
+    {
+        [DataMember]
+        public string Key { get; set; }
+
+        [DataMember]
+        public CategoryFilterValue[] Values
+        {
+            get;set;
+        }
+    }
+}
