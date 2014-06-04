9cf29bcf559c1d15ef684eaaf1dd2b50862117f3
adomass <asu@virtoway.com>
2014-06-04 11:24:46 +0300

    Mvc sitemap cache is cleared when all output cache is cleared

--- src/Extensions/Client/CommerceWebClient/Services/Cache/CacheService.cs
+++ src/Extensions/Client/CommerceWebClient/Services/Cache/CacheService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MvcSiteMapProvider;
+using System;
+using VirtoCommerce.Client;
 using VirtoCommerce.Foundation.AppConfig.Services;
+using VirtoCommerce.Foundation.Customers.Services;
 using VirtoCommerce.Foundation.Frameworks;
 using VirtoCommerce.Web.Client.Caching.Interfaces;
 
@@ -10,6 +13,8 @@ namespace VirtoCommerce.Web.Client.Services.Cache
     {
         private readonly ICacheRepository _cacheRepository;
         private readonly IOutputCacheManager _outputCacheManager;
+        private readonly StoreClient _storeClient;
+        private readonly ICustomerSessionService _sessionService;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CacheService"/> class.
@@ -21,19 +26,37 @@ namespace VirtoCommerce.Web.Client.Services.Cache
         /// </summary>
         /// <param name="cacheRepository">The cache repository.</param>
         /// <param name="outputCacheManager">The output cache manager.</param>
-        public CacheService(ICacheRepository cacheRepository, IReadWriteOutputCacheManager outputCacheManager)
-            :this()
+        /// <param name="storeClient"></param>
+        /// <param name="sessionService"></param>
+        public CacheService(ICacheRepository cacheRepository, 
+            IReadWriteOutputCacheManager outputCacheManager, 
+            ICustomerSessionService sessionService,
+            StoreClient storeClient):this()
         {
             _cacheRepository = cacheRepository;
             _outputCacheManager = outputCacheManager;
+            _storeClient = storeClient;
+            _sessionService = sessionService;
         }
 
         public int ClearOuputCache(string controller, string action)
         {
-            if (string.IsNullOrEmpty(controller) && !string.IsNullOrEmpty(action))
+            if (string.IsNullOrEmpty(controller))
             {
-                throw new ArgumentNullException(controller, "Controller must be provided when action not empty");
+                if (!string.IsNullOrEmpty(action))
+                {
+                    throw new ArgumentNullException(controller, "Controller must be provided when action not empty");
+                }
+
+                //Relase site map cache on clear all output cache for all stores
+                foreach (var store in _storeClient.GetStores())
+                {
+                    //Need to set storeId for session because SiteMapStoreCacheKeyGenerator uses it to generate cache key
+                    _sessionService.CustomerSession.StoreId = store.StoreId;
+                    SiteMaps.ReleaseSiteMap();
+                }
             }
+
 
            return _outputCacheManager.RemoveItems(controller, action); 
         }
