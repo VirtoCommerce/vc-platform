6467b17f38d77ff66289a9b07a1d7866886f40c0
adomass <asu@virtoway.com>
2014-07-02 11:55:39 +0300

    Added comment in checkout to orderformproperyvalue

--- src/Presentation/FrontEnd/StoreWebApp/Controllers/CheckoutController.cs
+++ src/Presentation/FrontEnd/StoreWebApp/Controllers/CheckoutController.cs
@@ -816,6 +816,19 @@ namespace VirtoCommerce.Web.Controllers
 
             var form = Ch.OrderForm;
 
+            #region Comment
+
+            var commentProp = form.OrderFormPropertyValues.FirstOrDefault(x => x.Name == "Comment");
+
+            if (commentProp == null)
+            {
+                commentProp = new OrderFormPropertyValue { Name = "Comment" };
+                form.OrderFormPropertyValues.Add(commentProp);
+            }
+            commentProp.LongTextValue = model.Comments;
+            
+            #endregion
+
             #region Process billing address
 
             var billingAddress = Ch.FindAddressByName("Billing");
