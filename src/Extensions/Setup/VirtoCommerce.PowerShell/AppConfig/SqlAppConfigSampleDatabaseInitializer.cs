6db5de18e3c2e804102cbe7f5b0a7864eed329f0
eMazeika <em@virtoway.com>
2014-07-31 09:57:50 +0300

    ADD: UpdateStatisticsWork, DbMigration (stored procedures)

--- src/Extensions/Setup/VirtoCommerce.PowerShell/AppConfig/SqlAppConfigSampleDatabaseInitializer.cs
+++ src/Extensions/Setup/VirtoCommerce.PowerShell/AppConfig/SqlAppConfigSampleDatabaseInitializer.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using VirtoCommerce.PowerShell.DatabaseSetup;
-using System.Reflection;
-using System.IO;
-using VirtoCommerce.Foundation.AppConfig.Model;
-using VirtoCommerce.Foundation.Data.AppConfig;
-using VirtoCommerce.Foundation.Data.AppConfig.Migrations;
+﻿using VirtoCommerce.Foundation.Data.AppConfig;
 
 namespace VirtoCommerce.PowerShell.AppConfig
 {
-	public class SqlAppConfigSampleDatabaseInitializer : SqlAppConfigDatabaseInitializer
-	{
-		protected override void Seed(EFAppConfigRepository context)
-		{
-			base.Seed(context);
-			FillAppConfigScripts(context);
-		}
-
-		private void FillAppConfigScripts(EFAppConfigRepository context)
+    public class SqlAppConfigSampleDatabaseInitializer : SqlAppConfigDatabaseInitializer
+    {
+        protected override void Seed(EFAppConfigRepository context)
         {
-			// fill db with sample data
-            RunCommand(context, "Statistic.sql", "AppConfig");
+            base.Seed(context);
+            FillAppConfigScripts(context);
+        }
+
+        private void FillAppConfigScripts(EFAppConfigRepository context)
+        {
+            // fill db with sample data
             RunCommand(context, "DisplayTemplateMapping.sql", "AppConfig");
             RunCommand(context, "SeoUrlKeyword.sql", "AppConfig");
         }
-	}
+    }
 }
