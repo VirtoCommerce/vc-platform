3ccb3cebe4f6e99fc74f3fad7f5aa0ece07393b5
eMazeika <em@virtoway.com>
2014-09-30 11:22:21 +0300

    UPD: category restriction to real catalogs only removed from selectors

--- src/Presentation/Admin/Presentation.AppConfig/Model/TypedExpressions/DisplayTemplateExpressionBlock.cs
+++ src/Presentation/Admin/Presentation.AppConfig/Model/TypedExpressions/DisplayTemplateExpressionBlock.cs
@@ -9,40 +9,40 @@ using linq = System.Linq.Expressions;
 
 namespace VirtoCommerce.ManagementClient.AppConfig.Model
 {
-	[Serializable]
-	public class DisplayTemplateExpressionBlock : TypedExpressionElementBase, IExpressionAdaptor
-	{
-		private ConditionAndOrBlock _ConditionBlock;
-		public ConditionAndOrBlock ConditionBlock
-		{
-			get
-			{
-				return _ConditionBlock;
-			}
-			private set
-			{
-				_ConditionBlock = value;
-				Children.Add(_ConditionBlock);
-			}
-		}
+    [Serializable]
+    public class DisplayTemplateExpressionBlock : TypedExpressionElementBase, IExpressionAdaptor
+    {
+        private ConditionAndOrBlock _ConditionBlock;
+        public ConditionAndOrBlock ConditionBlock
+        {
+            get
+            {
+                return _ConditionBlock;
+            }
+            private set
+            {
+                _ConditionBlock = value;
+                Children.Add(_ConditionBlock);
+            }
+        }
 
-		#region ctor
-		public DisplayTemplateExpressionBlock(IExpressionViewModel templateViewModel)
-			: base(null, templateViewModel)
-		{
-			InitializeFirstChild();
-			//InitializeAvailableExpressions();
-		}
-		#endregion
+        #region ctor
+        public DisplayTemplateExpressionBlock(IExpressionViewModel templateViewModel)
+            : base(null, templateViewModel)
+        {
+            InitializeFirstChild();
+            //InitializeAvailableExpressions();
+        }
+        #endregion
 
-		private void InitializeFirstChild()
-		{
-			this.ConditionBlock = new ConditionAndOrBlock("if".Localize(), ExpressionViewModel, "of these conditions are true".Localize());
-		}
+        private void InitializeFirstChild()
+        {
+            this.ConditionBlock = new ConditionAndOrBlock("if".Localize(), ExpressionViewModel, "of these conditions are true".Localize());
+        }
 
-		private void InitializeAvailableExpressions()
-		{
-			var availableElements = new Func<CompositeElement>[] { 
+        private void InitializeAvailableExpressions()
+        {
+            var availableElements = new Func<CompositeElement>[] { 
 				()=> { //Items conditions menu items
 					var group = new CompositeElement { DisplayName = "Item conditions".Localize() };
 					group.AvailableChildrenGetters.AddRange(new Func<ExpressionElement>[] {
@@ -52,7 +52,7 @@ namespace VirtoCommerce.ManagementClient.AppConfig.Model
 					});
 					return group;
 				},
-				()=> { //Items conditions menu items
+				()=> { //Category conditions menu items
 					var group = new CompositeElement { DisplayName = "Category conditions".Localize() };
 					group.AvailableChildrenGetters.AddRange(new Func<ExpressionElement>[] {
 						()=> new ConditionCategoryIs(this.ExpressionViewModel),
@@ -60,60 +60,60 @@ namespace VirtoCommerce.ManagementClient.AppConfig.Model
 					return group;
 				},
             };
-			ConditionBlock.WithAvailabeChildren(availableElements);
-			ConditionBlock.NewChildLabel = "+ add condition".Localize(null, LocalizationScope.DefaultCategory);
-		}
+            ConditionBlock.WithAvailabeChildren(availableElements);
+            ConditionBlock.NewChildLabel = "+ add condition".Localize(null, LocalizationScope.DefaultCategory);
+        }
 
-		public void ResetChildren()
-		{
-			this.Children.Clear();
-			InitializeFirstChild();
+        public void ResetChildren()
+        {
+            this.Children.Clear();
+            InitializeFirstChild();
 
-		}
+        }
 
-		public void InitializeAvailableExpressions(TargetTypes targetType)
-		{
-			ConditionBlock.AvailableChildrenGetters.Clear();
-			switch (targetType)
-			{
-				case TargetTypes.Item:
-					//Items conditions menu items;
-					var availableItemElements = new Func<CompositeElement>[] { 
+        public void InitializeAvailableExpressions(TargetTypes targetType)
+        {
+            ConditionBlock.AvailableChildrenGetters.Clear();
+            switch (targetType)
+            {
+                case TargetTypes.Item:
+                    //Items conditions menu items;
+                    var availableItemElements = new Func<CompositeElement>[] { 
 						()=> new ConditionEntryIs(this.ExpressionViewModel),
 						()=> new ConditionItemTypeIs(this.ExpressionViewModel),
 						()=> new ConditionItemPropertyIs(this.ExpressionViewModel),
 						()=> new ConditionItemsCategoryIs(this.ExpressionViewModel),
 						()=> new ConditionItemStoreIs(this.ExpressionViewModel),
 					};
-					ConditionBlock.WithAvailabeChildren(availableItemElements);
-					break;
-				case TargetTypes.Category:
-					//Category conditions menu items;
-					var availableCategoryElements = new Func<CompositeElement>[] { 
+                    ConditionBlock.WithAvailabeChildren(availableItemElements);
+                    break;
+                case TargetTypes.Category:
+                    //Category conditions menu items;
+                    var availableCategoryElements = new Func<CompositeElement>[] { 
 						()=> new ConditionCategoryIs(this.ExpressionViewModel),
 						()=> new ConditionCategoryPropertyIs(this.ExpressionViewModel),
 						()=> new ConditionCategoryIsSubcategory(this.ExpressionViewModel),
 						()=> new ConditionCategoryStoreIs(this.ExpressionViewModel),
 					};
-					ConditionBlock.WithAvailabeChildren(availableCategoryElements);
-					break;
-			}
+                    ConditionBlock.WithAvailabeChildren(availableCategoryElements);
+                    break;
+            }
 
-			ConditionBlock.NewChildLabel = "+ add condition".Localize(null, LocalizationScope.DefaultCategory);
-		}
+            ConditionBlock.NewChildLabel = "+ add condition".Localize(null, LocalizationScope.DefaultCategory);
+        }
 
-		public override void InitializeAfterDeserialized(IExpressionViewModel parentViewModel)
-		{
-			base.InitializeAfterDeserialized(parentViewModel);
-			InitializeAvailableExpressions();
+        public override void InitializeAfterDeserialized(IExpressionViewModel parentViewModel)
+        {
+            base.InitializeAfterDeserialized(parentViewModel);
+            InitializeAvailableExpressions();
 
-		}
+        }
 
-		public linq.Expression<Func<IEvaluationContext, bool>> GetExpression()
-		{
-			var retVal = ConditionBlock.GetExpression();
-			return retVal;
-		}
+        public linq.Expression<Func<IEvaluationContext, bool>> GetExpression()
+        {
+            var retVal = ConditionBlock.GetExpression();
+            return retVal;
+        }
 
-	}
+    }
 }
