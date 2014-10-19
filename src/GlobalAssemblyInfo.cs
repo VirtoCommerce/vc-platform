5626cbf80ca32bfdf077b508a7e4899c6b269608
build <ci@virtocommerce.com>
2014-10-19 09:27:47 +0000

    Updated version number

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("VirtoCommerce")]
 [assembly: AssemblyProduct("Virto Commerce 1.12")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2014")]
-[assembly: AssemblyFileVersion("1.12.941.0")]
+[assembly: AssemblyFileVersion("1.12.942.0")]
 [assembly: AssemblyVersion("1.12.0.0")]
 [assembly: AssemblyInformationalVersion("1.12")]
 
