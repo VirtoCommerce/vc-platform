a5bb92aafe56b675a1a8bdce440a2b5ad98b5cf9
aartiomov <andrej.artiomov@gmail.com>
2014-11-13 17:30:23 +0200

    - ADD. Slab logging added.

---
+++ src/Extensions/Logging/EventSources/EventSourceExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace VirtoCommerce.Slab.EventSources
+{
+	public static class EventSourceExtension
+	{
+		public static void Write(this EventSource log, int eventCode, object context)
+		{
+			try
+			{
+				var type = context.GetType();
+				var method =
+					log.GetType().GetMethods().FirstOrDefault(x => x.GetCustomAttributes(typeof(EventAttribute), true).Any() && ((EventAttribute)x.GetCustomAttributes(typeof(EventAttribute), true).First()).EventId == eventCode);
+				if (method != null)
+				{
+					var parameters = new List<object>();
+					method.GetParameters().ToList().ForEach(value => parameters.Add(type.GetProperty(value.Name).GetValue(context)));
+					method.Invoke(log, parameters.ToArray());
+				}
+			}
+			catch (Exception)
+			{
+				//try log error
+			}
+			
+		}
+	}
+}
---
+++ src/Extensions/Logging/SlabInvoker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using VirtoCommerce.Slab.Contexts;
+using VirtoCommerce.Slab.EventSources;
+
+namespace VirtoCommerce.Slab
+{
+    public class SlabInvoker : SlabInvoker<BaseSlabContext>
+    {
+    }
+
+    public class SlabInvoker<T> where T : BaseSlabContext
+    {
+        public SlabInvoker()
+        {
+            this.Context = Activator.CreateInstance<T>();
+        }
+
+        public delegate void SlabMethod(T context);
+        public delegate void WrappedMethod(T context);
+        public T Context { get; set; }
+
+        public static SlabInvoker<T> Execute(WrappedMethod codeBody)
+        {
+            var slabInvoker = new SlabInvoker<T>();
+
+            try
+            {
+                slabInvoker.Context.Start();
+                codeBody.Invoke(slabInvoker.Context);  
+            }
+            catch (Exception ex)
+            {
+                slabInvoker.Context.Error = SlabExtendedException.Create(ex);
+            }
+            finally
+            {
+                slabInvoker.Context.Stop();
+            }
+
+            return slabInvoker;
+        }
+
+        public SlabInvoker<T> OnSuccess(SlabMethod codeBody)
+        {
+            if (!this.Context.HasError)
+            {
+                codeBody.Invoke(this.Context);
+            }
+
+            return this;
+        }
+
+		public SlabInvoker<T> OnSuccess(EventSource eventSource, int eventCode)
+		{
+			if (!this.Context.HasError)
+			{
+				eventSource.Write(eventCode, this.Context);
+			}
+			return this;
+		}
+
+        public SlabInvoker<T> OnError(SlabMethod codeBody)
+        {
+            if (this.Context.HasError)
+            {
+                codeBody.Invoke(this.Context);
+            }
+
+            return this;
+        }
+
+		public SlabInvoker<T> OnError(EventSource eventSource, int eventCode)
+		{
+			if (this.Context.HasError)
+			{
+				eventSource.Write(eventCode, this.Context);
+			}
+
+			return this;
+		}
+
+        public SlabInvoker<T> OnFinished(SlabMethod codeBody)
+        {
+            codeBody.Invoke(this.Context);
+            return this;
+        }
+
+		public SlabInvoker<T> OnFinished(EventSource eventSource, int eventCode)
+		{
+			eventSource.Write(eventCode, this.Context);
+			return this;
+		}
+    }
+}
