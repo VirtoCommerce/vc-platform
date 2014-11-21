4f564693be09f9d0252c075eb1d25fc1c799c365
E.Tatarincev <et@virtoway.com>
2014-11-21 12:26:15 +0200

    -ADD GetSlug controller method

--- src/Core/CommerceFoundation/Frameworks/Extensions/StringExtensions.cs
+++ src/Core/CommerceFoundation/Frameworks/Extensions/StringExtensions.cs
@@ -131,5 +131,23 @@ namespace VirtoCommerce.Foundation.Frameworks.Extensions
         {
             return Regex.Replace(attribute, string.Format("([{0}])", "/[!\"#$%&'()*+,./:;<=>?@^`{|}~\\]"), @"\\$1");
         }
+
+		public static string GenerateSlug(this string phrase)
+		{
+			string str = phrase.RemoveAccent().ToLower();
+
+			str = Regex.Replace(str, @"[^a-z0-9\s-]", ""); // invalid chars           
+			str = Regex.Replace(str, @"\s+", " ").Trim(); // convert multiple spaces into one space   
+			str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim(); // cut and trim it   
+			str = Regex.Replace(str, @"\s", "-"); // hyphens   
+
+			return str;
+		}
+
+		public static string RemoveAccent(this string txt)
+		{
+			byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+			return System.Text.Encoding.ASCII.GetString(bytes);
+		}
     }
 }
