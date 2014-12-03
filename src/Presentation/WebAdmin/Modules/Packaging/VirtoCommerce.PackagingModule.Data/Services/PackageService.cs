a5e940867bca92b7aa9046beb65ba729ac8d5f76
artem-dudarev <ad@virtoway.com>
2014-12-03 14:39:35 +0200

    Added projects for Packaging Module.

---
+++ src/Presentation/WebAdmin/Modules/Packaging/VirtoCommerce.PackagingModule.Data/Services/PackageService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtoCommerce.PackagingModule.Model;
+using VirtoCommerce.PackagingModule.Services;
+
+namespace VirtoCommerce.PackagingModule.Data.Services
+{
+	public class PackageService : IPackageService
+	{
+		#region IPackageService Members
+
+		public IEnumerable<Package> List()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Install(string packageId, string version)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(string packageId, string version)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Uninstall(string packageId)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ src/Presentation/WebAdmin/Modules/Packaging/VirtoCommerce.PackagingModule/Services/IPackageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtoCommerce.PackagingModule.Model;
+
+namespace VirtoCommerce.PackagingModule.Services
+{
+	public interface IPackageService
+	{
+		IEnumerable<Package> List();
+		void Install(string packageId, string version);
+		void Update(string packageId, string version);
+		void Uninstall(string packageId);
+	}
+}
