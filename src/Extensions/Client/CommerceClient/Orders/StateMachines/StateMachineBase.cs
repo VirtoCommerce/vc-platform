2369de031745e3031dc1ef85f6375a12313f324d
artem-dudarev <ad@virtoway.com>
2014-12-03 17:51:38 +0200

    Fixed some CLS compliance warnings.

--- src/Extensions/Client/CommerceClient/Orders/StateMachines/StateMachineBase.cs
+++ src/Extensions/Client/CommerceClient/Orders/StateMachines/StateMachineBase.cs
@@ -6,51 +6,51 @@ using VirtoCommerce.Foundation.Orders.StateMachines;
 
 namespace VirtoCommerce.Client.Orders.StateMachines
 {
-    public abstract class StateMachineBase<T> : IStateMachine<T>
-    {
-        protected List<StateTransitionDef<T>> _transistions = new List<StateTransitionDef<T>>();
-        protected StateTransitionDef<T> RegisterStatus(T state)
-        {
-            var retVal = _transistions.FirstOrDefault(x => x.CurrentStatus.Equals(state));
-            if (retVal == null)
-            {
-                retVal = new StateTransitionDef<T>(state);
-                _transistions.Add(retVal);
-            }
+	public abstract class StateMachineBase<T> : IStateMachine<T>
+	{
+		protected List<StateTransitionDef<T>> Transitions = new List<StateTransitionDef<T>>();
+		protected StateTransitionDef<T> RegisterStatus(T state)
+		{
+			var retVal = Transitions.FirstOrDefault(x => x.CurrentStatus.Equals(state));
+			if (retVal == null)
+			{
+				retVal = new StateTransitionDef<T>(state);
+				Transitions.Add(retVal);
+			}
 
-            return retVal;
-        }
+			return retVal;
+		}
 
-        #region IStateMachine<T> Members
-        public IEnumerable<T> AllRegisteredStatuses
-        {
-            get
-            {
-                return _transistions.Select(x => x.CurrentStatus);
-            }
-        }
+		#region IStateMachine<T> Members
+		public IEnumerable<T> AllRegisteredStatuses
+		{
+			get
+			{
+				return Transitions.Select(x => x.CurrentStatus);
+			}
+		}
 
-        public IEnumerable<T> GetAvailableTransitions(T status)
-        {
-            IEnumerable<T> result;
+		public IEnumerable<T> GetAvailableTransitions(T status)
+		{
+			IEnumerable<T> result;
 
-            var currentState = _transistions.FirstOrDefault(x => x.CurrentStatus.Equals(status));
-            if (currentState == null)
-            {
-                result = Enumerable.Empty<T>();
-            }
-            else
-            {
-                result = currentState.AllTransitions.Select(x => x.CurrentStatus);
-            }
+			var currentState = Transitions.FirstOrDefault(x => x.CurrentStatus.Equals(status));
+			if (currentState == null)
+			{
+				result = Enumerable.Empty<T>();
+			}
+			else
+			{
+				result = currentState.AllTransitions.Select(x => x.CurrentStatus);
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        public bool IsTransitionAvailable(T curentStatus, T newStatus)
-        {
-            return GetAvailableTransitions(curentStatus).Contains(newStatus);
-        }
-        #endregion
-    }
+		public bool IsTransitionAvailable(T curentStatus, T newStatus)
+		{
+			return GetAvailableTransitions(curentStatus).Contains(newStatus);
+		}
+		#endregion
+	}
 }
