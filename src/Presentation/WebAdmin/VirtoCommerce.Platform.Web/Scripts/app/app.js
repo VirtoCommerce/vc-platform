f468491e78db12f5f55afeb3195bd4d23627b7f4
Mikhail Beliaev <mbe@virtoway.com>
2014-12-04 17:55:48 +0200

    -ADD HTTP error hendler

--- src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Scripts/app/app.js
+++ src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Scripts/app/app.js
@@ -19,33 +19,45 @@
   'platformWebApp.breadcrumbs'
 ];
 
-angular.module('platformWebApp', AppDependencies)
-.controller('appCtrl', ['$scope', '$state', 'mainMenuService', 'notificationService', function ($scope, $state, mainMenuService, notificationService) {
+angular.module('platformWebApp', AppDependencies).
+  constant('HTTP_DEFAULT_ERROR_MSG', 'An error has occured. Please contact customer support for assistance.').
+  constant('HTTP_NETWORK_ERROR_MSG', 'Unable to communicate with the server. Make sure you are connected to the internet and try again.').
+  controller('appCtrl', ['$scope', '$state', '$http', 'mainMenuService', 'notificationService', function ($scope, $state, $http, mainMenuService, notificationService) {
 	
 	notificationService.run();
 
     $scope.curentStateName = function () {
         return $state.current.name;
     };
+   
 }])
 .config(
-  ['$stateProvider', function ($stateProvider) {
-  	$stateProvider
-		.state('workspace', {
-			abstract: true,
-			templateUrl: 'Scripts/app/workspace.tpl.html'
-		});
+  ['$stateProvider', '$httpProvider', function ($stateProvider, $httpProvider) {
+    $stateProvider
+    .state('workspace', {
+      abstract: true,
+      templateUrl: 'Scripts/app/workspace.tpl.html'
+    });
+    $httpProvider.interceptors.push(function($rootScope) {
+      return {
+        requestError: function(rejection) {
+          $rootScope.$broadcast('httpRequestError', rejection);
+        },
+        responseError: function(response) {
+          $rootScope.$broadcast('httpResponseError', response);
+        }
+      };
+    });
   }
   ]
 )
 .run(
-  ['$rootScope', '$state', '$stateParams', 'authService', 'mainMenuService', 'editableOptions',
-    function ($rootScope, $state, $stateParams, authService, mainMenuService, editableOptions) {
+  ['$rootScope', '$state', '$stateParams', 'authService', 'mainMenuService', 'editableOptions', 'notificationService',
+    function ($rootScope, $state, $stateParams, authService, mainMenuService, editableOptions, notificationService) {
         editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
-
         var homeMenuItem = {
         	path: 'home',
         	title: 'Home',
@@ -76,6 +88,16 @@ angular.module('platformWebApp', AppDependencies)
             $state.go('loginDialog');
         });
 
+        $rootScope.$on('httpRequestError', function(event, rejection) {
+            notificationService.error({title: 'Some request error', description: 'Your request is not correct'});
+            console.log(rejection)
+        });
+
+        $rootScope.$on('httpResponseError', function(event, response) {
+            notificationService.error({title: 'HTTP server error', description: 'Sorry, but server does not answerd. Error data: ' + response.status + ' — ' + response.statusText});
+            console.log(response)
+        });
+
         $rootScope.$on('loginStatusChanged', function (event, authContext) {
             if (authContext.isAuthenticated) {
                 $state.go(homeMenuItem.state);
@@ -89,3 +111,4 @@ angular.module('platformWebApp', AppDependencies)
     }
   ]
 );
+
