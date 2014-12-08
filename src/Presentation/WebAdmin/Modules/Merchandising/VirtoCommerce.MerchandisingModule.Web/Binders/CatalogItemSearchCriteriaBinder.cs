5ed156b7cf8ddf8b15fae0e60b8abb1082332ebc
adomass <asu@virtoway.com>
2014-12-08 15:38:08 +0200

    Added sort field to criteria binder, added review total and avg fields to CatalogItem

--- src/Presentation/WebAdmin/Modules/Merchandising/VirtoCommerce.MerchandisingModule.Web/Binders/CatalogItemSearchCriteriaBinder.cs
+++ src/Presentation/WebAdmin/Modules/Merchandising/VirtoCommerce.MerchandisingModule.Web/Binders/CatalogItemSearchCriteriaBinder.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.ModelBinding;
 using VirtoCommerce.Foundation.Catalogs.Search;
 using VirtoCommerce.Foundation.Frameworks.Extensions;
+using VirtoCommerce.Foundation.Search;
 using VirtoCommerce.MerchandisingModule.Web.Model;
 
 namespace VirtoCommerce.MerchandisingModule.Web.Binders
@@ -51,10 +53,48 @@ namespace VirtoCommerce.MerchandisingModule.Web.Binders
 
 			
 			result.SearchPhrase = qs["q"].EmptyToNull();
-			//result.ClassTypes.Add("Product");
+			result.ClassTypes.Add("Product");
 			result.RecordsToRetrieve = qs["take"].TryParse(20);
 			result.StartingRecord = qs["skip"].TryParse(0);
 			result.Pricelists = null;
+
+		    var sortQuery = qs["sort"].EmptyToNull();
+            var sort = string.IsNullOrEmpty(sortQuery) ? "name" : sortQuery;
+		    var sortOrder = qs["sortorder"].EmptyToNull();
+
+            var isDescending = "desc".Equals(sortOrder, StringComparison.OrdinalIgnoreCase);
+
+            SearchSort sortObject = null;
+
+            switch (sort.ToLowerInvariant())
+            {
+                case "price":
+                    sortObject = new SearchSort("price", isDescending);
+                    break;
+                case "position":
+                    //TODO: need to get catalogId and categoryId
+                    //sortObject = new SearchSort(new SearchSortField(string.Format("sort{0}{1}", session.CatalogId, session.CategoryId).ToLower())
+                    //{
+                    //    IgnoredUnmapped = true,
+                    //    IsDescending = isDescending
+                    //});
+                    break;
+                case "name":
+                    sortObject = new SearchSort("name", isDescending);
+                    break;
+                case "rating":
+                    sortObject = new SearchSort(result.ReviewsAverageField, isDescending);
+                    break;
+                case "reviews":
+                    sortObject = new SearchSort(result.ReviewsTotalField, isDescending);
+                    break;
+                default:
+                    sortObject = CatalogItemSearchCriteria.DefaultSortOrder;
+                    break;
+
+            }
+
+            result.Sort = sortObject;
 
 
 			bindingContext.Model = result;
