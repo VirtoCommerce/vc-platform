00e2924fca00f7ec4992d7f5e6eadeb4d6274a3a
eMazeika <em@virtoway.com>
2014-12-11 19:59:03 +0200

    modulesUI - initial scripts added

---
+++ src/Presentation/WebAdmin/Modules/Packaging/VirtoCommerce.PackagingModule.Web/Scripts/resources/modules.js
@@ -0,0 +1,9 @@
+﻿angular.module('virtoCommerce.packaging.resources.modules', [])
+.factory('modules', ['$resource', function ($resource) {
+
+    return $resource('api/modules/:id', { id: '@id' }, {
+        getModules: { method: 'GET', url: 'api/modules', isArray: true },
+        get: { method: 'GET', url: 'api/modules/:id', isArray: false },
+    });
+}]);
+
