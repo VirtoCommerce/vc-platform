2ff5d8053a99d348770d6adc1c46f577304e6f1d
adomass <asu@virtoway.com>
2014-12-16 17:40:59 +0200

    security impl for external identity service

---
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Security/Models/ApplicationUserExtended.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtoCommerce.Foundation.Data.Security.Identity;
+
+namespace VirtoCommerce.CoreModule.Web.Security.Models
+{
+    public class ApplicationUserExtended : ApplicationUser
+    {
+        public string FullName { get; set; }
+
+        public string StoreId { get; set; }
+
+        public string Icon { get; set; }
+
+        public string[] Permissions { get; set; }
+    }
+}
