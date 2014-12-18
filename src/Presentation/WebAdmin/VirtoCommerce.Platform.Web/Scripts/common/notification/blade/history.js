d5f34175cc7e3ff5564e7843fe4ee467b28dc4b3
Mikhail Beliaev <mbe@virtoway.com>
2014-12-18 18:22:40 +0200

    -ADD page navigatin in notify blade and return styles

--- src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Scripts/common/notification/blade/history.js
+++ src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Scripts/common/notification/blade/history.js
@@ -5,15 +5,40 @@ angular.module('notifications.blades.history', [
 .controller('notificationsHistoryController', ['$scope', 'bladeNavigationService', 'notificationDetailResolver', 'notifications',
 function ($scope, bladeNavigationService, notificationDetailResolver, notifications) {
 
+    $scope.pageSettings = {};
+    $scope.pageSettings.totalItems = 0;
+    $scope.pageSettings.currentPage = 1;
+    $scope.pageSettings.numPages = 5;
+    $scope.pageSettings.itemsPerPageCount = 10;
+    $scope.pageSettings.orderBy = 'created';
+    $scope.pageSettings.order = 'DESC';
     $scope.notifications = [];
+
     $scope.blade.refresh = function () {
         $scope.blade.isLoading = true;
-
-        notifications.query({start:0, count: 1000 }, function (data, status, headers, config) {
+        var start = $scope.pageSettings.currentPage * $scope.pageSettings.itemsPerPageCount - $scope.pageSettings.itemsPerPageCount;
+        notifications.query({start:start, count: $scope.pageSettings.itemsPerPageCount }, function (data, status, headers, config) {
+            console.log(data);
             $scope.notifications = data.notifyEvents;
+            $scope.pageSettings.totalItems = angular.isDefined(data.totalCount) ? data.totalCount : 0;
             $scope.blade.isLoading = false;
         });
     };
+
+    $scope.setOrder = function(field) {
+        if($scope.pageSettings.orderBy === field) {
+            $scope.pageSettings.order = ($scope.pageSettings.order === 'DESC')?'ASC':'DESC';
+        } else {
+            $scope.pageSettings.orderBy = field;
+            $scope.pageSettings.order = 'DESC';
+        }
+    }
+
+    $scope.$watchGroup(['pageSettings.currentPage',
+                        'pageSettings.orderBy',
+                        'pageSettings.order'], function () {
+        $scope.blade.refresh();
+    });
 
 	//Excecute notify detail action
     $scope.selectNotify = function (notify) {
