2822fe417358027cf500c335c30ca4c1f143a0a1
eMazeika <em@virtoway.com>
2014-12-24 15:34:47 +0200

    catalog export UI added

---
+++ src/Presentation/WebAdmin/Modules/Catalog/VirtoCommerce.CatalogModule.Web/Scripts/app/catalog/wizards/export/export-wizard-format-step.js
@@ -0,0 +1,20 @@
+﻿angular.module('catalogModule.wizards.exportWizard')
+.controller('exportFormatController', ['$scope', function ($scope) {
+    $scope.blade.refresh = function () {
+        $scope.selectedImporter = $scope.blade.parentBlade.item.entityImporter;
+        $scope.blade.isLoading = false;
+        $scope.importers = [
+            "Csv"
+        ];
+
+    };
+
+    $scope.setImporter = function (importer) {
+        $scope.selectedImporter = importer;
+        $scope.blade.parentBlade.item.entityImporter = importer;
+        $scope.bladeClose();
+    }
+
+    $scope.blade.refresh();
+
+}]);
