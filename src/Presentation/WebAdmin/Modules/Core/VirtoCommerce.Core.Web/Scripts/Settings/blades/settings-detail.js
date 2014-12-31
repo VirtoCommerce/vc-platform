0e17fca69513529edae1999b9b3580dae6ce25ca
eMazeika <em@virtoway.com>
2014-12-31 17:38:20 +0200

    ADD: settings management UI (initial)

---
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Scripts/Settings/blades/settings-detail.js
@@ -0,0 +1,59 @@
+﻿angular.module('virtoCommerce.coreModule.settings.blades')
+.controller('settingsDetailController', ['$scope', 'dialogService', 'bladeNavigationService', 'settings', function ($scope, dialogService, bladeNavigationService, settings) {
+    $scope.settingGroups = [{ name: '-' }, { name: 'Advanced' }];
+
+    $scope.blade.refresh = function () {
+        $scope.blade.isLoading = true;
+
+        settings.getSettings({ moduleId: $scope.blade.moduleId }, function (results) {
+            // $scope.blade.objectsGrouped = _.groupBy(results, 'groupName');
+
+            $scope.blade.objects = angular.copy(results);
+            $scope.blade.origEntity = results;
+            $scope.blade.isLoading = false;
+        },
+        function (error) {
+            $scope.blade.isLoading = false;
+            bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+        });
+    }
+
+    function isDirty() {
+        return !angular.equals($scope.blade.objects, $scope.blade.origEntity);
+    };
+
+    function saveChanges() {
+        $scope.blade.isLoading = true;
+        settings.update({}, $scope.blade.objects, function (data, headers) {
+            $scope.blade.refresh();
+        });
+    };
+
+    $scope.bladeToolbarCommands = [
+        {
+            name: "Save", icon: 'icon-floppy',
+            executeMethod: function () {
+                saveChanges();
+            },
+            canExecuteMethod: function () {
+                return isDirty();
+            }
+        },
+        {
+            name: "Reset", icon: 'icon-undo',
+            executeMethod: function () {
+                angular.copy($scope.blade.origEntity, $scope.blade.objects);
+            },
+            canExecuteMethod: function () {
+                return isDirty();
+            }
+        }
+    ];
+
+    $scope.blade.onClose = function (closeCallback) {
+        closeCallback();
+    };
+
+    // actions on load
+    $scope.blade.refresh();
+}]);
---
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Scripts/Settings/resources/setting.js
@@ -0,0 +1,72 @@
+﻿angular.module('virtoCommerce.coreModule.settings.resources.setting', [])
+.factory('settings', ['$resource', function ($resource) {
+    return {
+        getSettingsSections: getSettingsSectionsMock,
+        getSettings: getSettingsMock,
+        update: updateMock
+    };
+    //return $resource('api/settings/:id', { id: '@Id' }, {
+    //    getSettingsSections: { method: 'GET', url: 'api/settings/settings', isArray: true }
+    //});
+
+    function getSettingsSectionsMock(param1, param2) {
+        var retVal = [
+            {
+                id: 'id1',
+                title: 'id1 title'
+            },
+            {
+                id: 'id2',
+                title: 'id2 title'
+            }
+        ];
+        param2(retVal);
+    }
+
+    function getSettingsMock(param1, param2) {
+        var retVal = [
+            {
+                name: 'VirtoCommerce.Core.Test.General.String',
+                groupName: '-',
+                valueType: 'string',
+                defaultValue: 'qwerty',
+                value: 'qwerty',
+                title: 'String ' + param1.moduleId,
+                description: 'A text setting'
+            },
+            {
+                name: 'VirtoCommerce.Core.Test.General.Password',
+                groupName: '-',
+                valueType: 'secureString',
+                defaultValue: 'qwerty',
+                value: '123',
+                title: 'Password ' + param1.moduleId,
+                description: 'A secure text setting'
+            },
+            {
+                name: 'VirtoCommerce.Core.Test.Advanced.Boolean1',
+                groupName: 'Advanced',
+                valueType: 'boolean',
+                defaultValue: 'true',
+                value: 'false',
+                title: 'Boolean1 ' + param1.moduleId,
+                description: 'A boolean setting'
+            },
+            {
+                name: 'VirtoCommerce.Core.Test.Advanced.Boolean2',
+                groupName: 'Advanced',
+                valueType: 'boolean',
+                defaultValue: 'true',
+                value: 'false',
+                title: 'Boolean2 ' + param1.moduleId,
+                description: 'A boolean setting'
+            }
+        ];
+        param2(retVal);
+    }
+
+
+    function updateMock(param0, param1, param2) {
+        param2();
+    }
+}]);
