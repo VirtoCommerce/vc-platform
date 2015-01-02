9e853c0daab6c66c1a89ceed7ffbbb0eb6aca708
eMazeika <em@virtoway.com>
2015-01-02 20:14:56 +0200

    UPD: settings management UI (using mock data)

--- src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Scripts/Settings/blades/settings-detail.js
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Scripts/Settings/blades/settings-detail.js
@@ -1,13 +1,36 @@
 ﻿angular.module('virtoCommerce.coreModule.settings.blades')
 .controller('settingsDetailController', ['$scope', 'dialogService', 'bladeNavigationService', 'settings', function ($scope, dialogService, bladeNavigationService, settings) {
-    $scope.settingGroups = [{ name: '-' }, { name: 'Advanced' }];
 
     $scope.blade.refresh = function () {
         $scope.blade.isLoading = true;
 
+        // parse values as they all are strings
         settings.getSettings({ moduleId: $scope.blade.moduleId }, function (results) {
-            // $scope.blade.objectsGrouped = _.groupBy(results, 'groupName');
+            var selectedSettings = _.where(results, { valueType: 'integer' });
+            _.forEach(selectedSettings, function (setting) {
+                setting.value = parseInt(setting.value, 10);
+                if (setting.allowedValues) {
+                    setting.allowedValues = _.map(setting.allowedValues, function (value) { return parseInt(value, 10); });
+                }
+            });
 
+            selectedSettings = _.where(results, { valueType: 'decimal' });
+            _.forEach(selectedSettings, function (setting) {
+                setting.value = parseFloat(setting.value);
+                if (setting.allowedValues) {
+                    setting.allowedValues = _.map(setting.allowedValues, function (value) { return parseFloat(value); });
+                }
+            });
+
+            selectedSettings = _.where(results, { valueType: 'boolean' });
+            _.forEach(selectedSettings, function (setting) {
+                setting.value = setting.value === 'true';
+                if (setting.allowedValues) {
+                    setting.allowedValues = _.map(setting.allowedValues, function (value) { return value === 'true'; });
+                }
+            });
+
+            $scope.objectsGrouped = _.groupBy(results, 'groupName');
             $scope.blade.objects = angular.copy(results);
             $scope.blade.origEntity = results;
             $scope.blade.isLoading = false;
@@ -43,6 +66,7 @@
             name: "Reset", icon: 'icon-undo',
             executeMethod: function () {
                 angular.copy($scope.blade.origEntity, $scope.blade.objects);
+                $scope.objectsGrouped = _.groupBy($scope.blade.objects, 'groupName');
             },
             canExecuteMethod: function () {
                 return isDirty();
@@ -51,7 +75,23 @@
     ];
 
     $scope.blade.onClose = function (closeCallback) {
-        closeCallback();
+        if (isDirty()) {
+            var dialog = {
+                id: "confirmItemChange",
+                title: "Save changes",
+                message: "The settings has been modified. Do you want to save changes?",
+                callback: function (needSave) {
+                    if (needSave) {
+                        saveChanges();
+                    }
+                    closeCallback();
+                }
+            };
+            dialogService.showConfirmationDialog(dialog);
+        }
+        else {
+            closeCallback();
+        }
     };
 
     // actions on load
