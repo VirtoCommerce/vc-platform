38cdcbad26ff02061a6f6dddb7eca3fd887f2082
E.Tatarincev <et@virtoway.com>
2015-01-08 14:23:30 +0200

    -ADD mock carts

--- src/Presentation/WebAdmin/Modules/Cart/VirtoCommerce.CartModule.Web/Module.cs
+++ src/Presentation/WebAdmin/Modules/Cart/VirtoCommerce.CartModule.Web/Module.cs
@@ -6,6 +6,7 @@ using Microsoft.Practices.Unity;
 using VirtoCommerce.CartModule.Data.Repositories;
 using VirtoCommerce.CartModule.Data.Services;
 using VirtoCommerce.CatalogModule.Web.Controllers.Api;
+using VirtoCommerce.Foundation.Money;
 using VirtoCommerce.Framework.Web.Modularity;
 
 namespace VirtoCommerce.CartModule.Web
@@ -21,7 +22,26 @@ namespace VirtoCommerce.CartModule.Web
 		public void Initialize()
 		{
 			var repository = new InMemoryCartRepository();
-
+			var cart1 = new VirtoCommerce.Domain.Cart.Model.ShoppingCart
+			{
+				Id = "cart1",
+				Currency = CurrencyCodes.USD,
+				SiteId = "site",
+				Name = "default",
+				CustomerId = "customer1",
+				CustomerName = "customer name"
+			};
+			var cart2 = new VirtoCommerce.Domain.Cart.Model.ShoppingCart
+			{
+				Id = "cart2",
+				Currency = CurrencyCodes.USD,
+				SiteId = "site",
+				Name = "default",
+				CustomerId = "customer2",
+				CustomerName = "customer2 name"
+			};
+			repository.Add(cart1);
+			repository.Add(cart2);
 			var cartService = new ShoppingCartServiceImpl(repository);
 			var searchService = new ShoppingCartSearchServiceImpl(repository);
 
