7bd199d207e526614818d52679e36b353f350499
E.Tatarincev <et@virtoway.com>
2015-01-08 13:37:36 +0200

    -ADD Shopping cart (daily commit)

---
+++ src/Presentation/WebAdmin/Modules/Cart/VirtoCommerce.CartModule.Web/Converters/SearchResultConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Omu.ValueInjecter;
+using coreModel = VirtoCommerce.Domain.Cart.Model;
+using webModel = VirtoCommerce.CatalogModule.Web.Model;
+
+namespace VirtoCommerce.CatalogModule.Web.Converters
+{
+	public static class SearchResultConverter
+	{
+		public static webModel.SearchResult ToWebModel(this coreModel.SearchResult result)
+		{
+			var retVal = new webModel.SearchResult();
+			retVal.InjectFrom(result);
+
+			return retVal;
+		}
+
+		public static coreModel.SearchResult ToCoreModel(this webModel.SearchResult result)
+		{
+			var retVal = new coreModel.SearchResult();
+			retVal.InjectFrom(result);
+			return retVal;
+		}
+
+
+	}
+}
---
+++ src/Presentation/WebAdmin/Modules/Cart/VirtoCommerce.CartModule.Web/Model/Discount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using VirtoCommerce.Foundation.Frameworks;
+using VirtoCommerce.Foundation.Money;
+
+namespace VirtoCommerce.CatalogModule.Web.Model
+{
+	public class Discount 
+	{
+		public string Id { get; set; }
+		public string PromotionId { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public CurrencyCodes? Currency { get; set; }
+		public decimal? DiscountAmount { get; set; }
+
+		public Coupon Coupon { get; set; }
+		public string Description { get; set; }
+	}
+}
