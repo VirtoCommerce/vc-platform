70866fe49f3abfdb973a4727185c575aa391b2ac
adomass <asu@virtoway.com>
2015-01-13 18:05:59 +0200

    added categoryInfo needed fro frontend url (de)construction

---
+++ src/Presentation/WebAdmin/Modules/Merchandising/VirtoCommerce.MerchandisingModule.Web/Model/CategoryInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtoCommerce.MerchandisingModule.Web.Model
+{
+    public class CategoryInfo
+    {
+        public string Id { get; set; }
+        public SeoKeyword[] SeoKeywords { get; set; }
+        public string Name { get; set; }
+    }
+}
