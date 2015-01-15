661a15095c4f7583f11f87b9c20a851d76610f2c
artem-dudarev <ad@virtoway.com>
2015-01-15 13:23:49 +0200

    Removed Sample.TestModule1

--- src/Presentation/WebAdmin/Modules/Sample.TestModule1/Scripts/blades/blade1.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module('testModule1.blades.blade1', [])
-.controller('tm1-blade1Controller', ['$scope', 'bladeNavigationService', function ($scope, bladeNavigationService) {
-
-	$scope.data = "testModule1 content";
-	$scope.blade.title = "testModule1 title";
-    $scope.blade.isLoading = false;
-}]);
