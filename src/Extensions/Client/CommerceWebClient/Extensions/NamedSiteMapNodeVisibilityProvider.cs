75c3bb1ff9f96707f5f9330e5bc4d99a6060d4cc
artem-dudarev <ad@virtoway.com>
2015-01-23 17:41:55 +0200

    Fixed compilation warnings.

--- src/Extensions/Client/CommerceWebClient/Extensions/NamedSiteMapNodeVisibilityProvider.cs
+++ src/Extensions/Client/CommerceWebClient/Extensions/NamedSiteMapNodeVisibilityProvider.cs
@@ -12,25 +12,26 @@ using VirtoCommerce.Web.Client.Helpers;
 
 namespace VirtoCommerce.Web.Client.Extensions
 {
-    using System.Linq;
+	using System.Linq;
 
-    /// <summary>
-    /// Filtered SiteMapNode Visibility Provider.
-    /// 
-    /// The Web.config setting should specify attributesToIgnore=&quot;visibility&quot; in order to use this class!
-    /// 
-    /// Rules are parsed left-to-right, first match wins. Asterisk can be used to match any control. Exclamation mark can be used to negate a match.
-    /// </summary>
-    public class NamedSiteMapNodeVisibilityProvider
-        : SiteMapNodeVisibilityProviderBase
-    {
-        #region ISiteMapNodeVisibilityProvider Members
+	/// <summary>
+	/// Filtered SiteMapNode Visibility Provider.
+	/// 
+	/// The Web.config setting should specify attributesToIgnore=&quot;visibility&quot; in order to use this class!
+	/// 
+	/// Rules are parsed left-to-right, first match wins. Asterisk can be used to match any control. Exclamation mark can be used to negate a match.
+	/// </summary>
+	[CLSCompliant(false)]
+	public class NamedSiteMapNodeVisibilityProvider
+		: SiteMapNodeVisibilityProviderBase
+	{
+		#region ISiteMapNodeVisibilityProvider Members
 
-        static ISecurityService _securityService;
-        public static ISecurityService SecurityService
-        {
-            get { return _securityService ?? (_securityService = DependencyResolver.Current.GetService<ISecurityService>()); }
-        }
+		static ISecurityService _securityService;
+		public static ISecurityService SecurityService
+		{
+			get { return _securityService ?? (_securityService = DependencyResolver.Current.GetService<ISecurityService>()); }
+		}
 
 		public ICustomerSession CustomerSession
 		{
@@ -42,80 +43,81 @@ namespace VirtoCommerce.Web.Client.Extensions
 		}
 
 
-        /// <summary>
-        /// Determines whether the node is visible.
-        /// </summary>
-        /// <param name="node">The node.</param>
-        /// <param name="sourceMetadata">The source metadata.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified node is visible; otherwise, <c>false</c>.
-        /// </returns>
-        public override bool IsVisible(ISiteMapNode node, IDictionary<string, object> sourceMetadata)
-        {
-            // Convert to MvcSiteMapNode
-            var mvcNode = node as MvcSiteMapProvider.SiteMapNode;
+		/// <summary>
+		/// Determines whether the node is visible.
+		/// </summary>
+		/// <param name="node">The node.</param>
+		/// <param name="sourceMetadata">The source metadata.</param>
+		/// <returns>
+		///   <c>true</c> if the specified node is visible; otherwise, <c>false</c>.
+		/// </returns>
+		[CLSCompliant(false)]
+		public override bool IsVisible(ISiteMapNode node, IDictionary<string, object> sourceMetadata)
+		{
+			// Convert to MvcSiteMapNode
+			var mvcNode = node as MvcSiteMapProvider.SiteMapNode;
 
-	        if (mvcNode != null)
-	        {
-		        var menu = String.Empty;
-                if (sourceMetadata != null && sourceMetadata.ContainsKey("name"))
-                    menu = sourceMetadata["name"].ToString();
+			if (mvcNode != null)
+			{
+				var menu = String.Empty;
+				if (sourceMetadata != null && sourceMetadata.ContainsKey("name"))
+					menu = sourceMetadata["name"].ToString();
 
-		        // Is a visibility attribute specified?
-		        var visibility = GetValue(mvcNode, "visibility");
+				// Is a visibility attribute specified?
+				var visibility = GetValue(mvcNode, "visibility");
 
-		        if (mvcNode.Attributes.ContainsKey("permissions"))
-		        {
-                    if (!CustomerSession.IsRegistered)
-				        return false;
+				if (mvcNode.Attributes.ContainsKey("permissions"))
+				{
+					if (!CustomerSession.IsRegistered)
+						return false;
 
-			        var allPermissions =
-				        GetValue(mvcNode, "permissions").Split(',').Select(x => new Permission {PermissionId = x});
-			        if (
-				        allPermissions.Any(
-					        permission => !SecurityService.CheckMemberPermission(StoreHelper.CustomerSession.CustomerId, permission)))
-			        {
-				        return false;
-			        }
-		        }
+					var allPermissions =
+						GetValue(mvcNode, "permissions").Split(',').Select(x => new Permission { PermissionId = x });
+					if (
+						allPermissions.Any(
+							permission => !SecurityService.CheckMemberPermission(StoreHelper.CustomerSession.CustomerId, permission)))
+					{
+						return false;
+					}
+				}
 
-		        if (!string.IsNullOrEmpty(visibility))
-		        {
-			        visibility = visibility.Trim();
+				if (!string.IsNullOrEmpty(visibility))
+				{
+					visibility = visibility.Trim();
 
-			        // All set. Now parse the visibility variable.
-			        if (visibility.Split(new[] {',', ';'})
+					// All set. Now parse the visibility variable.
+					if (visibility.Split(new[] { ',', ';' })
 						.TakeWhile(visibilityKeyword => visibilityKeyword != menu && visibilityKeyword != "*")
 						.Any(visibilityKeyword => visibilityKeyword == "!" + menu || visibilityKeyword == "!*"))
-			        {
-				        return false;
-			        }
-		        }
+					{
+						return false;
+					}
+				}
 
-		        var stores = GetValue(mvcNode, "stores");
-		        if (!string.IsNullOrEmpty(stores))
-		        {
-			        if(!stores.Trim().Split(new[] {',', ';'}, StringSplitOptions.RemoveEmptyEntries)
+				var stores = GetValue(mvcNode, "stores");
+				if (!string.IsNullOrEmpty(stores))
+				{
+					if (!stores.Trim().Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
 						.Any(store => store.Equals(CustomerSession.StoreId, StringComparison.OrdinalIgnoreCase)))
-			        {
-				        return false;
-			        }
-		        }
-	        }
+					{
+						return false;
+					}
+				}
+			}
 
-	        // Still nothing? Then it's OK!
-            return true;
-        }
+			// Still nothing? Then it's OK!
+			return true;
+		}
 
-        private string GetValue(ISiteMapNode node, string key)
-        {
-            if (node.Attributes.ContainsKey(key))
-            {
-                return node.Attributes[key] as string;
-            }
+		private string GetValue(ISiteMapNode node, string key)
+		{
+			if (node.Attributes.ContainsKey(key))
+			{
+				return node.Attributes[key] as string;
+			}
 
-            return null;
-        }
-        #endregion
-    }
+			return null;
+		}
+		#endregion
+	}
 }
