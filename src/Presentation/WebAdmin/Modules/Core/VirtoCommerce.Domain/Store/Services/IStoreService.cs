967217876de94a555f2e1779c96d0a2ec77bd017
E.Tatarincev <et@virtoway.com>
2015-02-09 17:03:06 +0200

    -ADD Store. API + tests

---
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Domain/Store/Services/IStoreService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using coreModel = VirtoCommerce.Domain.Store.Model;
+namespace VirtoCommerce.Domain.Store.Services
+{
+	public interface IStoreService
+	{
+		IEnumerable<coreModel.Store> GetStoreList();
+		coreModel.Store GetById(string id);
+		coreModel.Store Create(coreModel.Store store);
+		void Update(coreModel.Store[] stores);
+		void Delete(string[] ids);
+	}
+}
---
+++ src/Presentation/WebAdmin/Modules/Store/VirtoCommerce.StoreModule.Web/Converters/StoreConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Omu.ValueInjecter;
+using VirtoCommerce.Foundation.Money;
+using coreModel = VirtoCommerce.Domain.Store.Model;
+using webModel = VirtoCommerce.StoreModule.Web.Model;
+
+namespace VirtoCommerce.StoreModule.Web.Converters
+{
+	public static class StoreConverter
+	{
+		public static webModel.Store ToWebModel(this coreModel.Store store)
+		{
+			var retVal = new webModel.Store();
+			retVal.InjectFrom(store);
+
+			retVal.DefaultCurrency = store.DefaultCurrency;
+			retVal.StoreState = store.StoreState;
+			if (store.Settings != null)
+				retVal.Settings = store.Settings.Select(x => x.ToWebModel()).ToList();
+			if (store.ShipmentGateways != null)
+				retVal.ShipmentGateways = store.ShipmentGateways;
+			if (store.PaymentGateways != null)
+				retVal.PaymentGateways = store.PaymentGateways;
+			if (store.Languages != null)
+				retVal.Languages = store.Languages;
+			if (store.Currencies != null)
+				retVal.Currencies = store.Currencies;
+			if (store.ReturnsFulfillmentCenter != null)
+				retVal.ReturnsFulfillmentCenter = store.ReturnsFulfillmentCenter.ToWebModel();
+			if (store.FulfillmentCenter != null)
+				retVal.FulfillmentCenter = store.FulfillmentCenter.ToWebModel();
+
+			return retVal;
+		}
+
+		public static coreModel.Store ToCoreModel(this webModel.Store store)
+		{
+			var retVal = new coreModel.Store();
+			retVal.InjectFrom(store);
+
+			retVal.StoreState = store.StoreState;
+			if (store.Settings != null)
+				retVal.Settings = store.Settings.Select(x => x.ToCoreModel()).ToList();
+			if (store.ShipmentGateways != null)
+				retVal.ShipmentGateways = store.ShipmentGateways;
+			if (store.PaymentGateways != null)
+				retVal.PaymentGateways = store.PaymentGateways;
+			if (store.Languages != null)
+				retVal.Languages = store.Languages;
+			if (store.Currencies != null)
+				retVal.Currencies = store.Currencies;
+			if (store.ReturnsFulfillmentCenter != null)
+				retVal.ReturnsFulfillmentCenter = store.ReturnsFulfillmentCenter.ToCoreModel();
+			if (store.FulfillmentCenter != null)
+				retVal.FulfillmentCenter = store.FulfillmentCenter.ToCoreModel();
+
+			return retVal;
+		}
+
+
+	}
+}
