302e95c57a0d30c26cb00d08b54e0b5e30ac7f94
Woland2k <sasha@virtoway.com>
2015-02-13 15:38:31 -0800

    - removed not used modules

--- src/Presentation/WebAdmin/Modules/UnmanagedModule.Web/Scripts/blades/blade1.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('unmanagedModule.blades.blade1', [])
-.controller('um-blade1Controller', ['$scope', function ($scope) {
-    $scope.data = "unmanagedModule content";
-    $scope.blade.title = "unmanagedModule title";
-    $scope.blade.isLoading = false;
-}]);
