9c8c738fcc150d79c87518428bcaa74a0739c172
Woland2k <sasha@virtoway.com>
2015-02-12 16:23:07 -0800

    - removed themes

--- src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.Content.Tests/FileSystemRepositoryTest.cs
+++ src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.Content.Tests/FileSystemRepositoryTest.cs
@@ -69,9 +69,9 @@
 
 			var repository = new FileSystemFileRepositoryImpl(fullPath);
 
-			var item = repository.GetContentItem("Expo/layout/theme.liquid");
+			var item = repository.GetContentItem("Simple/layout/theme.liquid");
 
-			Assert.Equal(item.Path, "Expo/layout/theme.liquid");
+            Assert.Equal(item.Path, "Simple/layout/theme.liquid");
 			Assert.True(item.Content.Contains("!!!\n"));
 		}
 
@@ -84,38 +84,38 @@
 
 			var content = new ContentItem();
 			content.Content = "Some new stuff";
-			content.Path = "Expo/new/new123.liquid";
+            content.Path = "Simple/new/new123.liquid";
 
 			repository.SaveContentItem(content);
 
-			var items = repository.GetContentItems("Expo/new");
+            var items = repository.GetContentItems("Simple/new");
 
 			Assert.Equal(items.Length, 1);
 
-			var item = repository.GetContentItem("Expo/new/new123.liquid");
+            var item = repository.GetContentItem("Simple/new/new123.liquid");
 
 			Assert.True(item.Content.Contains("Some"));
 
 			content = new ContentItem();
 			content.Content = "Some new stuff. Changes";
-			content.Path = "Expo/new/new123.liquid";
+            content.Path = "Simple/new/new123.liquid";
 
 			repository.SaveContentItem(content);
 
-			items = repository.GetContentItems("Expo/new");
+            items = repository.GetContentItems("Simple/new");
 
 			Assert.Equal(items.Length, 1);
 
-			item = repository.GetContentItem("Expo/new/new123.liquid");
+            item = repository.GetContentItem("Simple/new/new123.liquid");
 
 			Assert.True(item.Content.Contains("Some") && item.Content.Contains("Changes"));
 
 			content = new ContentItem();
-			content.Path = "Expo/new/new123.liquid";
+            content.Path = "Simple/new/new123.liquid";
 
 			repository.DeleteContentItem(content);
 
-			items = repository.GetContentItems("Expo/new");
+            items = repository.GetContentItems("Simple/new");
 
 			Assert.Equal(items.Length, 0);
 		}
