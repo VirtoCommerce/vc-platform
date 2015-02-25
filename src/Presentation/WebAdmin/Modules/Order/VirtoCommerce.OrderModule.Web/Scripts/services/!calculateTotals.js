fe4287c7bc69cdb3f6f0a2b3e6edb6c7ebe97b54
E.Tatarincev <et@virtoway.com>
2015-02-25 17:53:34 +0200

    -FIX order merge.

--- src/Presentation/WebAdmin/Modules/Order/VirtoCommerce.OrderModule.Web/Scripts/services/!calculateTotals.js
+++ src/Presentation/WebAdmin/Modules/Order/VirtoCommerce.OrderModule.Web/Scripts/services/!calculateTotals.js
@@ -19,7 +19,19 @@
 				retVal.taxTotal += _.reduce(operation.items, function (memo, x) { return memo + parseFloat(x.tax); }, 0);
 				retVal.taxTotal += _.reduce(operation.shipments, function (memo, x) { return memo + parseFloat(x.tax); }, 0);
 
+				_.each(operation.items, function (x) {
+					x.total = parseInt(x.quantity) * (parseFloat(x.price) || 0) - (parseFloat(x.discountAmount) || 0) + (parseFloat(x.tax) || 0);
+					x.total = Math.round(x.total * 100) / 100;
+				});
+
+				
+				retVal.subTotal = Math.round(retVal.subTotal * 100) / 100;
+				retVal.shippingTotal = Math.round(retVal.shippingTotal * 100) / 100;
+				retVal.discountTotal = Math.round(retVal.discountTotal * 100) / 100;
+				retVal.taxTotal = Math.round(retVal.taxTotal * 100) / 100;
 				retVal.total = retVal.subTotal + retVal.shippingTotal + retVal.taxTotal - retVal.discountTotal;
+				retVal.total = Math.round(retVal.total * 100) / 100;
+				
 			}
 			else if (operation.operationType.toLowerCase() == 'shipment') {
 
@@ -27,6 +39,7 @@
 				retVal.taxTotal = parseFloat(operation.tax);
 				retVal.shippingTotal = parseFloat(operation.sum);
 				retVal.total = retVal.shippingTotal + retVal.taxTotal - retVal.discountTotal;
+				retVal.total = Math.round(retVal.total * 100) / 100;
 			}
 		}
 		return retVal;
