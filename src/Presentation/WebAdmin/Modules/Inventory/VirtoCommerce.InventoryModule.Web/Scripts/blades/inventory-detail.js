8f7a47fe38522bd6403a30059338b33645b3a4f5
eMazeika <em@virtoway.com>
2015-02-25 18:09:33 +0200

    UPD: input "date" changed to bootstrap datepicker

--- src/Presentation/WebAdmin/Modules/Inventory/VirtoCommerce.InventoryModule.Web/Scripts/blades/inventory-detail.js
+++ src/Presentation/WebAdmin/Modules/Inventory/VirtoCommerce.InventoryModule.Web/Scripts/blades/inventory-detail.js
@@ -79,6 +79,72 @@
             }
         }
     ];
+    // datepicker
+    //$scope.today = function () {
+    //    $scope.dt = new Date();
+    //};
+    //$scope.today();
+
+    //$scope.clear = function () {
+    //    $scope.dt = null;
+    //};
+    //$scope.open = function ($event) {
+    //    $event.preventDefault();
+    //    $event.stopPropagation();
+
+    //    $scope.opened = true;
+    //};
+    //$scope.dateOptions = {
+    //    formatYear: 'yy',
+    //    startingDay: 1
+    //};
+    $scope.datepickers = {
+        pod: false,
+        bod: false
+    }
+    $scope.formData = {};
+    $scope.today = function () {
+        $scope.formData.pod = new Date();
+
+        // ***** Q1  *****
+        $scope.formData.bod = new Date();
+    };
+    $scope.today();
+
+    $scope.showWeeks = true;
+    $scope.toggleWeeks = function () {
+        $scope.showWeeks = !$scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function (date, mode) {
+        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+    };
+
+    $scope.toggleMin = function () {
+        $scope.minDate = ($scope.minDate) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function ($event, which) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.datepickers[which] = true;
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yyyy'",
+        'starting-day': 1
+    };
+
+    $scope.formats = ['shortDate', 'dd-MMMM-yyyy', 'yyyy/MM/dd'];
+    $scope.format = $scope.formats[0];
+
 
     // on load
     initializeBlade($scope.blade.data);
