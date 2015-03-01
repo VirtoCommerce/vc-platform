de8128f7049f1b8c35b74b344de0aa62532341c8
eMazeika <em@virtoway.com>
2015-03-02 01:00:08 +0200

    ADD: stores - languages, currencies management added

---
+++ src/Presentation/WebAdmin/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/blades/store-currencies-list.js
@@ -0,0 +1,97 @@
+﻿angular.module('virtoCommerce.storeModule.blades')
+.controller('storeCurrenciesListController', ['$scope', 'bladeNavigationService', 'dialogService', function ($scope, bladeNavigationService, dialogService) {
+    $scope.selectedItem = null;
+    $scope.blade.currentEntities = [
+        { code: 'EUR', displayName: 'Euro' },
+        { code: 'USD', displayName: 'US Dollar' },
+        { code: 'GBP', displayName: 'British Pound' },
+        { code: 'JPY', displayName: 'Japanese yen' }
+    ];
+
+    function initializeBlade(data) {
+        _.each($scope.blade.currentEntities, function (x) {
+            x.isChecked = _.some(data.currencies, function (curr) { return curr === x.code; });
+        });
+        if (data.defaultCurrency) {
+            var defaultEntity = _.findWhere($scope.blade.currentEntities, { code: data.defaultCurrency });
+            if (defaultEntity) {
+                defaultEntity.isDefault = true;
+            }
+        }
+
+        $scope.blade.origEntity = $scope.blade.currentEntities;
+        $scope.blade.currentEntities = angular.copy($scope.blade.currentEntities);
+        $scope.blade.isLoading = false;
+    };
+
+    $scope.selectItem = function (listItem) {
+        $scope.selectedItem = listItem;
+    };
+
+    $scope.blade.onClose = function (closeCallback) {
+        if (isDirty()) {
+            var dialog = {
+                id: "confirmItemChange",
+                title: "Save changes",
+                message: "The Currencies has been modified. Do you want to save changes?"
+            };
+            dialog.callback = function (needSave) {
+                if (needSave) {
+                    $scope.saveChanges();
+                }
+                closeCallback();
+            };
+            dialogService.showConfirmationDialog(dialog);
+        }
+        else {
+            closeCallback();
+        }
+    };
+
+    function isDirty() {
+        return !angular.equals($scope.blade.currentEntities, $scope.blade.origEntity);
+    };
+
+    $scope.cancelChanges = function () {
+        $scope.bladeClose();
+    }
+
+    $scope.isValid = function () {
+        return true;
+    }
+
+    $scope.saveChanges = function () {
+        var checkedEntities = _.where($scope.blade.currentEntities, { isChecked: true });
+        $scope.blade.data.currencies = _.pluck(checkedEntities, 'code');
+
+        var defaultEntity = _.findWhere($scope.blade.currentEntities, { isDefault: true });
+        if (defaultEntity) {
+            $scope.blade.data.defaultCurrency = defaultEntity.code;
+        }
+
+        angular.copy($scope.blade.currentEntities, $scope.blade.origEntity);
+        $scope.bladeClose();
+    };
+
+    $scope.bladeToolbarCommands = [
+        {
+            name: "Set default", icon: 'fa fa-edit',
+            executeMethod: function () {
+                _.each($scope.blade.currentEntities, function (x) {
+                    x.isDefault = x.code === $scope.selectedItem.code;
+                });
+            },
+            canExecuteMethod: function () {
+                return $scope.selectedItem && $scope.selectedItem.isChecked;
+            }
+        }
+    ];
+
+    $scope.$watch('blade.parentBlade.currentEntity', function (currentEntity) {
+        $scope.blade.data = currentEntity;
+        initializeBlade($scope.blade.data);
+    });
+
+    // on load: 
+    // $scope.$watch('blade.parentBlade.currentEntity' gets fired
+}]);
--- src/Presentation/WebAdmin/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/widgets/storeLanguagesWidget.js
+++ src/Presentation/WebAdmin/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/widgets/storeLanguagesWidget.js
@@ -1,18 +1,16 @@
 ﻿angular.module('virtoCommerce.storeModule.widgets')
 .controller('storeLanguagesWidgetController', ['$scope', 'bladeNavigationService', function ($scope, bladeNavigationService) {
-    // var blade = $scope.widget.blade;
-    
+    var blade = $scope.widget.blade;
+
     $scope.openBlade = function () {
-        //if ($scope.elementCount !== '...') {
-        //    var newBlade = {
-        //        id: "storeChildBlade",
-        //        itemId: blade.itemId,
-        //        title: blade.title,
-        //        subtitle: 'Manage languages',
-        //        controller: 'storeLanguagesListController',
-        //        template: 'Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/blades/stores-languages-list.tpl.html'
-        //    };
-        //    bladeNavigationService.showBlade(newBlade, blade);
-        //}
+        var newBlade = {
+            id: "storeChildBlade",
+            itemId: blade.itemId,
+            title: blade.title,
+            subtitle: 'Manage languages',
+            controller: 'storeLanguagesListController',
+            template: 'Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/blades/store-languages-list.tpl.html'
+        };
+        bladeNavigationService.showBlade(newBlade, blade);
     };
 }]);
