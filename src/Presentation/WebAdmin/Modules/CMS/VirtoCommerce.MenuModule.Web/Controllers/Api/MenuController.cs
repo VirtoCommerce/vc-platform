b2d931db205ca62b06bdd9798f2db31ed5ef8bd1
Woland2k <sasha@virtoway.com>
2015-03-04 14:46:24 -0800

    ListClient

--- src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.MenuModule.Web/Controllers/Api/MenuController.cs
+++ src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.MenuModule.Web/Controllers/Api/MenuController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using VirtoCommerce.Content.Menu.Data.Services;
--- src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.MenuModule.Web/Models/MenuLink.cs
+++ src/Presentation/WebAdmin/Modules/CMS/VirtoCommerce.MenuModule.Web/Models/MenuLink.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VirtoCommerce.MenuModule.Web.Models
+﻿namespace VirtoCommerce.MenuModule.Web.Models
 {
 	public class MenuLink
 	{
