b12be3836688f7185a1812acdb8492a37b11dabb
eMazeika <em@virtoway.com>
2015-03-10 15:56:30 +0200

    fulfillment - implement Fulfillment centers management UI (unfinished)

---
+++ src/Presentation/WebAdmin/Modules/Core/VirtoCommerce.Core.Web/Scripts/Fulfillment/fulfillment.js
@@ -0,0 +1,19 @@
+﻿//Call this to register our module to main application
+var moduleName = "virtoCommerce.coreModule.fulfillment";
+
+if (AppDependencies != undefined) {
+    AppDependencies.push(moduleName);
+}
+
+angular.module(moduleName, [
+    'virtoCommerce.coreModule.fulfillment.widgets'
+])
+.run(
+  ['$rootScope', 'mainMenuService', 'widgetService', '$state', function ($rootScope, mainMenuService, widgetService, $state) {
+      //Register module widget
+      widgetService.registerWidget({
+          controller: 'fulfillmentWidgetController',
+          template: 'Modules/Core/VirtoCommerce.Core.Web/Scripts/fulfillment/widgets/fulfillmentWidget.tpl.html'
+      }, 'moduleDetail');
+  }])
+;
