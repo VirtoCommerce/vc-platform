491945c03ee79c82bc3b245439e4951604287ea3
artem-dudarev <ad@virtoway.com>
2015-03-17 13:05:05 +0200

    Fixed URL rewriting for configuration without virtual directory Modules.

--- src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Startup.cs
+++ src/Presentation/WebAdmin/VirtoCommerce.Platform.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Practices.Unity;
 using Owin;
 using VirtoCommerce.Framework.Web.Modularity;
 using VirtoCommerce.Platform.Web;
+using WebGrease.Extensions;
 
 [assembly: OwinStartup(typeof(Startup))]
 
@@ -19,7 +20,7 @@ namespace VirtoCommerce.Platform.Web
         public void Configuration(IAppBuilder app)
         {
             const string modulesVirtualPath = "~/Modules";
-            var modulesPhysicalPath = HostingEnvironment.MapPath(modulesVirtualPath);
+            var modulesPhysicalPath = HostingEnvironment.MapPath(modulesVirtualPath).EnsureEndSeparator();
 
             var bootstraper = new VirtoCommercePlatformWebBootstraper(modulesVirtualPath, modulesPhysicalPath);
             bootstraper.Run();
@@ -28,14 +29,14 @@ namespace VirtoCommerce.Platform.Web
             var moduleCatalog = bootstraper.Container.Resolve<IModuleCatalog>();
 
             // Register URL rewriter before modules initialization
-            var applicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            var applicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase.EnsureEndSeparator();
             var modulesRelativePath = MakeRelativePath(applicationBase, modulesPhysicalPath);
             var urlRewriterOptions = new UrlRewriterOptions();
 
             foreach (var module in moduleCatalog.Modules.OfType<ManifestModuleInfo>())
             {
                 var urlRewriteKey = string.Format(CultureInfo.InvariantCulture, "/Modules/$({0})", module.ModuleName);
-                var urlRewriteValue = MakeRelativePath(applicationBase, module.FullPhysicalPath);
+                var urlRewriteValue = MakeRelativePath(modulesPhysicalPath, module.FullPhysicalPath);
                 urlRewriterOptions.Items.Add(PathString.FromUriComponent(urlRewriteKey), "/" + urlRewriteValue);
             }
 
