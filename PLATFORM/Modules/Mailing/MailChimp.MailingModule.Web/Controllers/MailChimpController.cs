e272f39a167c52c1544ce5449a9215513d6d06af
artem-dudarev <ad@virtoway.com>
2015-03-24 09:54:46 +0200

    Fixed bugs in MailChimp.MailingModule.Web: invalid package references, compilation errors and warnings.
    Former-commit-id: 600c4c39ab48873747d966a7007b3d0d6b21a65e

--- PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Controllers/MailChimpController.cs
+++ PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Controllers/MailChimpController.cs
@@ -1,34 +1,26 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System;
 using System.Web.Http;
-using System.Web.Http.Description;
-using System.Web.Mvc;
 using VirtoCommerce.Domain.Mailing.Services;
-using VirtoCommerce.Framework.Web.Settings;
 
 namespace MailChimp.MailingModule.Web.Controllers
 {
-	[RoutePrefix("api/mailing")]
-	public class MailChimpController : ApiController
-	{
-		private string _mailChimpApiKey;
-		private IMailing _mailing;
+    [RoutePrefix("api/mailing")]
+    public class MailChimpController : ApiController
+    {
+        private string _mailChimpApiKey;
+        private IMailing _mailing;
 
         public MailChimpController(IMailing mailing, string mailChimpApiKey)
-		{
+        {
 
-			if (mailing == null)
+            if (mailing == null)
                 throw new ArgumentNullException("mailing");
-            
-			if (string.IsNullOrEmpty(mailChimpApiKey))
-				throw new ArgumentNullException("MailChimpApiKey");
 
-			_mailing = mailing;
+            if (string.IsNullOrEmpty(mailChimpApiKey))
+                throw new ArgumentNullException("mailChimpApiKey");
+
+            _mailing = mailing;
             _mailChimpApiKey = mailChimpApiKey;
-		}        
-	}
+        }
+    }
 }
