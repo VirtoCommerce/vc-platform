ea1885dac475d41cbfe086f1ca57480b553b5429
aartiomov <andrej.artiomov@gmail.com>
2015-03-24 10:04:44 +0200

    - FIX. Missing files.

---
+++ PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Services/IMailing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.MailChimp.Mailing.Services
+{
+    public interface IMailing
+    {
+        string Code { get; }
+        string Description { get; }
+        string LogoUrl { get; }
+    }
+}
