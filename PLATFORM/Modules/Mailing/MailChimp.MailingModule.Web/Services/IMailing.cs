d319217b10077d6c79990329073b9ae17a0e843f
artem-dudarev <ad@virtoway.com>
2015-03-24 10:25:46 +0200

    Refactored MailChimp.MailingModule.Web.

--- PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Services/IMailing.cs
+++ PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Services/IMailing.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VirtoCommerce.MailChimp.Mailing.Services
+﻿namespace MailChimp.MailingModule.Web.Services
 {
     public interface IMailing
     {
