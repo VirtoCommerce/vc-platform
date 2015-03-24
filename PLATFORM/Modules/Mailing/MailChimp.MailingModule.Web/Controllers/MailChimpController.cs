69c018eba4546618dd802e0998597e9e5387e757
aartiomov <andrej.artiomov@gmail.com>
2015-03-24 09:50:42 +0200

    - REF. Refactored Mailing module, moved services from Core to MailChimp module.
    Former-commit-id: 8436a8c25c6d70bce728d429d170f1a34b552be3

--- PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Controllers/MailChimpController.cs
+++ PLATFORM/Modules/Mailing/MailChimp.MailingModule.Web/Controllers/MailChimpController.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Mvc;
-using VirtoCommerce.Domain.Mailing.Services;
+using VirtoCommerce.MailChimp.Mailing.Services;
 using VirtoCommerce.Framework.Web.Settings;
 
 namespace MailChimp.MailingModule.Web.Controllers
