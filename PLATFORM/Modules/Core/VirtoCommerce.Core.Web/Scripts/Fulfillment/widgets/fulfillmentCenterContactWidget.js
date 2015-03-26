c94cf23c733f041a67d98fa3f106ae5119b096bc
eMazeika <em@virtoway.com>
2015-03-26 15:10:29 +0200

    fulfillment - implement Fulfillment centers management UI (test #1612)

---
+++ PLATFORM/Modules/Core/VirtoCommerce.Core.Web/Scripts/Fulfillment/widgets/fulfillmentCenterContactWidget.js
@@ -0,0 +1,16 @@
+﻿angular.module('virtoCommerce.coreModule.fulfillment')
+.controller('fulfillmentCenterContactWidgetController', ['$scope', 'bladeNavigationService', function ($scope, bladeNavigationService) {
+    var blade = $scope.widget.blade;
+    
+    $scope.openBlade = function () {
+        var newBlade = {
+            id: "fulfillmentDetailChild",
+            data: blade.currentEntity,
+            title: blade.title,
+            subtitle: 'Contact information',
+            controller: 'fulfillmentCenterContactController',
+            template: 'Modules/$(VirtoCommerce.Core)/Scripts/fulfillment/blades/fulfillment-center-contact.tpl.html'
+        };
+        bladeNavigationService.showBlade(newBlade, blade);
+    };
+}]);
