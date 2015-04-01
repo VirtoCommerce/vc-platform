7ee6135c38c6bf27522e52c679517bade24dbc8b
eMazeika <em@virtoway.com>
2015-04-01 14:10:17 +0300

    marketing - initial scripts added (unfinished)
    Former-commit-id: 24b43b222c420906a39e7128a96ffab0223eaa2e

---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/blades/promotion-detail.js
@@ -0,0 +1,147 @@
+﻿angular.module('virtoCommerce.marketingModule')
+.controller('promotionDetailController', ['$scope', 'bladeNavigationService', 'promotions', 'catalogs', 'stores', 'settings', 'dialogService', function ($scope, bladeNavigationService, promotions, catalogs, stores, settings, dialogService) {
+    $scope.blade.refresh = function (parentRefresh) {
+        if ($scope.blade.isNew) {
+            initializeBlade({ maxUsageCount: 0, maxPersonalUsageCount: 0, priority: 1 });
+        } else {
+            promotions.get({ id: $scope.blade.currentEntityId }, function (data) {
+                initializeBlade(data);
+                if (parentRefresh) {
+                    $scope.blade.parentBlade.refresh();
+                }
+            });
+        }
+    };
+
+    function initializeBlade(data) {
+        if (!$scope.blade.isNew) {
+            $scope.blade.title = data.name;
+        }
+
+        $scope.blade.currentEntity = angular.copy(data);
+        $scope.blade.origEntity = data;
+        $scope.blade.isLoading = false;
+    };
+
+    function isDirty() {
+        return !angular.equals($scope.blade.currentEntity, $scope.blade.origEntity);
+    };
+
+    $scope.cancelChanges = function () {
+        //angular.copy($scope.blade.origEntity, $scope.blade.currentEntity);
+        $scope.bladeClose();
+    };
+
+    $scope.saveChanges = function () {
+        $scope.blade.isLoading = true;
+
+        if ($scope.blade.isNew) {
+            promotions.save({}, $scope.blade.currentEntity, function (data) {
+                $scope.blade.isNew = undefined;
+                $scope.blade.currentEntityId = data.id;
+                initializeBlade(data);
+                initializeToolbar();
+                $scope.blade.parentBlade.refresh();
+            }, function (error) {
+                bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+            });
+        } else {
+            promotions.update({}, $scope.blade.currentEntity, function (data) {
+                $scope.blade.refresh(true);
+            }, function (error) {
+                bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+            });
+        }
+    };
+
+    $scope.setForm = function (form) {
+        $scope.formScope = form;
+    }
+
+    $scope.blade.onClose = function (closeCallback) {
+        closeChildrenBlades();
+        if (isDirty() && !$scope.blade.isNew) {
+            var dialog = {
+                id: "confirmCurrentBladeClose",
+                title: "Save changes",
+                message: "The promotion has been modified. Do you want to save changes?"
+            };
+            dialog.callback = function (needSave) {
+                if (needSave) {
+                    $scope.saveChanges();
+                }
+                closeCallback();
+            };
+            dialogService.showConfirmationDialog(dialog);
+        }
+        else {
+            closeCallback();
+        }
+    };
+
+    function closeChildrenBlades() {
+        angular.forEach($scope.blade.childrenBlades.slice(), function (child) {
+            bladeNavigationService.closeBlade(child);
+        });
+    }
+
+    $scope.bladeHeadIco = 'fa-flag';
+
+    function initializeToolbar() {
+        if (!$scope.blade.isNew) {
+            $scope.bladeToolbarCommands = [
+                {
+                    name: "Save",
+                    icon: 'fa fa-save',
+                    executeMethod: function () {
+                        $scope.saveChanges();
+                    },
+                    canExecuteMethod: function () {
+                        return isDirty() && $scope.formScope && $scope.formScope.$valid;
+                    }
+                },
+                {
+                    name: "Reset",
+                    icon: 'fa fa-undo',
+                    executeMethod: function () {
+                        angular.copy($scope.blade.origEntity, $scope.blade.currentEntity);
+                    },
+                    canExecuteMethod: function () {
+                        return isDirty();
+                    }
+                }
+            ];
+        }
+    }
+
+    // datepicker
+    $scope.datepickers = {
+        str: false,
+        end: false
+    }
+
+    $scope.open = function ($event, which) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.datepickers[which] = true;
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yyyy'",
+    };
+
+    // $scope.formats = ['shortDate', 'dd-MMMM-yyyy', 'yyyy/MM/dd'];
+    $scope.format = 'shortDate';
+
+
+    initializeToolbar();
+    $scope.blade.refresh(false);
+    $scope.catalogs = catalogs.getCatalogs();
+    $scope.stores = stores.query();
+    $scope.exclusivities = settings.getValues({ id: 'VirtoCommerce.Marketing.Promotions.Exclusivities' }, function (data) {
+        if ($scope.blade.isNew && data && data[0]) {
+            $scope.blade.currentEntity.exclusivity = data[0];
+        }
+    });
+}]);
