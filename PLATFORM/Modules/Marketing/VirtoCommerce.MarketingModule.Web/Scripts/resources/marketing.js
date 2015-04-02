5d14fa323d57804fcaaef0f980b7248887051b44
eMazeika <em@virtoway.com>
2015-04-03 00:39:29 +0300

    marketing - fixes and dynamic expression updates

--- PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/resources/marketing.js
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/resources/marketing.js
@@ -1,6 +1,7 @@
 ﻿angular.module('virtoCommerce.marketingModule')
     .factory('promotions', ['$resource', function ($resource) {
         return $resource('api/marketing/promotions/:id', { id: '@Id' }, {
+            getNew: { url: 'api/marketing/promotions/new' },
             update: { method: 'PUT' }
         });
     }]);
