5e323df623f6c5a8d88024ae77c319d4dc17ae68
eMazeika <em@virtoway.com>
2015-04-02 11:34:51 +0300

    marketing - dynamic expression directives added (used with promotions)

---
+++ PLATFORM/Modules/Core/VirtoCommerce.Core.Web/Scripts/common/dynamicExpression/expression-directives.js
@@ -0,0 +1,25 @@
+﻿angular.module('virtoCommerce.coreModule')
+// .controller('expressionBlockController', )
+    .directive('vaExpressionBlock', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'Modules/$(VirtoCommerce.Core)/Scripts/common/dynamicExpression/expression-block.tpl.html'
+        };
+    })
+    .directive('vaExpressionElement', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                element: '='
+            },
+            template: '<ng-include src="getTemplateUrl()"/>',
+            //templateUrl: unfortunately has no access to $scope
+            link: function ($scope) {
+                //function used on the ng-include to resolve the template
+                $scope.getTemplateUrl = function() {
+                    // return 'Modules/$(VirtoCommerce.Core)/Scripts/common/dynamicExpression/expression-' + $scope.element.type + '.tpl.html';
+                    return 'expression-' + $scope.element.type + '.html';
+                };
+            }
+        };
+    });
