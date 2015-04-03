cb226ef4d04940bdff3eac61b2a6060685cde97c
aartiomov <andrej.artiomov@gmail.com>
2015-04-03 18:59:21 +0300

    - UPD. Updates to mailChimp and GoogleShopping modules.
    Former-commit-id: cb2a68bd8c9384b0a379a9d51777f623e2a93e32

---
+++ PLATFORM/Modules/Merchant/GoogleShopping.Merchant.Web/AppFlowMetadata.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Util.Store;
+
+namespace GoogleShopping.MerchantModule.Web
+{
+    public class AppFlowMetadata : FlowMetadata
+    {
+        private static readonly IAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+                {
+                    ClientSecrets = new ClientSecrets
+                    {
+                        ClientId = "PUT_CLIENT_ID_HERE",
+                        ClientSecret = "PUT_CLIENT_SECRET_HERE"
+                    },
+                    Scopes = new[] { DriveService.Scope.Drive },
+                    DataStore = new FileDataStore("Shopping.Api.Auth.Store")
+                });
+
+        public override string GetUserId(Controller controller)
+        {
+            // In this sample we use the session to store the user identifiers.
+            // That's not the best practice, because you should have a logic to identify
+            // a user. You might want to use "OpenID Connect".
+            // You can read more about the protocol in the following link:
+            // https://developers.google.com/accounts/docs/OAuth2Login.
+            var user = controller.Session["user"];
+            if (user == null)
+            {
+                user = Guid.NewGuid();
+                controller.Session["user"] = user;
+            }
+            return user.ToString();
+
+        }
+
+        public override IAuthorizationCodeFlow Flow
+        {
+            get { return flow; }
+        }
+    }
+}
