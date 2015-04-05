e4b3a94be19579a83b14a24a05c6766c4b47aea6
artem-dudarev <ad@virtoway.com>
2015-04-05 12:31:50 +0200

    Use DateTime.UtcNow
    Former-commit-id: 883dc5198da5ba27f25c92a38c69fea26b069ca4

--- PLATFORM/Extensions/Jobs/VirtoCommerce.Scheduling.Windows/QuartzJob.cs
+++ PLATFORM/Extensions/Jobs/VirtoCommerce.Scheduling.Windows/QuartzJob.cs
@@ -5,18 +5,18 @@ using VirtoCommerce.Scheduling.LogicalCall;
 
 namespace VirtoCommerce.Scheduling.Windows
 {
-	public class QuartzJob : IJob
-	{
-		public void Execute(IJobExecutionContext quartzContext)
-		{
-			var data = quartzContext.JobDetail.JobDataMap;
-			var realization = data["realization"];
-			var jobActivity = ((Func<IJobActivity>)realization)();
-			var traceContext = (TraceContext)data["context"];
-		    var parameters = (IDictionary<string, string>) data["parameters"];
+    public class QuartzJob : IJob
+    {
+        public void Execute(IJobExecutionContext quartzContext)
+        {
+            var data = quartzContext.JobDetail.JobDataMap;
+            var realization = data["realization"];
+            var jobActivity = ((Func<IJobActivity>)realization)();
+            var traceContext = (TraceContext)data["context"];
+            var parameters = (IDictionary<string, string>)data["parameters"];
             var getAudit = (Func<DateTime, Action<string>>)data["getAudit"];
 
-            JobActivityTool.ControlledExecution(jobActivity, traceContext, getAudit(DateTime.Now), parameters);
-		}
-	}
+            JobActivityTool.ControlledExecution(jobActivity, traceContext, getAudit(DateTime.UtcNow), parameters);
+        }
+    }
 }
