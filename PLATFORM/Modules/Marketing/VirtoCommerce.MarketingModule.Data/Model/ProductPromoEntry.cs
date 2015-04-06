9e274f1ce6167646da4b5514405e81882004a64d
E.Tatarincev <et@virtoway.com>
2015-04-06 21:31:45 +0300

    -ADD missed files

---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Data/Model/ProductPromoEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.MarketingModule.Data
+{
+	public class ProductPromoEntry
+	{
+		public string Code { get; set; }
+		public decimal Quantity { get; set; }
+		public decimal Price { get; set; }
+		public decimal Discount { get; set; }
+		public string CatalogId { get; set; }
+		public string CategoryId { get; set; }
+		public string ProductId { get; set; }
+		public object Owner { get; set; }
+		public string Outline { get; set; }
+
+		public Dictionary<string, string> Attributes { get; set; }
+        
+	}
+}
