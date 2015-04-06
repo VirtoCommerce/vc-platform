1bea6ac60dae708027a953c0d2576da52be4e13d
E.Tatarincev <et@virtoway.com>
2015-04-06 11:53:25 +0300

    -UPD Inventory  API add multiple products support
    Former-commit-id: 4d2ea65f9add6bc228ddd4c853d2cc9e468f10de

--- PLATFORM/Modules/Inventory/VirtoCommerce.InventoryModule.Web/Scripts/resources/inventory.js
+++ PLATFORM/Modules/Inventory/VirtoCommerce.InventoryModule.Web/Scripts/resources/inventory.js
@@ -1,6 +1,6 @@
 ﻿angular.module('virtoCommerce.inventoryModule.resources', [])
 .factory('inventories', ['$resource', function ($resource) {
-    return $resource('api/catalog/products/:id/inventory', { id: '@Id' }, {
+    return $resource('api/inventory/products/:id', { id: '@Id' }, {
         // query: { },
         update: { method: 'PUT' }
     });
