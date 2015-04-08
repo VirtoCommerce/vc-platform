76a2178059bc56da5933c73473c657e53312ea50
E.Tatarincev <et@virtoway.com>
2015-04-08 18:06:36 +0300

    -ADD Marketing API (finished)
    Former-commit-id: 14843e057ceb951d1d181ec30a69fff83411032d

---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.DynamicExpression/DynamicExpressionBlock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using VirtoCommerce.Domain.Common;
+using VirtoCommerce.Foundation.Frameworks.Extensions;
+
+namespace VirtoCommerce.MarketingModule.DynamicExpression
+{
+	public abstract class DynamicExpressionBlock : DynamicExpression
+	{
+		public DynamicExpressionBlock()
+		{
+			AvailableChildren = new List<IDynamicExpression>();
+			Children = new List<IDynamicExpression>();
+		}
+
+		public T FindAvailableExpression<T>() where T : IDynamicExpression
+		{
+			var retVal = this.Traverse(x => x.AvailableChildren.OfType<DynamicExpressionBlock>()).SelectMany(x => x.AvailableChildren).OfType<T>().FirstOrDefault();
+			return retVal;
+		}
+		public T FindChildrenExpression<T>() where T : IDynamicExpression
+		{
+			var retVal =  this.Traverse(x => x.Children.OfType<DynamicExpressionBlock>()).SelectMany(x => x.Children).OfType<T>().FirstOrDefault();
+			return retVal;
+		}
+
+		[JsonProperty(ItemTypeNameHandling = TypeNameHandling.All)]
+		public ICollection<IDynamicExpression> AvailableChildren { get; set; }
+		[JsonProperty(ItemTypeNameHandling = TypeNameHandling.All)]
+		public ICollection<IDynamicExpression> Children { get; set; }
+	}
+}
---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.DynamicExpression/Promotion/Conditions/BlockConditionAndOr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VirtoCommerce.Domain.Marketing.Model;
+using VirtoCommerce.Foundation.Frameworks;
+using VirtoCommerce.Foundation.Frameworks.Extensions;
+
+namespace VirtoCommerce.MarketingModule.DynamicExpression.Promotion
+{
+	public abstract class BlockConditionAndOr : DynamicExpressionBlock, IConditionExpression
+	{
+		public bool All { get; set; }
+		#region IConditionExpression Members
+
+		public System.Linq.Expressions.Expression<Func<IPromotionEvaluationContext, bool>> GetConditionExpression()
+		{
+			System.Linq.Expressions.Expression<Func<IPromotionEvaluationContext, bool>> retVal = null;
+			if (Children != null && Children.Any())
+			{
+				retVal = All ? PredicateBuilder.True<IPromotionEvaluationContext>() : PredicateBuilder.False<IPromotionEvaluationContext>();
+				foreach (var expression in Children.OfType<IConditionExpression>().Select(x => x.GetConditionExpression()))
+				{
+					retVal = All ? retVal.And(expression) : retVal.Or(expression);
+				}
+			}
+			return retVal;
+		}
+
+		#endregion
+
+		
+	}
+}
