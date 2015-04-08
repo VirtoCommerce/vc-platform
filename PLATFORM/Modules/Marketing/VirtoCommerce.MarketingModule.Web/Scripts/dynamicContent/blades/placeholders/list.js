b1abf13d055a7e1e278b4266564ed99f8e373d44
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-04-08 16:36:45 +0300

    DC

---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/placeholders/list.js
@@ -0,0 +1,63 @@
+﻿angular.module('virtoCommerce.marketingModule')
+.controller('placeholdersDynamicContentListController', ['$scope', 'bladeNavigationService', function ($scope, bladeNavigationService) {
+	var blade = $scope.blade
+
+	$scope.selectedNodeId = null;
+
+	function initializeBlade() {
+		blade.isLoading = false;
+	};
+
+	blade.addNew = function () {
+		closeChildrenBlades();
+
+		var newBlade = {
+			id: 'listItemChild',
+			title: 'New placeholders element',
+			subtitle: 'Add new placeholders element',
+			isNew: true,
+			controller: 'addPlaceholderController',
+			template: 'Modules/$(VirtoCommerce.Marketing)/Scripts/dynamicContent/blades/placeholders/add.tpl.html'
+		};
+		bladeNavigationService.showBlade(newBlade, $scope.blade);
+	}
+
+	blade.addNewFolder = function () {
+		closeChildrenBlades();
+
+		var newBlade = {
+			id: 'listItemChild',
+			title: 'New placeholders element',
+			subtitle: 'Add new placeholders element',
+			isNew: true,
+			controller: 'addPlaceholderController',
+			template: 'Modules/$(VirtoCommerce.Marketing)/Scripts/dynamicContent/blades/placeholders/add.tpl.html'
+		};
+		bladeNavigationService.showBlade(newBlade, $scope.blade);
+	}
+
+	$scope.bladeToolbarCommands = [
+        {
+        	name: "Refresh", icon: 'fa fa-refresh',
+        	executeMethod: function () {
+        		$scope.blade.refresh();
+        	},
+        	canExecuteMethod: function () {
+        		return true;
+        	}
+        },
+        {
+        	name: "Add", icon: 'fa fa-plus',
+        	executeMethod: function () {
+        		blade.addNew();
+        	},
+        	canExecuteMethod: function () {
+        		return true;
+        	}
+        },
+	];
+
+	$scope.bladeHeadIco = 'fa fa-flag';
+
+	initializeBlade();
+}]);
