c2ca49ecc821b32129362b2ab049cb25cc1651cd
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-04-08 16:36:45 +0300

    DC
    Former-commit-id: 2a3f59b32d34613eb975d2f150071600e6d940db

---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/dynamicContent-list.js
@@ -0,0 +1,32 @@
+﻿angular.module('virtoCommerce.marketingModule')
+.controller('dynamicContentListController', ['$scope', 'bladeNavigationService', function ($scope, bladeNavigationService) {
+	var blade = $scope.blade
+
+	$scope.selectedNodeId = null;
+
+	function initializeBlade() {
+		var entities = [
+            { id: 'DC_1', name: 'Content items', subname: 'Content items list', entityName: 'items' },
+            { id: 'DC_2', name: 'Content placeholders', subname: 'Content items list', entityName: 'placeholders' },
+            { id: 'DC_3', name: 'Content publishing', subname: 'Content items list', entityName: 'publishing' }];
+		blade.currentEntities = entities;
+		blade.isLoading = false;
+	};
+
+	blade.openBlade = function (data) {
+		$scope.selectedNodeId = data.id;
+
+		var newBlade = {
+			id: 'marketingDetails',
+			title: data.name,
+			subtitle: 'Marketing service',
+			controller: data.entityName + 'DynamicContentListController',
+			template: 'Modules/$(VirtoCommerce.Marketing)/Scripts/dynamicContent/blades/' + data.entityName + '/list.tpl.html'
+		};
+		bladeNavigationService.showBlade(newBlade, blade);
+	}
+
+	$scope.bladeHeadIco = 'fa fa-flag';
+
+	initializeBlade();
+}]);
---
+++ PLATFORM/Modules/Marketing/VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/placeholders/add.js
@@ -0,0 +1,37 @@
+﻿angular.module('virtoCommerce.marketingModule')
+.controller('pla', ['$scope', 'bladeNavigationService', 'categories', 'items', function ($scope, bladeNavigationService, categories, items) {
+	var balde = $scope.blade;
+	var pb = $scope.blade.parentBlade;
+
+	$scope.addCategory = function () {
+		categories.newCategory({ catalogId: pb.catalogId, parentCategoryId: pb.categoryId },
+            function (data) {
+            	var newBlade = {
+            		id: "newCategoryWizard",
+            		currentEntity: data,
+            		title: "New category",
+            		subtitle: 'Fill category information',
+            		controller: 'newCategoryWizardController',
+            		template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/wizards/newCategory/category-wizard.tpl.html'
+            	};
+            	bladeNavigationService.showBlade(newBlade, pb);
+
+            	$scope.bladeClose();
+            });
+	};
+
+	$scope.addLinkedCategory = function () {
+		$scope.bladeClose();
+
+		var newBlade = {
+			id: 'selectCatalog',
+			title: 'Select Catalog',
+			subtitle: 'Creating a Link inside virtual catalog',
+			controller: 'catalogsSelectController',
+			template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/catalogs-select.tpl.html'
+		};
+		bladeNavigationService.showBlade(newBlade, $scope.blade.parentBlade);
+	};
+
+	$scope.blade.isLoading = false;
+}]);
