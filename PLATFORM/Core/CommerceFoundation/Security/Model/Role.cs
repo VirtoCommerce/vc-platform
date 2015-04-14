a57eaed6ec47e79b8cce1de7321768c7adfd3e2c
artem-dudarev <ad@virtoway.com>
2015-04-14 14:30:21 +0200

    Added PUT method for role management.
    Former-commit-id: e8a3390c0af82701c744071be631856a155cdac3

--- PLATFORM/Core/CommerceFoundation/Security/Model/Role.cs
+++ PLATFORM/Core/CommerceFoundation/Security/Model/Role.cs
@@ -6,109 +6,110 @@ using VirtoCommerce.Foundation.Frameworks;
 
 namespace VirtoCommerce.Foundation.Security.Model
 {
-	[DataContract]
-	[EntitySet("Roles")]
-	[DataServiceKey("RoleId")]
-	public class Role : StorageEntity
-	{
-		public Role()
-		{
-			_RoleId = GenerateNewKey();
-		}
+    [DataContract]
+    [EntitySet("Roles")]
+    [DataServiceKey("RoleId")]
+    public class Role : StorageEntity
+    {
+        public Role()
+        {
+            _RoleId = GenerateNewKey();
+        }
 
-		private string _RoleId;
-		[Key]
-		[DataMember]
-		[StringLength(64)]
-		public string RoleId
-		{
-			get
-			{
-				return _RoleId;
-			}
-			set
-			{
-				SetValue(ref _RoleId, () => this.RoleId, value);
-			}
-		}
+        private string _RoleId;
+        [Key]
+        [DataMember]
+        [StringLength(64)]
+        public string RoleId
+        {
+            get
+            {
+                return _RoleId;
+            }
+            set
+            {
+                SetValue(ref _RoleId, () => this.RoleId, value);
+            }
+        }
 
-		private string _Name;
-		[Required]
-		[DataMember]
-		[StringLength(128)]
-		public string Name
-		{
-			get
-			{
-				return _Name;
-			}
-			set
-			{
-				SetValue(ref _Name, () => this.Name, value);
-			}
-		}
+        private string _Name;
+        [Required]
+        [DataMember]
+        [StringLength(128)]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                SetValue(ref _Name, () => this.Name, value);
+            }
+        }
 
-		#region Navigation Properties
+        #region Navigation Properties
 
-		private ObservableCollection<RolePermission> _rolePermissions = null;
-		[DataMember]
-		public ObservableCollection<RolePermission> RolePermissions
-		{
-			get
-			{
-				if (_rolePermissions == null)
-				{
-					_rolePermissions = new ObservableCollection<RolePermission>();
-				}
-				return _rolePermissions;
-			}
-		}
+        private ObservableCollection<RolePermission> _rolePermissions = null;
+        [DataMember]
+        public ObservableCollection<RolePermission> RolePermissions
+        {
+            get
+            {
+                if (_rolePermissions == null)
+                {
+                    _rolePermissions = new ObservableCollection<RolePermission>();
+                }
+                return _rolePermissions;
+            }
+            set { _rolePermissions = value; }
+        }
 
-		private ObservableCollection<RoleAssignment> _assignments = null;
+        private ObservableCollection<RoleAssignment> _assignments = null;
 
-		[DataMember]
-		public ObservableCollection<RoleAssignment> RoleAssignments
-		{
-			get
-			{
-				if (_assignments == null)
-					_assignments = new ObservableCollection<RoleAssignment>();
+        [DataMember]
+        public ObservableCollection<RoleAssignment> RoleAssignments
+        {
+            get
+            {
+                if (_assignments == null)
+                    _assignments = new ObservableCollection<RoleAssignment>();
 
-				return _assignments;
-			}
-		}
+                return _assignments;
+            }
+        }
 
-		#endregion
+        #endregion
 
-		public override int GetHashCode()
-		{
-			return base.GetHashCode();
-		}
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
 
-		public override bool Equals(object obj)
-		{
-			if (obj == null)
-			{
-				return false;
-			}
-			if (obj == this)
-			{
-				return true;
-			}
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj == this)
+            {
+                return true;
+            }
 
-			bool retVal = false;
-			Role otherRole = obj as Role;
-			if (otherRole != null)
-			{
-				retVal = RoleId == otherRole.RoleId;
-			}
-			else
-			{
-				//default impl
-				base.Equals(obj);
-			}
+            bool retVal = false;
+            Role otherRole = obj as Role;
+            if (otherRole != null)
+            {
+                retVal = RoleId == otherRole.RoleId;
+            }
+            else
+            {
+                //default impl
+                base.Equals(obj);
+            }
 
-			return retVal;
-		}
-	}
+            return retVal;
+        }
+    }
 }
