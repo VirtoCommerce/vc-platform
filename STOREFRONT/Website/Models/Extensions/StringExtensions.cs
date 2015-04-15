097d9d009b469cb8f4534e05bfb545c535333c00
Woland2k <sasha@virtoway.com>
2015-04-15 13:56:31 -0700

    minor changes
    Former-commit-id: ec55163c2e3b19ee07b44af7c7042975b6290a3f

--- STOREFRONT/Website/Models/Extensions/StringExtensions.cs
+++ STOREFRONT/Website/Models/Extensions/StringExtensions.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Globalization;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 
 #endregion
 
@@ -25,8 +27,27 @@ namespace VirtoCommerce.Web.Models.Extensions
         public static string ToAbsoluteUrl(this string relative)
         {
             if (relative == null) return null;
+            //var urlHelper = GetUrlHelper();
+            //urlHelper.RouteUrl()
 
             return relative.StartsWith("~") ? VirtualPathUtility.ToAbsolute(relative) : relative;
+        }
+
+        private static UrlHelper GetUrlHelper()
+        {
+            var httpContext = HttpContext.Current;
+            if (httpContext == null)
+            {
+                return null;
+            }
+
+            var httpContextBase = new HttpContextWrapper(httpContext);
+            var routeData = new RouteData();
+            var requestContext = new RequestContext(httpContextBase, routeData);
+
+            var urlHelper = new UrlHelper(requestContext);
+            return urlHelper;
+
         }
         #endregion
     }
