6a250831a24b39eb10f1dee34e211ce99ef5686b
Artyom <artem-dudarev@users.noreply.github.com>
2015-04-24 11:57:57 +0200

    Added IAuditable to AuditableEntity
    Former-commit-id: f5840b8f21c21cd7e90dd751dba058eb2ab868c8

--- PLATFORM/VirtoCommerce.Platform.Core/Common/AuditableEntity.cs
+++ PLATFORM/VirtoCommerce.Platform.Core/Common/AuditableEntity.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace VirtoCommerce.Platform.Core.Common
 {
-	public abstract class AuditableEntity : Entity
-	{
-		#region IAuditable Members
+    public abstract class AuditableEntity : Entity, IAuditable
+    {
+        #region IAuditable Members
 
-		public DateTime CreatedDate { get; set; }
-		public DateTime? ModifiedDate { get; set; }
-		public string CreatedBy { get; set; }
-		public string ModifiedBy { get; set; }
-		#endregion
-	}
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+
+        #endregion
+    }
 }
