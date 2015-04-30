14777759a6be0233d4f0c3c1096d80fa0313918e
eMazeika <em@virtoway.com>
2015-04-30 15:25:36 +0300

    platform - animation on blade open/close added
    Former-commit-id: abcb90bb45fa1cc877b383545e05bc384db18a73

--- PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/navigation/blade/bladeNavigation.js
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/navigation/blade/bladeNavigation.js
@@ -1,23 +1,19 @@
 angular.module('platformWebApp')
-.directive('vaBladeContainer', ['$compile', 'bladeNavigationService', function ($compile, bladeNavigationService)
-{
+.directive('vaBladeContainer', ['$compile', 'bladeNavigationService', function ($compile, bladeNavigationService) {
     return {
         restrict: 'E',
         replace: true,
         templateUrl: 'Scripts/app/navigation/blade/bladeContainer.tpl.html',
-        link: function (scope)
-        {
+        link: function (scope) {
             scope.blades = bladeNavigationService.stateBlades();
         }
     }
 }])
-.directive('vaBlade', ['$compile', 'bladeNavigationService', '$timeout', function ($compile, bladeNavigationService, $timeout)
-{
+.directive('vaBlade', ['$compile', 'bladeNavigationService', '$timeout', function ($compile, bladeNavigationService, $timeout) {
     return {
         terminal: true,
         priority: 100,
-        link: function (scope, element)
-        {
+        link: function (scope, element) {
             element.attr('ng-controller', scope.blade.controller);
             element.attr('id', scope.blade.id);
             element.attr('ng-model', "blade");
@@ -26,14 +22,21 @@ angular.module('platformWebApp')
 
             var speed = (window.navigator.platform == 'MacIntel' ? .5 : 40);
 
-            $timeout(function ()
-            {
+            $timeout(function () {
                 var mainContent = $('.cnt');
                 var blade = $(element).parent('.blade');
                 var offset = parseInt(blade.offset().left + mainContent.scrollLeft() + blade.width() + 125 - mainContent[0].clientWidth);
                 if (offset > mainContent.scrollLeft()) {
                     mainContent.animate({ scrollLeft: offset + 'px' }, 500);
                 }
+
+                blade.css('margin-left', '-' + blade.width() + 'px');
+                
+                setTimeout(function () {
+                    blade.animate({ 'margin-left': 0 }, 250, function () {
+                        // $(this).removeAttr('style').removeClass('__animate');
+                    });
+                }, 100);
             }, 0, false);
 
 
@@ -45,9 +48,9 @@ angular.module('platformWebApp')
             // });
 
             $(element).find('.blade-container').on('mouseenter', function (e) {
-                var blade   = $(this),
-                    bladeI  = blade.find('.blade-inner'),
-                    bladeH  = bladeI.height(),
+                var blade = $(this),
+                    bladeI = blade.find('.blade-inner'),
+                    bladeH = bladeI.height(),
                     bladeIh = blade.find('.inner-block').height() + 5;
 
                 if (blade.length) {
@@ -63,8 +66,8 @@ angular.module('platformWebApp')
             });
 
             $('.dashboard-area').on('mouseenter', function (event) {
-                var dashboardA  = $(this),
-                    dashboardH  = dashboardA.height(),
+                var dashboardA = $(this),
+                    dashboardH = dashboardA.height(),
                     dashboardIh = dashboardA.find('.dashboard-inner').height();
 
                 if (dashboardA.length) {
@@ -78,25 +81,20 @@ angular.module('platformWebApp')
             }).on('mouseleave', function () {
                 horizontalScroll('on');
             });
-            
-            function horizontalScroll(flag)
-            {
-                if (flag != 'off')
-                {
-                    $('.cnt').off('mousewheel').on('mousewheel', function (event, delta)
-                    {
+
+            function horizontalScroll(flag) {
+                if (flag != 'off') {
+                    $('.cnt').off('mousewheel').on('mousewheel', function (event, delta) {
                         this.scrollLeft -= (delta * speed);
                         event.preventDefault();
                     });
                 }
-                else
-                {
+                else {
                     $('.cnt').unmousewheel();
                 }
             }
 
-            scope.bladeMaximize = function ()
-            {
+            scope.bladeMaximize = function () {
                 scope.maximized = true;
 
                 var blade = $(element);
@@ -105,12 +103,11 @@ angular.module('platformWebApp')
                 var offset = parseInt((blade.offset().left + $('.cnt').scrollLeft()) - leftMenu.width());
                 var contentblock = blade.find(".blade-content");
                 $(contentblock).animate({ width: (parseInt(window.innerWidth - leftMenu.width()) + 'px') }, 100);
-                $(contentblock).find('.inner-block').animate({width: parseInt(window.innerWidth - leftMenu.width() - 40) + 'px'}, 100);
+                $(contentblock).find('.inner-block').animate({ width: parseInt(window.innerWidth - leftMenu.width() - 40) + 'px' }, 100);
                 $('.cnt').animate({ scrollLeft: offset + 'px' }, 250);
             };
 
-            scope.bladeRestore = function ()
-            {
+            scope.bladeRestore = function () {
                 scope.maximized = false;
 
                 var blade = $(element);
@@ -124,93 +121,81 @@ angular.module('platformWebApp')
                 $('.cnt').animate({ scrollLeft: offset + 'px' }, 250);
             };
 
-            scope.bladeClose = function ()
-            {
-                bladeNavigationService.closeBlade(scope.blade);
+            scope.bladeClose = function () {
+                var blade = $(element),
+                    bladeW = blade.width();
+
+                blade.addClass('__animate').animate({ 'margin-left': '-' + bladeW + 'px' }, 250, function () {
+                    $timeout(function () {
+                        bladeNavigationService.closeBlade(scope.blade);
+                    }, 0, true);
+                });
             };
 
-            scope.executeCommand = function (toolbarCommand)
-            {
+            scope.executeCommand = function (toolbarCommand) {
                 if (toolbarCommand.canExecuteMethod())
-                	toolbarCommand.executeMethod(scope.blade);
+                    toolbarCommand.executeMethod(scope.blade);
             };
         }
     };
 }])
-.factory('bladeNavigationService', ['$rootScope', '$filter', '$state', function ($rootScope, $filter, $state)
-{
+.factory('bladeNavigationService', ['$rootScope', '$filter', '$state', function ($rootScope, $filter, $state) {
     var service = {
         blades: [],
         currentBlade: undefined,
-        closeBlade: function (blade, callback)
-        {
+        closeBlade: function (blade, callback) {
             //Need in case a copy was passed
             blade = service.findBlade(blade.id);
             var idx = service.stateBlades().indexOf(blade);
 
-            var doCloseBlade = function ()
-            {
+            var doCloseBlade = function () {
                 service.stateBlades().splice(idx, 1);
                 //remove blade from childs collection
-                if (angular.isDefined(blade.parentBlade))
-                {
+                if (angular.isDefined(blade.parentBlade)) {
                     var childIdx = blade.parentBlade.childrenBlades.indexOf(blade);
-                    if (childIdx >= 0)
-                    {
+                    if (childIdx >= 0) {
                         blade.parentBlade.childrenBlades.splice(childIdx, 1);
                     }
                 }
-                if (angular.isFunction(callback))
-                {
+                if (angular.isFunction(callback)) {
                     callback();
                 };
             };
-            if (idx >= 0)
-            {
-                if (angular.isFunction(blade.onClose))
-                {
+            if (idx >= 0) {
+                if (angular.isFunction(blade.onClose)) {
                     blade.onClose(doCloseBlade);
                 }
-                else
-                {
+                else {
                     doCloseBlade();
                 }
             }
         },
-        hasBlade: function (id)
-        {
+        hasBlade: function (id) {
             return service.findBlade(id) !== undefined;
         },
-        stateBlades: function (stateName)
-        {
-            if (angular.isUndefined(stateName))
-            {
+        stateBlades: function (stateName) {
+            if (angular.isUndefined(stateName)) {
                 stateName = $state.current.name;
             }
 
-            if (angular.isUndefined(service.blades[stateName]))
-            {
+            if (angular.isUndefined(service.blades[stateName])) {
                 service.blades[stateName] = [];
             }
 
             return service.blades[stateName];
         },
-        findBlade: function (id)
-        {
+        findBlade: function (id) {
 
             var found;
-            angular.forEach(service.stateBlades(), function (blade)
-            {
-                if (blade.id == id)
-                {
+            angular.forEach(service.stateBlades(), function (blade) {
+                if (blade.id == id) {
                     found = blade;
                 }
             });
 
             return found;
         },
-        showBlade: function (blade, parentBlade)
-        {
+        showBlade: function (blade, parentBlade) {
 
             blade.isLoading = true;
             blade.parentBlade = parentBlade;
@@ -219,48 +204,41 @@ angular.module('platformWebApp')
             var existingBlade = service.findBlade(blade.id);
 
             //Show blade in previous location
-            if (existingBlade != undefined)
-            {
+            if (existingBlade != undefined) {
                 //store prev blade x-index
                 blade.xindex = existingBlade.xindex;
             }
 
             //Show blade as last one by default
-            if (!angular.isDefined(blade.xindex))
-            {
+            if (!angular.isDefined(blade.xindex)) {
                 blade.xindex = service.stateBlades().length;
             }
 
 
-            if (angular.isDefined(parentBlade))
-            {
+            if (angular.isDefined(parentBlade)) {
                 blade.xindex = service.stateBlades().indexOf(parentBlade) + 1;
                 parentBlade.childrenBlades.push(blade);
             }
 
-            var showBlade = function ()
-            {
+            var showBlade = function () {
                 //show blade in same place where it been
                 service.stateBlades().splice(Math.min(blade.xindex, service.stateBlades().length), 0, blade);
                 service.currentBlade = blade;
             };
 
-            if (existingBlade != undefined)
-            {
+            if (existingBlade != undefined) {
                 service.closeBlade(existingBlade, showBlade);
             }
-            else
-            {
+            else {
                 showBlade();
             }
 
         },
         setError: function (msg, blade) {
-        	blade.isLoading = false;
-        	blade.error = msg;
+            blade.isLoading = false;
+            blade.error = msg;
         }
     };
 
     return service;
 }]);
-
