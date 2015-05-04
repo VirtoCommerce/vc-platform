918ccee0aa1b02980f679d2873fa6599b8678dd5
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-05-05 01:50:27 +0300

    folders
    Former-commit-id: d68a649255ed856de26651a422ab5ac25b1eeb55

---
+++ PLATFORM/Modules/CMS/VirtoCommerce.Content.Web/Models/PageFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.Content.Web.Models
+{
+	public class PageFolder
+	{
+		public string FolderName { get; set; }
+
+		private Collection<Page> _pages;
+		public Collection<Page> Pages { get { return _pages ?? (_pages = new Collection<Page>()); } }
+	}
+}
