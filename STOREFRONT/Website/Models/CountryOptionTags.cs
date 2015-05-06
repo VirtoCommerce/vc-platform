129498cb7bbf467c98d3600bf30bc910a2b9e73f
andrew-orlov <ao@virtoway.com>
2015-05-06 12:47:46 +0200

    CountryOptionTags as class
    Former-commit-id: 711dbdad1900933cc63b7bc84720a40de17b2190

---
+++ STOREFRONT/Website/Models/CountryOptionTags.cs
@@ -0,0 +1,47 @@
+﻿using DotLiquid;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirtoCommerce.Web.Models
+{
+    public class CountryOptionTags : Drop
+    {
+        private const string tagTemplate = "<option value=\"{0}\" data-provinces=\"{1}\">{0}</option>\r\n";
+        private IDictionary<string, ICollection<string>> _countries;
+
+        public CountryOptionTags(Dictionary<string, ICollection<string>> countries)
+        {
+            _countries = countries;
+        }
+
+        public override string ToString()
+        {
+            var tags = new StringBuilder();
+
+            foreach (var country in _countries)
+            {
+                var provinces = new StringBuilder("[");
+
+                int i = 1;
+                foreach (var province in country.Value.OrderBy(p => p))
+                {
+                    provinces.AppendFormat("&quot;{0}&quot;", province);
+
+                    if (i < country.Value.Count)
+                    {
+                        provinces.Append(",");
+                    }
+
+                    i++;
+                }
+
+                provinces.Append("]");
+
+                tags.AppendFormat(tagTemplate, country.Key, provinces.ToString());
+            }
+
+            return tags.ToString();
+        }
+    }
+}
