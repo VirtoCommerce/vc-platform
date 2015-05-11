67f73584ad36eecc4564bc78b9c53028ba34f39e
aartiomov <andrej.artiomov@gmail.com>
2015-05-11 16:54:45 +0300

    - ADD. Widgets, api. - REF. Refactored Zendesk api.
    Former-commit-id: 70ca28dcb2cd38ea1cde57bb2ac7627d4d019297

--- PLATFORM/Modules/Helpdesk/Zendesk.HelpdeskModule.Web/Scripts/helpdesk.js
+++ PLATFORM/Modules/Helpdesk/Zendesk.HelpdeskModule.Web/Scripts/helpdesk.js
@@ -9,8 +9,14 @@ angular.module(moduleName, [
 ])
 
 .run(
-  ['$rootScope', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state', function ($rootScope, mainMenuService, widgetService, $state) {
-     
+  ['$rootScope', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', 'platformWebApp.authService', function ($rootScope, mainMenuService, widgetService, authService) {
+
+      widgetService.registerWidget({
+          isVisible: function(blade) { return authService.checkPermission('helpdeskModule:manage'); },
+          controller: 'virtoCommerce.helpdeskModule.openTicketsWidgetController',
+          template: 'Modules/$(Zendesk.Helpdesk)/Scripts/widgets/openTicketsWidget.tpl.html'
+      }, 'customerDetail2');
+
       //Register widgets in catalog item details
       widgetService.registerWidget({
           isVisible: function (blade) { return blade.currentEntity.id == 'Zendesk.Helpdesk'; },
--- PLATFORM/Modules/Helpdesk/Zendesk.HelpdeskModule.Web/Services/IHelpdesk.cs
+++ PLATFORM/Modules/Helpdesk/Zendesk.HelpdeskModule.Web/Services/IHelpdesk.cs
@@ -4,6 +4,7 @@ namespace Zendesk.HelpdeskModule.Web.Services
     public interface IHelpdesk
     {
         string AccessToken { get; }
+        string Subdomain { get; }
         string Code { get; }
         string Description { get; }
         string LogoUrl { get; }
