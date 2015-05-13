dd992ebc85646474aa55e86a3dafb4b53ac4ca9a
aartiomov <andrej.artiomov@gmail.com>
2015-05-13 18:37:43 +0300

    - UPD. Path update.
    Former-commit-id: 8f26299dfd3bc8b0c4fe359ceef2ea211b35805d

--- PLATFORM/Modules/Merchant/GoogleShopping.Merchant.Web/Providers/ServiceGoogleContentServiceProvider.cs
+++ PLATFORM/Modules/Merchant/GoogleShopping.Merchant.Web/Providers/ServiceGoogleContentServiceProvider.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.ShoppingContent.v2;
@@ -8,16 +11,15 @@ namespace GoogleShopping.MerchantModule.Web.Providers
     public class ServiceGoogleContentServiceProvider : IGoogleContentServiceProvider
     {
         private readonly string serviceAccountEmail = "39718569872-p1gucbblanda96o6nr9bbrjdekv8euba@developer.gserviceaccount.com";
-        const string keyPath = @"D:\Virtoway\Projects\vc-community\PLATFORM\Modules\Merchant\GoogleShopping.Merchant.Web";
-        const string keyName = "key.p12";
+        const string keyPath = @"App_Data\Modules\key.p12";
         private ShoppingContentService _contentService;
 
         public ShoppingContentService GetShoppingContentService()
         {
             if (_contentService == null)
             {
-
-                var key = string.Format(@"{0}\{1}", keyPath, keyName);
+                string appPath = System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath;
+                var key = string.Format(@"{0}{1}{2}", appPath, Path.DirectorySeparatorChar, keyPath);
 
                 var certificate = new X509Certificate2(key, "notasecret", X509KeyStorageFlags.Exportable);
 
