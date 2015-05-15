02b2b0350858730bc74cf25cad60e9de3e1bec16
Woland2k <sasha@virtoway.com>
2015-05-14 17:34:56 -0700

    added globalassemblyinfo that should be shared by core assemblies
    Former-commit-id: 796f94684645b623e68e3191bfde555ca3b2c471

--- PLATFORM/GlobalAssemblyInfo.cs
+++ PLATFORM/GlobalAssemblyInfo.cs
@@ -10,11 +10,11 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("VirtoCommerce")]
-[assembly: AssemblyProduct("Virto Commerce 1.13")]
+[assembly: AssemblyProduct("Virto Commerce 2.0")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2015")]
-[assembly: AssemblyFileVersion("1.13.982.0")]
-[assembly: AssemblyVersion("1.13.0.0")]
-[assembly: AssemblyInformationalVersion("1.13")]
+[assembly: AssemblyFileVersion("2.0.982.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
