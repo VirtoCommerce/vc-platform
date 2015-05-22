5b896ec606c38fd007e26512916361f0a366d948
Woland2k <sasha@virtoway.com>
2015-05-22 16:15:21 -0700

    fix to store url and store switching
    Former-commit-id: e9628fdfb3c6c634502c052dc642df3649a7f56a

--- PLATFORM/VirtoCommerce.Platform.Data/Infrastructure/ServiceBase.cs
+++ PLATFORM/VirtoCommerce.Platform.Data/Infrastructure/ServiceBase.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.Platform.Data.Common;
