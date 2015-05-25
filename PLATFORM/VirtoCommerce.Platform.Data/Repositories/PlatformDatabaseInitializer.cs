a14933fdfeab9fdf90ad7a34cfeb8e2210fb085f
artem-dudarev <ad@virtoway.com>
2015-05-25 11:20:29 +0200

    Fixed default API account for frontend.
    Former-commit-id: 9abb2f759010fee5b5df97a989663bbf2ab08392

--- PLATFORM/VirtoCommerce.Platform.Data/Repositories/PlatformDatabaseInitializer.cs
+++ PLATFORM/VirtoCommerce.Platform.Data/Repositories/PlatformDatabaseInitializer.cs
@@ -73,6 +73,8 @@ namespace VirtoCommerce.Platform.Data.Repositories
             });
             frontendAccount.ApiAccounts.Add(new ApiAccountEntity
             {
+                Name = "Frontend",
+                ApiAccountType = ApiAccountType.Hmac,
                 AccountId = frontendAccount.Id,
                 AppId = "27e0d789f12641049bd0e939185b4fd2",
                 SecretKey = "34f0a3c12c9dbb59b63b5fece955b7b2b9a3b20f84370cba1524dd5c53503a2e2cb733536ecf7ea1e77319a47084a3a2c9d94d36069a432ecc73b72aeba6ea78",
