320912a1c4dd6aa93feee0272fca451a666ef3aa
artem-dudarev <ad@virtoway.com>
2015-05-27 15:35:23 +0200

    Fixed compilation warnings.
    Former-commit-id: 1980e99555d19dfe3524183f4c5433db7facd59d

--- PLATFORM/Modules/PaymentGateways/Klarna.PaymentGatewaysModule.Web/Module.cs
+++ PLATFORM/Modules/PaymentGateways/Klarna.PaymentGatewaysModule.Web/Module.cs
@@ -1,52 +1,45 @@
-﻿using Klarna.PaymentGatewaysModule.Web.Controllers;
-using Klarna.PaymentGatewaysModule.Web.Managers;
-using Klarna.PaymentGatewaysModule.Web.Managers;
-using Microsoft.Practices.ServiceLocation;
-using Microsoft.Practices.Unity;
-using System;
-using VirtoCommerce.Domain.Payment.Services;
+﻿using Microsoft.Practices.Unity;
 using VirtoCommerce.Platform.Core.Modularity;
-using VirtoCommerce.Platform.Core.Settings;
 
 namespace Klarna.PaymentGatewaysModule.Web
 {
-	public class Module : IModule
-	{
-		private readonly IUnityContainer _container;
+    public class Module : IModule
+    {
+        private readonly IUnityContainer _container;
 
-		public Module(IUnityContainer container)
-		{
-			_container = container;
-		}
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
 
-		#region IModule Members
+        #region IModule Members
 
-		public void SetupDatabase(SampleDataLevel sampleDataLevel)
-		{
-		}
+        public void SetupDatabase(SampleDataLevel sampleDataLevel)
+        {
+        }
 
-		public void Initialize()
-		{
-			//var settingsManager = ServiceLocator.Current.GetInstance<ISettingsManager>();
+        public void Initialize()
+        {
+            //var settingsManager = ServiceLocator.Current.GetInstance<ISettingsManager>();
 
-			//Func<KlarnaPaymentMethod> klarnaPaymentMethodFactory = () =>
-			//{
-			//	return new KlarnaPaymentMethod()
-			//	{
-			//		Name = "Klarna",
-			//		Description = "Klarna payment integration",
-			//		LogoUrl = "https://cdn.klarna.com/1.0/shared/image/generic/logo/nb_no/basic/blue-black.png?width=200",
-			//		Settings = settingsManager.GetModuleSettings("Klarna.PaymentGateway")
-			//	};
-			//};
-			//var paymentMethodsService = _container.Resolve<IPaymentMethodsService>();
-			//paymentMethodsService.RegisterPaymentMethod(klarnaPaymentMethodFactory);
-		}
+            //Func<KlarnaPaymentMethod> klarnaPaymentMethodFactory = () =>
+            //{
+            //	return new KlarnaPaymentMethod()
+            //	{
+            //		Name = "Klarna",
+            //		Description = "Klarna payment integration",
+            //		LogoUrl = "https://cdn.klarna.com/1.0/shared/image/generic/logo/nb_no/basic/blue-black.png?width=200",
+            //		Settings = settingsManager.GetModuleSettings("Klarna.PaymentGateway")
+            //	};
+            //};
+            //var paymentMethodsService = _container.Resolve<IPaymentMethodsService>();
+            //paymentMethodsService.RegisterPaymentMethod(klarnaPaymentMethodFactory);
+        }
 
-		public void PostInitialize()
-		{
-		}
+        public void PostInitialize()
+        {
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
