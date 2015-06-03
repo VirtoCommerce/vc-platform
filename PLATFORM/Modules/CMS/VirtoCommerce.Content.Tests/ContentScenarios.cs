01898ebf423f8b74ec243324cb469a52611fe043
Woland2k <sasha@virtoway.com>
2015-06-02 17:01:37 -0700

    improved unit testing for repositories
    Former-commit-id: dac53c3f1e4a9e1244756fabbefe960811be5e78

--- PLATFORM/Modules/CMS/VirtoCommerce.Content.Tests/ContentScenarios.cs
+++ PLATFORM/Modules/CMS/VirtoCommerce.Content.Tests/ContentScenarios.cs
@@ -1,53 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VirtoCommerce.Content.Data;
+using VirtoCommerce.Content.Data.Models;
 using VirtoCommerce.Content.Data.Repositories;
+using VirtoCommerce.Content.Data.Services;
 using VirtoCommerce.Platform.Tests.Bases;
+using Xunit;
 
 namespace VirtoCommerce.Content.Tests
 {
-    public class ContentScenarios : FunctionalTestBase, IDisposable
+    public class ContentScenarios : RepositoryTestBase<DatabaseContentRepositoryImpl, SqlContentDatabaseInitializer>
     {
-        public void Can_query_content_lists()
+        [Fact]
+        public void Can_content_query_menu_lists()
         {
-            /*
-            var lists = _menuService.GetListsByStoreId(storeId);
-            Assert.True(queryTime < new TimeSpan(0, 0, 1, 2));
-             * */
+            var repository = this.GetRepository();
+            var service = new MenuServiceImpl(repository);
+            var lists = service.GetListsByStoreId("SampleStore");
+            Assert.True(lists.Any());
         }
 
-        protected IContentRepository GetContentRepository()
+        [Fact]
+        public void Can_content_add_themes()
         {
-            EnsureDatabaseInitialized(() => new DatabaseContentRepositoryImpl(), () => Database.SetInitializer(new SqlContentDatabaseInitializer()));
-            return new DatabaseContentRepositoryImpl();
-        }
+            var repository = GetRepository();
 
-        #region Implementation of IDisposable
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            try
+            var theme1 = new Theme
             {
-                // Ensure LocalDb databases are deleted after use so that LocalDb doesn't throw if
-                // the temp location in which they are stored is later cleaned.
-                using (var context = new DatabaseContentRepositoryImpl())
-                {
-                    context.Database.Delete();
-                }
-            }
-            finally
+                ThemePath = "Apple/Simple",
+                Name = "Simple",
+                Id = "Apple/Simple"
+            };
+
+            var theme2 = new Theme
             {
-                //AppDomain.CurrentDomain.SetData("DataDirectory", _previousDataDirectory);
-            }
+                ThemePath = "Apple/Timber",
+                Name = "Timber",
+                Id = "Apple/Timber"
+            };
+
+            repository.Add(theme1);
+            repository.Add(theme2);
+            repository.UnitOfWork.Commit();
         }
 
-        #endregion
+        [Fact]
+        public void Can_content_add_items()
+        {
+            var fullPath = string.Format("{0}\\Themes\\", Environment.CurrentDirectory.Replace("\\bin\\Debug", string.Empty));
+
+            var fileSystemFileRepository = new FileSystemContentRepositoryImpl(fullPath);
+            var item = fileSystemFileRepository.GetContentItem("Apple/Simple/layout/theme.liquid").Result;
+
+            var repository = GetRepository();
+
+            var file1 = new ContentItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Path = "Apple/Simple/layout/theme.liquid",
+                Name = "theme.liquid",
+                CreatedDate = DateTime.UtcNow,
+                ByteContent = item.ByteContent
+            };
+
+            repository.Add(file1);
+
+            var file2 = new ContentItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Path = "Apple/Simple/templates/404.liquid",
+                Name = "404.liquid",
+                CreatedDate = DateTime.UtcNow,
+                ByteContent = item.ByteContent
+            };
+
+            repository.Add(file2);
+
+            var file3 = new ContentItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Path = "Apple/Timber/templates/404.liquid",
+                Name = "404.liquid",
+                CreatedDate = DateTime.UtcNow,
+                ByteContent = item.ByteContent
+            };
+
+            repository.Add(file3);
+            repository.UnitOfWork.Commit();
+        }
     }
 }
