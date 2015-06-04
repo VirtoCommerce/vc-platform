5b8b8c7d11276bdceb70f19b734577e7f5893d9c
eMazeika <em@virtoway.com>
2015-06-04 23:11:11 +0300

    catalog - variation redesign as on #1885; copy-paste added
    Former-commit-id: 499b9014627babce1e16a9f5f524c36ccbf46f82

--- PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Web/Scripts/resources/listEntries.js
+++ PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Web/Scripts/resources/listEntries.js
@@ -1,14 +1,11 @@
 ﻿angular.module('virtoCommerce.catalogModule')
 .factory('virtoCommerce.catalogModule.listEntries', ['$resource', function ($resource) {
-
-	return $resource('api/catalog/listentries', {},
+    return $resource('api/catalog/listentries', {},
     {
-    	listitemssearch: { method: 'GET', isArray: false, url: 'api/catalog/listentries' },
-    	createlinks: { method: 'POST', url: 'api/catalog/listentrylinks' },
-    	deletelinks: { method: 'POST', url: 'api/catalog/listentrylinks/delete' },
-    	query: { method: 'GET', isArray: false, url: 'api/catalog/listentries' }
+        listitemssearch: { url: 'api/catalog/listentries' },
+        createlinks: { method: 'POST', url: 'api/catalog/listentrylinks' },
+        deletelinks: { method: 'POST', url: 'api/catalog/listentrylinks/delete' },
+        paste: { method: 'POST', url: 'api/catalog/listentries/paste' }
     });
-
-
 }]);
 
