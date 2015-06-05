e16eb3f4f3630aff55c65ad7416232260050cdfb
E.Tatarincev <et@virtoway.com>
2015-06-05 10:26:37 +0200

    Change virtual categories link behaviour (not finished)
    Former-commit-id: d797979265facbc749960df485c87a5a958d5b23

--- PLATFORM/Modules/Core/VirtoCommerce.Domain/Catalog/Model/CategoryLink.cs
+++ PLATFORM/Modules/Core/VirtoCommerce.Domain/Catalog/Model/CategoryLink.cs
@@ -5,6 +5,8 @@ namespace VirtoCommerce.Domain.Catalog.Model
 	public class CategoryLink : ValueObject<CategoryLink>
     {
 		public string CatalogId { get; set; }
+		public Catalog Catalog { get; set; }
         public string CategoryId { get; set; }
+		public Category Category { get; set; }
     }
 }
