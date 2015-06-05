b011fd417fcd852154ed51cb590c17590d1b08ce
aartiomov <andrej.artiomov@gmail.com>
2015-06-05 17:06:30 +0300

    - FIX. Missing file.
    Former-commit-id: 13753af29bbf86dfa48f60116247772e333c2331

---
+++ PLATFORM/Modules/Tax/AvaTax.TaxModule.Web/Services/ITaxManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AvaTax.TaxModule.Web.Services
+{
+    public interface ITaxManager
+    {
+        void RegisterTax(ITax taxModule);
+        IEnumerable<ITax> Taxes { get; }
+    }
+}
