10e63e77f04a185a9e6fc03966bd2fc46729f5f3
E.Tatarincev <et@virtoway.com>
2015-06-16 13:16:38 +0200

    Add productType to listEntry
    Former-commit-id: 3a14c8422d3b51e359847f7399e644477e1532e3

--- PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Web/Model/ListEntryProduct.cs
+++ PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Web/Model/ListEntryProduct.cs
@@ -5,6 +5,7 @@ namespace VirtoCommerce.CatalogModule.Web.Model
 	public class ListEntryProduct : ListEntryBase
 	{
 		public static string TypeName = "product";
+		public string ProductType { get; set; }
 		public ListEntryProduct(Product product)
 			: base(TypeName)
 		{
@@ -12,6 +13,7 @@ namespace VirtoCommerce.CatalogModule.Web.Model
 			ImageUrl = product.ImgSrc;
 			Code = product.Code;
 			Name = product.Name;
+			ProductType = product.ProductType;
 			if (product.Links != null)
 			{
 				Links = product.Links.Select(x => new ListEntryLink(x) ).ToArray();
