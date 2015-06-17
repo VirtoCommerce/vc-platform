3648a647c4be3b6c788f2fb1da5cb8883211c5e8
build <ci@virtocommerce.com>
2015-06-17 14:00:35 +0000

    Updated version number
    Former-commit-id: b25a9a4a2394d0046490512a09caf9a665389545

--- PLATFORM/GlobalAssemblyInfo.cs
+++ PLATFORM/GlobalAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("VirtoCommerce")]
 [assembly: AssemblyProduct("Virto Commerce 2.1")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2015")]
-[assembly: AssemblyFileVersion("2.1.99.0")]
+[assembly: AssemblyFileVersion("2.1.100.0")]
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyInformationalVersion("2.1")]
 
