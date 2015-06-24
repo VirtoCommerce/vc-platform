5169639ded1adc27df31a2f59502da0b1226c575
aartiomov <andrej.artiomov@gmail.com>
2015-06-24 15:18:09 +0300

    - UPD. Test connection widget updated.
    Former-commit-id: 4a6f2194af9ccd65ede32b35fc9e592ba0bc5a9f

--- PLATFORM/Modules/Tax/AvaTax.TaxModule.Web/Scripts/widgets/avataxWidget.js
+++ PLATFORM/Modules/Tax/AvaTax.TaxModule.Web/Scripts/widgets/avataxWidget.js
@@ -1,4 +1,20 @@
 ﻿angular.module('virtoCommerce.avataxModule')
-.controller('virtoCommerce.avataxModule.avataxWidgetController', ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
-    
+.controller('virtoCommerce.avataxModule.avataxWidgetController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.avataxModule.ping', function ($scope, bladeNavigationService, ping) {
+    $scope.widget.refresh = function () {
+        $scope.result = "transparent";
+    }
+
+    $scope.testConnection = function () {
+        $scope.result = "transparent";
+        $scope.blade.isLoading = true;
+        ping.query(function () {
+            $scope.result = "green";
+            $scope.blade.isLoading = false;
+        }, function (error) {
+            bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+            $scope.result = "red";
+        });
+    }
+
+    $scope.widget.refresh();
 }]);
