e49e2589de9849e30054ac88bc89decfa4cf966f
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-06-29 19:16:46 +0300

    notification journal start
    Former-commit-id: b481696d1c92653ee5c1d77bf3af0f44952f1b8d

---
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/newnotifications/blades/notifications-journal.js
@@ -0,0 +1,26 @@
+﻿angular.module('platformWebApp')
+.controller('platformWebApp.editTemplateController', ['$scope', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService', 'platformWebApp.newnotifications', function ($scope, bladeNavigationService, dialogService, notifications) {
+	var blade = $scope.blade;
+	blade.selectedItemId = null;
+	blade.currentEntities = [];
+
+	blade.initialize = function () {
+		notifications.getNotifications({}, function (data) {
+			blade.currentEntities = data;
+		});
+	};
+
+	blade.openNotitification = function (data) {
+		var newBlade = {
+			id: 'notificationDetails',
+			title: 'Details of notification',
+			currentEntity: data,
+			controller: 'platformWebApp.notificationDetails',
+			template: 'Scripts/app/newnotifications/blades/notification-details.tpl.html'
+		};
+
+		bladeNavigationService.showBlade(newBlade, blade);
+	}
+
+	blade.initialize();
+}]);
