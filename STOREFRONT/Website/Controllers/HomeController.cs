6f05810aea6d6ecb2de289a402c087557280642d
Woland2k <sasha@virtoway.com>
2015-07-01 22:37:53 -0700

    removed complicated getting started check in owin
    Former-commit-id: da2b8d341e1715d62678ad69a96a2f3b028f5f67

--- STOREFRONT/Website/Controllers/HomeController.cs
+++ STOREFRONT/Website/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace VirtoCommerce.Web.Controllers
     public class HomeController : StoreControllerBase
     {
         #region Public Methods and Operators
+        //[OutputCache(CacheProfile = "HomeProfile")]
         public ActionResult Index()
         {
             return this.View();
