5f0a435dda702b8439cd0c594fc321a155b83254
eMazeika <em@virtoway.com>
2015-07-02 15:54:45 +0300

    platform - general data export / import UI added (mock)
    Former-commit-id: 49def81c5f34afd8d6cf96607dea33cf055a393f

---
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/exportImport/blades/export-main.js
@@ -0,0 +1,42 @@
+﻿angular.module('platformWebApp')
+.controller('platformWebApp.exportImport.exportMainController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.catalogModule.export', 'platformWebApp.notifications', function ($scope, bladeNavigationService, exportResourse, notifications) {
+    var blade = $scope.blade;
+    blade.headIcon = 'fa-upload';
+    blade.title = 'Data export';
+
+    function initializeBlade() {
+        //exportResourse.getExporters({ id: blade.currentEntityId }, function (data) {
+
+        blade.currentEntities = [{ name: 'data 1', description: 'some data exporter description' },
+            { name: 'data 1' },
+            { name: 'data 1', description: 'some data exporter description' },
+            { name: 'data 1', description: 'some data exporter description' },
+            { name: 'data 1' },
+            { name: 'data 1', description: 'some data exporter description' }
+        ];
+        blade.isLoading = false;
+        //},
+        //   function (error) { bladeNavigationService.setError('Error ' + error.status, $scope.blade); });
+    };
+
+    //$scope.setForm = function (form) {
+    //    $scope.formScope = form;
+    //}
+
+    $scope.$on("new-notification-event", function (event, notification) {
+        if (blade.notification && notification.id == blade.notification.id) {
+            angular.copy(notification, blade.notification);
+        }
+    });
+
+    $scope.startExport = function () {
+        exportResourse.run({
+            categoryIds: _.map(blade.selectedCategories, function (x) { return x.id })
+        },
+        function (data) { blade.notification = data; },
+        function (error) { bladeNavigationService.setError('Error ' + error.status, $scope.blade); });
+    }
+
+
+    initializeBlade();
+}]);
