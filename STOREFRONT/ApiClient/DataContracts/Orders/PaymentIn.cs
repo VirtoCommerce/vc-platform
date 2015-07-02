c83b6a67a7719224a79259bee0f500ab9bc8fff1
Andrew-Orlov <andrew.orlov@outlook.com>
2015-07-02 17:26:13 +0200

    Actualized shipment and payment data contracts with tax type
    Former-commit-id: 2775fd9552f85571fdc4431ffc971a0b166e4616

--- STOREFRONT/ApiClient/DataContracts/Orders/PaymentIn.cs
+++ STOREFRONT/ApiClient/DataContracts/Orders/PaymentIn.cs
@@ -5,23 +5,22 @@ namespace VirtoCommerce.ApiClient.DataContracts.Orders
 {
     public class PaymentIn : Operation
     {
-        #region Public Properties
-
-        public List<Address> Addresses { get; set; }
-        public string CustomerId { get; set; }
-        public string CustomerOrderId { get; set; }
-        public string GatewayCode { get; set; }
         public string Id { get; set; }
 
-        public DateTime? IncomingDate { get; set; }
+        public string Organization { get; set; }
+
         public string OrganizationId { get; set; }
 
-        public string OuterId { get; set; }
+        public string Customer { get; set; }
+
+        public string CustomerId { get; set; }
 
         public string Purpose { get; set; }
 
-        public string ShipmentId { get; set; }
+        public string GatewayCode { get; set; }
 
-        #endregion
+        public DateTime? IncomingDate { get; set; }
+
+        public string OuterId { get; set; }
     }
 }
