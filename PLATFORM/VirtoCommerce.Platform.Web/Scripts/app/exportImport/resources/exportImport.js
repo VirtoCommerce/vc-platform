c047c5b7dfe6178f784fbaa7d8dfd716eccddb1a
E.Tatarincev <et@virtoway.com>
2015-07-08 10:47:18 +0200

    Fix platform export resources url
    Former-commit-id: 4a76cb5d0dce55bc6b7f5f5628cb9f1d81af1efb

--- PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/exportImport/resources/exportImport.js
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/exportImport/resources/exportImport.js
@@ -2,7 +2,7 @@
 .factory('platformWebApp.exportImport.resource', ['$resource', function ($resource) {
 
     return $resource(null, null, {
-        getExportersList: { url: 'api/export/modules', isArray: true },
-        runExport: { method: 'POST', url: 'api/export' }
+        getExportersList: { url: 'api/platform/export/modules', isArray: true },
+        runExport: { method: 'POST', url: 'api/platform/export' }
     });
 }]);
