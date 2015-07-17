d6736cc2e8d1a054fb47c4509fba3fa9dd5a3fe0
artem-dudarev <ad@virtoway.com>
2015-07-17 15:36:47 +0200

    Fixed compilation warnings.
    Former-commit-id: 295a19db916f0df113931797ef66fea640e5c0d8

--- PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Data/Observers/CancelPaymentObserver.cs
+++ PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Data/Observers/CancelPaymentObserver.cs
@@ -10,59 +10,58 @@ using VirtoCommerce.Domain.Store.Services;
 
 namespace VirtoCommerce.OrderModule.Data.Observers
 {
-	public class CancelPaymentObserver : IObserver<OrderChangeEvent>
-	{
-		private readonly IPaymentMethodsService _paymentMethodService;
-		private readonly IStoreService _storeService;
+    public class CancelPaymentObserver : IObserver<OrderChangeEvent>
+    {
+        private readonly IStoreService _storeService;
 
-		public CancelPaymentObserver(IStoreService storeService)
-		{
-			_storeService = storeService;
-		}
+        public CancelPaymentObserver(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
 
-		public void OnCompleted()
-		{
-			
-		}
+        public void OnCompleted()
+        {
 
-		public void OnError(Exception error)
-		{
-			
-		}
+        }
 
-		public void OnNext(OrderChangeEvent value)
-		{
-			CancelationPayment(value);
-		}
+        public void OnError(Exception error)
+        {
 
-		private void CancelationPayment(OrderChangeEvent value)
-		{
-			if (value.OrigOrder != null && value.OrigOrder.InPayments != null && value.OrigOrder.InPayments.Count > 0)
-			{
-				if (value.ModifiedOrder.InPayments != null && value.ModifiedOrder.InPayments.Count(p => p.PaymentStatus == PaymentStatus.Cancelled) > 0)
-				{
-					foreach (var payment in value.ModifiedOrder.InPayments.Where(p => p.PaymentStatus == PaymentStatus.Cancelled))
-					{
-						var op = value.OrigOrder.InPayments.FirstOrDefault(p => p.Id == payment.Id);
-						if (op != null && op.PaymentStatus != PaymentStatus.Cancelled && op.PaymentStatus != PaymentStatus.Voided && op.PaymentStatus != PaymentStatus.Refunded)
-						{
-							var store = _storeService.GetById(value.ModifiedOrder.StoreId);
-							var method = store.PaymentMethods.FirstOrDefault(p => p.Code == op.GatewayCode);
-							if(method != null)
-							{
-								if(op.PaymentStatus == PaymentStatus.Authorized)
-								{
-									method.VoidProcessPayment(new VoidProcessPaymentEvaluationContext { Payment = payment });
-								}
-								else if(op.PaymentStatus == PaymentStatus.Paid)
-								{
-									method.RefundProcessPayment(new RefundProcessPaymentEvaluationContext { Payment = payment });
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+        }
+
+        public void OnNext(OrderChangeEvent value)
+        {
+            CancelationPayment(value);
+        }
+
+        private void CancelationPayment(OrderChangeEvent value)
+        {
+            if (value.OrigOrder != null && value.OrigOrder.InPayments != null && value.OrigOrder.InPayments.Count > 0)
+            {
+                if (value.ModifiedOrder.InPayments != null && value.ModifiedOrder.InPayments.Count(p => p.PaymentStatus == PaymentStatus.Cancelled) > 0)
+                {
+                    foreach (var payment in value.ModifiedOrder.InPayments.Where(p => p.PaymentStatus == PaymentStatus.Cancelled))
+                    {
+                        var op = value.OrigOrder.InPayments.FirstOrDefault(p => p.Id == payment.Id);
+                        if (op != null && op.PaymentStatus != PaymentStatus.Cancelled && op.PaymentStatus != PaymentStatus.Voided && op.PaymentStatus != PaymentStatus.Refunded)
+                        {
+                            var store = _storeService.GetById(value.ModifiedOrder.StoreId);
+                            var method = store.PaymentMethods.FirstOrDefault(p => p.Code == op.GatewayCode);
+                            if (method != null)
+                            {
+                                if (op.PaymentStatus == PaymentStatus.Authorized)
+                                {
+                                    method.VoidProcessPayment(new VoidProcessPaymentEvaluationContext { Payment = payment });
+                                }
+                                else if (op.PaymentStatus == PaymentStatus.Paid)
+                                {
+                                    method.RefundProcessPayment(new RefundProcessPaymentEvaluationContext { Payment = payment });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
