070a779f03f3d0d0ead23e97813219df2ceee27e
dmitry-virtoway <db@virtoway.com>
2015-07-28 11:10:38 +0200

    Update orders export
    Former-commit-id: 6a29b68bd81d8ac0613b6ca2535945bc4540596c

--- PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Web/ExportImport/OrderExportImport.cs
+++ PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Web/ExportImport/OrderExportImport.cs
@@ -14,7 +14,69 @@ namespace VirtoCommerce.OrderModule.Web.ExportImport
         public ICollection<CustomerOrder> CustomerOrders { get; set; }
     }
 
-    public sealed class OrderExportImport
+    public abstract class ExportImportBase
+    {
+        private string _notifyPattern;
+        private int _counter;
+        private int _notifyMinSize = 1;
+        protected Action<ExportImportProgressInfo> ProgressCallback;
+        private ExportImportProgressInfo _progressInfo = new ExportImportProgressInfo();
+
+        protected void InitProgressNotifier(string notifyPattern, int totalCount, int notifyMinSize)
+        {
+            _counter = 0;
+            _notifyMinSize = notifyMinSize;
+            _notifyPattern = notifyPattern;
+            _progressInfo = new ExportImportProgressInfo() { TotalCount = totalCount };
+        }
+
+        protected void Notify(int count = 1)
+        {
+            _counter += count;
+            if (ProgressCallback != null && _progressInfo != null)
+            {
+                _progressInfo.ProcessedCount = _counter;
+                _progressInfo.Description = string.Format(_notifyPattern, _progressInfo.ProcessedCount, _progressInfo.TotalCount);
+                ProgressCallback(_progressInfo);
+            }
+        }
+
+        protected void NotifyText(string description)
+        {
+            if (ProgressCallback != null)
+            {
+                ProgressCallback(new ExportImportProgressInfo { Description = "loading data..." });
+            }
+        }
+
+        protected void SetTotalCount(int totalCount)
+        {
+            if (_progressInfo != null)
+            {
+                _progressInfo.TotalCount = totalCount;
+            }
+        }
+
+        protected IEnumerable<Q> Converter<T, Q>(IEnumerable<T> source, Func<T, Q> converterFunc)
+        {
+            var result = new List<Q>();
+            if (source != null)
+            {
+                var sourceArray = source.ToArray();
+                SetTotalCount(sourceArray.Count());
+                for (var i = 0; i < sourceArray.Count(); i += _notifyMinSize)
+                {
+                    var part = sourceArray.Skip(i).Take(_notifyMinSize).Select(converterFunc).ToArray();
+                    result.AddRange(part);
+                    Notify(part.Count());
+                }
+            }
+            return result;
+        }
+
+    }
+
+    public sealed class OrderExportImport : ExportImportBase
     {
         private readonly ICustomerOrderSearchService _customerOrderSearchService;
         private readonly ICustomerOrderService _customerOrderService;
@@ -27,20 +89,16 @@ namespace VirtoCommerce.OrderModule.Web.ExportImport
 
         public void DoExport(Stream backupStream, Action<ExportImportProgressInfo> progressCallback)
         {
-            var prodgressInfo = new ExportImportProgressInfo { Description = "loading data..." };
-            progressCallback(prodgressInfo);
-
+            ProgressCallback = progressCallback;
             var backupObject = GetBackupObject(); 
             backupObject.SerializeJson(backupStream);
         }
 
         public void DoImport(Stream backupStream, Action<ExportImportProgressInfo> progressCallback)
         {
-            var prodgressInfo = new ExportImportProgressInfo { Description = "loading data..." };
-            progressCallback(prodgressInfo);
-
-            var backupObject = backupStream.DeserializeJson<BackupObject>();
+            ProgressCallback = progressCallback;
             var originalObject = GetBackupObject();
+            var backupObject = backupStream.DeserializeJson<BackupObject>();
 
             UpdateOrders(originalObject.CustomerOrders, backupObject.CustomerOrders);
         }
@@ -66,15 +124,17 @@ namespace VirtoCommerce.OrderModule.Web.ExportImport
 
         private BackupObject GetBackupObject()
         {
-            var responce = _customerOrderSearchService.Search(new SearchCriteria { Count = int.MaxValue });
-            var orderIds = responce.CustomerOrders.Select(x => x.Id);
-            const CustomerOrderResponseGroup filter = CustomerOrderResponseGroup.WithAddresses | CustomerOrderResponseGroup.WithItems
+            NotifyText("loading data...");
+
+            const CustomerOrderResponseGroup responseGroup = CustomerOrderResponseGroup.WithAddresses | CustomerOrderResponseGroup.WithItems
                 | CustomerOrderResponseGroup.WithShipments | CustomerOrderResponseGroup.WithInPayments;
 
-            return new BackupObject
-            {
-                CustomerOrders = orderIds.Select(id => _customerOrderService.GetById(id, filter)).ToArray(),
-            };
+            var searchResponse = _customerOrderSearchService.Search(new SearchCriteria { Count = int.MaxValue });
+            
+            InitProgressNotifier("{0} of {1} orders loaded", 0, 20);
+            var customerOrders = Converter(searchResponse.CustomerOrders, (x) => _customerOrderService.GetById(x.Id, responseGroup));
+            
+            return new BackupObject { CustomerOrders = customerOrders.ToArray()};
         }
 
     }
