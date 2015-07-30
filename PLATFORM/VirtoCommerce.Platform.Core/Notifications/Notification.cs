ef3f819d12a1013b6562b500e3559933d011bcec
artem-dudarev <ad@virtoway.com>
2015-07-30 15:52:14 +0200

    Abstract classes should not have public constructors.
    Former-commit-id: 600eef2b51e4f1d1070a432735663e5ae9c5f6a9

--- PLATFORM/VirtoCommerce.Platform.Core/Notifications/Notification.cs
+++ PLATFORM/VirtoCommerce.Platform.Core/Notifications/Notification.cs
@@ -7,99 +7,99 @@ using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.Platform.Core.Notifications
 {
-	public abstract class Notification : AuditableEntity
-	{
-		private readonly INotificationSendingGateway _notificationSendingGateway;
+    public abstract class Notification : AuditableEntity
+    {
+        private readonly INotificationSendingGateway _notificationSendingGateway;
 
-		public Notification(INotificationSendingGateway notificationSendingGateway)
-		{
-			_notificationSendingGateway = notificationSendingGateway;
-			MaxAttemptCount = 10;
-			Type = GetType().Name;
-		}
+        protected Notification(INotificationSendingGateway notificationSendingGateway)
+        {
+            _notificationSendingGateway = notificationSendingGateway;
+            MaxAttemptCount = 10;
+            Type = GetType().Name;
+        }
 
-		public string DisplayName { get; set; }
-		public string Description { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
 
-		/// <summary>
-		/// Must be made sending
-		/// </summary>
-		public bool IsActive { get; set; }
+        /// <summary>
+        /// Must be made sending
+        /// </summary>
+        public bool IsActive { get; set; }
 
-		/// <summary>
-		/// Notification was successfully sent
-		/// </summary>
-		public bool IsSuccessSend { get; set; }
+        /// <summary>
+        /// Notification was successfully sent
+        /// </summary>
+        public bool IsSuccessSend { get; set; }
 
-		public string Type { get; set; }
+        public string Type { get; set; }
 
-		public string ObjectId { get; set; }
+        public string ObjectId { get; set; }
 
-		/// <summary>
-		/// Subject of notification
-		/// </summary>
-		public string Subject { get; set; }
+        /// <summary>
+        /// Subject of notification
+        /// </summary>
+        public string Subject { get; set; }
 
-		/// <summary>
-		/// Body of notification
-		/// </summary>
-		public string Body { get; set; }
+        /// <summary>
+        /// Body of notification
+        /// </summary>
+        public string Body { get; set; }
 
-		/// <summary>
-		/// Sender info (e-mail, phone number and etc.) of notification
-		/// </summary>
-		public string Sender { get; set; }
+        /// <summary>
+        /// Sender info (e-mail, phone number and etc.) of notification
+        /// </summary>
+        public string Sender { get; set; }
 
-		/// <summary>
-		/// Recipient info (e-mail, phone number and etc.) of notification
-		/// </summary>
-		public string Recipient { get; set; }
+        /// <summary>
+        /// Recipient info (e-mail, phone number and etc.) of notification
+        /// </summary>
+        public string Recipient { get; set; }
 
-		/// <summary>
-		/// Number of current attemp
-		/// </summary>
-		public int AttemptCount { get; set; }
+        /// <summary>
+        /// Number of current attemp
+        /// </summary>
+        public int AttemptCount { get; set; }
 
-		/// <summary>
-		/// Maximum number of attempts to send a message
-		/// </summary>
-		public int MaxAttemptCount { get; set; }
+        /// <summary>
+        /// Maximum number of attempts to send a message
+        /// </summary>
+        public int MaxAttemptCount { get; set; }
 
-		/// <summary>
-		/// Last fail attempt error message
-		/// </summary>
-		public string LastFailAttemptMessage { get; set; }
+        /// <summary>
+        /// Last fail attempt error message
+        /// </summary>
+        public string LastFailAttemptMessage { get; set; }
 
-		public string SendingGateway { get; set; }
+        public string SendingGateway { get; set; }
 
-		/// <summary>
-		/// Date of last fail attempt
-		/// </summary>
-		public DateTime? LastFailAttemptDate { get; set; }
+        /// <summary>
+        /// Date of last fail attempt
+        /// </summary>
+        public DateTime? LastFailAttemptDate { get; set; }
 
-		/// <summary>
-		/// Date of start sending notification
-		/// </summary>
-		public DateTime? StartSendingDate { get; set; }
+        /// <summary>
+        /// Date of start sending notification
+        /// </summary>
+        public DateTime? StartSendingDate { get; set; }
 
-		/// <summary>
-		/// Date of success sent result
-		/// </summary>
-		public DateTime? SentDate { get; set; }
+        /// <summary>
+        /// Date of success sent result
+        /// </summary>
+        public DateTime? SentDate { get; set; }
 
-		public string ObjectTypeId { get; set; }
+        public string ObjectTypeId { get; set; }
 
-		public string Language { get; set; }
+        public string Language { get; set; }
 
-		public INotificationSendingGateway NotificationSendingGateway { get { return _notificationSendingGateway; } }
+        public INotificationSendingGateway NotificationSendingGateway { get { return _notificationSendingGateway; } }
 
-		public NotificationTemplate NotificationTemplate { get; set; }
+        public NotificationTemplate NotificationTemplate { get; set; }
 
-		public virtual SendNotificationResult SendNotification()
-		{
-			var result = NotificationSendingGateway.SendNotification(this);
+        public virtual SendNotificationResult SendNotification()
+        {
+            var result = NotificationSendingGateway.SendNotification(this);
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
