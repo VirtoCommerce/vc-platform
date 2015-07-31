769f4f97dcacb06aa3054fb580bd6fe57ba892d0
artem-dudarev <ad@virtoway.com>
2015-07-31 09:28:27 +0200

    Fixed compilation warning.
    Former-commit-id: b687cde3b28cbe5b3599e3fdf049000a318192a7

--- PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Web/Converters/InPaymentConverter.cs
+++ PLATFORM/Modules/Order/VirtoCommerce.OrderModule.Web/Converters/InPaymentConverter.cs
@@ -1,51 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Omu.ValueInjecter;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Omu.ValueInjecter;
-using coreModel = VirtoCommerce.Domain.Order.Model;
-using webModel = VirtoCommerce.OrderModule.Web.Model;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Domain.Payment.Model;
 using VirtoCommerce.Platform.Core.Common;
+using coreModel = VirtoCommerce.Domain.Order.Model;
+using webModel = VirtoCommerce.OrderModule.Web.Model;
 
 namespace VirtoCommerce.OrderModule.Web.Converters
 {
-	public static class InPaymentConverter
-	{
-		public static webModel.PaymentIn ToWebModel(this coreModel.PaymentIn payment)
-		{
-			var retVal = new webModel.PaymentIn();
-			retVal.InjectFrom(payment);
-			retVal.Currency = payment.Currency;
+    public static class InPaymentConverter
+    {
+        public static webModel.PaymentIn ToWebModel(this coreModel.PaymentIn payment)
+        {
+            var retVal = new webModel.PaymentIn();
+            retVal.InjectFrom(payment);
+            retVal.Currency = payment.Currency;
 
 
-			retVal.ChildrenOperations = payment.GetFlatObjectsListWithInterface<coreModel.IOperation>().Select(x => x.ToWebModel()).ToList();
+            retVal.ChildrenOperations = payment.GetFlatObjectsListWithInterface<coreModel.IOperation>().Select(x => x.ToWebModel()).ToList();
 
-			if (payment.DynamicProperties != null)
-				retVal.DynamicProperties = payment.DynamicProperties;
+            if (payment.DynamicProperties != null)
+                retVal.DynamicProperties = payment.DynamicProperties;
 
-			return retVal;
-		}
+            return retVal;
+        }
 
-		public static coreModel.PaymentIn ToCoreModel(this webModel.PaymentIn payment)
-		{
-			var retVal = new coreModel.PaymentIn();
-			retVal.InjectFrom(payment);
-			retVal.PaymentStatus = EnumUtility.SafeParse<PaymentStatus>(payment.Status, PaymentStatus.Custom);
-
-		
-			retVal.Currency = payment.Currency;
+        public static coreModel.PaymentIn ToCoreModel(this webModel.PaymentIn payment)
+        {
+            var retVal = new coreModel.PaymentIn();
+            retVal.InjectFrom(payment);
+            retVal.PaymentStatus = EnumUtility.SafeParse<PaymentStatus>(payment.Status, PaymentStatus.Custom);
 
 
-			if (payment.DynamicProperties != null)
-				retVal.DynamicProperties = payment.DynamicProperties;
+            retVal.Currency = payment.Currency;
 
 
-			return retVal;
-		}
+            if (payment.DynamicProperties != null)
+                retVal.DynamicProperties = payment.DynamicProperties;
 
 
-	}
+            return retVal;
+        }
+
+
+    }
 }
