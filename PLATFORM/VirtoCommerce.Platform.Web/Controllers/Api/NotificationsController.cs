aab8584d62d2c33a70099a93a1949162fc281973
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-08-12 15:42:04 +0300

    add export models menu link lists content module, add notification test model
    Former-commit-id: 6987fec185c451096a1f536067af17ebf2b8a405

--- PLATFORM/VirtoCommerce.Platform.Web/Controllers/Api/NotificationsController.cs
+++ PLATFORM/VirtoCommerce.Platform.Web/Controllers/Api/NotificationsController.cs
@@ -140,10 +140,10 @@ namespace VirtoCommerce.Platform.Web.Controllers.Api
 		}
 
 		/// <summary>
-		/// Get rendered notification html
+		/// Get rendered notification content
 		/// </summary>
 		/// <remarks>
-		/// Method returns rendered html, that based on notification template. Template for rendering choosen by type, objectId, objectTypeId, language.
+        /// Method returns rendered content, that based on notification template. Template for rendering choosen by type, objectId, objectTypeId, language.
 		/// Parameters for template may be prepared by the method of getTestingParameters.
 		/// </remarks>
 		/// <param name="parameters">Notification special parameters</param>
@@ -152,10 +152,11 @@ namespace VirtoCommerce.Platform.Web.Controllers.Api
 		/// <param name="objectTypeId">Object type id</param>
 		/// <param name="language">Locale</param>
 		[HttpPost]
-		[ResponseType(typeof(webModels.Notification))]
-		[Route("template/{type}/{objectId}/{objectTypeId}/{language}/rendernotificationhtml")]
-		public IHttpActionResult ResolveNotification([FromBody]List<KeyValuePair<string, string>> parameters, string type, string objectId, string objectTypeId, string language)
+		[ResponseType(typeof(webModels.RenderNotificationContentResult))]
+		[Route("template/{type}/{objectId}/{objectTypeId}/{language}/rendernotificationcontent")]
+		public IHttpActionResult RenderNotificationContent([FromBody]List<KeyValuePair<string, string>> parameters, string type, string objectId, string objectTypeId, string language)
 		{
+            var retVal = new webModels.RenderNotificationContentResult();
 			var notification = _notificationManager.GetNewNotification(type, objectId, objectTypeId, language);
 			foreach (var param in parameters)
 			{
@@ -164,7 +165,10 @@ namespace VirtoCommerce.Platform.Web.Controllers.Api
 			}
 			_eventTemplateResolver.ResolveTemplate(notification);
 
-			return Ok(notification.ToWebModel());
+            retVal.Subject = notification.Subject;
+            retVal.Body = notification.Body;
+
+            return Ok(retVal);
 		}
 
 		/// <summary>
