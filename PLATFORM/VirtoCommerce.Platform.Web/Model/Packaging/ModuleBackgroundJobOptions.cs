44ee4bf6af7c664dc6e13c8cacd554d07db94939
artem-dudarev <ad@virtoway.com>
2015-08-17 12:01:14 +0200

    Serialize ModuleAction as string.
    Former-commit-id: a68479bb60ec4fc6e1384a44c3925d48960dbfe8

--- PLATFORM/VirtoCommerce.Platform.Web/Model/Packaging/ModuleBackgroundJobOptions.cs
+++ PLATFORM/VirtoCommerce.Platform.Web/Model/Packaging/ModuleBackgroundJobOptions.cs
@@ -1,7 +1,11 @@
-﻿namespace VirtoCommerce.Platform.Web.Model.Packaging
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace VirtoCommerce.Platform.Web.Model.Packaging
 {
     public class ModuleBackgroundJobOptions
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public ModuleAction Action { get; set; }
         public string PackageId { get; set; }
         public string PackageFilePath { get; set; }
