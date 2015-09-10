1e735d941043e66425f82620cc4d5026b0cacf4e
E.Tatarincev <et@virtoway.com>
2015-09-10 14:39:23 +0200

    Revert AuditableEntity changes (is led to EF migration desync)

--- PLATFORM/VirtoCommerce.Platform.Core/Common/AuditableEntity.cs
+++ PLATFORM/VirtoCommerce.Platform.Core/Common/AuditableEntity.cs
@@ -6,17 +6,11 @@ namespace VirtoCommerce.Platform.Core.Common
 {
     public abstract class AuditableEntity : Entity, IAuditable
     {
-		public AuditableEntity()
-		{
-			ChangeLogs = new List<OperationLog>();
-		}
-        #region IAuditable Members
-
+	    #region IAuditable Members
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public string CreatedBy { get; set; }
         public string ModifiedBy { get; set; }
-		public ICollection<OperationLog> ChangeLogs { get; set; }
         #endregion
     }
 }
