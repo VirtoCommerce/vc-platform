428304965dcf3db1c55f3df3dfa94be790b94d30
Andrew Orlov <ao@virtoway.com>
2015-09-14 17:20:14 +0200

    Add B2B quote client

---
+++ STOREFRONT/ApiClient/DataContracts/Quotes/QuoteRequestSearchCriteria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VirtoCommerce.ApiClient.DataContracts.Quotes
+{
+    public class QuoteRequestSearchCriteria
+    {
+        public string Keyword { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public string StoreId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int Start { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ STOREFRONT/ApiClient/DataContracts/Quotes/QuoteRequestSearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VirtoCommerce.ApiClient.DataContracts.Quotes
+{
+    public class QuoteRequestSearchResult
+    {
+        public ICollection<QuoteRequest> QuoteRequests { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
