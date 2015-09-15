51ae000dce9a81565c44c744f1a528878f0f9861
Andrew Orlov <ao@virtoway.com>
2015-09-15 10:40:05 +0200

    Actualization of B2B quotes data contracts

--- STOREFRONT/ApiClient/DataContracts/Quotes/QuoteRequestTotals.cs
+++ STOREFRONT/ApiClient/DataContracts/Quotes/QuoteRequestTotals.cs
@@ -2,14 +2,20 @@
 {
     public class QuoteRequestTotals
     {
-        public decimal Total { get; set; }
+        public decimal OriginalSubTotalExlTax { get; set; }
 
-        public decimal SubTotal { get; set; }
+        public decimal SubTotalExlTax { get; set; }
 
         public decimal ShippingTotal { get; set; }
 
         public decimal DiscountTotal { get; set; }
 
         public decimal TaxTotal { get; set; }
+
+        public decimal AdjustmentQuoteExlTax { get; set; }
+
+        public decimal GrandTotalExlTax { get; set; }
+
+        public decimal GrandTotalInclTax { get; set; }
     }
 }
