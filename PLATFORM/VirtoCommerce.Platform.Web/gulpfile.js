285211226f6efcb7448778a34dcf4e8f29969289
eMazeika <em@virtoway.com>
2015-09-16 09:46:36 +0300

    platform - gulp build tasks updated

--- PLATFORM/VirtoCommerce.Platform.Web/gulpfile.js
+++ PLATFORM/VirtoCommerce.Platform.Web/gulpfile.js
@@ -7,52 +7,12 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require("gulp"),
     mainBowerFiles = require('main-bower-files'),
-    // rimraf = require("rimraf"),
     concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
+    //cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify");
-
-//var paths = {
-//    webroot: "./" + project.webroot + "/"
-//};
-
-//paths.js = paths.webroot + "js/**/*.js";
-//paths.minJs = paths.webroot + "js/**/*.min.js";
-//paths.css = paths.webroot + "css/**/*.css";
-//paths.minCss = paths.webroot + "css/**/*.min.css";
-//paths.concatJsDest = paths.webroot + "js/site.min.js";
-//paths.concatCssDest = paths.webroot + "css/site.min.css";
-
-//gulp.task("clean:js", function (cb) {
-//    rimraf(paths.concatJsDest, cb);
-//});
-
-//gulp.task("clean:css", function (cb) {
-//    rimraf(paths.concatCssDest, cb);
-//});
-
-//gulp.task("clean", ["clean:js", "clean:css"]);
-
-//gulp.task("min:js", function () {
-//    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-//        .pipe(concat(paths.concatJsDest))
-//        .pipe(uglify())
-//        .pipe(gulp.dest("."));
-//});
-
-//gulp.task("min:css", function () {
-//    gulp.src([paths.css, "!" + paths.minCss])
-//        .pipe(concat(paths.concatCssDest))
-//        .pipe(cssmin())
-//        .pipe(gulp.dest("."));
-//});
-
-//gulp.task("min", ["min:js", "min:css"]);
-
 
 // Concatenate JS Files
 gulp.task('packScripts', function () {
-    // Only return the JavaScript files
     return gulp.src(mainBowerFiles({
         // Only return the JavaScript files
         filter: /.*\.js$/i
@@ -63,20 +23,21 @@ gulp.task('packScripts', function () {
 });
 
 gulp.task('packCss', function () {
-    // Only return the JavaScript files
     return gulp.src(mainBowerFiles({
         // Only return the CSS files
         filter: /.*\.css$/i
     }))
       .pipe(concat('allStyles.css'))
       // .pipe(cssmin())
-      .pipe(gulp.dest('Scripts'));
+      .pipe(gulp.dest('Content'));
 });
 
-gulp.task('packAll', ['packScripts', 'packCss']);
+gulp.task('packFonts', function () {
+    return gulp.src(mainBowerFiles({
+        // Only return the font files
+        filter: /.*\.(eot|svg|ttf|woff)$/i
+    }))
+      .pipe(gulp.dest('Content'));
+});
 
-//gulp.task('watch', function () {
-//    // All files in client_packages
-//    gulp.watch('client_packages/**/*.js', ['packScripts']);
-//    gulp.watch('client_packages/**/*.css', ['packCss']);
-//});
+gulp.task('packAll', ['packScripts', 'packCss', 'packFonts']);
