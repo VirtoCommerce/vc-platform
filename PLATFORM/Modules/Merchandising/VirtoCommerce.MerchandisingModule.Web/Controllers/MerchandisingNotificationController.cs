a98a849ff20028564b1ee397c84dc7f872d0a872
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-10-01 18:48:05 +0200

    notification merch changes

--- PLATFORM/Modules/Merchandising/VirtoCommerce.MerchandisingModule.Web/Controllers/MerchandisingNotificationController.cs
+++ PLATFORM/Modules/Merchandising/VirtoCommerce.MerchandisingModule.Web/Controllers/MerchandisingNotificationController.cs
@@ -4,31 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
+using VirtoCommerce.Domain.Store.Services;
 using VirtoCommerce.MerchandisingModule.Web.Model.Notificaitons;
 using VirtoCommerce.Platform.Core.Notifications;
 
 namespace VirtoCommerce.MerchandisingModule.Web.Controllers
 {
-    [RoutePrefix("api/mp/notifications")]
+    [RoutePrefix("api/mp/{storeId}/{language}")]
     public class MerchandisingNotificationController : ApiController
     {
         private readonly INotificationManager _notificationManager;
+        private readonly IStoreService _storeService;
 
-        public MerchandisingNotificationController(INotificationManager notificationManager)
+        public MerchandisingNotificationController(INotificationManager notificationManager, IStoreService storeService)
         {
+            _storeService = storeService;
             _notificationManager = notificationManager;
         }
 
         [HttpPost]
-        [Route("send/{type}/{objectId}/{objectTypeId}/{language}/{recipient}/{sender}")]
+        [Route("notification")]
         [ResponseType(typeof(void))]
-        public IHttpActionResult SendNotification(string type, string objectId, string objectTypeId, string language, string recipient, string sender)
+        public IHttpActionResult SendNotification(string storeId, string language)
         {
-            var notification = _notificationManager.GetNewNotification<DynamicMerchandisingNotification>(objectId, objectTypeId, language);
+            var store = _storeService.GetById(storeId);
 
-            notification.FormType = type;
-            notification.Recipient = recipient;
-            notification.Sender = sender;
+            if (store == null)
+                throw new NullReferenceException(string.Format("no store with this id = {0}", storeId));
+
+            if (string.IsNullOrEmpty(store.Email) && string.IsNullOrEmpty(store.AdminEmail))
+                throw new NullReferenceException(string.Format("set email or admin email for store with id = {0}", storeId));
+
+            var notification = _notificationManager.GetNewNotification<DynamicMerchandisingNotification>(storeId, "Store", language);
+
+            notification.FormType = HttpContext.Current.Request.Form["formtype"];
+            notification.Recipient = !string.IsNullOrEmpty(store.Email) ? store.Email : store.AdminEmail;
+            notification.IsActive = true;
 
             var form = HttpContext.Current.Request.Form;
             var dict = new Dictionary<string, string>();
