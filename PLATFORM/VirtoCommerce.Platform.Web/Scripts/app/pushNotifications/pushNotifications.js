4e872c0a476fecae7f05728fb596c81e4de5928b
eMazeika <em@virtoway.com>
2015-10-12 11:19:44 +0300

    push notifications - fixed and bell animation added

--- PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/pushNotifications/pushNotifications.js
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/pushNotifications/pushNotifications.js
@@ -73,77 +73,95 @@
 
     return retVal;
 }])
-.factory('platformWebApp.pushNotificationService', ['$rootScope', 'platformWebApp.signalRHubProxy', '$interval', '$state', 'platformWebApp.mainMenuService', 'platformWebApp.pushNotificationTemplateResolver', 'platformWebApp.pushNotifications', 'platformWebApp.signalRServerName', function ($rootScope, signalRHubProxy, $interval, $state, mainMenuService, eventTemplateResolver, notifications, signalRServerName) {
+.factory('platformWebApp.pushNotificationService', ['$rootScope', 'platformWebApp.signalRHubProxy', '$timeout', '$interval', '$state', 'platformWebApp.mainMenuService', 'platformWebApp.pushNotificationTemplateResolver', 'platformWebApp.pushNotifications', 'platformWebApp.signalRServerName',
+    function ($rootScope, signalRHubProxy, $timeout, $interval, $state, mainMenuService, eventTemplateResolver, notifications, signalRServerName) {
 
-	var clientPushHubProxy = signalRHubProxy(signalRServerName, 'clientPushHub', { logging: true });
-    clientPushHubProxy.on('notification', function (data) {
-    	var notifyMenu = mainMenuService.findByPath('pushNotifications');
-        var notificationTemplate = eventTemplateResolver.resolve(data, 'menu');
-		//broadcast event
-        $rootScope.$broadcast("new-notification-event", data);
+        var clientPushHubProxy = signalRHubProxy(signalRServerName, 'clientPushHub', { logging: true });
+        clientPushHubProxy.on('notification', function (data) {
+            var notifyMenu = mainMenuService.findByPath('pushNotifications');
+            var notificationTemplate = eventTemplateResolver.resolve(data, 'menu');
+            //broadcast event
+            $rootScope.$broadcast("new-notification-event", data);
 
-        var menuItem = {
-        	parent: notifyMenu,
-        	path: 'pushNotifications/notifications',
-        	icon: 'fa fa-bell-o',
-        	title: data.title,
-        	priority: 2,
-        	permission: '',
-        	children: [],
-        	action: notificationTemplate.action,
-        	template: notificationTemplate.template,
-        	notify: data
-        };
+            var menuItem = {
+                parent: notifyMenu,
+                path: 'pushNotifications/notifications',
+                icon: 'fa fa-bell-o',
+                title: data.title,
+                priority: 2,
+                permission: '',
+                children: [],
+                action: notificationTemplate.action,
+                template: notificationTemplate.template,
+                notify: data
+            };
 
-        var alreadyExitstItem = _.find(notifyMenu.children, function (x) { return x.id == menuItem.id; });
-        if (alreadyExitstItem) {
-        	angular.copy(menuItem, alreadyExitstItem);
-        }
-        else {
-        	notifyMenu.children.push(menuItem);
-        	notifyMenu.newCount++;
-        }
-        notifyMenu.incremented = true;
-    });
+            var alreadyExitstItem = _.find(notifyMenu.children, function (x) { return x.notify.id == menuItem.notify.id; });
+            if (alreadyExitstItem) {
+                angular.copy(menuItem, alreadyExitstItem);
+            }
+            else {
+                notifyMenu.children.push(menuItem);
+                notifyMenu.newCount++;
 
-    function markAllAsRead() {
-        notifications.markAllAsRead(null, function (data, status, headers, config) {
-        	var notifyMenu = mainMenuService.findByPath('pushNotifications');
-            notifyMenu.incremented = false;
-            notifyMenu.newCount = 0;
-        }, function (error) {
-            //bladeNavigationService.setError('Error ' + error.status, blade);
+                if (angular.isDefined(notifyMenu.intervalPromise)) {
+                    $interval.cancel(notifyMenu.intervalPromise);
+                }
+                animateNotify();
+                notifyMenu.intervalPromise = $interval(animateNotify, 30000);
+            }
         });
 
-    };
+        function animateNotify() {
+            var notifyMenu = mainMenuService.findByPath('pushNotifications');
+            notifyMenu.incremented = true;
 
-    var retVal = {
-        run: function () {
-            if (!this.running) {
-            	var notifyMenu = mainMenuService.findByPath('pushNotifications');
-                if (!angular.isDefined(notifyMenu)) {
-                    notifyMenu = {
-                    	path: 'pushNotifications',
-                        icon: 'fa fa-bell-o',
-                        title: 'Notifications',
-                        priority: 2,
-                        permission: '',
-                        headerTemplate: '$(Platform)/Scripts/app/pushNotifications/menuHeader.tpl.html',
-                        listTemplate: '$(Platform)/Scripts/app/pushNotifications/menuList.tpl.html',
-                        template: '$(Platform)/Scripts/app/pushNotifications/menu.tpl.html',
-                        action: function () { markAllAsRead(); if (this.children.length == 0) { this.showHistory(); } },
-                        showHistory: function () { $state.go('pushNotificationsHistory'); },
-                        clearRecent: function () { notifyMenu.children.splice(0, notifyMenu.children.length); },
-                        children: [],
-                        newCount: 0
-                    };
-                    mainMenuService.addMenuItem(notifyMenu);
-                }
-                this.running = true;
-            };
-        },
-        running: false
-    };
-    return retVal;
+            $timeout(function () {
+                notifyMenu.incremented = false;
+            }, 1500);
+        }
 
-}]);
+        function markAllAsRead() {
+            var notifyMenu = mainMenuService.findByPath('pushNotifications');
+            if (angular.isDefined(notifyMenu.intervalPromise)) {
+                $interval.cancel(notifyMenu.intervalPromise);
+            }
+
+            notifications.markAllAsRead(null, function (data, status, headers, config) {
+                notifyMenu.incremented = false;
+                notifyMenu.newCount = 0;
+            }, function (error) {
+                //bladeNavigationService.setError('Error ' + error.status, blade);
+            });
+        }
+
+        var retVal = {
+            run: function () {
+                if (!this.running) {
+                    var notifyMenu = mainMenuService.findByPath('pushNotifications');
+                    if (!angular.isDefined(notifyMenu)) {
+                        notifyMenu = {
+                            path: 'pushNotifications',
+                            icon: 'fa fa-bell-o',
+                            title: 'Notifications',
+                            priority: 2,
+                            permission: '',
+                            headerTemplate: '$(Platform)/Scripts/app/pushNotifications/menuHeader.tpl.html',
+                            listTemplate: '$(Platform)/Scripts/app/pushNotifications/menuList.tpl.html',
+                            template: '$(Platform)/Scripts/app/pushNotifications/menu.tpl.html',
+                            action: function () { markAllAsRead(); if (this.children.length == 0) { this.showHistory(); } },
+                            showHistory: function () { $state.go('pushNotificationsHistory'); },
+                            clearRecent: function () { notifyMenu.children.splice(0, notifyMenu.children.length); },
+                            children: [],
+                            newCount: 0
+                        };
+                        mainMenuService.addMenuItem(notifyMenu);
+                    }
+                    this.running = true;
+                };
+            },
+            running: false
+        };
+        return retVal;
+
+    }]);
