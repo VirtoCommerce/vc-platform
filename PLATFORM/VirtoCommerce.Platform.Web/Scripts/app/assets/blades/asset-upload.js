d867e0eb802dda9ad1903219f67030166857df22
eMazeika <em@virtoway.com>
2015-10-14 17:46:32 +0300

    assets - initial (draft) UI added

---
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/assets/blades/asset-upload.js
@@ -0,0 +1,44 @@
+﻿angular.module('platformWebApp')
+.controller('platformWebApp.assets.assetUploadController', ['$scope', 'platformWebApp.assets.api', 'platformWebApp.bladeNavigationService', 'FileUploader', function ($scope, assets, bladeNavigationService, FileUploader) {
+    var blade = $scope.blade;
+    // $scope.currentEntities = blade.currentEntities = blade.currentEntity.attachments;
+    
+    function initialize() {
+        if (!$scope.uploader) {
+            // Create the uploader
+            var uploader = $scope.uploader = new FileUploader({
+                scope: $scope,
+                headers: { Accept: 'application/json' },
+                // url: 'api/platform/assets',
+                method: 'POST',
+                autoUpload: true,
+                removeAfterUpload: true
+            });
+
+            uploader.onSuccessItem = function (fileItem, assets, status, headers) {
+                angular.forEach(assets, function (asset) {
+                    //ADD uploaded asset
+                    blade.currentEntities.push(asset);
+                });
+            };
+        }
+    }
+   
+    $scope.copyUrl = function (data) {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", data.url);
+    };
+
+    $scope.addImageFromUrl = function () {
+        if (blade.newExternalImageUrl) {
+            assets.uploadFromUrl({ folder: blade.currentEntityId, url: blade.newExternalImageUrl }, function (data) {
+                //blade.currentEntity.images.push(data);
+                blade.newExternalImageUrl = undefined;
+            });
+        }
+    };
+
+    blade.headIcon = 'fa-file-text-o';
+    
+    initialize();
+    blade.isLoading = false;
+}]);
---
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/assets/resources/assetApi.js
@@ -0,0 +1,9 @@
+﻿angular.module('platformWebApp')
+.factory('platformWebApp.assets.api', ['$resource', function ($resource) {
+    return $resource('api/platform/assets/', {}, {
+        search: {},
+        move: { method: 'POST', url: 'api/platform/assets/move' },
+        uploadFromUrl: { method: 'POST', params: { url: '@url', folder: '@folder' }, url: 'api/platform/assets/:folder' }
+    });
+}]);
+
