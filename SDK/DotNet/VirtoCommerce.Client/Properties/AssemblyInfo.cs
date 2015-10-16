d4605bd80fa9425a7f6d29e5783c62b1ba0fe9b9
artem-dudarev <ad@virtoway.com>
2015-10-16 15:06:25 +0200

    Renamed VirtoCommerce.SwaggerApiClient to VirtoCommerce.Client.

---
+++ SDK/DotNet/VirtoCommerce.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VirtoCommerce.Client")]
+[assembly: AssemblyDescription("Virto Commerce API Client generated with Swagger")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
--- SDK/DotNet/VirtoCommerce.SwaggerApiClient/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("VirtoCommerce.SwaggerApiClient")]
-[assembly: AssemblyDescription("Virto Commerce API Client generated with Swagger")]
-
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
