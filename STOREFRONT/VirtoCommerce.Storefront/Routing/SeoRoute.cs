f111aef4ea25ef9f6185ec94b4688ec827d250c4
artem-dudarev <ad@virtoway.com>
2015-10-19 18:08:35 +0200

    Refactored SeoRoute.cs

--- STOREFRONT/VirtoCommerce.Storefront/Routing/SeoRoute.cs
+++ STOREFRONT/VirtoCommerce.Storefront/Routing/SeoRoute.cs
@@ -44,89 +44,89 @@ namespace VirtoCommerce.Storefront.Routing
             if (data != null)
             {
                 var slug = data.Values["generic_se_name"] as string;
-                //performance optimization.
-                //we load a cached verion here. it reduces number of SQL requests for each page load
                 var urlRecord = _urlRecordService.GetBySlug(slug);
+
                 if (urlRecord == null)
                 {
-                    //no URL record found
-
+                    // Slug not found
                     data.Values["controller"] = "Common";
                     data.Values["action"] = "PageNotFound";
-                    return data;
                 }
-
-                //ensre that URL record is active
-                if (!urlRecord.IsActive)
+                else
                 {
-                    //URL record is not active. let's find the latest one
-                    var activeSlug = _urlRecordService.GetActiveSlug(urlRecord.EntityType, urlRecord.EntityId, urlRecord.Language);
-                    if (string.IsNullOrWhiteSpace(activeSlug))
+                    // Ensure the slug is active
+                    if (!urlRecord.IsActive)
                     {
-                        //no active slug found
+                        // Slug is not active. Try to find the active one for the same entity.
+                        var activeSlug = _urlRecordService.GetActiveSlug(urlRecord.EntityType, urlRecord.EntityId, urlRecord.Language);
 
-                        data.Values["controller"] = "Common";
-                        data.Values["action"] = "PageNotFound";
-                        return data;
+                        if (string.IsNullOrWhiteSpace(activeSlug))
+                        {
+                            // No active slug found
+                            data.Values["controller"] = "Common";
+                            data.Values["action"] = "PageNotFound";
+                        }
+                        else
+                        {
+                            // The active slug is found
+                            var response = httpContext.Response;
+                            response.Status = "301 Moved Permanently";
+                            response.RedirectLocation = string.Format("{0}{1}", _workContext.GetStoreUrl(false), activeSlug);
+                            response.End();
+                            data = null;
+                        }
                     }
+                    else
+                    {
+                        // Redirect to the slug for current language if it differes from requested slug
+                        var slugForCurrentLanguage = GetSlug(urlRecord.EntityType, urlRecord.EntityId, _workContext.WorkingLanguage);
 
-                    //the active one is found
-                    var response = httpContext.Response;
-                    response.Status = "301 Moved Permanently";
-                    response.RedirectLocation = string.Format("{0}{1}", _workContext.GetStoreUrl(false), activeSlug);
-                    response.End();
-                    return null;
-                }
-
-                // Redirect to the slug for current language if it differes from requested slug
-                var slugForCurrentLanguage = GetSlug(urlRecord.EntityType, urlRecord.EntityId, _workContext.WorkingLanguage, true, true);
-                if (!string.IsNullOrEmpty(slugForCurrentLanguage) && !slugForCurrentLanguage.Equals(slug, StringComparison.OrdinalIgnoreCase))
-                {
-                    var response = httpContext.Response;
-                    response.Status = "302 Moved Temporarily";
-                    response.RedirectLocation = string.Format("{0}{1}", _workContext.GetStoreUrl(false), slugForCurrentLanguage);
-                    response.End();
-                    return null;
-                }
-
-                //process URL
-                switch (urlRecord.EntityType)
-                {
-                    case "Product":
+                        if (!string.IsNullOrEmpty(slugForCurrentLanguage) && !slugForCurrentLanguage.Equals(slug, StringComparison.OrdinalIgnoreCase))
                         {
-                            data.Values["controller"] = "Product";
-                            data.Values["action"] = "ProductDetails";
-                            data.Values["productid"] = urlRecord.EntityId;
-                            data.Values["SeName"] = urlRecord.Slug;
+                            var response = httpContext.Response;
+                            response.Status = "302 Moved Temporarily";
+                            response.RedirectLocation = string.Format("{0}{1}", _workContext.GetStoreUrl(false), slugForCurrentLanguage);
+                            response.End();
+                            data = null;
                         }
-                        break;
-                    case "Category":
+                        else
                         {
-                            data.Values["controller"] = "Catalog";
-                            data.Values["action"] = "Category";
-                            data.Values["categoryid"] = urlRecord.EntityId;
-                            data.Values["SeName"] = urlRecord.Slug;
+                            // Process the URL
+                            switch (urlRecord.EntityType)
+                            {
+                                case "Product":
+                                    data.Values["controller"] = "Product";
+                                    data.Values["action"] = "ProductDetails";
+                                    data.Values["productid"] = urlRecord.EntityId;
+                                    data.Values["SeName"] = urlRecord.Slug;
+                                    break;
+                                case "Category":
+                                    data.Values["controller"] = "Catalog";
+                                    data.Values["action"] = "Category";
+                                    data.Values["categoryid"] = urlRecord.EntityId;
+                                    data.Values["SeName"] = urlRecord.Slug;
+                                    break;
+                            }
                         }
-                        break;
+                    }
                 }
             }
 
             return data;
         }
 
-        public string GetSlug(string entityType, int entityId, string language, bool returnDefaultValue, bool ensureTwoPublishedLanguages)
+        private string GetSlug(string entityType, int entityId, string language)
         {
             var result = string.Empty;
 
-            if (!string.IsNullOrEmpty(language)
-                && (!ensureTwoPublishedLanguages || _workContext.StoreLanguages.Length >= 2)
-                )
+            // Get slug for requested language
+            if (!string.IsNullOrEmpty(language) && _workContext.StoreLanguages.Length >= 2)
             {
                 result = _urlRecordService.GetActiveSlug(entityType, entityId, language);
             }
 
-            // Set default value if required
-            if (string.IsNullOrEmpty(result) && returnDefaultValue)
+            // Get slug for default language
+            if (string.IsNullOrEmpty(result))
             {
                 result = _urlRecordService.GetActiveSlug(entityType, entityId, null);
             }
