885d9db72ff61e998ea9e3fd02be9fc3b3af79b8
eMazeika <em@virtoway.com>
2015-10-22 17:48:58 +0300

    account-list refactored to use ui-grid

--- PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/security/blades/account-list.js
+++ PLATFORM/VirtoCommerce.Platform.Web/Scripts/app/security/blades/account-list.js
@@ -1,6 +1,8 @@
 ﻿angular.module('platformWebApp')
 .controller('platformWebApp.accountListController', ['$scope', 'platformWebApp.accounts', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService', 'uiGridConstants',
 function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstants) {
+    var blade = $scope.blade;
+
     //pagination settings
     $scope.pageSettings = {};
     $scope.pageSettings.totalItems = 0;
@@ -11,34 +13,34 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
     $scope.filter = { searchKeyword: undefined };
     var selectedNode = null;
 
-    $scope.blade.refresh = function () {
-        $scope.blade.isLoading = true;
-        $scope.blade.selectedAll = false;
+    blade.refresh = function () {
+        blade.isLoading = true;
+        blade.selectedAll = false;
 
         accounts.search({
             keyword: $scope.filter.searchKeyword,
             skipCount: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
             takeCount: $scope.pageSettings.itemsPerPageCount
         }, function (data) {
-            $scope.blade.isLoading = false;
+            blade.isLoading = false;
 
             $scope.pageSettings.totalItems = angular.isDefined(data.totalCount) ? data.totalCount : 0;
-            $scope.blade.currentEntities = data.users;
+            blade.currentEntities = data.users;
 
             if (selectedNode != null) {
                 //select the node in the new list
-                angular.forEach($scope.blade.currentEntities, function (node) {
+                angular.forEach(blade.currentEntities, function (node) {
                     if (selectedNode.id === node.id) {
                         selectedNode = node;
                     }
                 });
             }
         }, function (error) {
-            bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+            bladeNavigationService.setError('Error ' + error.status, blade);
         });
     };
 
-    $scope.blade.selectNode = function (node) {
+    blade.selectNode = function (node) {
         selectedNode = node;
         $scope.selectedNodeId = selectedNode.userName;
 
@@ -46,22 +48,22 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
             id: 'listItemChild',
             data: selectedNode,
             title: selectedNode.userName,
-            subtitle: $scope.blade.subtitle,
+            subtitle: blade.subtitle,
             controller: 'platformWebApp.accountDetailController',
             template: '$(Platform)/Scripts/app/security/blades/account-detail.tpl.html'
         };
 
-        bladeNavigationService.showBlade(newBlade, $scope.blade);
+        bladeNavigationService.showBlade(newBlade, blade);
     };
 
     $scope.toggleAll = function () {
-        angular.forEach($scope.blade.currentEntities, function (item) {
-            item.selected = $scope.blade.selectedAll;
+        angular.forEach(blade.currentEntities, function (item) {
+            item.selected = blade.selectedAll;
         });
     };
 
     function isItemsChecked() {
-        return $scope.blade.currentEntities && _.any($scope.blade.currentEntities, function (x) { return x.selected; });
+        return _.any(blade.currentEntities, function (x) { return x.selected; });
     }
 
     function deleteChecked() {
@@ -73,12 +75,12 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
                 if (remove) {
                     closeChildrenBlades();
 
-                    var selection = _.where($scope.blade.currentEntities, { selected: true });
+                    var selection = $scope.gridApi.selection.getSelectedRows();
                     var itemIds = _.pluck(selection, 'userName');
                     accounts.remove({ names: itemIds }, function (data, headers) {
-                        $scope.blade.refresh();
+                        blade.refresh();
                     }, function (error) {
-                        bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+                        bladeNavigationService.setError('Error ' + error.status, blade);
                     });
                 }
             }
@@ -87,18 +89,18 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
     }
 
     function closeChildrenBlades() {
-        angular.forEach($scope.blade.childrenBlades.slice(), function (child) {
+        angular.forEach(blade.childrenBlades.slice(), function (child) {
             bladeNavigationService.closeBlade(child);
         });
     }
 
-    $scope.blade.headIcon = 'fa-key';
+    blade.headIcon = 'fa-key';
 
-    $scope.blade.toolbarCommands = [
+    blade.toolbarCommands = [
         {
             name: "Refresh", icon: 'fa fa-refresh',
             executeMethod: function () {
-                $scope.blade.refresh();
+                blade.refresh();
             },
             canExecuteMethod: function () {
                 return true;
@@ -113,11 +115,11 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
                     id: 'listItemChild',
                     currentEntity: { roles: [], userType: 'SiteAdministrator' },
                     title: 'New Account',
-                    subtitle: $scope.blade.subtitle,
+                    subtitle: blade.subtitle,
                     controller: 'platformWebApp.newAccountWizardController',
                     template: '$(Platform)/Scripts/app/security/wizards/newAccount/new-account-wizard.tpl.html'
                 };
-                bladeNavigationService.showBlade(newBlade, $scope.blade);
+                bladeNavigationService.showBlade(newBlade, blade);
             },
             canExecuteMethod: function () {
                 return true;
@@ -130,7 +132,8 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
                 deleteChecked();
             },
             canExecuteMethod: function () {
-                return isItemsChecked();
+                return $scope.gridApi && _.any($scope.gridApi.selection.getSelectedRows());
+                // return isItemsChecked();
             },
             permission: 'platform:security:delete'
         }
@@ -139,49 +142,41 @@ function ($scope, accounts, bladeNavigationService, dialogService, uiGridConstan
     // ui-grid
     $scope.gridOptions = {
         data: 'blade.currentEntities',
-        enableFullRowSelection: true,
-        enableSelectAll: true,
-        multiSelect: false,
-        noUnselect: true,
+        enableRowHeaderSelection: true,
+        // enableFullRowSelection: true,
         //selectionRowHeaderWidth: 35,
         //rowHeight: 35,
-        showGridFooter: true
-    };
-
-    $scope.gridOptions.columnDefs = [
-        //{
-        //    name: 'selected'
-        //},
+        rowTemplate: "<div ng-click=\"grid.appScope.blade.selectNode(row.entity)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.uid\" ui-grid-one-bind-id-grid=\"rowRenderIndex + '-' + col.uid + '-cell'\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader, '__selected': row.entity.userName === grid.appScope.selectedNodeId }\" role=\"{{col.isRowHeader ? 'rowheader' : 'gridcell'}}\" ui-grid-cell></div>",
+        columnDefs: [
         {
+            displayName: 'Name',
             name: 'userName',
             sort: {
                 direction: uiGridConstants.ASC,
                 priority: 1
             }
         },
-        { name: 'userType' },
-        { name: 'userState' }
-    ];
+        { displayName: 'Account type', name: 'userType' },
+        { displayName: 'State', name: 'userState' }
+        ],
+        onRegisterApi: function (gridApi) {
+            //set gridApi on scope
+            $scope.gridApi = gridApi;
+            //gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+            //    if (row.isSelected) {
+            //        //blade.selectNode(row.entity);
+            //    }
+            //});
 
-    //$scope.gridOptions.onRegisterApi = function (gridApi) {
-    //    //set gridApi on scope
-    //    $scope.gridApi = gridApi;
-    //    gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-    //         assignRole(row.entity, row.isSelected);
-    //    });
+            //gridApi.selection.on.rowSelectionChangedBatch($scope, function (rows) {
+            //});
+        }
+    };
 
-    //    gridApi.selection.on.rowSelectionChangedBatch($scope, function (rows) {
-    //        _.each(rows, function (row) {
-    //            assignRole(row.entity, row.isSelected);
-    //        });
-    //    });
-    //};
 
-    $scope.$watch('pageSettings.currentPage', function () {
-        $scope.blade.refresh();
-    });
+    $scope.$watch('pageSettings.currentPage', blade.refresh);
 
     // actions on load
     //No need to call this because page 'pageSettings.currentPage' is watched!!! It would trigger subsequent duplicated req...
-    //$scope.blade.refresh();
+    //blade.refresh();
 }]);
