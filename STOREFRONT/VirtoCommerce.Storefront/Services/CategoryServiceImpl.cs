8a5b6790157e4767338a22417151d2a198cab048
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2015-11-20 14:55:41 +0200

    add category service impl

---
+++ STOREFRONT/VirtoCommerce.Storefront/Services/CategoryServiceImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VirtoCommerce.Storefront.Model.Catalog;
+using VirtoCommerce.Storefront.Model.Services;
+using VirtoCommerce.Client.Api;
+using VirtoCommerce.Storefront.Converters;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.Storefront.Services
+{
+    public class CategoryServiceImpl : ICategoryService
+    {
+        private readonly ICatalogModuleApi _catalogModuleApi;
+
+        public CategoryServiceImpl(ICatalogModuleApi catalogModuleApi)
+        {
+            _catalogModuleApi = catalogModuleApi;
+        }
+
+        public async Task<Category[]> GetCategoriesByCatalog(string catalogId)
+        {
+            var result = await _catalogModuleApi.CatalogModuleSearchSearchAsync("WithCategories", null, null, null, catalogId, null, null, null, null, null, null, null, null, null, 0, 100, null, false);
+
+            return result.Categories.Select(c => c.ToWebModel()).ToArray();
+        }
+
+        public async Task<Category> GetCategoryById(string id, string catalogId, string language, string currencyCode)
+        {
+            var category = await _catalogModuleApi.CatalogModuleCategoriesGetAsync(id);
+
+            var result = await _catalogModuleApi.CatalogModuleSearchSearchAsync("1", null, id, null, null, null, language, currencyCode, null, null, null, null, null, null, 0, 5, null, true);
+
+            return category.ToWebModel(result.Products.ToArray());
+        }
+    }
+}
