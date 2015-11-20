1878ad906f89deb3703f973eac4ddecfcd46e876
E.Tatarincev <et@virtoway.com>
2015-11-20 17:32:12 +0200

    Add error handling and logging

---
+++ STOREFRONT/VirtoCommerce.Storefront/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VirtoCommerce.Storefront.Controllers
+{
+    [RoutePrefix("Errors")]
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        [Route("404")]
+        public ActionResult Http404()
+        {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            Response.TrySkipIisCustomErrors = true;
+
+            return View("404");
+        }
+
+    }
+}
