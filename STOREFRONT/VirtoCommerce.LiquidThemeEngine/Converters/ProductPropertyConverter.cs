a254edb53c174ddd26d794ceb6e7a7f5f7f95013
E.Tatarincev <et@virtoway.com>
2015-12-02 17:02:54 +0200

    Add product properties in product detail Prices redesign

---
+++ STOREFRONT/VirtoCommerce.LiquidThemeEngine/Converters/ProductPropertyConverter.cs
@@ -0,0 +1,18 @@
+﻿using Omu.ValueInjecter;
+using System.Linq;
+using VirtoCommerce.LiquidThemeEngine.Converters.Injections;
+using VirtoCommerce.LiquidThemeEngine.Objects;
+using storefrontModel = VirtoCommerce.Storefront.Model;
+
+namespace VirtoCommerce.LiquidThemeEngine.Converters
+{
+    public static class ProductPropertyConverter
+    {
+        public static ProductProperty ToShopifyModel(this storefrontModel.Catalog.ProductProperty property)
+        {
+            var retVal = new ProductProperty();
+            retVal.InjectFrom(property);
+            return retVal;
+        }
+    }
+}
