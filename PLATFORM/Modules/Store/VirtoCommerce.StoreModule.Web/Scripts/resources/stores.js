b743d761c9e9eb21b6342528f483c7c2b6901417
eMazeika <em@virtoway.com>
2015-12-09 10:03:06 +0200

    search - filtering properties moved to search resource in JS

--- PLATFORM/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
+++ PLATFORM/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
@@ -2,8 +2,6 @@
 .factory('virtoCommerce.storeModule.stores', ['$resource', function ($resource) {
     return $resource('api/stores', {}, {
         get: { url: 'api/stores/:id' },
-        update: { method: 'PUT' },
-        queryFilterProperties: { url: 'api/search/storefilterproperties/:id', isArray: true },
-        saveFilterProperties: { url: 'api/search/storefilterproperties/:id', method: 'PUT' }
+        update: { method: 'PUT' }
     });
 }]);
