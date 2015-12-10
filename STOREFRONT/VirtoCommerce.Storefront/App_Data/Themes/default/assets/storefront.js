3d403610204eb6f7e206b04825c60d23e84688fc
Andrew Orlov <ao@virtoway.com>
2015-12-10 13:56:38 +0200

    Fixed checkout case with physical products

--- STOREFRONT/VirtoCommerce.Storefront/App_Data/Themes/default/assets/storefront.js
+++ STOREFRONT/VirtoCommerce.Storefront/App_Data/Themes/default/assets/storefront.js
@@ -195,6 +195,10 @@ app.controller('checkoutController', ['$scope', '$location', '$sce', 'cartServic
         $scope.checkout.bankCardInfo.CardholderName = $scope.checkout.bankCardInfo.CardholderName.toUpperCase();
     }
 
+    $scope.setBillingAddressEqualsShipping = function () {
+        setBillingAddressEqualsShipping();
+    }
+
     $scope.completeOrder = function () {
         $scope.checkout.orderProcessing = true;
         cartService.setPaymentMethod($scope.checkout.selectedPaymentMethod.GatewayCode).then(function (response) {
@@ -216,6 +220,7 @@ app.controller('checkoutController', ['$scope', '$location', '$sce', 'cartServic
 
     function initialize() {
         $scope.checkout.orderSummaryExpanded = false;
+        $scope.checkout.billingAddressEqualsShipping = true;
         $scope.checkout.shippingAddress = {};
         $scope.checkout.billingAddress = {};
         $scope.checkout.bankCardInfo = {};
@@ -328,6 +333,27 @@ app.controller('checkoutController', ['$scope', '$location', '$sce', 'cartServic
         if ($scope.checkout.billingAddress.CountryCode) {
             getRegions($scope.checkout.billingAddress.CountryCode);
         }
+        setBillingAddressEqualsShipping();
+    }
+
+    function setBillingAddressEqualsShipping() {
+        $scope.checkout.billingAddress.Name = $scope.checkout.shippingAddress.Name;
+        $scope.checkout.billingAddress.Organization = $scope.checkout.shippingAddress.Organization;
+        $scope.checkout.billingAddress.CountryCode = $scope.checkout.shippingAddress.CountryCode;
+        $scope.checkout.billingAddress.CountryName = $scope.checkout.shippingAddress.CountryName;
+        $scope.checkout.billingAddress.City = $scope.checkout.shippingAddress.City;
+        $scope.checkout.billingAddress.PostalCode = $scope.checkout.shippingAddress.PostalCode;
+        $scope.checkout.billingAddress.Zip = $scope.checkout.shippingAddress.Zip;
+        $scope.checkout.billingAddress.Line1 = $scope.checkout.shippingAddress.Line1;
+        $scope.checkout.billingAddress.Line2 = $scope.checkout.shippingAddress.Line2;
+        $scope.checkout.billingAddress.RegionId = $scope.checkout.shippingAddress.RegionId;
+        $scope.checkout.billingAddress.RegionName = $scope.checkout.shippingAddress.RegionName;
+        $scope.checkout.billingAddress.FirstName = $scope.checkout.shippingAddress.FirstName;
+        $scope.checkout.billingAddress.MiddleName = $scope.checkout.shippingAddress.MiddleName;
+        $scope.checkout.billingAddress.LastName = $scope.checkout.shippingAddress.LastName;
+        $scope.checkout.billingAddress.Phone = $scope.checkout.shippingAddress.Phone;
+        $scope.checkout.billingAddress.Email = $scope.checkout.shippingAddress.Email;
+        $scope.checkout.billingAddressEqualsShipping = true;
     }
 
     function checkAddress(address, provinceRequired) {
@@ -453,332 +479,6 @@ app.controller('checkoutController', ['$scope', '$location', '$sce', 'cartServic
     }
 
     //======================================================================================================
-
-
-    //$scope.checkout = {
-    //    hasCustomerInformation: false,
-    //    hasShippingMethod: false,
-    //    cart: {},
-    //    orderSummaryExpanded: false,
-    //    shippingAddress: {},
-    //    billingAddress: {},
-    //    countries: [],
-    //    countryRegions: [],
-    //    couponProcessing: false,
-    //    couponError: null,
-    //    customerInformationProcessing: false,
-    //    availableShippingMethods: [],
-    //    selectedShippingMethod: {},
-    //    shippingMethodProcessing: false,
-    //    availablePaymentMethods: [],
-    //    selectedPaymentMethod: {},
-    //    bankCardInfo: { Type: 'Unknown', ExpirationDate: null, BankCardMonth: null, BankCardYear: null },
-    //    billingAddressEqualsShipping: true,
-    //    orderProcessing: false,
-    //    paymentFormHtml: null
-    //}
-
-    //$scope.toggleOrderSummary = function (orderSummaryExpanded) {
-    //    $scope.checkout.orderSummaryExpanded = !orderSummaryExpanded;
-    //}
-
-    //$scope.setCountry = function (addressType, countryName)
-    //{
-    //    var country = _.find($scope.checkout.countries, function (c) { return c.Name == countryName; });
-    //    if (!country) {
-    //        return;
-    //    }
-    //    if (addressType == 'Shipping') {
-    //        $scope.checkout.shippingAddress.CountryCode = country.Code3;
-    //        $scope.checkout.shippingAddress.RegionId = null;
-    //        $scope.checkout.shippingAddress.RegionName = null;
-    //    }
-    //    if (addressType == 'Billing') {
-    //        $scope.checkout.billingAddress.CountryCode = country.Code3;
-    //        $scope.checkout.billingAddress.RegionId = null;
-    //        $scope.checkout.billingAddress.RegionName = null;
-    //    }
-    //    cartService.getCountryRegions(country.Code3).then(function (response) {
-    //        $scope.checkout.countryRegions = response.data;
-    //    });
-    //}
-
-    //$scope.setCountryRegion = function (addressType, regionName) {
-    //    var region = _.find($scope.checkout.countryRegions, function (r) { return r.Name == regionName });
-    //    if (!region) {
-    //        return;
-    //    }
-    //    if (addressType == 'Shipping') {
-    //        $scope.checkout.shippingAddress.RegionId = region.Code;
-    //    }
-    //    if (addressType == 'Billing') {
-    //        $scope.checkout.billingAddress.RegionId = region.Code;
-    //    }
-    //}
-
-    //$scope.addCoupon = function (couponCode) {
-    //    if (!couponCode) {
-    //        return;
-    //    }
-    //    $scope.checkout.couponProcessing = true;
-    //    cartService.addCoupon(couponCode).then(function (response) {
-    //        updateCheckout(response.data);
-    //    });
-    //}
-
-    //$scope.removeCoupon = function () {
-    //    $scope.checkout.couponProcessing = true;
-    //    cartService.removeCoupon().then(function (response) {
-    //        updateCheckout(response.data);
-    //    });
-    //}
-
-    //$scope.setCustomerInformation = function (address) {
-    //    $scope.checkout.customerInformationProcessing = true;
-    //    cartService.addAddress(address).then(function (response) {
-    //        updateCheckout(response.data);
-    //        $scope.innerRedirect('shipping-method');
-    //    });
-    //}
-
-    //$scope.setShippingMethod = function (shippingMethodCode, isPreview) {
-    //    $scope.checkout.shippingMethodProcessing = true;
-    //    cartService.setShippingMethod(shippingMethodCode, isPreview).then(function (response) {
-    //        updateCheckout(response.data);
-    //        if (!isPreview) {
-    //            $scope.innerRedirect('payment-method');
-    //        }
-    //    });
-    //}
-
-    //$scope.capitalizeString = function (string) {
-    //    $scope.checkout.bankCardInfo.CardholderName = string.toUpperCase();
-    //}
-
-    //$scope.setBillingAddressEqualsShipping = function () {
-    //    setBillingAddressEqualsShipping();
-    //}
-
-    //$scope.validateBankCardNumber = function (bankCardNumber) {
-    //    var type = 'Unknown';
-    //    var cardNumberPattern = /^\d{12,19}$/;
-    //    var cvvPattern = /^\d{3,3}$/;
-
-    //    var firstOneSymbol = bankCardNumber.substring(0, 1);
-    //    var firstTwoSymbols = bankCardNumber.substring(0, 2);
-    //    var firstThreeSymbols = bankCardNumber.substring(0, 3);
-    //    var firstFourSymbols = bankCardNumber.substring(0, 4);
-    //    var firstSixSymbols = bankCardNumber.substring(0, 6);
-
-    //    if (firstTwoSymbols == '34' || firstTwoSymbols == '37') {
-    //        type = 'AmericanExpress';
-    //        cardNumberPattern = /^\d{15}$/;
-    //        cvvPattern = /^\d{4,4}$/;
-    //    }
-    //    if (firstTwoSymbols == '62' || firstTwoSymbols == '88') {
-    //        type = 'UnionPay';
-    //        cardNumberPattern = /^\d{16,19}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstThreeSymbols >= '300' && firstThreeSymbols <= '305' || firstThreeSymbols == '309' || firstTwoSymbols == '36' || firstTwoSymbols == '38' || firstTwoSymbols == '39') {
-    //        type = 'Diners';
-    //        cardNumberPattern = /^\d{14,16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstFourSymbols == '6011' || firstSixSymbols >= '622126' && firstSixSymbols <= '622925' || firstThreeSymbols >= '644' && firstThreeSymbols <= '649' || firstTwoSymbols == '65') {
-    //        type = 'Discover';
-    //        cardNumberPattern = /^\d{16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstFourSymbols >= '3528' && firstFourSymbols <= '3589') {
-    //        type = 'Jcb';
-    //        cardNumberPattern = /^\d{16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstFourSymbols == '6304' || firstFourSymbols == '6706' || firstFourSymbols == '6771' || firstFourSymbols == '6709') {
-    //        type = 'Laser';
-    //        cardNumberPattern = /^\d{16,19}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstFourSymbols == '5018' || firstFourSymbols == '5020' || firstFourSymbols == '5038' || firstFourSymbols == '5612' || firstFourSymbols == '5893' || firstFourSymbols == '6304' ||
-    //        firstFourSymbols >= '6759' && firstFourSymbols <= '6763' || firstFourSymbols == '0604' || firstFourSymbols == '6390') {
-    //        type = 'Maestro';
-    //        cardNumberPattern = /^\d{12,19}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstFourSymbols == '5019') {
-    //        type = 'Dankort';
-    //        cardNumberPattern = /^\d{16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstTwoSymbols >= '51' && firstTwoSymbols <= '55') {
-    //        type = 'MasterCard';
-    //        cardNumberPattern = /^\d{16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-    //    if (firstOneSymbol == '4') {
-    //        type = 'Visa';
-    //        cardNumberPattern = /^\d{13,16}$/;
-    //        cvvPattern = /^\d{3,3}$/;
-    //    }
-
-    //    $scope.checkout.bankCardInfo.Type = type;
-    //    $scope.checkout.bankCardInfo.CardNumberPattern = cardNumberPattern;
-    //    $scope.checkout.bankCardInfo.CvvPattern = cvvPattern;
-    //}
-
-    //$scope.validateBankCardExpirationDate = function (date) {
-    //    $scope.checkout.bankCardInfo.BankCardMonth = null;
-    //    $scope.checkout.bankCardInfo.BankCardYear = null;
-    //    var separator = ' / ';
-    //    $scope.checkout.bankCardInfo.ExpirationDate = date;
-    //    if (date.length == 2) {
-    //        $scope.checkout.bankCardInfo.ExpirationDate += separator;
-    //    }
-    //    var dateParts = date.split(separator);
-    //    if (dateParts.length == 2) {
-    //        var currentYear = (new Date()).getFullYear();
-    //        var currentMonth = (new Date()).getMonth() + 1;
-    //        var expirationMonth = parseInt(dateParts[0]);
-    //        var expirationYear = parseInt(dateParts[1]) + 2000;
-    //        if (expirationMonth >= 1 && expirationMonth <= 12) {
-    //            if (expirationYear > currentYear || expirationYear == currentYear && expirationMonth >= currentMonth) {
-    //                alert(expirationMonth + ' ' + expirationYear);
-    //                $scope.checkout.bankCardInfo.BankCardMonth = expirationMonth;
-    //                $scope.checkout.bankCardInfo.BankCardYear = expirationYear;
-    //            }
-    //        }
-    //    }
-    //}
-
-    //$scope.completeOrder = function (paymentMethodCode) {
-    //    $scope.checkout.orderProcessing = true;
-    //    cartService.addAddress($scope.checkout.billingAddress).then(function (response) {
-    //        cartService.setPaymentMethod(paymentMethodCode).then(function (response) {
-    //            var cart = response.data;
-    //            cartService.createOrder(cart.Id).then(function (response) {
-    //                var order = response.data;
-    //                var incomingPayment = order.InPayments.length ? order.InPayments[0] : null;
-    //                cartService.processPayment(order.Id, incomingPayment.Id, $scope.checkout.bankCardInfo).then(function (response) {
-    //                    handlePaymentProcessingResult(response.data, order.Id);
-    //                });
-    //            });
-    //        });
-    //    });
-    //}
-
-    //cartService.getCountries().then(function (response) {
-    //    $scope.checkout.countries = response.data;
-    //});
-
-    //cartService.getCart().then(function (response) {
-    //    var cart = response.data;
-    //    handleInnerRedirects(cart);
-    //    updateCheckout(cart);
-    //});
-
-    //function addressIsValid(address) {
-    //    var isValid = false;
-    //    if (address.FirstName && address.LastName && address.Email && address.Line1 && address.City && address.CountryCode && address.CountryName && address.PostalCode) {
-    //        isValid = true;
-    //    }
-    //    return isValid;
-    //}
-
-    //function handleInnerRedirects(cart) {
-    //    if (!cart.ItemsCount) {
-    //        $scope.outerRedirect($scope.baseUrl + '/cart');
-    //    }
-    //    var hasCustomerInformation = addressIsValid(cart.DefaultShippingAddress);
-    //    var hasShippingMethod = cart.Shipments.length;
-    //    if (!cart.HasPhysicalProducts) {
-    //        $scope.innerRedirect('payment-method');
-    //    } else {
-    //        if (hasCustomerInformation && hasShippingMethod) {
-    //            $scope.innerRedirect('payment-method');
-    //            return;
-    //        }
-    //        if (hasCustomerInformation && !hasShippingMethod) {
-    //            $scope.innerRedirect('shipping-method');
-    //            return;
-    //        }
-    //        if (!hasCustomerInformation) {
-    //            $scope.innerRedirect('customer-information');
-    //            return;
-    //        }
-    //    }
-    //}
-
-    //function updateCheckout(cart) {
-    //    $scope.checkout.cart = cart;
-    //    $scope.checkout.shippingAddress = cart.DefaultShippingAddress;
-    //    if (cart.DefaultShippingAddress.CountryCode) {
-    //        cartService.getCountryRegions(cart.DefaultShippingAddress.CountryCode).then(function (response) {
-    //            $scope.checkout.countryRegions = response.data;
-    //        });
-    //    }
-    //    $scope.checkout.billingAddress = cart.DefaultBillingAddress;
-    //    if ($scope.checkout.billingAddressEqualsShipping) {
-    //        setBillingAddressEqualsShipping();
-    //    }
-    //    $scope.checkout.couponProcessing = false;
-    //    $scope.checkout.couponError = _.find(cart.Errors, function (e) { return e == 'InvalidCouponCode' });
-    //    $scope.checkout.customerInformationProcessing = false;
-    //    $scope.checkout.shippingMethodProcessing = false;
-    //    $scope.checkout.orderProcessing = false;
-    //    $scope.checkout.hasCustomerInformation = addressIsValid(cart.DefaultShippingAddress);
-    //    $scope.checkout.hasShippingMethod = cart.Shipments.length;
-    //    cartService.getAvailableShippingMethods(cart.Id).then(function (response) {
-    //        var availableShippingMethods = response.data;
-    //        $scope.checkout.availableShippingMethods = availableShippingMethods;
-    //        for (var i = 0; i < availableShippingMethods.length; i++) {
-    //            var selectedShippingMethod = _.find(cart.Shipments, function (e) { return e.ShipmentMethodCode == availableShippingMethods[i].ShipmentMethodCode });
-    //            $scope.checkout.selectedShippingMethod = selectedShippingMethod || {};
-    //        }
-    //    });
-    //    cartService.getAvailablePaymentMethods(cart.Id).then(function (response) {
-    //        $scope.checkout.availablePaymentMethods = response.data;
-    //    });
-    //}
-
-    //function setBillingAddressEqualsShipping() {
-    //    $scope.checkout.billingAddress.Name = $scope.checkout.shippingAddress.Name;
-    //    $scope.checkout.billingAddress.Organization = $scope.checkout.shippingAddress.Organization;
-    //    $scope.checkout.billingAddress.CountryCode = $scope.checkout.shippingAddress.CountryCode;
-    //    $scope.checkout.billingAddress.CountryName = $scope.checkout.shippingAddress.CountryName;
-    //    $scope.checkout.billingAddress.City = $scope.checkout.shippingAddress.City;
-    //    $scope.checkout.billingAddress.PostalCode = $scope.checkout.shippingAddress.PostalCode;
-    //    $scope.checkout.billingAddress.Zip = $scope.checkout.shippingAddress.Zip;
-    //    $scope.checkout.billingAddress.Line1 = $scope.checkout.shippingAddress.Line1;
-    //    $scope.checkout.billingAddress.Line2 = $scope.checkout.shippingAddress.Line2;
-    //    $scope.checkout.billingAddress.RegionId = $scope.checkout.shippingAddress.RegionId;
-    //    $scope.checkout.billingAddress.RegionName = $scope.checkout.shippingAddress.RegionName;
-    //    $scope.checkout.billingAddress.FirstName = $scope.checkout.shippingAddress.FirstName;
-    //    $scope.checkout.billingAddress.MiddleName = $scope.checkout.shippingAddress.MiddleName;
-    //    $scope.checkout.billingAddress.LastName = $scope.checkout.shippingAddress.LastName;
-    //    $scope.checkout.billingAddress.Phone = $scope.checkout.shippingAddress.Phone;
-    //    $scope.checkout.billingAddress.Email = $scope.checkout.shippingAddress.Email;
-    //    $scope.checkout.billingAddressEqualsShipping = true;
-    //}
-
-    //function handlePaymentProcessingResult(paymentProcessingResult, orderId) {
-    //    if (!paymentProcessingResult.isSuccess) {
-    //        return;
-    //    }
-    //    if (paymentProcessingResult.paymentMethodType == 'PreparedForm' && paymentProcessingResult.htmlForm) {
-    //        $scope.checkout.paymentFormHtml = $sce.trustAsHtml(paymentProcessingResult.htmlForm);
-    //        $scope.innerRedirect('payment-form');
-    //    }
-    //    if (paymentProcessingResult.paymentMethodType == 'Standard') {
-    //        $scope.outerRedirect($scope.baseUrl + '/cart/checkout/thanks?id=' + orderId);
-    //    }
-    //    if (paymentProcessingResult.paymentMethodType == 'Unknown') {
-    //        $scope.outerRedirect($scope.baseUrl + '/cart/checkout/thanks?id=' + orderId);
-    //    }
-    //    if (paymentProcessingResult.paymentMethodType == 'Redirection' && paymentProcessingResult.redirectUrl) {
-    //        window.location.href = paymentProcessingResult.redirectUrl;
-    //    }
-    //}
 }]);
 
 app.controller('productController', ['$scope', '$window', 'productService', function ($scope, $window, productService) {
