6534f695e4d89cc4fac8058129ec7dc7df507459
aartiomov <andrej.artiomov@gmail.com>
2015-12-14 14:45:43 +0200

    - Payment method callback method definition update. Use parameters collection instead of orderid.

---
+++ PLATFORM/Modules/Core/VirtoCommerce.Core.Web/Model/PaymentCallbackParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.CoreModule.Web.Model
+{
+    public class PaymentCallbackParameters
+    {
+        public KeyValuePair[] Parameters { get; set; }
+    }
+}
