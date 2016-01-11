d5b3f4eda9078894e8ecc7f7aea0c406b4f73e55
eMazeika <em@virtoway.com>
2016-01-11 16:46:29 +0200

    core - currencies management implemented

---
+++ PLATFORM/Modules/Core/VirtoCommerce.Core.Web/Scripts/Currency/currency.js
@@ -0,0 +1,11 @@
+﻿angular.module("virtoCommerce.coreModule.currency", [])
+.run(
+  ['platformWebApp.widgetService', function (widgetService) {
+      //Register module widget
+      widgetService.registerWidget({
+          isVisible: function (blade) { return blade.currentEntity.id == 'VirtoCommerce.Core'; },
+          controller: 'virtoCommerce.coreModule.currency.currencyWidgetController',
+          template: 'Modules/$(VirtoCommerce.Core)/Scripts/currency/widgets/currencyWidget.tpl.html'
+      }, 'moduleDetail');
+  }])
+;
