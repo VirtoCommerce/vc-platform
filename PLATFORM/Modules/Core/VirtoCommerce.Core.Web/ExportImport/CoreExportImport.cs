be0a88f79241a47a0359069fad5b18ea94a8f8d8
Tatarincev <et@virtoway.com>
2016-01-15 10:44:18 +0200

    Add export-import currencies

--- PLATFORM/Modules/Core/VirtoCommerce.Core.Web/ExportImport/CoreExportImport.cs
+++ PLATFORM/Modules/Core/VirtoCommerce.Core.Web/ExportImport/CoreExportImport.cs
@@ -12,6 +12,7 @@ namespace VirtoCommerce.CoreModule.Web.ExportImport
 	public sealed class BackupObject
 	{
 		public FulfillmentCenter[] FulfillmentCenters { get; set; }
+        public Currency[] Currencies { get; set; }
 	}
 
 	public sealed class CoreExportImport
@@ -27,16 +28,23 @@ namespace VirtoCommerce.CoreModule.Web.ExportImport
 		{
 			var backupObject = GetBackupObject(progressCallback);
 			backupObject.SerializeJson(backupStream);
-			progressCallback(new ExportImportProgressInfo("export fulfillmentCenters done"));
 		}
 
 		public void DoImport(Stream backupStream, Action<ExportImportProgressInfo> progressCallback)
 		{
 			var backupObject = backupStream.DeserializeJson<BackupObject>();
+            progressCallback(new ExportImportProgressInfo("importing fulfillmentCenters"));
+            if (backupObject.FulfillmentCenters != null)
+            {
+                backupObject.FulfillmentCenters.ForEach(x => _commerceService.UpsertFulfillmentCenter(x));
+            }
+            progressCallback(new ExportImportProgressInfo("imporing currencies"));
+            if (backupObject.Currencies != null)
+            {
+                _commerceService.UpsertCurrencies(backupObject.Currencies);
+            }
 
-			backupObject.FulfillmentCenters.ForEach(x => _commerceService.UpsertFulfillmentCenter(x));
-			progressCallback(new ExportImportProgressInfo("update fulfillmentCenters done"));
-		}
+        }
 
 		private BackupObject GetBackupObject(Action<ExportImportProgressInfo> progressCallback)
 		{
@@ -44,9 +52,13 @@ namespace VirtoCommerce.CoreModule.Web.ExportImport
 			var fulfillments = _commerceService.GetAllFulfillmentCenters().ToArray();
 			progressCallback(new ExportImportProgressInfo(string.Format("fulfillmentCenters loaded: {0}", fulfillments.Count())));
 
-			return new BackupObject()
-			{
-				FulfillmentCenters = fulfillments
+            progressCallback(new ExportImportProgressInfo("currencies loading"));
+            var currencies = _commerceService.GetAllCurrencies().ToArray();
+            progressCallback(new ExportImportProgressInfo(string.Format("currencies loaded: {0}", currencies.Count())));
+            return new BackupObject()
+            {
+                FulfillmentCenters = fulfillments,
+                Currencies = currencies
 			};
 		}
 	}
