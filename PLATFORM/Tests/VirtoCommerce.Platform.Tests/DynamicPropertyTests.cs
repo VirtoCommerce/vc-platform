a8e3116262be6ec56ac3f9a39bc3c067b88d5ea7
Tatarincev <et@virtoway.com>
2016-02-15 18:12:17 +0200

    Add new unit test to dynamic properties functionality

--- PLATFORM/Tests/VirtoCommerce.Platform.Tests/DynamicPropertyTests.cs
+++ PLATFORM/Tests/VirtoCommerce.Platform.Tests/DynamicPropertyTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.DynamicProperties;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Data.DynamicProperties;
@@ -10,17 +11,81 @@ using VirtoCommerce.Platform.Data.Security;
 
 namespace VirtoCommerce.Platform.Tests
 {
-    public class TestType : IHasDynamicProperties
+    public class Cart : Entity, IHasDynamicProperties
     {
-        public string Id { get; set; }
+        public Cart(string id)
+        {
+            Id = id;
+            LineItems = new List<LineItem>();
+            DynamicProperties = new List<DynamicObjectProperty>();
+        }
 		public string ObjectType { get; set; }
 		public ICollection<DynamicObjectProperty> DynamicProperties { get; set; }
+        public ICollection<LineItem> LineItems { get; set; }
     }
 
+    public class LineItem : Entity, IHasDynamicProperties
+    {
+        public LineItem(string id)
+        {
+            Id = id;
+            DynamicProperties = new List<DynamicObjectProperty>();
+        }
+        public string ObjectType { get; set; }
+        public ICollection<DynamicObjectProperty> DynamicProperties { get; set; }
+    }
 
     [TestClass]
     public class DynamicPropertyTests
     {
+
+        [TestMethod]
+        public void IterativeSavingObjectsWithproperties_PropertiesSholdSaved()
+        {
+            var propertyService = GetDynamicPropertyService();
+            var property = new DynamicProperty
+            {
+                Id = "LineItemProperty",
+                Name = "Property",
+                ObjectType = typeof(LineItem).FullName,
+                ValueType = DynamicPropertyValueType.ShortText,
+            };
+            //Create new dynamic property for lineItem type
+            propertyService.SaveProperties(new[] { property });
+
+            var cart = new Cart("Cart #1");
+            var lineItem1 = new LineItem("LineItem #1");
+            lineItem1.DynamicProperties.Add(new DynamicObjectProperty { Name = property.Name, Values = new[] { new DynamicPropertyObjectValue { Value = "value1" } } });
+
+            var lineItem2 = new LineItem("LineItem #2");
+            lineItem2.DynamicProperties.Add(new DynamicObjectProperty { Name = property.Name, Values = new[] { new DynamicPropertyObjectValue { Value = "value2" } } });
+            cart.LineItems.AddRange(new[] { lineItem1, lineItem2 });
+
+            //Save aggregate cart object with line items with initial property values
+            propertyService.SaveDynamicPropertyValues(cart);
+            //cleanup
+            lineItem1.DynamicProperties.Clear();
+            lineItem2.DynamicProperties.Clear();
+
+            propertyService.LoadDynamicPropertyValues(cart);
+
+            //Simulate iterative update (as in you UpdateCartWrapper class)
+            lineItem1.DynamicProperties.First().Values.First().Value = "value11";
+            propertyService.SaveDynamicPropertyValues(lineItem1);
+
+            lineItem2.DynamicProperties.First().Values.First().Value = "value22";
+            propertyService.SaveDynamicPropertyValues(lineItem2);
+
+            //cleanup
+            lineItem1.DynamicProperties.Clear();
+            lineItem2.DynamicProperties.Clear();
+
+            propertyService.LoadDynamicPropertyValues(cart);
+
+            Assert.IsTrue(lineItem1.DynamicProperties.First().Values.First().Value.ToString() == "value11");
+            Assert.IsTrue(lineItem2.DynamicProperties.First().Values.First().Value.ToString() == "value22");
+        }
+
         [TestMethod]
         public void PartialUpdateObjectDynamicPropertiesValues_ShouldUpdateOnlyPassedPropertiesValues()
         {
@@ -29,7 +94,7 @@ namespace VirtoCommerce.Platform.Tests
             {
                 Id = "TestType-Property1",
                 Name = "Property1",
-                ObjectType = typeof(TestType).FullName,
+                ObjectType = typeof(Cart).FullName,
                 ValueType = DynamicPropertyValueType.ShortText,
                 CreatedBy = "Auto"
             };
@@ -37,18 +102,14 @@ namespace VirtoCommerce.Platform.Tests
             {
                 Id = "TestType-Property2",
                 Name = "Property2",
-                ObjectType = typeof(TestType).FullName,
+                ObjectType = typeof(Cart).FullName,
                 ValueType = DynamicPropertyValueType.ShortText,
                 CreatedBy = "Auto"
             };
 
             propertyService.SaveProperties(new[] { prop1, prop2 });
 
-            var obj = new TestType()
-            {
-                Id = "1",
-                DynamicProperties = new List<DynamicObjectProperty>()
-            };
+            var obj = new Cart("1");
             //Add new properties values for object
             obj.DynamicProperties.Add(new DynamicObjectProperty { Name = "Property1",  Values = new[] { new DynamicPropertyObjectValue { Value = "value1" } } });
             obj.DynamicProperties.Add(new DynamicObjectProperty { Name = "Property2",  Values = new[] { new DynamicPropertyObjectValue { Value = "value2" } } });
