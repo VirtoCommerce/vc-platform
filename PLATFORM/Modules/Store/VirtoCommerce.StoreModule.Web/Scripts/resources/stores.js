478b81396783538bd54658bb2b37bad86d59693d
eMazeika <em@virtoway.com>
2016-03-01 10:17:55 +0200

    stores - login on behalf store filtering added

--- PLATFORM/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
+++ PLATFORM/Modules/Store/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
@@ -1,8 +1,9 @@
 ﻿angular.module('virtoCommerce.storeModule')
 .factory('virtoCommerce.storeModule.stores', ['$resource', function ($resource) {
-	return $resource('api/stores', {}, {
-		search: { method: 'POST', url: 'api/stores/search' },
+    return $resource('api/stores', {}, {
+        search: { method: 'POST', url: 'api/stores/search' },
         get: { url: 'api/stores/:id' },
-        update: { method: 'PUT' }
+        update: { method: 'PUT' },
+        queryLoginOnBehalfStores: { url: 'api/stores/allowed/:userId', isArray: true }
     });
 }]);
