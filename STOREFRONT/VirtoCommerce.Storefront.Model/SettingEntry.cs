32b69ca31711a38b22bbab0cfcc93a35054e0813
Tatarincev <et@virtoway.com>
2016-03-10 14:16:39 +0200

    Add settings to store in Storefront and also add settings and dynamic properties to Shop.Metafileds in liquid model

---
+++ STOREFRONT/VirtoCommerce.Storefront.Model/SettingEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.Storefront.Model
+{
+    public class SettingEntry 
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ValueType { get; set; }
+        public string[] AllowedValues { get; set; }
+        public string DefaultValue { get; set; }
+        public bool IsArray { get; set; }
+        public string[] ArrayValues { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
