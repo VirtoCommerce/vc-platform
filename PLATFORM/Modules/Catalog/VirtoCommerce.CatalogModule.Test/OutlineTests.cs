bc04ccf86a78deb561e09dd374e54e6692415b33
artem-dudarev <ad@virtoway.com>
2016-04-08 16:30:44 +0200

    Use mocks for outline tests.

--- PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Test/OutlineTests.cs
+++ PLATFORM/Modules/Catalog/VirtoCommerce.CatalogModule.Test/OutlineTests.cs
@@ -1,32 +1,110 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.CatalogModule.Data.Services;
+using VirtoCommerce.CoreModule.Data.Model;
 using VirtoCommerce.CoreModule.Data.Repositories;
 using VirtoCommerce.CoreModule.Data.Services;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Domain.Commerce.Services;
-using VirtoCommerce.Platform.Data.Infrastructure.Interceptors;
+using Catalog = VirtoCommerce.CatalogModule.Data.Model.Catalog;
+using Category = VirtoCommerce.CatalogModule.Data.Model.Category;
 
 namespace VirtoCommerce.CatalogModule.Test
 {
     [TestClass]
     public class OutlineTests
     {
+        #region Mocks
+
+        private static readonly List<Category> _categories = new List<Category>
+        {
+            new Category
+            {
+                CatalogId = "v",
+                Id = "v1",
+                Catalog = new Catalog { Id = "v", Virtual = true },
+            },
+
+            new Category
+            {
+                CatalogId = "c",
+                Id = "c1",
+                Catalog = new Catalog { Id = "c" },
+                OutgoingLinks = new ObservableCollection<CategoryRelation>
+                {
+                    new CategoryRelation { TargetCatalogId  = "v", TargetCategoryId = "v1" },
+                },
+            },
+            new Category
+            {
+                CatalogId = "c",
+                Id = "c2",
+                ParentCategoryId = "c1",
+                Catalog = new Catalog { Id = "c" },
+                OutgoingLinks = new ObservableCollection<CategoryRelation>
+                {
+                    new CategoryRelation { TargetCatalogId  = "v", TargetCategoryId = "v1" },
+                },
+            },
+            new Category
+            {
+                CatalogId = "c",
+                Id = "c3",
+                ParentCategoryId = "c2",
+                Catalog = new Catalog { Id = "c" },
+                OutgoingLinks = new ObservableCollection<CategoryRelation>
+                {
+                    new CategoryRelation { TargetCatalogId  = "v" },
+                    new CategoryRelation { TargetCatalogId  = "v", TargetCategoryId = "v1" },
+                },
+            },
+        };
+
+        private static readonly List<Item> _items = new List<Item>
+        {
+            new Item
+            {
+                CatalogId = "c",
+                CategoryId = "c3",
+                Id = "p1",
+                Catalog = new Catalog { Id = "c" },
+                CategoryLinks = new ObservableCollection<CategoryItemRelation>
+                {
+                    new CategoryItemRelation { CatalogId  = "v" },
+                    new CategoryItemRelation { CatalogId  = "v", CategoryId = "v1" },
+                },
+            },
+        };
+
+        private static readonly List<SeoUrlKeyword> _seo = new List<SeoUrlKeyword>
+        {
+            new SeoUrlKeyword { ObjectType = "Category", ObjectId = "v1" },
+            new SeoUrlKeyword { ObjectType = "Category", ObjectId = "c1" },
+            new SeoUrlKeyword { ObjectType = "Category", ObjectId = "c2" },
+            new SeoUrlKeyword { ObjectType = "Category", ObjectId = "c3" },
+            new SeoUrlKeyword { ObjectType = "CatalogProduct", ObjectId = "p1" },
+        };
+
+        #endregion
+
         [TestMethod]
         public void SearchCategories_When_PhysicalCatalog_Expect_SinglePhysicalOutline()
         {
             var service = GetCategoryService();
-            var result = service.GetByIds(new[] { "fe4d82e57c20435c8a91b6ac75dc557f" }, CategoryResponseGroup.WithOutlines | CategoryResponseGroup.WithSeo, "b61aa9d1d0024bc4be12d79bf5786e9f");
+            var result = service.GetByIds(new[] { "c3" }, CategoryResponseGroup.WithOutlines | CategoryResponseGroup.WithSeo, "c");
 
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
 
             var category = result.First();
             Assert.IsNotNull(category);
-            Assert.AreEqual("Summer-Dresses", category.Code);
-            Assert.AreEqual("fe4d82e57c20435c8a91b6ac75dc557f", category.Id);
+            Assert.AreEqual("c3", category.Id);
             Assert.IsNotNull(category.Outlines);
             Assert.AreEqual(1, category.Outlines.Count);
 
@@ -35,7 +113,7 @@ namespace VirtoCommerce.CatalogModule.Test
             Assert.AreEqual(4, outline.Items.Count);
 
             var outlineString = string.Join("/", outline.Items.Select(c => c.Id));
-            Assert.AreEqual("b61aa9d1d0024bc4be12d79bf5786e9f/7c51d90394f145d0859810e38a48a41e/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f", outlineString);
+            Assert.AreEqual("c/c1/c2/c3", outlineString);
 
             var allOutlineItems = category.Outlines.SelectMany(o => o.Items.Where(i => i.SeoObjectType != "Catalog")).ToList();
             Assert.IsTrue(allOutlineItems.All(i => i.SeoInfos != null && i.SeoInfos.Any()));
@@ -45,23 +123,22 @@ namespace VirtoCommerce.CatalogModule.Test
         public void SearchCategories_When_VirtualCatalog_Expect_MultipleVirtualOutlines()
         {
             var service = GetCategoryService();
-            var result = service.GetByIds(new[] { "fe4d82e57c20435c8a91b6ac75dc557f" }, CategoryResponseGroup.WithOutlines | CategoryResponseGroup.WithSeo, "25f5ea1b52e54ec1aa903d44cc889324");
+            var result = service.GetByIds(new[] { "c3" }, CategoryResponseGroup.WithOutlines | CategoryResponseGroup.WithSeo, "v");
 
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
 
             var category = result.First();
             Assert.IsNotNull(category);
-            Assert.AreEqual("Summer-Dresses", category.Code);
-            Assert.AreEqual("fe4d82e57c20435c8a91b6ac75dc557f", category.Id);
+            Assert.AreEqual("c3", category.Id);
             Assert.IsNotNull(category.Outlines);
             Assert.AreEqual(4, category.Outlines.Count);
 
             var outlineStrings = category.Outlines.Select(o => string.Join("/", o.Items.Select(c => c.Id))).ToList();
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/7c51d90394f145d0859810e38a48a41e/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/fe4d82e57c20435c8a91b6ac75dc557f"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/fe4d82e57c20435c8a91b6ac75dc557f"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c1/c2/c3"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c2/c3"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c3"));
+            Assert.IsTrue(outlineStrings.Contains("v/c3"));
 
             var allOutlineItems = category.Outlines.SelectMany(o => o.Items.Where(i => i.SeoObjectType != "Catalog")).ToList();
             Assert.IsTrue(allOutlineItems.All(i => i.SeoInfos != null && i.SeoInfos.Any()));
@@ -71,15 +148,14 @@ namespace VirtoCommerce.CatalogModule.Test
         public void SearchProducts_When_PhysicalCatalog_Expect_SinglePhysicalOutline()
         {
             var service = GetItemService();
-            var result = service.GetByIds(new[] { "6b9797660c28496faef5fbfae871d735" }, ItemResponseGroup.Outlines | ItemResponseGroup.Seo, "b61aa9d1d0024bc4be12d79bf5786e9f");
+            var result = service.GetByIds(new[] { "p1" }, ItemResponseGroup.Outlines | ItemResponseGroup.Seo, "c");
 
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
 
             var product = result.First();
             Assert.IsNotNull(product);
-            Assert.AreEqual("355789384", product.Code);
-            Assert.AreEqual("6b9797660c28496faef5fbfae871d735", product.Id);
+            Assert.AreEqual("p1", product.Id);
             Assert.IsNotNull(product.Outlines);
             Assert.AreEqual(1, product.Outlines.Count);
 
@@ -88,7 +164,7 @@ namespace VirtoCommerce.CatalogModule.Test
             Assert.AreEqual(5, outline.Items.Count);
 
             var outlineString = string.Join("/", outline.Items.Select(c => c.Id));
-            Assert.AreEqual("b61aa9d1d0024bc4be12d79bf5786e9f/7c51d90394f145d0859810e38a48a41e/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f/6b9797660c28496faef5fbfae871d735", outlineString);
+            Assert.AreEqual("c/c1/c2/c3/p1", outlineString);
 
             var allOutlineItems = product.Outlines.SelectMany(o => o.Items.Where(i => i.SeoObjectType != "Catalog")).ToList();
             Assert.IsTrue(allOutlineItems.All(i => i.SeoInfos != null && i.SeoInfos.Any()));
@@ -98,25 +174,24 @@ namespace VirtoCommerce.CatalogModule.Test
         public void SearchProducts_When_VirtualCatalog_Expect_MultipleVirtualOutlines()
         {
             var service = GetItemService();
-            var result = service.GetByIds(new[] { "6b9797660c28496faef5fbfae871d735" }, ItemResponseGroup.Outlines | ItemResponseGroup.Seo, "25f5ea1b52e54ec1aa903d44cc889324");
+            var result = service.GetByIds(new[] { "p1" }, ItemResponseGroup.Outlines | ItemResponseGroup.Seo, "v");
 
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
 
             var product = result.First();
             Assert.IsNotNull(product);
-            Assert.AreEqual("355789384", product.Code);
-            Assert.AreEqual("6b9797660c28496faef5fbfae871d735", product.Id);
+            Assert.AreEqual("p1", product.Id);
             Assert.IsNotNull(product.Outlines);
             Assert.AreEqual(6, product.Outlines.Count);
 
             var outlineStrings = product.Outlines.Select(o => string.Join("/", o.Items.Select(c => c.Id))).ToList();
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/7c51d90394f145d0859810e38a48a41e/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f/6b9797660c28496faef5fbfae871d735"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/33505982c1134ff5b10c7a461f10a4cb/fe4d82e57c20435c8a91b6ac75dc557f/6b9797660c28496faef5fbfae871d735"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/fe4d82e57c20435c8a91b6ac75dc557f/6b9797660c28496faef5fbfae871d735"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/fe4d82e57c20435c8a91b6ac75dc557f/6b9797660c28496faef5fbfae871d735"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/87fcc2308e46478192d6a31b7f505580/6b9797660c28496faef5fbfae871d735"));
-            Assert.IsTrue(outlineStrings.Contains("25f5ea1b52e54ec1aa903d44cc889324/6b9797660c28496faef5fbfae871d735"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c1/c2/c3/p1"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c2/c3/p1"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/c3/p1"));
+            Assert.IsTrue(outlineStrings.Contains("v/c3/p1"));
+            Assert.IsTrue(outlineStrings.Contains("v/v1/p1"));
+            Assert.IsTrue(outlineStrings.Contains("v/p1"));
 
             var allOutlineItems = product.Outlines.SelectMany(o => o.Items.Where(i => i.SeoObjectType != "Catalog")).ToList();
             Assert.IsTrue(allOutlineItems.All(i => i.SeoInfos != null && i.SeoInfos.Any()));
@@ -145,14 +220,36 @@ namespace VirtoCommerce.CatalogModule.Test
 
         private static IСommerceRepository GetCommerceRepository()
         {
-            var retVal = new CommerceRepositoryImpl("VirtoCommerce", new EntityPrimaryKeyGeneratorInterceptor(), new AuditableInterceptor(null));
-            return retVal;
+            var mock = new Mock<IСommerceRepository>();
+
+            mock.SetupGet(r => r.SeoUrlKeywords).Returns(_seo.AsQueryable());
+
+            return mock.Object;
         }
 
         private static ICatalogRepository GetCatalogRepository()
         {
-            var retVal = new CatalogRepositoryImpl("VirtoCommerce", new EntityPrimaryKeyGeneratorInterceptor(), new AuditableInterceptor(null));
-            return retVal;
+            var mock = new Mock<ICatalogRepository>();
+
+            mock
+                .Setup(r => r.GetCategoriesByIds(It.IsAny<string[]>(), It.IsAny<CategoryResponseGroup>()))
+                .Returns<string[], CategoryResponseGroup>(GetCategoriesByIds);
+
+            mock
+                .Setup(r => r.GetItemByIds(It.IsAny<string[]>(), It.IsAny<ItemResponseGroup>()))
+                .Returns<string[], ItemResponseGroup>(GetItemsByIds);
+
+            return mock.Object;
+        }
+
+        private static Category[] GetCategoriesByIds(string[] categoryIds, CategoryResponseGroup responseGroup)
+        {
+            return _categories.Where(c => categoryIds.Contains(c.Id)).ToArray();
+        }
+
+        private static Item[] GetItemsByIds(string[] itemIds, ItemResponseGroup responseGroup)
+        {
+            return _items.Where(c => itemIds.Contains(c.Id)).ToArray();
         }
     }
 }
