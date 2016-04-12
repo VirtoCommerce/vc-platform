5a8ee39a4010d03fef75786186136d2c769530ef
eMazeika <em@virtoway.com>
2016-04-12 15:44:01 +0300

    CMS - store widget UI updated

--- PLATFORM/Modules/CMS/VirtoCommerce.Content.Web/Scripts/widgets/storeCMSWidget.js
+++ PLATFORM/Modules/CMS/VirtoCommerce.Content.Web/Scripts/widgets/storeCMSWidget.js
@@ -1,19 +1,23 @@
 ﻿angular.module('virtoCommerce.contentModule')
 .controller('virtoCommerce.contentModule.storeCMSWidgetController', ['$state', '$scope', 'virtoCommerce.contentModule.contentApi', 'platformWebApp.bladeNavigationService', function ($state, $scope, contentApi, bladeNavigationService) {
-	var blade = $scope.widget.blade;
+    var blade = $scope.widget.blade;
 
-	$scope.widget.initialize = function () {
-		$scope.themesCount = '...';
-		return contentApi.query({ contentType: 'themes', storeId: blade.currentEntityId }, function (data) {
-			$scope.themesCount = data.length;
-		}, function (error) {
-		    //bladeNavigationService.setError('Error ' + error.status, $scope.blade);
-		});
-	}
+    function initialize() {
+        $scope.statistics = {
+            activeThemeName: '...',
+            themesCount: '...',
+            pagesCount: '...',
+            blogsCount: '...',
+            listLinksCount: '...'
+        };
+        contentApi.getStatistics({ storeId: blade.currentEntityId }, function (data) {
+            angular.extend($scope.statistics, data);
+        });
+    }
 
-	$scope.openBlade = function () {
-		$state.go('workspace.content', { storeId: blade.currentEntity.id});
-	};
+    $scope.openBlade = function () {
+        $state.go('workspace.content', { storeId: blade.currentEntityId });
+    };
 
-	$scope.widget.initialize();
+    initialize();
 }]);
