c518663055eda37076a56fdb27d72844b37bcec8
artem-dudarev <ad@virtoway.com>
2016-04-21 12:10:25 +0200

    Fixed XML formatter for Web API requests.

--- PLATFORM/VirtoCommerce.Platform.Web/App_Start/WebApiConfig.cs
+++ PLATFORM/VirtoCommerce.Platform.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.OData.Extensions;
@@ -19,30 +20,29 @@ namespace VirtoCommerce.Platform.Web
             config.MapHttpAttributeRoutes();
 
             config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
-            
-            var formatter = config.Formatters.JsonFormatter;
-            formatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            formatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            formatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-            //formatter.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
-            formatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-            formatter.SerializerSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
-            //config.Formatters.JsonFormatter.SerializerSettings.
+
+            var jsonFormatter = config.Formatters.JsonFormatter;
+
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            jsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            jsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            jsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            jsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+
+            jsonFormatter.SerializerSettings.Error += (sender, args) =>
+            {
+                // Expose any JSON serialization exception as HTTP error
+                HttpContext.Current.AddError(args.ErrorContext.Error);
+            };
+
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
             //config.EnableQuerySupport();
             config.AddODataQueryFilter();
-
-            var json = config.Formatters.JsonFormatter;
-            json.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
-            json.SerializerSettings.Formatting = Formatting.Indented;
-            json.SerializerSettings.Error += (sender, args) =>
-            {
-                //Need escalate any JSON serialization exception as HTTP error
-                HttpContext.Current.AddError(args.ErrorContext.Error);
-            };
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
