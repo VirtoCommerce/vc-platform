efad6d67f80227a12dbb7d1f7847425109f93179
Tatarincev <et@virtoway.com>
2016-04-27 10:35:53 +0200

    Fix culture replacement  in  routing

--- STOREFRONT/VirtoCommerce.Storefront/Owin/StorefrontUrlRewriterOwinMiddleware.cs
+++ STOREFRONT/VirtoCommerce.Storefront/Owin/StorefrontUrlRewriterOwinMiddleware.cs
@@ -37,8 +37,8 @@ namespace VirtoCommerce.Storefront.Owin
 
                 //add remaining path part without store and language
                 var requestPath = context.Request.Path.Value;
-                requestPath = Regex.Replace(requestPath, "/" + currentStoreId + "/", "/", RegexOptions.IgnoreCase);
-                requestPath = Regex.Replace(requestPath, "/" + currentCultureName + "/", "/", RegexOptions.IgnoreCase);
+                requestPath = Regex.Replace(requestPath, "/" + currentStoreId + "/?", "/", RegexOptions.IgnoreCase);
+                requestPath = Regex.Replace(requestPath, "/" + currentCultureName + "/?", "/", RegexOptions.IgnoreCase);
                 normalizedPath = normalizedPath.Add(new PathString(requestPath));
 
                 context.Request.Path = normalizedPath;
