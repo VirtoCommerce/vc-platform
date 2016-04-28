d48596c442bfdbd385f8e6146cde2d4c2e4a88a1
Tatarincev <et@virtoway.com>
2016-04-28 12:43:26 +0200

    Fix X-Response-Time multithreaded issue

--- PLATFORM/VirtoCommerce.Platform.Web/App_Start/FilterConfig.cs
+++ PLATFORM/VirtoCommerce.Platform.Web/App_Start/FilterConfig.cs
@@ -5,35 +5,32 @@ using System.Web.Mvc;
 
 namespace VirtoCommerce.Platform.Web
 {
-	public class FilterConfig
-	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
-			filters.Add(new HandleErrorAttribute());
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
             System.Web.Http.GlobalConfiguration.Configuration.Filters.Add(new ResponseTimeHeaderFilter());
         }
-	}
+    }
     /// <summary>
     /// Filter add X-Response-Time header to response contains elapsed response time in milliseconds 
     /// </summary>
     public class ResponseTimeHeaderFilter : System.Web.Http.Filters.ActionFilterAttribute
     {
-        private Stopwatch _timer { get; set; }
-
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            _timer = Stopwatch.StartNew();
-         }
+            actionContext.Request.Properties["X-Response-Time"] = Stopwatch.StartNew();
+        }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
-            if (_timer != null)
+            object timer;
+            if (actionExecutedContext != null && actionExecutedContext.Request != null && actionExecutedContext.Request.Properties.TryGetValue("X-Response-Time", out timer))
             {
-                _timer.Stop();
-                if (actionExecutedContext != null && actionExecutedContext.Response != null && actionExecutedContext.Response.Headers != null)
-                {
-                    actionExecutedContext.Response.Headers.Add("X-Response-Time", _timer.ElapsedMilliseconds.ToString());
-                }
+                var stopWatch = timer as Stopwatch;
+                stopWatch.Stop();
+                actionExecutedContext.Response.Headers.Add("X-Response-Time", stopWatch.ElapsedMilliseconds.ToString());
             }
         }
     }
