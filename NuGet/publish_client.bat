beee61eda73a93242ad49a7441492ee2cec9fe89
artem-dudarev <ad@virtoway.com>
2016-06-01 15:09:02 +0200

    Added VirtoCommerce.Platform.Client

--- NuGet/publish_client.bat
+++ NuGet/publish_client.bat
@@ -1,2 +1,4 @@
-set V=2.10.2
+set V=2.10.4
+nuget push VirtoCommerce.Platform.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.Platform.Client.Security.%V%.nupkg -Source nuget.org -ApiKey %1
+pause
