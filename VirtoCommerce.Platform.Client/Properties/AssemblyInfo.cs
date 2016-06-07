9c1e5bac4a41d42be5d79323176d2702d85ee31a
artem-dudarev <ad@virtoway.com>
2016-06-07 12:10:37 +0200

    Changed assembly version to 2.11

--- VirtoCommerce.Platform.Client/Properties/AssemblyInfo.cs
+++ VirtoCommerce.Platform.Client/Properties/AssemblyInfo.cs
@@ -2,33 +2,8 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("VirtoCommerce.Platform.Client")]
 [assembly: AssemblyDescription("VirtoCommerce.Platform API client generated from a Swagger doc")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VirtoCommerce")]
-[assembly: AssemblyProduct("VirtoCommerce.Platform.Client")]
-[assembly: AssemblyCopyright("Copyright (C) VirtoCommerce 2011-2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.10.4.0")]
-[assembly: AssemblyFileVersion("2.10.4.0")]
 [assembly: CLSCompliant(true)]
