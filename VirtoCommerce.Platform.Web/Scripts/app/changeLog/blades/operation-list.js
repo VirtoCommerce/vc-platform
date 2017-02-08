4b5c9b089c135b10fc39a2cb4f5f6fe913187a34
Tatarincev <et@virtoway.com>
2017-02-08 12:23:59 +0200

    Switch operations-log list to ui-grid

--- VirtoCommerce.Platform.Web/Scripts/app/changeLog/blades/operation-list.js
+++ VirtoCommerce.Platform.Web/Scripts/app/changeLog/blades/operation-list.js
@@ -2,4 +2,8 @@
 .controller('platformWebApp.changeLog.operationListController', ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
     
     $scope.blade.isLoading = false;
+    // ui-grid
+    $scope.setGridOptions = function (gridOptions) {
+        $scope.gridOptions = gridOptions;
+    };
 }]);
