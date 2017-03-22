e3c6b1ee5b23128b6d05932cd7798fd651524f94
Aleksandr Vishnyakov <av@virtoway.com>
2017-03-22 23:27:31 +0500

    Removed autorun gulp 'watch' action at project open

--- VirtoCommerce.Platform.Web/gulpfile.js
+++ VirtoCommerce.Platform.Web/gulpfile.js
@@ -1,4 +1,3 @@
-/// <binding ProjectOpened='watch' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
