2e586712cb403baaee8eb630c9e6c4ee54f43f87
Aleksandr Vishnyakov <av@virtoway.com>
2017-05-26 10:33:39 +0500

    Quickfix for date filter

--- VirtoCommerce.Platform.Web/Scripts/common/directives/date.js
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/date.js
@@ -2,6 +2,7 @@
 .config(['$provide', function ($provide) {
     $provide.decorator('dateFilter', ['$delegate', 'moment', function ($delegate, moment) {
         var filter = function (date, format, timeZone) {
+            date = moment.isMoment(date) ? date : moment(date);
             if (moment.isMoment(date)) {
                 timeZone = date.format("ZZ");
                 date = date.toDate();
