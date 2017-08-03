bc1d6938902b97fab9a784554bdfe523d58a577a
Aleksandr Vishnyakov <av@virtoway.com>
2017-08-03 20:47:51 +0500

    Fixed bug with Hmac api accounts key generation; Few bugfixes and refactoring

--- VirtoCommerce.Platform.Core/Security/ISecurityService.cs
+++ VirtoCommerce.Platform.Core/Security/ISecurityService.cs
@@ -12,6 +12,7 @@ namespace VirtoCommerce.Platform.Core.Security
         Task<SecurityResult> UpdateAsync(ApplicationUserExtended user);
         Task DeleteAsync(string[] names);
         ApiAccount GenerateNewApiAccount(ApiAccountType type);
+        ApiAccount GenerateNewApiKey(ApiAccount account);
         Task<string> GeneratePasswordResetTokenAsync(string userId);
         Task<SecurityResult> ChangePasswordAsync(string name, string oldPassword, string newPassword);
         Task<SecurityResult> ResetPasswordAsync(string name, string newPassword);
