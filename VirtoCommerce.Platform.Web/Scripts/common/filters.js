32a4ac8abcd04f045375dd7b9b1f30adc8340b49
Egis <em@virtoway.com>
2017-09-13 11:25:04 +0300

    dynamic property ui refactored (#1095)
    * fix(moduleWizard):fixed popup size
    
    * Dynamic property UI updated #1058

--- VirtoCommerce.Platform.Web/Scripts/common/filters.js
+++ VirtoCommerce.Platform.Web/Scripts/common/filters.js
@@ -3,24 +3,39 @@
         return function (input, trueValue, falseValue) {
             return input ? trueValue : falseValue;
         };
-    }).filter('slice', function ()
-    {
-        return function (arr, start, end)
-        {
+    })
+    .filter('slice', function () {
+        return function (arr, start, end) {
             return (arr || []).slice(start, end);
         };
-    }).filter('readablesize', function() {
+    })
+    .filter('readablesize', function () {
         return function (input) {
             if (!input)
                 return null;
-             
-            var sizes = [ "Bytes", "KB", "MB", "GB", "TB" ];
+
+            var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
             var order = 0;
-            while (input >= 1024 && order + 1 < sizes.length)
-            {
+            while (input >= 1024 && order + 1 < sizes.length) {
                 order++;
                 input = input / 1024;
             }
             return Math.round(input) + ' ' + sizes[order];
         };
-    });;
+    })
+    // translate the given properties in the input array
+    .filter('translateArray', ['$translate', function ($translate) {
+        return function (inputArray, propertiesList) {
+            _.each(inputArray, function (inputItem) {
+                _.each(propertiesList, function (prop) {
+                    if (angular.isString(inputItem[prop])) {
+                        var translateKey = inputItem[prop].toLowerCase();
+                        var result = $translate.instant(translateKey);
+                        if (result !== translateKey) inputItem[prop] = result;
+                    }
+                });
+            });
+            return inputArray;
+        }
+    }])
+;
