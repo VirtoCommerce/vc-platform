51062e99773082e97decb336d004ab491e473b2d
Nikolai Prohorov <prohorov.nn@gmail.com>
2018-03-05 13:09:00 +0200

    added stream upload file to UploadAsset

---
+++ VirtoCommerce.Platform.Web/Swagger/UploadFileAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.Platform.Web.Swagger
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class UploadFileAttribute : Attribute
+    {
+        public UploadFileAttribute(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+        public string Type { get; set; } = "text";
+
+        public bool Required { get; set; } = false;
+    }
+}
