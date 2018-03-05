360c525690ddf9afd2a389c79b8d3dd58c187b8c
Eugeny Tatarincev <et@virtoway.com>
2018-03-05 14:41:33 +0200

    Minor refactoring

--- VirtoCommerce.Platform.Web/Swagger/FileUploadOperationFilter.cs
+++ VirtoCommerce.Platform.Web/Swagger/FileUploadOperationFilter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Http.Description;
 using Swashbuckle.Swagger;
 
--- VirtoCommerce.Platform.Web/Swagger/UploadFileAttribute.cs
+++ VirtoCommerce.Platform.Web/Swagger/UploadFileAttribute.cs
@@ -1,24 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace VirtoCommerce.Platform.Web.Swagger
 {
+    /// <summary> 
+    /// Use this attribute for controllers methods for allow to upload file via Swagger 
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public sealed class UploadFileAttribute : Attribute
     {
-        public UploadFileAttribute()
-        {
-            Name = "file";
-            Description = "Upload file";
-            Type = "file";
-            Required = true;
-        }
-
-        public string Name { get; }
-        public string Description { get; }
-        public string Type { get; }
-        public bool Required { get; } 
+        /// <summary>
+        /// The parameter name in the resulting swagger doc 
+        /// </summary>
+        public string Name { get; set; } = "uploadedFile";
+        /// <summary>
+        /// The parameter description in the resulting swagger doc 
+        /// </summary>
+        public string Description { get; set;  } = "Upload File";
+        /// <summary>
+        /// Parameter type (only File value supported)
+        /// </summary>
+        public string Type { get; set; } = "Type";
+        /// <summary>
+        /// Set true for required parameter
+        /// </summary>
+        public bool Required { get; set; } = true; 
     }
 }
