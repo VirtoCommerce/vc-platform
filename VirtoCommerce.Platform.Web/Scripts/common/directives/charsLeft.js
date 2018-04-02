9ade0f4d8e82acb114a09d83d8804dd8d65f70c1
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-04-02 21:31:54 +0300

    charsLeft directory added

---
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/charsLeft.js
@@ -0,0 +1,17 @@
+﻿angular.module('platformWebApp')
+.directive('charsLeft', function () {
+    return {
+        restrict: 'A',
+        compile: function compile() {
+            return {
+                post: function postLink(scope, iElement, iAttrs) {
+                    iElement.bind('keyup', function () {
+                        scope.$apply(function () {
+                            scope.charsLeftToMax = scope.maxlength - iElement.val().length;
+                        });
+                    });
+                }
+            }
+        }
+    }
+})
