dbe1c12efc9e70cbf77225fe8df767bf9bb35816
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-04-16 20:13:38 +0300

    rework directive charsLeft

---
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/charsLeftTo255.js
@@ -0,0 +1,26 @@
+﻿angular.module('platformWebApp')
+.directive('charsLeftTo255', function () {
+    return {
+        restrict: 'A',
+        compile: function compile() {
+            return {
+                post: function postLink(scope, iElement, iAttrs) {
+                    iElement.bind('keyup', function () {
+                        scope.$apply(function () {
+                            if (scope.maxlength255) {
+                                var diff = 255 - iElement.val().length;
+                                scope.charsLeftTo255 = diff >= 0 ? "Maximum 255 characters(" + diff + " remaining)" :
+                                    diff < 0 ? "Maximum 255 characters(" + (-diff) + " to many)" : "";
+                                var elem = iElement.parent().siblings("span");
+                                if (diff >= 0)
+                                    elem.attr("style", "color: #999;");
+                                if (diff < 0)
+                                    elem.attr("style", "color: #e51400;");
+                            }
+                        });
+                    });
+                }
+            }
+        }
+    }
+})
