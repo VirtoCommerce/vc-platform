f8d68f3cfd613282747448e27c20d417dd5af30d
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-04-19 22:19:38 +0300

    New variant of directive

--- VirtoCommerce.Platform.Web/Scripts/common/directives/charsLeftToMax.js
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/charsLeftToMax.js
@@ -1,5 +1,5 @@
 ﻿angular.module('platformWebApp')
-.directive('charsLeftTo255', function () {
+.directive('charsLeftToMax', function () {
     return {
         restrict: 'A',
         compile: function compile() {
@@ -7,10 +7,20 @@
                 post: function postLink(scope, iElement, iAttrs) {
                     iElement.bind('keyup', function () {
                         scope.$apply(function () {
-                            if (scope.maxlength255) {
+                            if (scope.maxlength255 && iElement.hasClass("title")) {
                                 var diff = scope.maxlength255 - iElement.val().length;
                                 scope.charsLeftTo255 = diff >= 0 ? "Maximum " + scope.maxlength255 + " characters(" + diff + " remaining)" :
                                     diff < 0 ? "Maximum " + scope.maxlength255 + " characters(" + (-diff) + " to many)" : "";
+                                var elem = iElement.parent().siblings("span");
+                                if (diff >= 0)
+                                    elem.attr("style", "color: #999;");
+                                if (diff < 0)
+                                    elem.attr("style", "color: #e51400;");
+                            }
+                            if (scope.maxlength1024 && iElement.hasClass("description")) {
+                                var diff = scope.maxlength1024 - iElement.val().length;
+                                scope.charsLeftTo1024 = diff >= 0 ? "Maximum " + scope.maxlength1024 + " characters(" + diff + " remaining)" :
+                                    diff < 0 ? "Maximum " + scope.maxlength1024 + " characters(" + (-diff) + " to many)" : "";
                                 var elem = iElement.parent().siblings("span");
                                 if (diff >= 0)
                                     elem.attr("style", "color: #999;");
