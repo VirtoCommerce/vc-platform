f99d42cc8b935a1e94e6bd33f631ea409e33dce3
Eugeny Tatarincev <et@virtoway.com>
2018-06-23 08:54:18 +0200

    Added the new API api/platform/changelog/search which  serve object changes log widget

---
+++ VirtoCommerce.Platform.Web/Controllers/Api/ChangeLogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+using VirtoCommerce.Platform.Core.ChangeLog;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.Platform.Web.Controllers.Api
+{
+    [RoutePrefix("api/platform/changelog")]
+    public class ChangeLogController : ApiController
+    {
+        private readonly IChangeLogService _changeLog;
+        public ChangeLogController(IChangeLogService changeLog)
+        {
+            _changeLog = changeLog;
+        }
+
+        [HttpPost]
+        [Route("search")]
+        [ResponseType(typeof(OperationLog[]))]
+        public IHttpActionResult SearchObjectChangeLogHistory([FromBody]TenantIdentity tenant)
+        {
+            var result = _changeLog.FindObjectChangeHistory(tenant.TenantId, tenant.TenantType).ToArray();
+            return Ok(result);
+        }
+    }
+}
