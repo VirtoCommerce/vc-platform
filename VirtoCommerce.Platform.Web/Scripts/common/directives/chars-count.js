2b2a8da125ea12b403a7076a74aa47cb7ed1d1cb
Tigran Petrosyan <tp@virtoway.com>
2018-07-05 16:56:36 +0200

    fixed and added chars counter

---
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/chars-count.js
@@ -0,0 +1,28 @@
+﻿angular.module('platformWebApp')
+    .directive('vaCharsCount', function () {
+        return {
+            template: `<div class="chars-counter">
+                           <span>{{vaField == undefined ? '0' : vaField.length}}</span> / <span>{{vaMaxlength}}</span> 
+                       </div>`,
+            scope: {
+                vaField: "=",
+                vaMaxlength: "@"
+            },
+            link: function (scope, element) {
+                scope.$parent.vaMaxlength = scope.vaMaxlength;
+
+                scope.$watch('vaField', function (newValue, oldValue) {
+                    scope.$parent.vaField = scope.vaField;
+
+                    if (newValue != undefined) {
+                        if (newValue.length > scope.vaMaxlength) {
+                            element.addClass('invalid');
+                        } else {
+                            element.removeClass('invalid');
+                        }
+                    }
+                });
+            },
+        }
+    }
+);
