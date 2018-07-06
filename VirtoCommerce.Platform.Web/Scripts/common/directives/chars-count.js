467a0d9e333f9c58599647e50c32910ea9db6ad9
Tigran Petrosyan <tp@virtoway.com>
2018-07-06 09:14:55 +0200

    Corrections after review

--- VirtoCommerce.Platform.Web/Scripts/common/directives/chars-count.js
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/chars-count.js
@@ -2,24 +2,18 @@
     .directive('vaCharsCount', function () {
         return {
             template: `<div class="chars-counter">
-                           <span>{{vaField == undefined ? '0' : vaField.length}}</span> / <span>{{vaMaxlength}}</span> 
+                            <span>{{!!vaField ? vaField.length : 0}}</span> / <span>{{vaMaxlength}}</span> 
                        </div>`,
             scope: {
-                vaField: "=",
+                vaField: "@",
                 vaMaxlength: "@"
             },
             link: function (scope, element) {
-                scope.$parent.vaMaxlength = scope.vaMaxlength;
-
-                scope.$watch('vaField', function (newValue, oldValue) {
-                    scope.$parent.vaField = scope.vaField;
-
-                    if (newValue != undefined) {
-                        if (newValue.length > scope.vaMaxlength) {
-                            element.addClass('invalid');
-                        } else {
-                            element.removeClass('invalid');
-                        }
+                scope.$watch('vaField', function () {
+                    if (scope.vaField.length > scope.vaMaxlength) {
+                        element.addClass('invalid');
+                    } else {
+                        element.removeClass('invalid');
                     }
                 });
             },
