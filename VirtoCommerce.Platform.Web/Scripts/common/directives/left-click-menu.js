cb9556c1337d9bf044ca0d8d92a025a607dc4e1a
Tigran Petrosyan <tp@virtoway.com>
2018-07-11 09:10:17 +0200

    Fixed context-menu position

--- VirtoCommerce.Platform.Web/Scripts/common/directives/left-click-menu.js
+++ VirtoCommerce.Platform.Web/Scripts/common/directives/left-click-menu.js
@@ -33,21 +33,24 @@ angular
                         left = left - (totalWidth - docWidth);
                     }
 
-                    let scrollbarHeight = 20;
-                    if (totalHeight > docHeight) {
-                        top = top - (totalHeight - docHeight - elementHeight + scrollbarHeight);
-                    }
-
-                    let maxChildTopOffset = 0;
+                    let maxTopOffset = 0;
+                    // get max bottom position of children
                     $.map(menuElement.children(), function (item) {
                         let height = $(item).find('ul').height();
                         let topPosition = $(item).position().top;
                         let bottomPosition = height + topPosition;
-                        if (bottomPosition > maxChildTopOffset)
-                            maxChildTopOffset = bottomPosition;
+                        if (bottomPosition > maxTopOffset)
+                            maxTopOffset = bottomPosition;
                     });
 
-                    top -= maxChildTopOffset;
+                    // get max bottom position of all context menu
+                    if (elementHeight > maxTopOffset)
+                        maxTopOffset = elementHeight;
+
+                    let scrollbarHeight = 20;
+                    if (event.pageY + maxTopOffset > docHeight) {
+                        top = top - (event.pageY - docHeight + maxTopOffset + scrollbarHeight);
+                    }
 
                     menuElement.css('top', top + 'px');
                     menuElement.css('left', left + 'px');
@@ -73,7 +76,6 @@ angular
                             document.getElementById($attrs.target)
                         );
                         ContextMenuService.element = event.target;
-                        //console.log('set', ContextMenuService.element);
 
                         event.preventDefault();
                         event.stopPropagation();
@@ -87,7 +89,6 @@ angular
                 });
 
                 function handleKeyUpEvent(event) {
-                    //console.log('keyup');
                     if (!$scope.disabled() && opened && event.keyCode === 27) {
                         $scope.$apply(function () {
                             close(ContextMenuService.menuElement);
@@ -113,7 +114,6 @@ angular
                 $document.bind('contextmenu', handleClickEvent);
 
                 $scope.$on('$destroy', function () {
-                    //console.log('destroy');
                     $document.unbind('keyup', handleKeyUpEvent);
                     $document.unbind('click', handleClickEvent);
                     $document.unbind('contextmenu', handleClickEvent);
