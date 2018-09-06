d41d8f1447c3b4d6b468ffadb3b47b57dc5e19a1
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-09-06 12:34:54 +0700

    Password reset blade: increased width, changed separate error flags to error list to simplify view code.

--- VirtoCommerce.Platform.Web/Scripts/app/security/blades/account-resetPassword.js
+++ VirtoCommerce.Platform.Web/Scripts/app/security/blades/account-resetPassword.js
@@ -8,11 +8,7 @@ angular.module('platformWebApp')
             forcePasswordChange: true,
             passwordIsValid: true,
             minPasswordLength: 0,
-            passwordViolatesMinLength: false,
-            passwordMustContainLowerCaseLetters: false,
-            passwordMustContainUpperCaseLetters: false,
-            passwordMustContainDigits: false,
-            passwordMustContainSpecialCharacters: false,
+            errors: []
         };
         
         blade.isLoading = false;
@@ -21,13 +17,34 @@ angular.module('platformWebApp')
     $scope.handlePasswordChange = function() {
         accounts.validatePassword(JSON.stringify(blade.currentEntity.newPassword),
             function(response) {
-                blade.currentEntity.passwordIsValid = response.passwordIsValid;
-                blade.currentEntity.minPasswordLength = response.minPasswordLength;
-                blade.currentEntity.passwordViolatesMinLength = response.passwordViolatesMinLength;
-                blade.currentEntity.passwordMustContainUpperCaseLetters = response.passwordMustHaveUpperCaseLetters;
-                blade.currentEntity.passwordMustContainLowerCaseLetters = response.passwordMustHaveLowerCaseLetters;
-                blade.currentEntity.passwordMustContainDigits = response.passwordMustHaveDigits;
-                blade.currentEntity.passwordMustContainSpecialCharacters = response.passwordMustHaveSpecialCharacters;
+                blade.currentEntity.errors = [];
+
+                if (response.passwordIsValid) {
+                    blade.currentEntity.passwordIsValid = true;
+                } else {
+                    blade.currentEntity.passwordIsValid = false;
+                    blade.currentEntity.minPasswordLength = response.minPasswordLength;
+
+                    if (response.passwordViolatesMinLength) {
+                        blade.currentEntity.errors.push("platform.blades.account-resetPassword.validations.password-too-short");
+                    }
+
+                    if (response.passwordMustHaveUpperCaseLetters) {
+                        blade.currentEntity.errors.push("platform.blades.account-resetPassword.validations.password-must-contain-uppercase-letters");
+                    }
+
+                    if (response.passwordMustHaveLowerCaseLetters) {
+                        blade.currentEntity.errors.push("platform.blades.account-resetPassword.validations.password-must-contain-lowercase-letters");
+                    }
+
+                    if (response.passwordMustHaveDigits) {
+                        blade.currentEntity.errors.push("platform.blades.account-resetPassword.validations.password-must-contain-digits");
+                    }
+
+                    if (response.passwordMustHaveSpecialCharacters) {
+                        blade.currentEntity.errors.push("platform.blades.account-resetPassword.validations.password-must-contain-special-characters");
+                    }
+                }
             });
     }
 
