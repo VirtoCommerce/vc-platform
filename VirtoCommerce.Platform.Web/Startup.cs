927b4467a0135f1bab8c09991fd2d39606f8a880
Andrey Kozlov <yecli.github@gmail.com>
2018-12-10 17:34:39 +0500

    Changed default icon path (#1493)

--- VirtoCommerce.Platform.Web/Startup.cs
+++ VirtoCommerce.Platform.Web/Startup.cs
@@ -624,7 +624,7 @@ namespace VirtoCommerce.Platform.Web
                                                "  \"title\": \"Virto Commerce\",\n" +
                                                "  \"logo\": \"Content/themes/main/images/logo.png\",\n" +
                                                "  \"contrast_logo\": \"Content/themes/main/images/contrast-logo.png\"\n" +
-                                               "  \"favicon\": \"~/favicon.ico\"\n" +
+                                               "  \"favicon\": \"favicon.ico\"\n" +
                                                "}"
                             }
                         }
