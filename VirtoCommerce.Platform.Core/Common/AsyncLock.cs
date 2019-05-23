76718cc24e9aed9a9b6ac5d80a88fe694597cdbc
Eugeny Tatarincev <et@virtoway.com>
2019-05-23 14:33:00 +0200

    Switched the AsyncLock static type  to use AsyncLock from Nito.AsyncEx library

--- VirtoCommerce.Platform.Core/Common/AsyncLock.cs
+++ VirtoCommerce.Platform.Core/Common/AsyncLock.cs
@@ -1,99 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace VirtoCommerce.Platform.Core.Common
 {
-    // http://sanjeev.dwivedi.net/?p=292
-    // http://blogs.msdn.com/b/pfxteam/archive/2012/02/12/10266983.aspx
-    public class AsyncSemaphore
-    {
-        private readonly static Task s_completed = Task.FromResult(true);
-        private readonly Queue<TaskCompletionSource<bool>> m_waiters = new Queue<TaskCompletionSource<bool>>();
-        private int m_currentCount;
-
-        public AsyncSemaphore(int initialCount)
-        {
-            if (initialCount < 0) throw new ArgumentOutOfRangeException("initialCount");
-            m_currentCount = initialCount;
-        }
-
-        public Task WaitAsync()
-        {
-            lock (m_waiters)
-            {
-                if (m_currentCount > 0)
-                {
-                    --m_currentCount;
-                    return s_completed;
-                }
-                else
-                {
-                    var waiter = new TaskCompletionSource<bool>();
-                    m_waiters.Enqueue(waiter);
-                    return waiter.Task;
-                }
-            }
-        }
-
-        public void Release()
-        {
-            TaskCompletionSource<bool> toRelease = null;
-            lock (m_waiters)
-            {
-                if (m_waiters.Count > 0)
-                    toRelease = m_waiters.Dequeue();
-                else
-                    ++m_currentCount;
-            }
-            if (toRelease != null)
-                toRelease.SetResult(true);
-        }
-    }
-
-    // http://blogs.msdn.com/b/pfxteam/archive/2012/02/12/10266988.aspx
     public class AsyncLock
     {
-        private readonly AsyncSemaphore m_semaphore;
-        private readonly Task<Releaser> m_releaser;
         private static ConcurrentDictionary<string, AsyncLock> _lockMap = new ConcurrentDictionary<string, AsyncLock>();
+        private readonly Nito.AsyncEx.AsyncLock _asyncLock;
 
-        public AsyncLock()
+        private AsyncLock(Nito.AsyncEx.AsyncLock asyncLock)
         {
-            m_semaphore = new AsyncSemaphore(1);
-            m_releaser = Task.FromResult(new Releaser(this));
+            _asyncLock = asyncLock;
         }
-
         public static AsyncLock GetLockByKey(string key)
         {
-            return _lockMap.GetOrAdd(key, (x) => new AsyncLock());
+            return _lockMap.GetOrAdd(key, (x) => new AsyncLock(new Nito.AsyncEx.AsyncLock()));
         }
 
-        public Task<Releaser> LockAsync()
+        public async Task<Releaser> LockAsync()
         {
-            var wait = m_semaphore.WaitAsync();
-            return wait.IsCompleted ?
-                m_releaser :
-                wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
-                    this, CancellationToken.None,
-                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+            return new Releaser(await _asyncLock.LockAsync());
         }
 
         public struct Releaser : IDisposable
         {
-            private readonly AsyncLock m_toRelease;
+            private readonly IDisposable _guardDisposable;
 
-            internal Releaser(AsyncLock toRelease) { m_toRelease = toRelease; }
+            internal Releaser(IDisposable disposable)
+            {
+                _guardDisposable = disposable;
+            }
 
             public void Dispose()
             {
-                if (m_toRelease != null)
-                    m_toRelease.m_semaphore.Release();
+                if (_guardDisposable != null)
+                {
+                    _guardDisposable.Dispose();
+                }
+                GC.SuppressFinalize(this);
             }
         }
     }
+
 }
+
