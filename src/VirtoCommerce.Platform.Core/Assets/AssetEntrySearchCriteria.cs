59b4f6cd42d6967703f8fd324a8e2953a1ee9ed6
Eugeny Tatarincev <et@virtoway.com>
2019-10-24 17:05:36 +0200

    backward compatibility fix

--- src/VirtoCommerce.Platform.Core/Assets/AssetEntrySearchCriteria.cs
+++ src/VirtoCommerce.Platform.Core/Assets/AssetEntrySearchCriteria.cs
@@ -4,10 +4,6 @@ namespace VirtoCommerce.Platform.Core.Assets
 {
     public class AssetEntrySearchCriteria : SearchCriteriaBase
     {
-        /// <summary>
-        /// Phrase to search in Name and RelativeUrl
-        /// </summary>
-        public string SearchPhrase { get; set; }
 
         public TenantIdentity[] Tenants { get; set; }
 
