b19c89487dfd45c6791f231ef7a6a8e188b308be
trueboroda <superiorboroda@gmail.com>
2019-12-04 11:03:52 +0300

    remove empty lines

--- VirtoCommerce.Platform.Web/Model/Diagnostics/SystemInfo.cs
+++ VirtoCommerce.Platform.Web/Model/Diagnostics/SystemInfo.cs
@@ -5,9 +5,7 @@ namespace VirtoCommerce.Platform.Web.Model.Diagnostics
     public class SystemInfo
     {
         public PlatformInfo Platform { get; set; }
-
         public License License { get; set; }
-
         public InstalledModuleInfo[] InstalledModules { get; set; }
     }
 }
