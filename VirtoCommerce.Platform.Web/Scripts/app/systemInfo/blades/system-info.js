00f64411f5d0b884d68b1d736695f695ce6b605c
trueboroda <superiorboroda@gmail.com>
2019-12-05 14:10:23 +0300

    copy plain text. change button titles

--- VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
+++ VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
@@ -3,22 +3,47 @@ angular.module('platformWebApp')
         function ($scope, validators, dialogService, contentApi, $timeout, bladeNavigationService, $http, diagnostics) {
             var blade = $scope.blade;
 
-            function stringify(data) {
+            function convertToText(data) {
                 return JSON.stringify(data, null, "\t");
             }
 
+            // convert platform info to user-friendly text presentation
+            function convertToPlainText(data) {
+                var platformInfo = 'Platform Version\r\n' + data.platformVersion;
+                
+                var licenseInfo;
+                if (!data.license) {
+                    licenseInfo = "Community license";
+                } else {
+                    licenseInfo = data.license.type + "license\r\n"
+                                + data.license.customerName
+                                + 'Expiration Date: ' + data.license.expirationDate;
+                }
+
+                var modulesInfo = "Installed modules\r\n";
+                for (var i = 0; i < data.installedModules.length; i++) {
+                    modulesInfo += data.installedModules[i].id + ' ' + data.installedModules[i].version +'\r\n';
+                }
+
+                var result = platformInfo + '\r\n\r\n'
+                           + licenseInfo  + '\r\n\r\n'
+                           + modulesInfo;
+                return result;                               
+            }
+
             blade.initializeBlade = function () {
-                diagnostics.getSystemInfo({}, function (results) {
+                diagnostics.getSystemInfo({}, function (data) {
                     blade.isLoading = false;
                     blade.title = 'platform.blades.system-info.title';
-                    blade.currentEntity = results;
-                    blade.content = stringify(results);
+                    blade.currentEntity = data;
+                    blade.jsonTextInfo = convertToText(data);
+                    blade.plainTextInfo = convertToPlainText(data);
                 });
             };           
 
             $scope.downloadInfo = function () {
                 var a = document.createElement("a");
-                var file = new Blob([blade.content], { type: 'application/json' });
+                var file = new Blob([blade.jsonTextInfo], { type: 'application/json' });
                 a.href = URL.createObjectURL(file);
                 a.download = 'vc-platform-info.json';
                 a.click();
@@ -27,7 +52,7 @@ angular.module('platformWebApp')
             $scope.copyToClipboard = function () {
                 var textarea = document.createElement("textarea");
                 document.body.appendChild(textarea);
-                textarea.value = blade.content;
+                textarea.value = blade.plainTextInfo;
                 textarea.select();
                 document.execCommand("copy");
                 document.body.removeChild(textarea);
@@ -35,14 +60,14 @@ angular.module('platformWebApp')
 
             blade.toolbarCommands = [
                 {
-                    name: "Download Info", icon: 'fa fa-download',
+                    name: "Download manifest", icon: 'fa fa-download',
                     executeMethod: $scope.downloadInfo,
                     canExecuteMethod: function () {
                         return true;
                     }
                 },
                 {
-                    name: "Copy Info", icon: 'fa fa-copy',
+                    name: "Copy", icon: 'fa fa-copy',
                     executeMethod: $scope.copyToClipboard,
                     canExecuteMethod: function () {
                         return true;
