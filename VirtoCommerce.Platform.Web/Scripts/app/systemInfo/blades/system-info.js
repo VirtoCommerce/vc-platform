6e722ba8dcafc9cc5cbb435ac64f13cd28f78abf
trueboroda <superiorboroda@gmail.com>
2019-12-05 21:50:54 +0300

    use for the copy innerText of div with platform info. like user self copy style

--- VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
+++ VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
@@ -1,43 +1,14 @@
 angular.module('platformWebApp')
-    .controller('platformWebApp.systemInfoController', ['$scope', 'platformWebApp.validators', 'platformWebApp.dialogService', 'virtoCommerce.contentModule.contentApi', '$timeout', 'platformWebApp.bladeNavigationService', '$http', 'platformWebApp.diagnostics',
-        function ($scope, validators, dialogService, contentApi, $timeout, bladeNavigationService, $http, diagnostics) {
-            var blade = $scope.blade;
-
-            function convertToText(data) {
-                return JSON.stringify(data, null, "\t");
-            }
-
-            // convert platform info to user-friendly text presentation
-            function convertToPlainText(data) {
-                var platformInfo = 'Platform Version\r\n' + data.platformVersion;
-                
-                var licenseInfo;
-                if (!data.license) {
-                    licenseInfo = "Community license";
-                } else {
-                    licenseInfo = data.license.type + "license\r\n"
-                                + data.license.customerName
-                                + 'Expiration Date: ' + data.license.expirationDate;
-                }
-
-                var modulesInfo = "Installed modules\r\n";
-                for (var i = 0; i < data.installedModules.length; i++) {
-                    modulesInfo += data.installedModules[i].id + ' ' + data.installedModules[i].version +'\r\n';
-                }
-
-                var result = platformInfo + '\r\n\r\n'
-                           + licenseInfo  + '\r\n\r\n'
-                           + modulesInfo;
-                return result;                               
-            }
+    .controller('platformWebApp.systemInfoController', ['$scope', 'platformWebApp.diagnostics',
+        function ($scope, diagnostics) {
+            var blade = $scope.blade;                   
 
             blade.initializeBlade = function () {
                 diagnostics.getSystemInfo({}, function (data) {
                     blade.isLoading = false;
                     blade.title = 'platform.blades.system-info.title';
                     blade.currentEntity = data;
-                    blade.jsonTextInfo = convertToText(data);
-                    blade.plainTextInfo = convertToPlainText(data);
+                    blade.jsonTextInfo = JSON.stringify(data, null, "\t");                    
                 });
             };           
 
@@ -50,10 +21,12 @@ angular.module('platformWebApp')
             };
 
             $scope.copyToClipboard = function () {
+                var platformInfoElement = document.getElementById('platform-system-info');
+                var infoText = platformInfoElement.innerText;
                 var textarea = document.createElement("textarea");
                 document.body.appendChild(textarea);
-                textarea.value = blade.plainTextInfo;
-                textarea.select();
+                textarea.value = infoText;
+                textarea.select();                
                 document.execCommand("copy");
                 document.body.removeChild(textarea);
             };          
