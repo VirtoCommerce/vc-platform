49335f4c358d89054a988c19c7b0e53e9ef89046
Anton <superiorboroda@gmail.com>
2019-12-06 13:52:55 +0300

    dowinload manifest.v2 copy plain text. change button titles (#1739)
    * copy plain text. change button titles
    
    * use for the copy innerText of div with platform info. like user self copy style
    
    * update license info UI

--- VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
+++ VirtoCommerce.Platform.Web/Scripts/app/systemInfo/blades/system-info.js
@@ -1,48 +1,46 @@
 angular.module('platformWebApp')
-    .controller('platformWebApp.systemInfoController', ['$scope', 'platformWebApp.validators', 'platformWebApp.dialogService', 'virtoCommerce.contentModule.contentApi', '$timeout', 'platformWebApp.bladeNavigationService', '$http', 'platformWebApp.diagnostics',
-        function ($scope, validators, dialogService, contentApi, $timeout, bladeNavigationService, $http, diagnostics) {
-            var blade = $scope.blade;
-
-            function stringify(data) {
-                return JSON.stringify(data, null, "\t");
-            }
+    .controller('platformWebApp.systemInfoController', ['$scope', 'platformWebApp.diagnostics',
+        function ($scope, diagnostics) {
+            var blade = $scope.blade;                   
 
             blade.initializeBlade = function () {
-                diagnostics.getSystemInfo({}, function (results) {
+                diagnostics.getSystemInfo({}, function (data) {
                     blade.isLoading = false;
                     blade.title = 'platform.blades.system-info.title';
-                    blade.currentEntity = results;
-                    blade.content = stringify(results);
+                    blade.currentEntity = data;
+                    blade.jsonTextInfo = JSON.stringify(data, null, "\t");                    
                 });
             };           
 
             $scope.downloadInfo = function () {
                 var a = document.createElement("a");
-                var file = new Blob([blade.content], { type: 'application/json' });
+                var file = new Blob([blade.jsonTextInfo], { type: 'application/json' });
                 a.href = URL.createObjectURL(file);
                 a.download = 'vc-platform-info.json';
                 a.click();
             };
 
             $scope.copyToClipboard = function () {
+                var platformInfoElement = document.getElementById('platform-system-info');
+                var infoText = platformInfoElement.innerText;
                 var textarea = document.createElement("textarea");
                 document.body.appendChild(textarea);
-                textarea.value = blade.content;
-                textarea.select();
+                textarea.value = infoText;
+                textarea.select();                
                 document.execCommand("copy");
                 document.body.removeChild(textarea);
             };          
 
             blade.toolbarCommands = [
                 {
-                    name: "Download Info", icon: 'fa fa-download',
+                    name: "Download manifest", icon: 'fa fa-download',
                     executeMethod: $scope.downloadInfo,
                     canExecuteMethod: function () {
                         return true;
                     }
                 },
                 {
-                    name: "Copy Info", icon: 'fa fa-copy',
+                    name: "Copy", icon: 'fa fa-copy',
                     executeMethod: $scope.copyToClipboard,
                     canExecuteMethod: function () {
                         return true;
