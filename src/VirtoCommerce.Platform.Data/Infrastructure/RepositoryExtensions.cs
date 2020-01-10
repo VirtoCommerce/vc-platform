4cfcff42d8cd9d6dd18886c551a5f5f943b76fb9
Eugeny Tatarincev <et@virtoway.com>
2020-01-10 11:31:37 +0200

    Disable QueryTrackingBehavior.NoTracking policy. This leads to stop work of breaking-queries dependency loading

--- src/VirtoCommerce.Platform.Data/Infrastructure/RepositoryExtensions.cs
+++ src/VirtoCommerce.Platform.Data/Infrastructure/RepositoryExtensions.cs
@@ -9,21 +9,21 @@ namespace VirtoCommerce.Platform.Data.Infrastructure
     {
         public static void DisableChangesTracking(this IRepository repository)
         {
-            //http://stackoverflow.com/questions/29106477/nullreferenceexception-in-entity-framework-from-trygetcachedrelatedend
             if (repository.UnitOfWork is DbContextUnitOfWork dbContextUoW)
             {
                 dbContextUoW.DbContext.ChangeTracker.AutoDetectChangesEnabled = false;
-                dbContextUoW.DbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+                //We can't use QueryTrackingBehavior.NoTracking  because this will cause stop breaking-queries working that we use load dependencies in many places.
+                // dbContextUoW.DbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             }
         }
 
         /// <summary>
-        /// This extension is allow to avoid this breaking changes is introduced in EF Core 3.0 that leads to throw
+        /// This extension is allow to get around breaking changes is introduced in EF Core 3.0 that leads to throw
         /// Database operation expected to affect 1 row(s) but actually affected 0 row(s) exception when trying to add the new children entities with manually set keys
         /// https://docs.microsoft.com/en-us/ef/core/what-is-new/ef-core-3.0/breaking-changes#detectchanges-honors-store-generated-key-values
         /// </summary>
         /// <param name="repository"></param>
-        public static void AllowUseManuallySetKeysForNewChildren(this IRepository repository, IEntity entity)
+        public static void TrackModifiedAsAddedForNewChildEntities(this IRepository repository, IEntity entity)
         {
             if (repository.UnitOfWork is DbContextUnitOfWork dbContextUoW)
             {
