6a1fc71d4fc9f7a355c201fee4b64f17c8099c06
Andrei Kostyrin <kostyrin2@gmail.com>
2020-01-29 21:20:09 +0200

    #vp-961 set LibraryPath by default

--- src/VirtoCommerce.Platform.Core/PlatformOptions.cs
+++ src/VirtoCommerce.Platform.Core/PlatformOptions.cs
@@ -25,7 +25,7 @@ namespace VirtoCommerce.Platform.Core
         //Default path to store export files 
         public string DefaultExportFolder = "App_Data/Export/";
         public string DefaultExportFileName = "exported_data.zip";
-        public string LibraryPath { get; set; }
+        public string LibraryPath { get; set; } = "lib";
     }
 
 }
