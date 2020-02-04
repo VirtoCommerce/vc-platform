55d003b82e1f36086b89a2a297acb47c75d6c24c
Andrei Kolchanov <akak1977@gmail.com>
2020-02-04 23:06:24 +0500

    Accordingly to commit https://github.com/VirtoCommerce/vc-platform/commit/1971333a9db8bc152c16f515383f0c440fef6923. Copy localization resource files to related subfolders

--- src/VirtoCommerce.Platform.Core/Modularity/LocalStorageModuleCatalogOptions.cs
+++ src/VirtoCommerce.Platform.Core/Modularity/LocalStorageModuleCatalogOptions.cs
@@ -6,7 +6,8 @@ namespace VirtoCommerce.Platform.Core.Modularity
         //Can use absolute or relative path
         public string DiscoveryPath { get; set; }
         public string ProbingPath { get; set; } = "App_Data/Modules";
-        public string[] AssemblyFileExtensions { get; set; } = new[] { ".dll", ".exe", };
+        public string[] LocalizationFileExtensions { get; set; } = new[] { "resources.dll"};
+        public string[] AssemblyFileExtensions { get; set; } = new[] { ".dll", ".exe"};
         public string[] AssemblyServiceFileExtensions { get; set; } = new[] { ".pdb", ".xml", ".deps.json", ".runtimeconfig.json", ".runtimeconfig.dev.json", ".dep" };
     }
 }
