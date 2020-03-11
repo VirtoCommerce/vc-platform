59f55f7fbbb6d755f517da743d5c5b1990fb74e8
Andrei Kostyrin <kostyrin2@gmail.com>
2020-03-11 19:44:26 +0200

    #vp-961 remove WkhtmltopdfArguments

--- tests/VirtoCommerce.Platform.Tests/IntegrationTests/ProcessHelperIntegrationTests.cs
+++ tests/VirtoCommerce.Platform.Tests/IntegrationTests/ProcessHelperIntegrationTests.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using VirtoCommerce.Platform.Core;
 using VirtoCommerce.Platform.Core.ProcessSettings;
 using VirtoCommerce.Platform.Data.Helpers;
