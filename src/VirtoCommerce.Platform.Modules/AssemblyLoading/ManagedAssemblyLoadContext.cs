8146e02644207fa8538a55db6b7382b0310f8383
Andrei Kostyrin <kostyrin2@gmail.com>
2020-03-18 15:14:58 +0200

    Revert "#VP-1658 work with native libs (#1796)"
    This reverts commit 53da9acfa27de50228235c6fdbc43ffc51825e00.
    
    # Conflicts:
    #	src/VirtoCommerce.Platform.Modules/LoadContextAssemblyResolver.cs

--- src/VirtoCommerce.Platform.Modules/AssemblyLoading/ManagedAssemblyLoadContext.cs
+++ src/VirtoCommerce.Platform.Modules/AssemblyLoading/ManagedAssemblyLoadContext.cs
@@ -1,21 +1,10 @@
-using System;
 using System.Collections.Generic;
-using System.Runtime.Loader;
 
 namespace VirtoCommerce.Platform.Modules.AssemblyLoading
 {
-    public class ManagedAssemblyLoadContext : AssemblyLoadContext
+    public class ManagedAssemblyLoadContext
     {
         public string BasePath { get; set; }
         public IEnumerable<string> AdditionalProdingPath { get; set; }
-
-        public IntPtr LoadUnmanagedLibrary(string absolutePath)
-        {
-            return LoadUnmanagedDll(absolutePath);
-        }
-        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
-        {
-            return LoadUnmanagedDllFromPath(unmanagedDllName);
-        }
     }
 }
