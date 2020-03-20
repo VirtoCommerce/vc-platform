4981fcf25d364811664e65d3bceaa20be14a13f1
Aleksandr Vishniakov <av@virtoway.com>
2020-03-20 09:42:46 +0200

    Fixed problem with security:api_call permission checking

--- src/VirtoCommerce.Platform.Core/Security/ClaimsPrincipalExtensions.cs
+++ src/VirtoCommerce.Platform.Core/Security/ClaimsPrincipalExtensions.cs
@@ -27,8 +27,9 @@ namespace VirtoCommerce.Platform.Core.Security
 
             if (!result)
             {
-                result = !principal.IsInRole(PlatformConstants.Security.SystemRoles.Customer) && principal.HasClaim(PlatformConstants.Security.Claims.PermissionClaimType, PlatformConstants.Security.Permissions.SecurityCallApi);
-                if (result)
+                result = principal.IsInRole(PlatformConstants.Security.SystemRoles.Customer) ||
+                         permissionName == PlatformConstants.Security.Permissions.SecurityCallApi;
+                if (!result)
                 {
                     result = principal.HasClaim(PlatformConstants.Security.Claims.PermissionClaimType, permissionName);
                 }
