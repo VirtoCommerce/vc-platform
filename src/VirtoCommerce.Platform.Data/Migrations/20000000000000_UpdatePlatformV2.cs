542c8b8854ca48877e5e1dc4f8ac86e186f57643
Andrei Kostyrin <kostyrin2@gmail.com>
2020-05-26 16:15:15 +0200

    #VP-2526 remove redundant script

--- src/VirtoCommerce.Platform.Data/Migrations/20000000000000_UpdatePlatformV2.cs
+++ src/VirtoCommerce.Platform.Data/Migrations/20000000000000_UpdatePlatformV2.cs
@@ -16,23 +16,6 @@ namespace VirtoCommerce.Platform.Data.Migrations
                         ALTER TABLE [PlatformSetting] DROP COLUMN [IsMultiValue]
                         ALTER TABLE [PlatformSetting] DROP COLUMN [IsLocaleDependant]
                         UPDATE [PlatformOperationLog] SET [ObjectType] = REPLACE([ObjectType], 'Entity', '')
-
-                        BEGIN
-                            ALTER TABLE [HangFire].[State] DROP CONSTRAINT [FK_HangFire_State_Job];
-                            ALTER TABLE [HangFire].[JobParameter] DROP CONSTRAINT [FK_HangFire_JobParameter_Job];
-                            DROP TABLE [HangFire].[Schema];
-                            DROP TABLE [HangFire].[Job];
-                            DROP TABLE [HangFire].[State];
-                            DROP TABLE [HangFire].[JobParameter];
-                            DROP TABLE [HangFire].[JobQueue];
-                            DROP TABLE [HangFire].[Server];
-                            DROP TABLE [HangFire].[List];
-                            DROP TABLE [HangFire].[Set];
-                            DROP TABLE [HangFire].[Counter];
-                            DROP TABLE [HangFire].[Hash];
-                            DROP TABLE [HangFire].[AggregatedCounter];
-                            DROP SCHEMA [HangFire];
-                        END
                     END");
 
             migrationBuilder.Sql(@"IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'HangFire'))
