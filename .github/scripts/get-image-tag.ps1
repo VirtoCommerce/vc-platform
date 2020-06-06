5969dc6e672e6f3a35c19ffe9fe2bcbe42d2f66d
Leo <leon@entechsolutions.com>
2020-06-06 12:30:53 +0400

    VP-2713: Start using external action

--- .github/scripts/get-image-tag.ps1
+++ .github/scripts/get-image-tag.ps1
@@ -1,4 +1,9 @@
-[Xml]$buildPropsXml = Get-Content -Path 'Directory.Build.Props'
+param (
+  [Parameter(Position=0, Mandatory=$true)]
+  [string]$SHA
+)
+
+Xml]$buildPropsXml = Get-Content -Path 'Directory.Build.Props'
       
 $versionPrefixNode = $buildPropsXml.SelectNodes('Project/PropertyGroup/VersionPrefix') | Select-Object -First 1
 $versionSuffixNode = $buildPropsXml.SelectNodes('Project/PropertyGroup/VersionSuffix') | Select-Object -First 1
@@ -6,10 +11,8 @@ $versionPrefix = "$($versionPrefixNode.InnerText.Trim())"
 if (-not ([string]::IsNullOrEmpty($versionSuffixNode.InnerText))) 
 { 
   $versionSuffix = "-$($versionSuffixNode.InnerText.Trim())" 
-}  
-
-$sha = (git rev-parse HHEAD).Substring(0, 8)
+}
   
-$version = "$($versionPrefix)$versionSuffix-$($sha)"
+$version = "$($versionPrefix)$versionSuffix-$($SHA.Substring(0, 8))"
 
 Write-Output "::set-output name=tag::$version"
