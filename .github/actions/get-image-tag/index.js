3ca7943b125ab7196f3c93622e5e9b313d52cab2
Leo <leon@entechsolutions.com>
2020-06-06 10:02:14 +0400

    VP-2713: Fix action logic

--- .github/actions/get-image-tag/index.js
+++ .github/actions/get-image-tag/index.js
@@ -1,24 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const xml2js = require('xml2js');
 
 String.isNullOrEmpty = function(value) {
     return !(typeof value === "string" && value.length > 0);
 }
 
-fs.readFile('Directory.Build.Props', function (err, doc) {
+fs.readFile('Directory.Build.Props', function (err, data) {
     if (!err) {
-        let prefixes = doc.SelectNodes('Project/PropertyGroup/VersionPrefix');
-        if (prefixes.length === 0) {
-        core.setFailed("Version prefix cannot be read");
-        return;
-        }
+        var json = JSON.parse(xml2js.toJson(data, {reversible: false}));
 
-        let suffixes = doc.SelectNodes('Project/PropertyGroup/VersionSuffix');
+        var prefix = json["Project"]["PropertyGroup"]["VersionPrefix"].trim();
+        var suffix = json["Project"]["PropertyGroup"]["VersionSuffix"].trim();
 
-        let version = prefixes[0].innerText + 
-            (suffixes.length > 0 && suffixes[0].innerText.trim() != '' ? '-' + suffixes[0].innerText.trim() : '') + 
-            github.sha.substring(0, 8)
+        let version = prefix + (suffix != '' ? '-' + suffixes[0].innerText : '') + github.sha.substring(0, 8)
 
         core.setOutput("tag", version);
 
