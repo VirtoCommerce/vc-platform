bbe17de5e4b29fbb86013f51028cd5c7e41e3e41
Leo <leon@entechsolutions.com>
2020-06-06 10:13:16 +0400

    VP-2713: Fix logic

--- .github/actions/get-image-tag/index.js
+++ .github/actions/get-image-tag/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const xml2js = require('xml2js');
-const parser = new xml2js.Parser()
+const parser = new xml2js.Parser();
 
 String.isNullOrEmpty = function(value) {
     return !(typeof value === "string" && value.length > 0);
@@ -10,15 +10,18 @@ String.isNullOrEmpty = function(value) {
 
 fs.readFile('Directory.Build.Props', function (err, data) {
     if (!err) {
-        var json = JSON.parse(parser.toJson(data, {reversible: false}));
 
-        var prefix = json["Project"]["PropertyGroup"]["VersionPrefix"].trim();
-        var suffix = json["Project"]["PropertyGroup"]["VersionSuffix"].trim();
-
-        let version = prefix + (suffix != '' ? '-' + suffixes[0].innerText : '') + github.sha.substring(0, 8)
-
-        core.setOutput("tag", version);
-
-        console.log(`Version tag is: ${version}`);
+        parser.parseString(data, function (err, json) {
+            if (!err) {
+                var prefix = json["Project"]["PropertyGroup"]["VersionPrefix"].trim();
+                var suffix = json["Project"]["PropertyGroup"]["VersionSuffix"].trim();
+        
+                let version = prefix + (suffix != '' ? '-' + suffix : '') + github.sha.substring(0, 8)
+        
+                core.setOutput("tag", version);
+        
+                console.log(`Version tag is: ${version}`);
+            }
+        });
     }
 });
