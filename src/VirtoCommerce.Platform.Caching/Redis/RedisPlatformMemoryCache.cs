bb18c0f2c78cce9ff3b129650322f5b47dd6ee79
Aleksandr Vishniakov <av@virtoway.com>
2020-06-23 10:33:11 +0200

    Added explanation for disabling cache in comments

--- src/VirtoCommerce.Platform.Caching/Redis/RedisPlatformMemoryCache.cs
+++ src/VirtoCommerce.Platform.Caching/Redis/RedisPlatformMemoryCache.cs
@@ -54,7 +54,7 @@ namespace VirtoCommerce.Platform.Redis
             });
         }
 
-        protected virtual void OnConnectionFailed(object? sender, ConnectionFailedEventArgs e)
+        protected virtual void OnConnectionFailed(object sender, ConnectionFailedEventArgs e)
         {
             _log.LogError($"Redis disconnected from instance { _cacheId }. Endpoint is {e.EndPoint}, failure type is {e.FailureType}");
             _telemetryClient.TrackException(e.Exception);
@@ -66,11 +66,15 @@ namespace VirtoCommerce.Platform.Redis
                 {"failureType", e.FailureType.ToString()}
             });
 
+            // If we have no connection to Redis, we can't invalidate cache on another platform instances,
+            // so the better idea is to disable cache at all for data consistence
             CacheEnabled = false;
+            // We should fully clear cache because we don't know
+            // what's changed until platform found Redis is unavailable
             GlobalCacheRegion.ExpireRegion();
         }
 
-        protected virtual void OnConnectionRestored(object? sender, ConnectionFailedEventArgs e)
+        protected virtual void OnConnectionRestored(object sender, ConnectionFailedEventArgs e)
         {
             _log.LogInformation($"Redis connection restored for instance { _cacheId }");
             _telemetryClient.TrackEvent("RedisConnectionRestored", new Dictionary<string, string>
@@ -79,7 +83,11 @@ namespace VirtoCommerce.Platform.Redis
                 {"cacheId", _cacheId}
             });
 
+            // Return cache to the same state as it was initially.
+            // Don't set directly true because it may be disabled in app settings
             CacheEnabled = _cachingOptions.CacheEnabled;
+            // We should fully clear cache because we don't know
+            // what's changed in another instances since Redis became unavailable
             GlobalCacheRegion.ExpireRegion();
         }
         
