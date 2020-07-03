0240fca88f29642f3c5d47bb525d83c35b5ea26a
Aleksandr Vishniakov <av@virtoway.com>
2020-07-03 10:51:13 +0200

    Fix using

--- src/VirtoCommerce.Platform.Caching/ServiceCollectionExtensions.cs
+++ src/VirtoCommerce.Platform.Caching/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 using VirtoCommerce.Platform.Core.Caching;
 using VirtoCommerce.Platform.Redis;
 
