1eead02fc5ec1f20fd5cef306dbf0882708f05a8
OlegoO <zhukoo@gmail.com>
2020-07-17 14:41:25 +0200

    Fix ExecuteCmd

--- deploy.cmd
+++ deploy.cmd
@@ -3,3 +3,12 @@ dotnet restore "%DEPLOYMENT_SOURCE%\VirtoCommerce.Platform.sln"
 dotnet publish "%DEPLOYMENT_SOURCE%\src\VirtoCommerce.Platform.Web\VirtoCommerce.Platform.Web.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
 
 call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+
+:: Execute command routine that will echo out when error 
+:ExecuteCmd
+setlocal
+set _CMD_=%*
+echo command=%_CMD_%
+call %_CMD_%
+if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
+exit /b %ERRORLEVEL% 
