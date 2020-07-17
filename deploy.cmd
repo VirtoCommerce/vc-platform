6212dca8e081170bc7be106ca081c8c4f3f9b236
OlegoO <zhukoo@gmail.com>
2020-07-17 12:37:55 +0200

    Fix deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,21 +1,17 @@
-@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
-
-:: ----------------------
-:: KUDU Deployment Script
-:: Version: 0.1.11
-:: ----------------------
-
-:: Prerequisites
-dotnet tool update --global VirtoCommerce.GlobalTool
-
-:: Build
-:: -------------
-%USERPROFILE%\.dotnet\tools\vc-build publish
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo Virto Commerce Platform deployment.
 
-:: Post deployment stub
+:: 1. Restore nuget packages
+call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\VirtoCommerce.Platform.sln"
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 2. Build and publish
+call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\src\VirtoCommerce.Platform.Web\VirtoCommerce.Platform.Web.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 3. KuduSync
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+IF !ERRORLEVEL! NEQ 0 goto error
