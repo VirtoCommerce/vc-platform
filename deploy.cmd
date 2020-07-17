759158506be2a1eaf35d69c74d68a56c01eaf042
OlegoO <zhukoo@gmail.com>
2020-07-17 14:48:04 +0200

    Add npm

--- deploy.cmd
+++ deploy.cmd
@@ -2,6 +2,10 @@ dotnet restore "%DEPLOYMENT_SOURCE%\VirtoCommerce.Platform.sln"
 
 dotnet publish "%DEPLOYMENT_SOURCE%\src\VirtoCommerce.Platform.Web\VirtoCommerce.Platform.Web.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
 
+npm ci
+
+npm run webpack:build
+
 call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
 
 goto end
