51daa9a582e3d39ca879b0c8802d3d8336f44f7e
Andrei Kostyrin <kostyrin2@gmail.com>
2020-08-19 21:02:53 +0200

    #VP-4303 migration with DbContext

---
+++ tests/VirtoCommerce.Platform.Tests/UnitTests/MigrationNameUnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace VirtoCommerce.Platform.Tests.UnitTests
+{
+    public class MigrationNameUnitTests
+    {
+        [Fact]
+        public void GetUpdateV2MigrationName_GetNameForVirtoCommerce()
+        {
+
+        }
+    }
+}
