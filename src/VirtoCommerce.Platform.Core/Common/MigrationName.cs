05dea6327346f0d5fb8ac2751beba183b2489c84
Andrei Kostyrin <kostyrin2@gmail.com>
2020-08-20 11:34:01 +0200

    #VP-4303 obsolete method GetUpdateV2MigrationNameByOwnerName

--- src/VirtoCommerce.Platform.Core/Common/MigrationName.cs
+++ src/VirtoCommerce.Platform.Core/Common/MigrationName.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace VirtoCommerce.Platform.Core.Common
 {
     public static class MigrationName
@@ -5,6 +7,12 @@ namespace VirtoCommerce.Platform.Core.Common
         public static string GetUpdateV2MigrationName(string moduleName)
         {
             return $"20000000000000_Update{moduleName.Replace(".", "").Replace("VirtoCommerce", "")}V2";
+        }
+
+        [Obsolete("use GetUpdateV2MigrationName")]
+        public static string GetUpdateV2MigrationNameByOwnerName(string moduleName, string ownerName)
+        {
+            return GetUpdateV2MigrationName(moduleName);
         }
     }
 }
