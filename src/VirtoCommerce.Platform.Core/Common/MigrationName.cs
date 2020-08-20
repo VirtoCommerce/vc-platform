f3450c56327a1f7a740c18f0e59d50e47e0e4736
Andrei Kostyrin <kostyrin2@gmail.com>
2020-08-20 11:20:22 +0200

    #VP-4303 Throw exception in migration if there is no table

--- src/VirtoCommerce.Platform.Core/Common/MigrationName.cs
+++ src/VirtoCommerce.Platform.Core/Common/MigrationName.cs
@@ -4,12 +4,7 @@ namespace VirtoCommerce.Platform.Core.Common
     {
         public static string GetUpdateV2MigrationName(string moduleName)
         {
-            return GetUpdateV2MigrationNameByOwnerName(moduleName, "VirtoCommerce");
-        }
-
-        public static string GetUpdateV2MigrationNameByOwnerName(string moduleName, string ownerName)
-        {
-            return $"20000000000000_Update{moduleName.Replace($"{ownerName}.", "")}V2";
+            return $"20000000000000_Update{moduleName.Replace(".", "").Replace("VirtoCommerce", "")}V2";
         }
     }
 }
---
+++ src/VirtoCommerce.Platform.Data/Repositories/ContextAwareMigrationsAssembly.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+using System;
+using System.Reflection;
+
+namespace VirtoCommerce.Platform.Data.Repositories
+{
+    public class ContextAwareMigrationsAssembly : MigrationsAssembly
+    {
+        private readonly DbContext context;
+
+        public ContextAwareMigrationsAssembly(
+            ICurrentDbContext currentContext,
+            IDbContextOptions options,
+            IMigrationsIdGenerator idGenerator,
+            IDiagnosticsLogger<DbLoggerCategory.Migrations> logger) : base(currentContext, options, idGenerator, logger)
+        {
+            context = currentContext.Context;
+        }
+
+        /// <summary>
+        /// Modified from http://weblogs.thinktecture.com/pawel/2018/06/entity-framework-core-changing-db-migration-schema-at-runtime.html
+        /// </summary>
+        /// <param name="migrationClass"></param>
+        /// <param name="activeProvider"></param>
+        /// <returns></returns>
+        public override Migration CreateMigration(TypeInfo migrationClass, string activeProvider)
+        {
+            var hasCtorWithDbContext = migrationClass
+                    .GetConstructor(new[] { typeof(DbContext) }) != null;
+
+            if (hasCtorWithDbContext)
+            {
+                var instance = (Migration)Activator.CreateInstance(migrationClass.AsType(), context);
+                instance.ActiveProvider = activeProvider;
+                return instance;
+            }
+
+            return base.CreateMigration(migrationClass, activeProvider);
+        }
+    }
+}
