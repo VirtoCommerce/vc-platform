5cc387a482ae246954a4b4268ce00a7a0da82d43
Andrei Kolchanov <akak1977@gmail.com>
2020-09-08 16:08:52 +0500

    VP-4670: Remove dbo schema prefix in migration scripts

--- src/VirtoCommerce.Platform.Security/Migrations/20000000000000_UpdateSecurityV2.cs
+++ src/VirtoCommerce.Platform.Security/Migrations/20000000000000_UpdateSecurityV2.cs
@@ -15,7 +15,7 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    ALTER TABLE [AspNetRoles] ADD [ConcurrencyStamp] nvarchar(max);
 		                    ALTER TABLE [AspNetRoles] ADD [Description] nvarchar(max);
 		                    EXEC ('DROP INDEX AspNetRoles.RoleNameIndex')
-		                    EXEC ('CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex] ON [dbo].[AspNetRoles] ([NormalizedName]) WHERE ([NormalizedName] IS NOT NULL)')
+		                    EXEC ('CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE ([NormalizedName] IS NOT NULL)')
 	                    END
 	                    
 	                    BEGIN
@@ -31,11 +31,11 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    ALTER TABLE [AspNetUsers] ADD [UserType] nvarchar(64);
 		                    ALTER TABLE [AspNetUsers] ADD [PasswordExpired] bit NOT NULL Default(0);
 
-                            EXEC ('UPDATE [dbo].[AspNetUsers] SET StoreId = pa.StoreId, MemberId = pa.MemberId, IsAdministrator = pa.IsAdministrator, UserType = pa.UserType, PasswordExpired = pa.PasswordExpired FROM PlatformAccount pa WHERE [dbo].[AspNetUsers].Id = pa.Id')
+                            EXEC ('UPDATE [AspNetUsers] SET StoreId = pa.StoreId, MemberId = pa.MemberId, IsAdministrator = pa.IsAdministrator, UserType = pa.UserType, PasswordExpired = pa.PasswordExpired FROM PlatformAccount pa WHERE [AspNetUsers].Id = pa.Id')
 	                    END
 
 	                    BEGIN
-		                    CREATE TABLE [dbo].[AspNetRoleClaims](
+		                    CREATE TABLE [AspNetRoleClaims](
 			                    [Id] [int] IDENTITY(1,1) NOT NULL,
 			                    [RoleId] [nvarchar](128) NOT NULL,
 			                    [ClaimType] [nvarchar](max) NULL,
@@ -48,16 +48,16 @@ namespace VirtoCommerce.Platform.Security.Migrations
 	                    END
 
 	                    BEGIN
-		                    ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
-			                    REFERENCES [dbo].[AspNetRoles] ([Id])
+		                    ALTER TABLE [AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+			                    REFERENCES [AspNetRoles] ([Id])
 			                    ON DELETE CASCADE
-		                    ALTER TABLE [dbo].[AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+		                    ALTER TABLE [AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
 
-		                    CREATE NONCLUSTERED INDEX [IX_AspNetRoleClaims_RoleId] ON [dbo].[AspNetRoleClaims] ([RoleId])
+		                    CREATE NONCLUSTERED INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId])
 	                    END
 
                         BEGIN
-                            CREATE TABLE [dbo].[AspNetUserTokens](
+                            CREATE TABLE [AspNetUserTokens](
 	                            [UserId] [nvarchar](128) NOT NULL,
 	                            [LoginProvider] [nvarchar](450) NOT NULL,
 	                            [Name] [nvarchar](450) NOT NULL,
@@ -70,15 +70,15 @@ namespace VirtoCommerce.Platform.Security.Migrations
                             )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
                             ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-                            EXEC (N'ALTER TABLE [dbo].[AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
-                                REFERENCES [dbo].[AspNetUsers] ([Id])
+                            EXEC (N'ALTER TABLE [AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
+                                REFERENCES [AspNetUsers] ([Id])
                                 ON DELETE CASCADE')
 
-                            EXEC (N'ALTER TABLE [dbo].[AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]')
+                            EXEC (N'ALTER TABLE [AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]')
                         END
 
 	                    BEGIN
-		                    CREATE TABLE [dbo].[OpenIddictApplications](
+		                    CREATE TABLE [OpenIddictApplications](
 			                    [ClientId] [nvarchar](450) NOT NULL,
 			                    [ClientSecret] [nvarchar](max) NULL,
 			                    [ConcurrencyToken] [nvarchar](max) NULL,
@@ -96,7 +96,7 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 		                    ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 	                    
-		                    CREATE TABLE [dbo].[OpenIddictAuthorizations](
+		                    CREATE TABLE [OpenIddictAuthorizations](
 			                    [ApplicationId] [nvarchar](450) NULL,
 			                    [ConcurrencyToken] [nvarchar](max) NULL,
 			                    [Id] [nvarchar](450) NOT NULL,
@@ -111,12 +111,12 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 		                    ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-		                    ALTER TABLE [dbo].[OpenIddictAuthorizations]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
-		                    REFERENCES [dbo].[OpenIddictApplications] ([Id])
+		                    ALTER TABLE [OpenIddictAuthorizations]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
+		                    REFERENCES [OpenIddictApplications] ([Id])
 
-		                    ALTER TABLE [dbo].[OpenIddictAuthorizations] CHECK CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId]
+		                    ALTER TABLE [OpenIddictAuthorizations] CHECK CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId]
 
-		                    CREATE TABLE [dbo].[OpenIddictScopes](
+		                    CREATE TABLE [OpenIddictScopes](
 			                    [ConcurrencyToken] [nvarchar](max) NULL,
 			                    [Description] [nvarchar](max) NULL,
 			                    [DisplayName] [nvarchar](max) NULL,
@@ -130,7 +130,7 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 		                    ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-		                    CREATE TABLE [dbo].[OpenIddictTokens](
+		                    CREATE TABLE [OpenIddictTokens](
 			                    [ApplicationId] [nvarchar](450) NULL,
 			                    [AuthorizationId] [nvarchar](450) NULL,
 			                    [CreationDate] [datetimeoffset](7) NULL,
@@ -149,26 +149,26 @@ namespace VirtoCommerce.Platform.Security.Migrations
 		                    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 		                    ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-		                    ALTER TABLE [dbo].[OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
-		                    REFERENCES [dbo].[OpenIddictApplications] ([Id])
+		                    ALTER TABLE [OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
+		                    REFERENCES [OpenIddictApplications] ([Id])
 
-		                    ALTER TABLE [dbo].[OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId]
+		                    ALTER TABLE [OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId]
 
-		                    ALTER TABLE [dbo].[OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId] FOREIGN KEY([AuthorizationId])
-		                    REFERENCES [dbo].[OpenIddictAuthorizations] ([Id])
+		                    ALTER TABLE [OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId] FOREIGN KEY([AuthorizationId])
+		                    REFERENCES [OpenIddictAuthorizations] ([Id])
 
-		                    ALTER TABLE [dbo].[OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId]
+		                    ALTER TABLE [OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId]
 	                    END
 
 	                    BEGIN
-		                    INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20180411094757_InitialSecurity', '2.2.3-servicing-35854')
+		                    INSERT INTO [__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20180411094757_InitialSecurity', '2.2.3-servicing-35854')
 	                    END
 
 	                    BEGIN
 		                    EXEC (N'UPDATE [AspNetUsers] SET NormalizedUserName = UPPER([UserName]), [NormalizedEmail] = UPPER([Email])')
 		                    EXEC (N'INSERT INTO [AspNetRoles] ([Id],[Name],[NormalizedName],[Description]) SELECT Id, [Name], UPPER([NAME]), [Description] FROM PlatformRole')
-                            EXEC (N'INSERT INTO [dbo].[AspNetUserRoles] ([UserId],[RoleId]) SELECT AccountId, RoleId FROM PlatformRoleAssignment')
-                            EXEC (N'INSERT INTO [dbo].[AspNetRoleClaims] ([RoleId],[ClaimType],[ClaimValue]) SELECT [RoleId], ''permission'', [PermissionId] FROM [PlatformRolePermission]')
+                            EXEC (N'INSERT INTO [AspNetUserRoles] ([UserId],[RoleId]) SELECT AccountId, RoleId FROM PlatformRoleAssignment')
+                            EXEC (N'INSERT INTO [AspNetRoleClaims] ([RoleId],[ClaimType],[ClaimValue]) SELECT [RoleId], ''permission'', [PermissionId] FROM [PlatformRolePermission]')
 	                    END
 
                     END");
