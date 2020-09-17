a693ea6622b77bdef3e5c35af3592be30fbb4709
Andrei Kolchanov <akak1977@users.noreply.github.com>
2020-09-17 20:29:04 +0500

    Revert "VP-4971: Add mainApp as UI app entry point (#2032)"
    This reverts commit be7bf33e5a0e8e83e7493f3af2ed1e0393809532.

--- src/VirtoCommerce.Platform.Web/wwwroot/js/app/app.js
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/app/app.js
@@ -1,6 +1,4 @@
-angular.module('mainApp', AppDependencies);
-
-angular.module('platformWebApp', []).controller('platformWebApp.appCtrl', ['$rootScope', '$scope', '$window', 'platformWebApp.mainMenuService',
+angular.module('platformWebApp', AppDependencies).controller('platformWebApp.appCtrl', ['$rootScope', '$scope', '$window', 'platformWebApp.mainMenuService',
     'platformWebApp.i18n', '$timeout', 'platformWebApp.modules', '$state', 'platformWebApp.bladeNavigationService', 'platformWebApp.userProfile', 'platformWebApp.settings',
     function ($rootScope, $scope, $window, mainMenuService,
         i18n, $timeout, modules, $state, bladeNavigationService, userProfile, settings) {
@@ -120,7 +118,7 @@ angular.module('platformWebApp', []).controller('platformWebApp.appCtrl', ['$roo
         var httpErrorInterceptor = {};
 
         httpErrorInterceptor.request = function (config) {
-        // Need to pass localization request despite on the auth state
+	    // Need to pass localization request despite on the auth state
             if (config.url == 'api/platform/localization') {
                 return config;
             }
