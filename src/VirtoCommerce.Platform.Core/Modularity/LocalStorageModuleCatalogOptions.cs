492cc6c2e4cb42d14c716f5a15fb2b159bdbb308
Egidijus Mazeika <em@virtoway.com>
2020-09-25 10:31:53 +0300

    VP-2262: set lowercase names for app_data, modules, certificates folders

--- src/VirtoCommerce.Platform.Core/Modularity/LocalStorageModuleCatalogOptions.cs
+++ src/VirtoCommerce.Platform.Core/Modularity/LocalStorageModuleCatalogOptions.cs
@@ -5,9 +5,9 @@ namespace VirtoCommerce.Platform.Core.Modularity
         //Folder where platform will discover installed modules
         //Can use absolute or relative path
         public string DiscoveryPath { get; set; }
-        public string ProbingPath { get; set; } = "App_Data/Modules";
-        public string[] LocalizationFileExtensions { get; set; } = new[] { "resources.dll"};
-        public string[] AssemblyFileExtensions { get; set; } = new[] { ".dll", ".exe"};
+        public string ProbingPath { get; set; } = "app_data/modules";
+        public string[] LocalizationFileExtensions { get; set; } = new[] { "resources.dll" };
+        public string[] AssemblyFileExtensions { get; set; } = new[] { ".dll", ".exe" };
         public string[] AssemblyServiceFileExtensions { get; set; } = new[] { ".pdb", ".xml", ".deps.json", ".runtimeconfig.json", ".runtimeconfig.dev.json", ".dep" };
     }
 }
