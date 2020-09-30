5104dec638b3f539727b41e9da5d1387a4cecc64
Filip Kuksov <vectorfield4@gmail.com>
2020-09-30 10:27:22 +0200

    feat: allow saving UserName for all jobs

--- src/VirtoCommerce.Platform.Web/Middleware/Hangfire/HangfireUserContextMiddleware.cs
+++ src/VirtoCommerce.Platform.Web/Middleware/Hangfire/HangfireUserContextMiddleware.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Linq;
 using System.Threading;
 using Hangfire.Client;
 using Hangfire.Server;
 using Microsoft.AspNetCore.Http;
-using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Security;
 
 namespace VirtoCommerce.Platform.Web.Middleware.Hangfire
@@ -18,34 +15,12 @@ namespace VirtoCommerce.Platform.Web.Middleware.Hangfire
         /// </summary>
         public HangfireUserContextMiddleware(IHttpContextAccessor contextAccessor) => _contextAccessor = contextAccessor;
 
-        /// <summary>
-        /// Check if argument contains information about log operation
-        /// </summary>
-        private readonly Func<object, bool> AuditableArgument = arg => arg is OperationLog || arg is OperationLog[];
-
-        /// <summary>
-        /// Check if job contains operation log
-        /// </summary>
-        /// <param name="context">Context before job is created</param>
-        /// <returns>True or false</returns>
-        private bool IsAuditableOperation(CreatingContext context) => context.Job.Args.Any(AuditableArgument);
-
-        /// <summary>
-        /// Check if job contains operation log
-        /// </summary>
-        /// <param name="context">Context when job is started</param>
-        /// <returns>True or false</returns>
-        private bool IsAuditableOperation(PerformingContext context) => context.BackgroundJob.Job.Args.Any(AuditableArgument);
-
         private string ContextUserName => _contextAccessor?.HttpContext?.User?.Identity?.Name;
 
         #region IClientFilter Members
 
         public void OnCreating(CreatingContext filterContext)
         {
-            if (!IsAuditableOperation(filterContext))
-                return;
-
             if (ContextUserName is null)
                 return;
 
@@ -63,9 +38,6 @@ namespace VirtoCommerce.Platform.Web.Middleware.Hangfire
 
         public void OnPerforming(PerformingContext filterContext)
         {
-            if (!IsAuditableOperation(filterContext))
-                return;
-
             var userName = filterContext.GetJobParameter<string>(HttpContextUserResolver.USER_NAME_THREAD_SLOT_NAME);
 
             Thread.SetData(Thread.GetNamedDataSlot(HttpContextUserResolver.USER_NAME_THREAD_SLOT_NAME), userName);
