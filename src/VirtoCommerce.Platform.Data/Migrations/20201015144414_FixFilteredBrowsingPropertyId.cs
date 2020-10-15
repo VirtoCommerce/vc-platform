aa4d1bebcc44de72c36c9f9b0c9fe15a250cb698
Egidijus Mazeika <em@virtoway.com>
2020-10-15 22:34:16 +0300

    VP-5586: Update FixFilteredBrowsingPropertyId migration

--- src/VirtoCommerce.Platform.Data/Migrations/20201015144414_FixFilteredBrowsingPropertyId.cs
+++ src/VirtoCommerce.Platform.Data/Migrations/20201015144414_FixFilteredBrowsingPropertyId.cs
@@ -9,11 +9,11 @@ namespace VirtoCommerce.Platform.Data.Migrations
             migrationBuilder.Sql(@"IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'PlatformDynamicProperty'))
                 BEGIN
                     DELETE FROM PlatformDynamicPropertyName WHERE PropertyId IN (select Id from [PlatformDynamicProperty]
-                        WHERE Name = 'FilteredBrowsing' AND ObjectType IN ('VirtoCommerce.StoreModule.Core.Model.Store','VirtoCommerce.Domain.Store.Model.Store'))
+                        WHERE Name = 'FilteredBrowsing' AND ObjectType = 'VirtoCommerce.Domain.Store.Model.Store')
                     DELETE FROM PlatformDynamicPropertyObjectValue WHERE PropertyId IN (select Id from [PlatformDynamicProperty]
-                        WHERE Name = 'FilteredBrowsing' AND ObjectType IN ('VirtoCommerce.StoreModule.Core.Model.Store','VirtoCommerce.Domain.Store.Model.Store'))
+                        WHERE Name = 'FilteredBrowsing' AND ObjectType = 'VirtoCommerce.Domain.Store.Model.Store')
                     UPDATE [PlatformDynamicProperty] SET Id = 'VirtoCommerce.Catalog_FilteredBrowsing_Property'
-                        WHERE Name = 'FilteredBrowsing' AND ObjectType IN ('VirtoCommerce.StoreModule.Core.Model.Store','VirtoCommerce.Domain.Store.Model.Store') AND Id != 'VirtoCommerce.Catalog_FilteredBrowsing_Property'
+                        WHERE Name = 'FilteredBrowsing' AND ObjectType = 'VirtoCommerce.Domain.Store.Model.Store' AND Id != 'VirtoCommerce.Catalog_FilteredBrowsing_Property'
                 END");
         }
 
