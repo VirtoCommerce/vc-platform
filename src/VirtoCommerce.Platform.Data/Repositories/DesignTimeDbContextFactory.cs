59574ff4df7c7eb870ec0acb59e10063faf9b502
Andrei Kostyrin <kostyrin2@gmail.com>
2020-10-28 14:22:34 +0200

    #RND-24 comment UseSql in DesignTimeDbContextFactory

--- src/VirtoCommerce.Platform.Data/Repositories/DesignTimeDbContextFactory.cs
+++ src/VirtoCommerce.Platform.Data/Repositories/DesignTimeDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace VirtoCommerce.Platform.Data.Repositories
         {
             var builder = new DbContextOptionsBuilder<PlatformDbContext>();
 
-            builder.UseSqlServer("Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=virto;Password=virto;MultipleActiveResultSets=True;Connect Timeout=30");
+            //builder.UseSqlServer("Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=virto;Password=virto;MultipleActiveResultSets=True;Connect Timeout=30");
 
             return new PlatformDbContext(builder.Options);
         }
