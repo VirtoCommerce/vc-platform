6d2a48d5d722f4044348204a9505346394151a9f
Andrei Kolchanov <akak1977@gmail.com>
2020-11-09 12:00:24 +0500

    VP-5618: And support for any query substring

--- src/VirtoCommerce.Platform.Web/Telemetry/IgnoreSignalRTelemetryProcessor.cs
+++ src/VirtoCommerce.Platform.Web/Telemetry/IgnoreSignalRTelemetryProcessor.cs
@@ -2,27 +2,24 @@ using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 
-
-namespace VirtoCommerce.Platform.Web.Hangfire
+namespace VirtoCommerce.Platform.Web.Telemetry
 {
     /// <summary>
-    /// Application insight telemetry processor which exclude all dependency SQL queries related to Hangfire.
+    /// Application insight telemetry processor which exclude all SignalR requests from statistic.
     /// </summary>
-    public class IgnoreHangfireTelemetryProcessor : ITelemetryProcessor
+    public class IgnoreSignalRTelemetryProcessor : ITelemetryProcessor
     {
         private ITelemetryProcessor Next { get; set; }
 
         // Link processors to each other in a chain.
-        public IgnoreHangfireTelemetryProcessor(ITelemetryProcessor next)
+        public IgnoreSignalRTelemetryProcessor(ITelemetryProcessor next)
         {
             Next = next;
         }
 
         public void Process(ITelemetry item)
         {
-            if (item is DependencyTelemetry dependencyTelemetry &&
-                dependencyTelemetry.Type == "SQL" &&
-                dependencyTelemetry.Data.Contains("HangFire"))
+            if (item is RequestTelemetry request && request.Url.AbsolutePath.ToLowerInvariant().Contains("/pushnotificationhub"))
             {
                 // To filter out an item, just terminate the chain:
                 return;
