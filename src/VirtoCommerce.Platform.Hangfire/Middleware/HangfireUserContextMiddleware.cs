17a387d664d4eb739913b3dc5591d21ed5e44fd4
Tatarincev Eugeney <et@virtoway.com>
2020-11-17 16:26:20 +0200

    VP-4321: get a request username from resolver (#2098)
    * get user name from resolver for hangfire jobs
    
    * VP-4321: Fix middleware creation
    
    * VP-4321: Get `IUserNameResolver` properly
    
    * VP-4321: Remove redundant using
    
    Co-authored-by: Andrey Kozlov <yecli.github@gmail.com>

--- src/VirtoCommerce.Platform.Hangfire/Middleware/HangfireUserContextMiddleware.cs
+++ src/VirtoCommerce.Platform.Hangfire/Middleware/HangfireUserContextMiddleware.cs
@@ -1,7 +1,7 @@
 using System.Threading;
 using Hangfire.Client;
 using Hangfire.Server;
-using Microsoft.AspNetCore.Http;
+using VirtoCommerce.Platform.Core.Security;
 using static VirtoCommerce.Platform.Core.Common.ThreadSlotNames;
 
 namespace VirtoCommerce.Platform.Hangfire.Middleware
@@ -12,11 +12,14 @@ namespace VirtoCommerce.Platform.Hangfire.Middleware
     /// </summary>
     public class HangfireUserContextMiddleware : IClientFilter, IServerFilter
     {
-        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IUserNameResolver _userNameResolver;
 
-        public HangfireUserContextMiddleware(IHttpContextAccessor contextAccessor) => _contextAccessor = contextAccessor;
+        public HangfireUserContextMiddleware(IUserNameResolver userNameResolver)
+        {
+            _userNameResolver = userNameResolver;
+        }
 
-        private string ContextUserName => _contextAccessor?.HttpContext?.User?.Identity?.Name;
+        private string ContextUserName => _userNameResolver.GetCurrentUserName();
 
         #region IClientFilter Members
 
