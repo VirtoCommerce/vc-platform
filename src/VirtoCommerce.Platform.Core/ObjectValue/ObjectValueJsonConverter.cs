e7dc7dfc981fb9cec81534b2327059db4d32d5fd
Andrei Kostyrin <kostyrin2@gmail.com>
2020-12-17 16:30:13 +0200

    #VP=6107 add ObjectValue Attribute and converter

---
+++ src/VirtoCommerce.Platform.Core/ObjectValue/ObjectValueJsonConverter.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VirtoCommerce.Platform.Core.ObjectValue
+{
+    public class ObjectValueJsonConverter : JsonConverter
+    {
+        public override bool CanWrite => true;
+        public override bool CanRead => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.GetCustomAttributes(true).Any(a => a.GetType() == typeof(ObjectValueSerializedAsStringForIndexAttribute));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var obj = JObject.FromObject(value, new JsonSerializer() { ContractResolver = serializer.ContractResolver });
+
+            if (value != null
+                && obj.TryGetValue("value", out var objValue)
+                && obj.TryGetValue("valueType", out var objValueType))
+            {
+                obj["value"] = ConvertValue(objValue, objValueType.ToObject<string>());
+            }
+
+            obj.WriteTo(writer);
+        }
+
+        private string ConvertValue(object value, string valueType)
+        {
+            string result;
+
+            switch (valueType)
+            {
+                case "LongText":
+                case "ShortText":
+                case "GeoPoint":
+                case "Integer":
+                    result = Convert.ToString(value);
+                    break;
+                case "Number":
+                    result = Convert.ToString(value, CultureInfo.InstalledUICulture);
+                    break;
+                case "DateTime":
+                    result = Convert.ToDateTime(value, CultureInfo.InvariantCulture).ToString("O");
+                    break;
+                case "Boolean":
+                    result = value.ToString().ToLower();
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+
+            return result;
+        }
+    }
+
+
+    
+
+}
---
+++ src/VirtoCommerce.Platform.Core/ObjectValue/ObjectValueSerializedAsStringForIndexAttribute.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace VirtoCommerce.Platform.Core.ObjectValue
+{
+    public class ObjectValueSerializedAsStringForIndexAttribute : Attribute { }
+}
