d59cd58a29c62bc55cb15e9a71b94f331b8a6c5c
Andrei Kolchanov <akak1977@gmail.com>
2021-02-03 13:32:12 +0500

    VP-6844: Clean up OpenIDDict tokens and authorizations

---
+++ src/VirtoCommerce.Platform.Security/Migrations/20210203073421_TruncateOpenIDDictTokens.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.Platform.Security.Migrations
+{
+    public partial class TruncateOpenIDDictTokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // This removes all OpenIDDict tokens and authorizations
+            // Truncate used to avoid timeouts.
+            migrationBuilder.Sql(@"
+                ALTER TABLE [OpenIddictTokens] DROP CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId]
+
+                TRUNCATE TABLE OpenIddictTokens
+
+                TRUNCATE TABLE OpenIddictAuthorizations
+
+                ALTER TABLE [OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId] FOREIGN KEY([AuthorizationId])
+                REFERENCES [OpenIddictAuthorizations] ([Id])
+
+                ALTER TABLE [OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId]
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
