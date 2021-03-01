4377be2d15a0bd601e4b42a58261343cb14fecd0
Egis <em@virtoway.com>
2021-03-01 17:50:28 +0200

    VP-6894: Regenerate SecurityStamp on email change; Define ApplicationDiscriminator (#2169)

--- src/VirtoCommerce.Platform.Web/Startup.cs
+++ src/VirtoCommerce.Platform.Web/Startup.cs
@@ -326,6 +326,10 @@ namespace VirtoCommerce.Platform.Web
                 });
 
             services.Configure<IdentityOptions>(Configuration.GetSection("IdentityOptions"));
+            services.AddDataProtection(options =>
+            {
+                options.ApplicationDiscriminator = "VirtoCommerceGroup";
+            });
 
             //always  return 401 instead of 302 for unauthorized  requests
             services.ConfigureApplicationCookie(options =>
@@ -500,7 +504,7 @@ namespace VirtoCommerce.Platform.Web
             app.UsePruneExpiredTokensJob();
 
             app.UseModules();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
