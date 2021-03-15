6d6cc978a577651ae6c68d39f2ac320d81a37a17
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-03-15 10:04:30 +0200

    PT-332 modify DetectUserChanges method (#2180)

--- src/VirtoCommerce.Platform.Core/Security/ApplicationUserExtensions.cs
+++ src/VirtoCommerce.Platform.Core/Security/ApplicationUserExtensions.cs
@@ -7,17 +7,30 @@ namespace VirtoCommerce.Platform.Core.Security
         public static ListDictionary<string, string> DetectUserChanges(this ApplicationUser newUser, ApplicationUser oldUser)
         {
             var result = new ListDictionary<string, string>();
+
             if (newUser.UserName != oldUser.UserName)
             {
                 result.Add(PlatformConstants.Security.Changes.UserUpdated, $"Changes: user name: {oldUser.UserName} -> {newUser.UserName}");
             }
+
+            if (newUser.Email != oldUser.Email)
+            {
+                result.Add(PlatformConstants.Security.Changes.UserUpdated, $"Changes: email: {oldUser.Email} -> {newUser.Email}");
+            }
+
             if (newUser.UserType != oldUser.UserType)
             {
                 result.Add(PlatformConstants.Security.Changes.UserUpdated, $"Changes: user type: {oldUser.UserType} -> {newUser.UserType}");
             }
+
             if (newUser.IsAdministrator != oldUser.IsAdministrator)
             {
                 result.Add(PlatformConstants.Security.Changes.UserUpdated, $"Changes: root: {oldUser.IsAdministrator} -> {newUser.IsAdministrator}");
+            }
+
+            if (newUser.MemberId != oldUser.MemberId)
+            {
+                result.Add(PlatformConstants.Security.Changes.UserUpdated, $"Changes: member ID: {oldUser.MemberId} -> {newUser.MemberId}");
             }
 
             if (newUser.PasswordHash != oldUser.PasswordHash)
