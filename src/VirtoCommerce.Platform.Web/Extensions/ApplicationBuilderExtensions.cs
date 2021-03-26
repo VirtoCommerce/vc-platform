670548bc30ec2fff3a75b4a458f8aab25b909075
Andrei Kolchanov <akak1977@users.noreply.github.com>
2021-03-26 16:44:26 +0500

    PT-548: Add a distributed lock for migrations applying (#2185)

--- src/VirtoCommerce.Platform.Web/Extensions/ApplicationBuilderExtensions.cs
+++ src/VirtoCommerce.Platform.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using StackExchange.Redis;
+using VirtoCommerce.Platform.Core.Exceptions;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.Platform.Web.Licensing;
@@ -60,6 +66,51 @@ namespace VirtoCommerce.Platform.Web.Extensions
                 .OfType<ManifestModuleInfo>()
                 .Where(x => x.State == ModuleState.Initialized && !x.Errors.Any())
                 .ToArray();
+        }
+
+        /// <summary>
+        /// Run specified payload in sync between several instances
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder WithDistributedLock(this IApplicationBuilder app, Action payload)
+        {
+            var redisConnMultiplexer = app.ApplicationServices.GetService<IConnectionMultiplexer>();
+
+            var logger = app.ApplicationServices.GetService<ILogger<Startup>>();
+
+            if (redisConnMultiplexer != null)
+            {
+                var distributedLockWait = app.ApplicationServices.GetRequiredService<IOptions<LocalStorageModuleCatalogOptions>>().Value.DistributedLockWait;
+
+                // Try to acquire distributed lock
+                using (var redlockFactory = RedLockFactory.Create(new RedLockMultiplexer[] { new RedLockMultiplexer(redisConnMultiplexer) }))
+                using (var redLock = redlockFactory.CreateLock(nameof(WithDistributedLock),
+                    TimeSpan.FromSeconds(120 + distributedLockWait) /* Successfully acquired lock expiration time */,
+                    TimeSpan.FromSeconds(distributedLockWait) /* Total time to wait until the lock is available */,
+                    TimeSpan.FromSeconds(3) /* The span to acquire the lock in retries */))
+                {
+                    if (redLock.IsAcquired)
+                    {
+                        logger.LogInformation("Distributed lock acquired");
+                        payload();
+                    }
+                    else
+                    {
+                        // Lock not acquired even after migrationDistributedLockOptions.Wait
+                        throw new PlatformException($"Can't apply migrations. It seems another platform instance still applies migrations. Consider to increase MigrationDistributedLockOptions.Wait timeout.");
+                    }
+                }
+            }
+            else
+            {
+                // One-instance configuration, no Redis, just run
+                logger.Log(LogLevel.Information, "Distributed lock not acquired, Redis ConnectionMultiplexer is null (No Redis connection ?)");
+                payload();
+            }
+
+            return app;
         }
     }
 
--- src/VirtoCommerce.Platform.Web/Startup.cs
+++ src/VirtoCommerce.Platform.Web/Startup.cs
@@ -41,7 +41,6 @@ using VirtoCommerce.Platform.Core.Localizations;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Data.Extensions;
-using VirtoCommerce.Platform.Data.Repositories;
 using VirtoCommerce.Platform.Hangfire.Extensions;
 using VirtoCommerce.Platform.Modules;
 using VirtoCommerce.Platform.Security.Authorization;
@@ -52,6 +51,7 @@ using VirtoCommerce.Platform.Web.Extensions;
 using VirtoCommerce.Platform.Web.Infrastructure;
 using VirtoCommerce.Platform.Web.Licensing;
 using VirtoCommerce.Platform.Web.Middleware;
+using VirtoCommerce.Platform.Web.Migrations;
 using VirtoCommerce.Platform.Web.PushNotifications;
 using VirtoCommerce.Platform.Web.Redis;
 using VirtoCommerce.Platform.Web.Security;
@@ -474,31 +474,31 @@ namespace VirtoCommerce.Platform.Web
             app.UseAuthentication();
             app.UseAuthorization();
 
-            //Force migrations
-            using (var serviceScope = app.ApplicationServices.CreateScope())
+            app.WithDistributedLock(() =>
             {
-                var platformDbContext = serviceScope.ServiceProvider.GetRequiredService<PlatformDbContext>();
-                platformDbContext.Database.MigrateIfNotApplied(MigrationName.GetUpdateV2MigrationName("Platform"));
-                platformDbContext.Database.Migrate();
+                // This method contents will run inside of critical section of instance distributed lock.
+                // Main goal is to apply the migrations (Platform, Hangfire, modules) sequentially instance by instance.
+                // This ensures only one active EF-migration ran simultaneously to avoid DB-related side-effects.
 
-                var securityDbContext = serviceScope.ServiceProvider.GetRequiredService<SecurityDbContext>();
-                securityDbContext.Database.MigrateIfNotApplied(MigrationName.GetUpdateV2MigrationName("Security"));
-                securityDbContext.Database.Migrate();
-            }
+                // Apply platform migrations
+                app.UsePlatformMigrations();
 
-            app.UseDbTriggers();
-            //Register platform settings
-            app.UsePlatformSettings();
+                app.UseDbTriggers();
 
-            // Complete hangfire init
-            app.UseHangfire(Configuration);
+                // Register platform settings
+                app.UsePlatformSettings();
 
-            //Register platform permissions
-            app.UsePlatformPermissions();
-            app.UseSecurityHandlers();
-            app.UsePruneExpiredTokensJob();
+                // Complete hangfire init and apply Hangfire migrations
+                app.UseHangfire(Configuration);
 
-            app.UseModules();
+                // Register platform permissions
+                app.UsePlatformPermissions();
+                app.UseSecurityHandlers();
+                app.UsePruneExpiredTokensJob();
+
+                // Complete modules startup and apply their migrations
+                app.UseModules();
+            });
 
             app.UseEndpoints(endpoints =>
             {
@@ -514,11 +514,10 @@ namespace VirtoCommerce.Platform.Web
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
-
             // Use app insights telemetry 
             app.UseAppInsightsTelemetry();
 
-            
+
             var mvcJsonOptions = app.ApplicationServices.GetService<IOptions<MvcNewtonsoftJsonOptions>>();
             mvcJsonOptions.Value.SerializerSettings.Converters.Add(new PolymorphJsonConverter());
             PolymorphJsonConverter.RegisterTypeForDiscriminator(typeof(PermissionScope), nameof(PermissionScope.Type));
