982898301d5f26a1a68986fcbe6c9a83d15fd0cc
Egidijus Mazeika <em@virtoway.com>
2021-04-11 22:27:34 +0300

    PT-85: Set up user password repeat generation policy

--- src/VirtoCommerce.Platform.Security/Repositories/ISecurityRepository.cs
+++ src/VirtoCommerce.Platform.Security/Repositories/ISecurityRepository.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Security.Model;
 
@@ -6,6 +7,10 @@ namespace VirtoCommerce.Platform.Security.Repositories
 {
     public interface ISecurityRepository : IRepository
     {
-        IQueryable<UserApiKeyEntity> UserApiKeys { get; }      
+        IQueryable<UserApiKeyEntity> UserApiKeys { get; }
+
+        IQueryable<UserPasswordHistoryEntity> UserPasswordsHistory { get; }
+
+        Task<UserPasswordHistoryEntity[]> GetUserPasswordsHistoryAsync(string userId);
     }
 }
--- src/VirtoCommerce.Platform.Security/Repositories/SecurityRepository.cs
+++ src/VirtoCommerce.Platform.Security/Repositories/SecurityRepository.cs
@@ -1,4 +1,9 @@
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Data.Infrastructure;
 using VirtoCommerce.Platform.Security.Model;
 
@@ -6,13 +11,27 @@ namespace VirtoCommerce.Platform.Security.Repositories
 {
     public class SecurityRepository : DbContextRepositoryBase<SecurityDbContext>, ISecurityRepository
     {
-        public SecurityRepository(SecurityDbContext dbContext)
+        private readonly PasswordOptionsExtended _passwordOptions;
+
+        public SecurityRepository(SecurityDbContext dbContext, IOptions<PasswordOptionsExtended> passwordOptions)
             : base(dbContext)
         {
+            _passwordOptions = passwordOptions.Value;
         }
 
-        public virtual IQueryable<UserApiKeyEntity> UserApiKeys { get { return DbContext.Set<UserApiKeyEntity>(); } }
+        public virtual IQueryable<UserApiKeyEntity> UserApiKeys => DbContext.Set<UserApiKeyEntity>();
 
+        public virtual IQueryable<UserPasswordHistoryEntity> UserPasswordsHistory => DbContext.Set<UserPasswordHistoryEntity>();
 
+        public async Task<UserPasswordHistoryEntity[]> GetUserPasswordsHistoryAsync(string userId)
+        {
+            var passwordHistoryLength = _passwordOptions.PasswordHistory.GetValueOrDefault();
+
+            return await UserPasswordsHistory
+                .Where(x => x.UserId == userId)
+                .OrderByDescending(x => x.CreatedDate)
+                .Take(passwordHistoryLength)
+                .ToArrayAsync();
+        }
     }
 }
