3fe0e099d68d0471695217728ec580d5be7d1156
Eugeny Tatarincev <et@virtoway.com>
2021-04-29 15:21:28 +0200

    remove using

--- src/VirtoCommerce.Platform.Core/JsonConverters/DynamicObjectPropertyJsonConverter.cs
+++ src/VirtoCommerce.Platform.Core/JsonConverters/DynamicObjectPropertyJsonConverter.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using VirtoCommerce.Platform.Core.DynamicProperties;
 
 namespace VirtoCommerce.Platform.Core.JsonConverters
