436a3029410efae2decd61552c0040706a2461db
Filip Kuksov <vectorfield4@gmail.com>
2021-05-11 05:10:53 +0200

    PT-1663: Add mention to add constraint PT-1471: Add tests for enumerable extensions

--- tests/VirtoCommerce.Testing/Helpers/MockHelper.cs
+++ tests/VirtoCommerce.Testing/Helpers/MockHelper.cs
@@ -1,9 +1,27 @@
+using System;
 using AutoFixture;
 
 namespace VirtoCommerce.Testing.Helpers
 {
     public class MockHelper
     {
-        protected readonly Fixture _fixture = new Fixture();
+        protected const int DEFAULT_PAGE_SIZE = 50;
+        protected readonly Fixture _fixture = new();
+
+        /// <summary>
+        /// Helper for testing
+        /// </summary>
+#pragma warning disable S1210 // "Equals" and the comparison operators should be overridden when implementing "IComparable"
+
+        public class TestComparableClass : IComparable
+#pragma warning restore S1210 // "Equals" and the comparison operators should be overridden when implementing "IComparable"
+        {
+            public int Value { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Value.CompareTo((obj as TestComparableClass).Value);
+            }
+        }
     }
 }
