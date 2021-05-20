3d0e47ed60110a00f693059d4aab06c1d22119e5
Andrei Kolchanov <akak1977@gmail.com>
2021-05-20 16:14:52 +0500

    PT-469: Add comments to DetectChangesAttribute

--- src/VirtoCommerce.Platform.Core/ChangesUtils/DetectChangesAttribute.cs
+++ src/VirtoCommerce.Platform.Core/ChangesUtils/DetectChangesAttribute.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace VirtoCommerce.Platform.Core.Utils.ChangeDetector
 {
+    /// <summary>
+    /// Allows to mark public properties to use with <see cref="ChangeDetector"/>.
+    /// </summary>
     public sealed class DetectChangesAttribute: Attribute
     {
         public string ChangeKey { get; private set; }
