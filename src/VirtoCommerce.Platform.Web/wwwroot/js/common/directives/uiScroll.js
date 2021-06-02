2aa246c15c0bcac4b2705a5a319e61c1106d3970
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-06-02 10:48:00 +0200

    PT-2010 removed old directive

--- src/VirtoCommerce.Platform.Web/wwwroot/js/common/directives/uiScroll.js
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/common/directives/uiScroll.js
@@ -1,59 +1,135 @@
 angular.module('platformWebApp')
-    .directive('uiScrollDropDown', [function () {
+    .directive('uiScrollDropDown', [function() {
         const defaultPageSize = 20;
+        const defaultResponseGroup = 'none';
+        const defaultSelectedItemsPropertyName = 'objectIds';
+
         return {
             restrict: 'E',
+            require: 'ngModel',
             replace: true,
             scope: {
-                data: '&',
-                label: '=',
-                placeholder: '=',
-                selectedId: '=',
+                data: '&?',
+                onSelect: '&?',
+                onRemove: '&?',
+                predefined: '=?',
+                disabled: '=?',
+                multiple: '=?',
+                pageSize: '=?',
+                placeholder: '=?',
+                required: '=?',
+                entitiesToHide: '=?',
+                responseGroup: '=?',
+                selectedItemsPropertyName: '=?'
             },
-            templateUrl: '$(Platform)/Scripts/common/directives/UiScroll.tpl.html',
-            controller: ['$scope', function(scope) {
-                scope.list = [];
-                
-                scope.pageSize = defaultPageSize;
+            templateUrl: '$(Platform)/Scripts/common/directives/uiScroll.tpl.html',
+            link: function ($scope, element, attrs, ngModelController) {
+                $scope.context = {
+                    modelValue: null,
+                    required: angular.isDefined(attrs.required) && (attrs.required === '' || attrs.required.toLowerCase() === 'true'),
+                    multiple: angular.isDefined(attrs.multiple) && (attrs.multiple === '' || attrs.multiple.toLowerCase() === 'true')
+                };
 
-                scope.setValue = (item) => {
-                    scope.selectedId = item.id;
+                // if Predefined list is binded paginaiton is disabled
+                $scope.items = $scope.predefined || [];
+                $scope.isNoItems = !$scope.predefined;
+
+                // PageSize amount must be enough to show scrollbar in dropdown list container.
+                // If scrollbar doesn't appear auto loading won't work.
+                var pageSize = $scope.pageSize || defaultPageSize;
+                var responseGroup = $scope.responseGroup || defaultResponseGroup;
+                var selectedItemsPropertyName = $scope.selectedItemsPropertyName || defaultSelectedItemsPropertyName;
+                var lastSearchPhrase = '';
+                var totalCount = 0;
+                var hiddenCount = angular.isArray($scope.entitiesToHide) ? $scope.entitiesToHide.length : 0;
+
+                $scope.selectValue = (item, model) => {
+                    if ($scope.onSelect) {
+                        $scope.onSelect({ item: item, model: model });
+                    }
                 }
 
-                scope.fetch = (select) => {
-                    if (scope.list.length == 0) {
-                        select.page = 0;
-                        if (scope.selectedId) {
-                            let criteria = {
-                                ObjectIds: [scope.selectedId]
-                            }
-                            scope.data({criteria: criteria}).then((data) => {
-                                scope.list = data.results;
-                                scope.fetchNext(select);
-                            });
-                        }
-                        else {
-                            scope.fetchNext(select);
-                        }
+                $scope.removeValue = (item, model) => {
+                    if ($scope.onRemove) {
+                        $scope.onRemove({ item: item, model: model });
+                    }
+                }
+
+                $scope.fetch = function ($select) {
+                    if ($scope.predefined)
+                        return;
+
+                    load();
+
+                    if (!$scope.disabled) {
+                        $scope.fetchNext($select);
                     }
                 };
 
-                scope.fetchNext = (select) => {
-                    let criteria = {
-                        SearchPhrase: select.search,
-                        take: scope.pageSize,
-                        skip: select.page * scope.pageSize
+                $scope.fetchNext = ($select) => {
+                    if ($scope.predefined)
+                        return;
+
+                    $select.page = $select.page || 0;
+
+                    if (lastSearchPhrase !== $select.search && totalCount > $scope.items.length) {
+                        lastSearchPhrase = $select.search;
+                        $select.page = 0;
                     }
-            
-                    scope.data({criteria: criteria}).then((data) => {
-                        scope.list = scope.list.concat(data.results);
-                        select.page++;
-            
-                        if (scope.list.length < data.totalCount) {
-                            scope.$broadcast('scrollCompleted');
-                        }
-                    });
+
+                    var criteria = {
+                        searchPhrase: $select.search,
+                        take: pageSize,
+                        skip: $select.page * pageSize,
+                        responseGroup: responseGroup
+                    };
+
+                    return $scope.data({ criteria: criteria }).$promise.then((data) => {
+                            join(data.results);
+                            $select.page++;
+
+                            if ($select.page * pageSize < data.totalCount) {
+                                $scope.$broadcast('scrollCompleted');
+                            }
+
+                            totalCount = Math.max(totalCount, data.totalCount - hiddenCount);
+                        });
+                };
+
+                $scope.$watch('context.modelValue', function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        ngModelController.$setViewValue($scope.context.modelValue);
+                    }
+                });
+
+                ngModelController.$render = function () {
+                    $scope.context.modelValue = ngModelController.$modelValue;
+                };
+
+                function load() {
+                    var selectedIds = $scope.context.multiple ? $scope.context.modelValue : [$scope.context.modelValue];
+
+                    if ($scope.isNoItems && _.any(selectedIds)) {
+                        var criteria = {
+                            take: selectedIds.length,
+                            responseGroup: responseGroup
+                        };
+
+                        criteria[selectedItemsPropertyName] = selectedIds;
+
+                        $scope.data({ criteria: criteria }).$promise.then((data) => {
+                            join(data.results);
+                        });
+                    }
                 }
-            }]
+
+                function join(newItems) {
+                    newItems = _.reject(newItems, x => _.any($scope.items, y => y.id === x.id) || _.indexOf($scope.entitiesToHide, x.id) > -1);
+                    if (_.any(newItems)) {
+                        $scope.items = $scope.items.concat(newItems);
+                        $scope.isNoItems = $scope.items.length === 0;
+                    }
+                }
+            }
         }
     }]);
