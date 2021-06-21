c76cd6ed43ac9d95acede581e59ff5e6a7c4d18c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-06-21 14:48:55 +0200

    PT-2247 added System.Linq.Async

--- src/VirtoCommerce.Platform.Web/Controllers/Api/OAuthAppsController.cs
+++ src/VirtoCommerce.Platform.Web/Controllers/Api/OAuthAppsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -79,13 +80,12 @@ namespace VirtoCommerce.Platform.Web.Controllers.Api
         [Route("")]
         public async Task<ActionResult> DeleteAsync([FromQuery] string[] clientIds)
         {
-            //var apps = await _manager.ListAsync(x =>
-            //    x.Where(y => clientIds.Contains(y.ClientId)));
+            var apps = _manager.ListAsync(x => x.Where(y => clientIds.Contains(y.ClientId))).ToEnumerable();
 
-            //foreach (var app in apps)
-            //{
-            //    await _manager.DeleteAsync(app);
-            //}
+            foreach (var app in apps)
+            {
+                await _manager.DeleteAsync(app);
+            }
 
             return Ok();
         }
@@ -94,31 +94,28 @@ namespace VirtoCommerce.Platform.Web.Controllers.Api
         [Route("search")]
         public async Task<ActionResult<OAuthAppSearchResult>> SearchAsync(OAuthAppSearchCriteria criteria)
         {
-            //if (criteria.Sort.IsNullOrEmpty())
-            //{
-            //    criteria.Sort = "DisplayName:ASC";
-            //}
+            if (criteria.Sort.IsNullOrEmpty())
+            {
+                criteria.Sort = "DisplayName:ASC";
+            }
 
-            //var apps = await _manager.ListAsync(x => x.OrderBySortInfos(criteria.SortInfos).Skip(criteria.Skip).Take(criteria.Take));
+            var apps = _manager.ListAsync(x => x.OrderBySortInfos(criteria.SortInfos).Skip(criteria.Skip).Take(criteria.Take)).ToEnumerable();
 
-            //var appsTasks = apps.Select(async x =>
-            //    {
-            //        var descriptor = new OpenIddictApplicationDescriptor();
-            //        await _manager.PopulateAsync(descriptor, x);
-            //        descriptor.ClientSecret = "";
-            //        return descriptor;
-            //    }).ToList();
+            var appsTasks = apps.Select(async x =>
+                {
+                    var descriptor = new OpenIddictApplicationDescriptor();
+                    await _manager.PopulateAsync(descriptor, x);
+                    descriptor.ClientSecret = "";
+                    return descriptor;
+                }).ToList();
 
-            //var result = new OAuthAppSearchResult
-            //{
-            //    Results = await Task.WhenAll(appsTasks),
-            //    TotalCount = (int)await _manager.CountAsync()
-            //};
+            var result = new OAuthAppSearchResult
+            {
+                Results = await Task.WhenAll(appsTasks),
+                TotalCount = (int)await _manager.CountAsync()
+            };
 
-            //return result;
-
-            return Ok();
-
+            return result;
         }
     }
 }
