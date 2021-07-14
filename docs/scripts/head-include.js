e8624fbde2a8464e452b64b0a33876d7ab4d850e
Sergey Podguzov <spo@virtoway.com>
2021-07-14 20:25:00 +0500

    Add noindex meta to all pages in docs.

--- docs/scripts/head-include.js
+++ docs/scripts/head-include.js
@@ -6,6 +6,11 @@ var gTagManager = document.createElement('script');
 gTagManager.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-NKMNNN';
 gTagManager.async = true;
 
+var noindex = document.createElement('meta');
+noindex.name = 'robots';
+noindex.content = 'noindex';
+
 var head = document.getElementsByTagName('head')[0];
+head.appendChild(noindex);
 head.appendChild(gTagAnalytics);
 head.appendChild(gTagManager);
