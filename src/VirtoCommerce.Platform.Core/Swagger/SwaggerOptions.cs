9c31a60305b4840a14897bffe9f44b45652df08d
Andrei Kolchanov <akak1977@gmail.com>
2021-08-26 15:21:43 +0500

    PT-3733

---
+++ src/VirtoCommerce.Platform.Core/Swagger/SwaggerOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtoCommerce.Platform.Core.Swagger
+{
+    public class SwaggerPlatformOptions
+    {
+        /// <summary>
+        /// Disable swagger at the startup (switch off endpoints for swagger UI and docs)
+        /// </summary>
+        public bool Disable { get; set; } = false;
+    }
+}
