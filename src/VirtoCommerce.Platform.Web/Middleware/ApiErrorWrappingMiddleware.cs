fa16c46385cc82d67e1af585d3a9a86d1cdd5e4c
BelyakovSergey <Belyakov.Sergey@virtoway.com>
2021-09-24 09:47:49 +0300

    PT-3771: Checking error handling

--- src/VirtoCommerce.Platform.Web/Middleware/ApiErrorWrappingMiddleware.cs
+++ src/VirtoCommerce.Platform.Web/Middleware/ApiErrorWrappingMiddleware.cs
@@ -30,12 +30,11 @@ namespace VirtoCommerce.Platform.Web.Middleware
             }
             catch (Exception ex)
             {
-                var isDevelopment = _env.IsDevelopment();
-                var message = !isDevelopment ? ex.Message : $@"An exception occurred while processing the request [{context.Request.Path}]: {ex}";
-
                 //Need handle only storefront api errors
                 if (!context.Response.HasStarted && context.Request.Path.ToString().Contains("/api/"))
                 {
+                    var isDevelopment = _env.IsDevelopment();
+                    var message = !isDevelopment ? $"Message: {ex.Message}" : $@"An exception occurred while processing the request [{context.Request.Path}]: {ex}";
                     _logger.LogError(ex, message);
                     var httpStatusCode = HttpStatusCode.InternalServerError;
                     var json = JsonConvert.SerializeObject(new { message, stackTrace = isDevelopment ? ex.StackTrace : null });
@@ -46,7 +45,7 @@ namespace VirtoCommerce.Platform.Web.Middleware
                 else
                 {
                     //Continue default error handling
-                    await context.Response.WriteAsync(message);
+                    throw;
                 }
             }
         }
