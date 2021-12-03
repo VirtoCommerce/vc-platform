97f8a039725847c6020fd73fcce9713b177b9b1c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-12-03 11:11:07 +0200

    feat: Add ability to configure login types

---
+++ src/VirtoCommerce.Platform.Web/Model/Security/LoginType.cs
@@ -0,0 +1,11 @@
+namespace VirtoCommerce.Platform.Web.Model.Security
+{
+    public class LoginType
+    {
+        public bool Enabled { get; set; }
+
+        public string AuthenticationType { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
