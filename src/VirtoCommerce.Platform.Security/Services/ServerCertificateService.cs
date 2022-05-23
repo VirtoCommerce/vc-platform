08447a0f3189fcb12bacc2bfeef2d0221f8b273b
Andrei Kolchanov <akak1977@gmail.com>
2022-05-23 21:14:05 +0500

    fix: Remove minor smells

--- src/VirtoCommerce.Platform.Security/Services/ServerCertificateService.cs
+++ src/VirtoCommerce.Platform.Security/Services/ServerCertificateService.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using VirtoCommerce.Platform.Core.Caching;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
