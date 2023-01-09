ad4d8999bbe589158b7b8af47af9e5312bcef650
artem-dudarev <ad@virtoway.com>
2023-01-09 16:56:34 +0300

    Fix XML documentation

--- src/VirtoCommerce.Platform.Data/GenericCrud/CrudService.cs
+++ src/VirtoCommerce.Platform.Data/GenericCrud/CrudService.cs
@@ -309,7 +309,7 @@ namespace VirtoCommerce.Platform.Data.GenericCrud
 
         /// <summary>
         /// Create cache region.
-        /// Default implementation creates <see cref="GenericCachingRegion&lt;TModel&gt;"/>.
+        /// Default implementation creates <see cref="GenericCachingRegion{TModel}"/>.
         /// Can be overridden to create some different region.
         /// </summary>
         /// <param name="ids"></param>
@@ -327,7 +327,7 @@ namespace VirtoCommerce.Platform.Data.GenericCrud
 
         /// <summary>
         /// Clear the cache.
-        /// Default implementation expires <see cref="GenericSearchCachingRegion&lt;TModel&gt;"/> region and <see cref="GenericCachingRegion&lt;TModel&gt;"/> regions for every entity
+        /// Default implementation expires <see cref="GenericSearchCachingRegion{TModel}"/> region and <see cref="GenericCachingRegion{TModel}"/> regions for every entity
         /// Can be overridden to expire different regions/tokens.
         /// </summary>
         /// <param name="models"></param>
