ce73e0f29b127d48a4d7d6de78b68107d8ab29a1
Evgeny Okhrimenko <eo@virtoway.com>
2023-02-08 14:53:41 +0200

    fix code smell

--- src/VirtoCommerce.Platform.Core/Exceptions/SettingsTypeNotRegisteredException.cs
+++ src/VirtoCommerce.Platform.Core/Exceptions/SettingsTypeNotRegisteredException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace VirtoCommerce.Platform.Core.Exceptions
 {
-    [Obsolete]
+    [Obsolete("Not used in the platform, will be removed when migrating to a new version of .net")]
     public class SettingsTypeNotRegisteredException : PlatformException
     {
         public SettingsTypeNotRegisteredException(string settingsType)
