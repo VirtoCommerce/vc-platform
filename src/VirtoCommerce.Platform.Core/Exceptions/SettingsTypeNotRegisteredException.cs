9aa4189ef5dff8784699e084b6303d2ab641aa80
Evgeny Okhrimenko <eo@virtoway.com>
2023-02-09 14:51:24 +0200

    PT-10515: Fix no settings exception, SettingsTypeNotRegisteredException obsolete (#2611)
    Co-authored-by: Oleg Zhuk <zhukoo@gmail.com>

--- src/VirtoCommerce.Platform.Core/Exceptions/SettingsTypeNotRegisteredException.cs
+++ src/VirtoCommerce.Platform.Core/Exceptions/SettingsTypeNotRegisteredException.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace VirtoCommerce.Platform.Core.Exceptions
 {
+    [Obsolete("Not used in the platform, will be removed when migrating to a new version of .net")]
     public class SettingsTypeNotRegisteredException : PlatformException
     {
         public SettingsTypeNotRegisteredException(string settingsType)
