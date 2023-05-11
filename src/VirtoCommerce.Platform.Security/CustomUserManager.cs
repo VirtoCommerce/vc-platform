d18ab1d9d6626f2d350544554db8218ba8ab12b7
artem-dudarev <ad@virtoway.com>
2023-05-11 13:36:46 +0300

    Fix ResetPasswordAsync and ChangePasswordAsync

--- src/VirtoCommerce.Platform.Security/CustomUserManager.cs
+++ src/VirtoCommerce.Platform.Security/CustomUserManager.cs
@@ -109,41 +109,45 @@ namespace VirtoCommerce.Platform.Security
             return result;
         }
 
-        public override async Task<IdentityResult> ResetPasswordAsync(ApplicationUser user, string token, string newPassword)
+        public override Task<IdentityResult> ResetPasswordAsync(ApplicationUser user, string token, string newPassword)
         {
-            //It is important to call base.FindByIdAsync method to avoid of update a cached user.
-            var existUser = await base.FindByIdAsync(user.Id);
-            existUser.LastPasswordChangedDate = DateTime.UtcNow;
-
-            var result = await base.ResetPasswordAsync(existUser, token, newPassword);
-            if (result == IdentityResult.Success)
-            {
-                SecurityCacheRegion.ExpireUser(user);
-
-                await SavePasswordHistory(user, newPassword);
-
-                // Calculate password hash for external hash storage. This provided as workaround until password hash storage would implemented
-                var customPasswordHash = _passwordHasher.HashPassword(user, newPassword);
-                await _eventPublisher.Publish(new UserResetPasswordEvent(user.Id, customPasswordHash));
-            }
-
-            return result;
+            return UpdatePasswordAsync(user, newPassword,
+                (user, newPassword) => base.ResetPasswordAsync(user, token, newPassword),
+                (userId, customPasswordHash) => new UserResetPasswordEvent(userId, customPasswordHash));
         }
 
-        public override async Task<IdentityResult> ChangePasswordAsync(ApplicationUser user, string currentPassword, string newPassword)
+        public override Task<IdentityResult> ChangePasswordAsync(ApplicationUser user, string currentPassword, string newPassword)
         {
+            return UpdatePasswordAsync(user, newPassword,
+                (user, newPassword) => base.ChangePasswordAsync(user, currentPassword, newPassword),
+                (userId, customPasswordHash) => new UserPasswordChangedEvent(userId, customPasswordHash));
+        }
+
+        protected virtual async Task<IdentityResult> UpdatePasswordAsync<TEvent>(
+            ApplicationUser user,
+            string newPassword,
+            Func<ApplicationUser, string, Task<IdentityResult>> updatePassword,
+            Func<string, string, TEvent> buildEvent)
+            where TEvent : class, IEvent
+        {
+            var previousPasswordChangedDate = user.LastPasswordChangedDate;
             user.LastPasswordChangedDate = DateTime.UtcNow;
 
-            var result = await base.ChangePasswordAsync(user, currentPassword, newPassword);
+            var result = await updatePassword(user, newPassword);
             if (result == IdentityResult.Success)
             {
                 SecurityCacheRegion.ExpireUser(user);
 
                 await SavePasswordHistory(user, newPassword);
 
-                // Calculate password hash for external hash storage. This provided as workaround until password hash storage would implemented
+                // Calculate password hash for external hash storage. This provided as workaround until password hash storage is implemented.
                 var customPasswordHash = _passwordHasher.HashPassword(user, newPassword);
-                await _eventPublisher.Publish(new UserPasswordChangedEvent(user.Id, customPasswordHash));
+                var @event = buildEvent(user.Id, customPasswordHash);
+                await _eventPublisher.Publish(@event);
+            }
+            else
+            {
+                user.LastPasswordChangedDate = previousPasswordChangedDate;
             }
 
             return result;
