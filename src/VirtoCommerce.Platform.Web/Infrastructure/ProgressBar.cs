8727950d704807625bf540264adb477bc71d06a4
Oleg Zhuk <zhukoo@gmail.com>
2023-06-08 07:58:13 +0200

    fix code smell

--- src/VirtoCommerce.Platform.Web/Infrastructure/ProgressBar.cs
+++ src/VirtoCommerce.Platform.Web/Infrastructure/ProgressBar.cs
@@ -9,28 +9,13 @@ namespace VirtoCommerce.Platform.Data.Helpers
     /// </summary>
     public class ProgressBar : IDisposable, IProgress<double>
     {
-        private const int blockCount = 30;
-        private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
-        private const string animation = @"|/-\";
-
-        private readonly Timer timer;
+        private const int blockCount = 32;
 
         private double currentProgress = 0;
         private string currentText = string.Empty;
-        private bool disposed = false;
-        private int animationIndex = 0;
 
         public ProgressBar()
         {
-            timer = new Timer(TimerHandler);
-
-            // A progress bar is only for temporary display in a console window.
-            // If the console output is redirected to a file, draw nothing.
-            // Otherwise, we'll end up with a lot of garbage in the target file.
-            if (!Console.IsOutputRedirected)
-            {
-                ResetTimer();
-            }
         }
 
         public void Report(double value)
@@ -38,25 +23,25 @@ namespace VirtoCommerce.Platform.Data.Helpers
             // Make sure value is in [0..1] range
             value = Math.Max(0, Math.Min(1, value));
             Interlocked.Exchange(ref currentProgress, value);
+
+            // A progress bar is only for temporary display in a console window.
+            // If the console output is redirected to a file, draw nothing.
+            // Otherwise, we'll end up with a lot of garbage in the target file.
+            if (!Console.IsOutputRedirected)
+            {
+                UpdateTimer();
+            }
         }
 
-        private void TimerHandler(object state)
+        private void UpdateTimer()
         {
-            lock (timer)
-            {
-                if (disposed)
-                    return;
-
-                int progressBlockCount = (int)(currentProgress * blockCount);
-                int percent = (int)(currentProgress * 100);
-                string text = string.Format("[{0}{1}] {2,3}% {3}",
-                    new string('■', progressBlockCount), new string('-', blockCount - progressBlockCount),
-                    percent,
-                    animation[animationIndex++ % animation.Length]);
-                UpdateText(text);
-
-                ResetTimer();
-            }
+            var progressBlockCount = (int)(currentProgress * blockCount);
+            var percent = (int)(currentProgress * 100);
+            var text = string.Format("[{0}{1}] {2,3}%",
+                new string('■', progressBlockCount),
+                new string('-', blockCount - progressBlockCount),
+                percent);
+            UpdateText(text);
         }
 
         private void UpdateText(string text)
@@ -70,7 +55,7 @@ namespace VirtoCommerce.Platform.Data.Helpers
             }
 
             // Backtrack to the first differing character
-            StringBuilder outputBuilder = new StringBuilder();
+            var outputBuilder = new StringBuilder();
             outputBuilder.Append('\b', currentText.Length - commonPrefixLength);
 
             // Output new suffix
@@ -88,18 +73,16 @@ namespace VirtoCommerce.Platform.Data.Helpers
             currentText = text;
         }
 
-        private void ResetTimer()
-        {
-            timer.Change(animationInterval, TimeSpan.FromMilliseconds(-1));
-        }
-
         public void Dispose()
         {
-            lock (timer)
-            {
-                disposed = true;
-                UpdateText(string.Empty);
-            }
+            UpdateText(string.Empty);
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
         }
 
     }
