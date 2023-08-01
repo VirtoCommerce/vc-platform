ef81edab99fe4c652a3370c12a41fb6b72a0d94f
Oleg Zhuk <zhukoo@gmail.com>
2023-08-01 18:20:06 +0200

    PT-10550: adds the security headers to all responses (#2680)
    feat:  adds the security headers to all responses (#2680)
    * X-Content-Type-Options: nosniff
    * Strict-Transport-Security: max-age=31536000; includeSubDomains - only applied to HTTPS responses
    * X-Frame-Options: Deny - only applied to "document" responses
    * X-XSS-Protection: 1; mode=block - only applied to "document" responses
    * Referrer-Policy: strict-origin-when-cross-origin - only applied to "document" responses
    * Content-Security-Policy: object-src 'none'; form-action 'self'; frame-ancestors 'none' - only applied to "document" responses

--- src/VirtoCommerce.Platform.Web/Extensions/ApplicationBuilderExtensions.cs
+++ src/VirtoCommerce.Platform.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Primitives;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.Platform.DistributedLock;
@@ -15,11 +13,6 @@ namespace VirtoCommerce.Platform.Web.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
-        private static readonly Dictionary<string, StringValues> CustomHeaders = new()
-        {
-            { "X-Frame-Options", new StringValues("SAMEORIGIN") }
-        };
-        
         public static IApplicationBuilder UsePlatformSettings(this IApplicationBuilder appBuilder)
         {
             var settingsRegistrar = appBuilder.ApplicationServices.GetRequiredService<ISettingsRegistrar>();
@@ -54,17 +47,6 @@ namespace VirtoCommerce.Platform.Web.Extensions
                     moduleManager.PostInitializeModule(module, appBuilder);
                 }
             }
-            return appBuilder;
-        }
-
-        public static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder appBuilder)
-        {
-            appBuilder.Use(async (context, next) =>
-            {
-                context.Response.Headers.AddRange(CustomHeaders);
-                await next();
-            });
-
             return appBuilder;
         }
 
