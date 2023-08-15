4be3190f00c1b04881da39ea2bcbcbdce610d35c
Oleg Zhuk <zhukoo@gmail.com>
2023-08-15 15:20:11 +0200

    PT-12665: Improve IDistributedLockService description (#2686)
    feat: Improve IDistributedLockService description

--- src/VirtoCommerce.Platform.Core/DistributedLock/IDistributedLockService.cs
+++ src/VirtoCommerce.Platform.Core/DistributedLock/IDistributedLockService.cs
@@ -4,10 +4,37 @@ using System.Threading.Tasks;
 
 namespace VirtoCommerce.Platform.Core.DistributedLock
 {
+    /// <summary>
+    /// Povides a unified mechanism for acquiring and managing distributed locks. It encapsulates the execution of critical sections of code in a distributed environment, ensuring that only one process can access the locked resource at a time. 
+    /// </summary>
     public interface IDistributedLockService
     {
-        T Execute<T>(string resourceKey, Func<T> resolver, TimeSpan? expireTime = null, TimeSpan? waitTime = null, TimeSpan? retryTime = null, CancellationToken? cancellationToken = null);
+        /// <summary>
+        /// Executes critical sections of code within a distributed lock, allowing only one process to execute the enclosed code block at a time.
+        /// </summary>
+        /// <typeparam name="T">The type of the result produced by resolver.</typeparam>
+        /// <param name="resourceKey">The resource string to lock on.</param>
+        /// <param name="resolver">The delegate to execute within the lock.</param>
+        /// <param name="lockTimeout">The maximum duration the resource will be locked. Default is 30 seconds.</param>
+        /// <param name="tryLockTimeout">The duration to attempt acquiring the locked resource. Null throws an exception immediately if the resource is locked.</param>
+        /// <param name="retryInterval">The interval between lock acquisition retries. Used with tryLockTimeout, otherwise ignored.</param>
+        /// <param name="cancellationToken">A CancellationToken to abort waiting for a blocking lock.</param>
+        /// <returns>The result of executing the resolver delegate under the acquired lock.</returns>
+        /// <exception cref="VirtoCommerce.Platform.Core.Exceptions.PlatformException">Thrown if the resource is currently unavailable due to high demand.</exception>
+        T Execute<T>(string resourceKey, Func<T> resolver, TimeSpan? lockTimeout = null, TimeSpan? tryLockTimeout = null, TimeSpan? retryInterval = null, CancellationToken? cancellationToken = null);
 
-        Task<T> ExecuteAsync<T>(string resourceKey, Func<Task<T>> resolver, TimeSpan? expireTime = null, TimeSpan? waitTime = null, TimeSpan? retryTime = null, CancellationToken? cancellationToken = null);
+        /// <summary>
+        /// Executes asynchronous critical sections of code within a distributed lock, allowing only one process to execute the enclosed code block at a time.
+        /// </summary>
+        /// <typeparam name="T">The type of the result produced by resolver.</typeparam>
+        /// <param name="resourceKey">The resource string to lock on.</param>
+        /// <param name="resolver">The asynchronous delegate to execute within the lock.</param>
+        /// <param name="lockTimeout">The maximum duration the resource will be locked. Default is 30 seconds.</param>
+        /// <param name="tryLockTimeout">The duration to attempt acquiring the locked resource. Null throws an exception immediately if the resource is locked.</param>
+        /// <param name="retryInterval">The interval between lock acquisition retries. Used with tryLockTimeout, otherwise ignored.</param>
+        /// <param name="cancellationToken">A CancellationToken to abort waiting for a blocking lock.</param>
+        /// <returns>A task representing the asynchronous execution of the resolver delegate under the acquired lock.</returns>
+        /// <exception cref="VirtoCommerce.Platform.Core.Exceptions.PlatformException">Thrown if the resource is currently unavailable due to high demand.</exception>
+        Task<T> ExecuteAsync<T>(string resourceKey, Func<Task<T>> resolver, TimeSpan? lockTimeout = null, TimeSpan? tryLockTimeout = null, TimeSpan? retryInterval = null, CancellationToken? cancellationToken = null);
     }
 }
--- src/VirtoCommerce.Platform.DistributedLock/Redis/DistributedLockService.cs
+++ src/VirtoCommerce.Platform.DistributedLock/Redis/DistributedLockService.cs
@@ -10,38 +10,38 @@ namespace VirtoCommerce.Platform.DistributedLock.Redis
     public class DistributedLockService : IDistributedLockService
     {
         private readonly IDistributedLockFactory _distributedLockFactory;
-        private readonly TimeSpan _expiry = TimeSpan.FromSeconds(20);
+        private readonly TimeSpan _expiry = TimeSpan.FromSeconds(30);
 
         public DistributedLockService(IDistributedLockFactory distributedLockFactory)
         {
             _distributedLockFactory = distributedLockFactory;
         }
 
-        public T Execute<T>(string resourceKey, Func<T> resolver, TimeSpan? expireTime = null, TimeSpan? waitTime = null, TimeSpan? retryTime = null, CancellationToken? cancellationToken = null)
+        public T Execute<T>(string resourceKey, Func<T> resolver, TimeSpan? lockTimeout = null, TimeSpan? tryLockTimeout = null, TimeSpan? retryInterval = null, CancellationToken? cancellationToken = null)
         {
-            using var redLock = waitTime.HasValue && retryTime.HasValue
-                ? _distributedLockFactory.CreateLock(resourceKey, expireTime ?? _expiry, waitTime.Value, retryTime.Value, cancellationToken)
-                : _distributedLockFactory.CreateLock(resourceKey, expireTime ?? _expiry);
+            using var redLock = tryLockTimeout.HasValue && retryInterval.HasValue
+                ? _distributedLockFactory.CreateLock(resourceKey, lockTimeout ?? _expiry, tryLockTimeout.Value, retryInterval.Value, cancellationToken)
+                : _distributedLockFactory.CreateLock(resourceKey, lockTimeout ?? _expiry);
 
             if (!redLock.IsAcquired)
             {
-                throw new PlatformException("Service is busy.");
+                throw new PlatformException($"Resource `{resourceKey}' is currently unavailable due to high demand.");
             }
 
             return resolver();
         }
 
-        public async Task<T> ExecuteAsync<T>(string resourceKey, Func<Task<T>> resolver, TimeSpan? expireTime = null, TimeSpan? waitTime = null, TimeSpan? retryTime = null, CancellationToken? cancellationToken = null)
+        public async Task<T> ExecuteAsync<T>(string resourceKey, Func<Task<T>> resolver, TimeSpan? lockTimeout = null, TimeSpan? tryLockTimeout = null, TimeSpan? retryInterval = null, CancellationToken? cancellationToken = null)
         {
-            var lockTask = waitTime.HasValue && retryTime.HasValue
-                ? _distributedLockFactory.CreateLockAsync(resourceKey, expireTime ?? _expiry, waitTime.Value, retryTime.Value, cancellationToken)
-                : _distributedLockFactory.CreateLockAsync(resourceKey, expireTime ?? _expiry);
+            var lockTask = tryLockTimeout.HasValue && retryInterval.HasValue
+                ? _distributedLockFactory.CreateLockAsync(resourceKey, lockTimeout ?? _expiry, tryLockTimeout.Value, retryInterval.Value, cancellationToken)
+                : _distributedLockFactory.CreateLockAsync(resourceKey, lockTimeout ?? _expiry);
 
             using var redLock = await lockTask;
 
             if (!redLock.IsAcquired)
             {
-                throw new PlatformException("Service is busy.");
+                throw new PlatformException($"Resource `{resourceKey}' is currently unavailable due to high demand.");
             }
 
             return await resolver();
