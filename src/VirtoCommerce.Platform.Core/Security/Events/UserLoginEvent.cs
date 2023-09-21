47abefc73ff1ab49511ccd6a057861bb15ed28cc
Oleg Zhuk <zhukoo@gmail.com>
2023-09-21 16:30:17 +0200

    PT-13706: Extend UserLoginEvent event with ExternalLoginInfo (#2696)
    feat: Extend UserLoginEvent event with ExternalLoginInfo which allow to get information from SSO context and refresh the user data from external login information. Ex: roles, details, etc. 
    feat: Remove obsolete methods from ExternalSigninService.
    feat: Obsolete AD option. Moved to VirtoCommerce.AzureAD module.

--- src/VirtoCommerce.Platform.Core/Security/Events/UserLoginEvent.cs
+++ src/VirtoCommerce.Platform.Core/Security/Events/UserLoginEvent.cs
@@ -9,6 +9,20 @@ namespace VirtoCommerce.Platform.Core.Security.Events
             User = user;
         }
 
+        public UserLoginEvent(ApplicationUser user, object externalLoginInfo)
+        {
+            User = user;
+            ExternalLoginInfo = externalLoginInfo;
+        }
+
+        /// <summary>
+        /// Gets the user for the current login
+        /// </summary>
         public ApplicationUser User { get; set; }
+
+        /// <summary>
+        /// Gets the external login information for the current login
+        /// </summary>
+        public object ExternalLoginInfo { get; set; }
     }
 }
