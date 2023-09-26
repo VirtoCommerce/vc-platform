d6cdd64ea9d08b72f58ce0cefbeb9cbe32784120
Oleg Zhuk <zhukoo@gmail.com>
2023-09-26 15:09:52 +0200

    PT-12062: Hangfire.PostgreSql configuration is not applied (#2697)
    fix: Hangfire.PostgreSql configuration specified in appsettings is not applied.

--- src/VirtoCommerce.Platform.Hangfire/Extensions/ServiceCollectionExtensions.cs
+++ src/VirtoCommerce.Platform.Hangfire/Extensions/ServiceCollectionExtensions.cs
@@ -1,16 +1,8 @@
-using Hangfire;
-using Hangfire.MemoryStorage;
-using Hangfire.MySql;
-using Hangfire.PostgreSql;
-using Hangfire.SqlServer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace VirtoCommerce.Platform.Hangfire.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IGlobalConfiguration AddHangfireStorage(this IGlobalConfiguration globalConfiguration, IConfiguration configuration)
+        public static IGlobalConfiguration AddHangfireStorage(this IGlobalConfiguration globalConfiguration, IConfiguration configuration, HangfireOptions hangfireOptions)
         {
             var databaseProvider = configuration.GetValue("DatabaseProvider", "SqlServer");
             var connectionString = configuration.GetConnectionString("VirtoCommerce.Hangfire") ?? configuration.GetConnectionString("VirtoCommerce");
@@ -18,17 +10,13 @@ namespace VirtoCommerce.Platform.Hangfire.Extensions
             switch (databaseProvider)
             {
                 case "PostgreSql":
-                    globalConfiguration.UsePostgreSqlStorage(connectionString);
+                    globalConfiguration.UsePostgreSqlStorage(connectionString, hangfireOptions.PostgreSqlStorageOptions);
                     break;
                 case "MySql":
-                    globalConfiguration.UseStorage(new MySqlStorage(connectionString,
-                        new MySqlStorageOptions { PrepareSchemaIfNecessary = false }));
+                    globalConfiguration.UseStorage(new MySqlStorage(connectionString, hangfireOptions.MySqlStorageOptions));
                     break;
                 default:
-                    // Call UseSqlServerStorage with fake SqlServerStorageOptions to avoid Hangfire tries to apply its migrations because these never do in case of database absence.
-                    // Real options provided in ApplicationBuilderExtensions.UseHangfire where migrations forced to apply.
-                    globalConfiguration.UseSqlServerStorage(connectionString,
-                        new SqlServerStorageOptions { PrepareSchemaIfNecessary = false });
+                    globalConfiguration.UseSqlServerStorage(connectionString, hangfireOptions.SqlServerStorageOptions);
                     break;
             }
 
@@ -42,15 +30,19 @@ namespace VirtoCommerce.Platform.Hangfire.Extensions
             section.Bind(hangfireOptions);
             services.AddOptions<HangfireOptions>().Bind(section).ValidateDataAnnotations();
 
+            hangfireOptions.SqlServerStorageOptions.PrepareSchemaIfNecessary = false;
+            hangfireOptions.PostgreSqlStorageOptions.PrepareSchemaIfNecessary = false;
+            hangfireOptions.MySqlStorageOptions.PrepareSchemaIfNecessary = false;
+
             GlobalJobFilters.Filters.Add(new AutomaticRetryAttribute { Attempts = hangfireOptions.AutomaticRetryCount });
 
             if (hangfireOptions.JobStorageType == HangfireJobStorageType.SqlServer ||
-                hangfireOptions.JobStorageType == HangfireJobStorageType.Database )
+                hangfireOptions.JobStorageType == HangfireJobStorageType.Database)
             {
                 services.AddHangfire(c => c.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
                     .UseSimpleAssemblyNameTypeSerializer()
                     .UseRecommendedSerializerSettings()
-                    .AddHangfireStorage(configuration));
+                    .AddHangfireStorage(configuration, hangfireOptions));
             }
             else
             {
