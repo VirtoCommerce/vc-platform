a18e67b98aa95c6c830f322c3d9709065af34f21
artem-dudarev <ad@virtoway.com>
2023-10-04 17:08:23 +0200

    Fix code smell

--- src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
+++ src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
@@ -4,24 +4,18 @@ namespace VirtoCommerce.Platform.Core.Swagger
 {
     public class SwaggerPlatformOptions
     {
-        private bool _enable = true;
-
         /// <summary>
         /// Disable swagger at the startup (switch off endpoints for swagger UI and docs)
         /// </summary>
         [Obsolete("Use Enable", DiagnosticId = "VC0005", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public bool Disable
         {
-            get => !_enable;
-            set => _enable = !value;
+            get => !Enable;
+            set => Enable = !value;
         }
 
         // We want the Enable property to have a higher priority than Disable,
         // so Enable should come after the Disable because of the implementation details of IConfiguration.Bind().
-        public bool Enable
-        {
-            get => _enable;
-            set => _enable = value;
-        }
+        public bool Enable { get; set; } = true;
     }
 }
