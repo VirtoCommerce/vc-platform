1eea600eacca242fcb89314c501e66dc7b7ca50f
artem-dudarev <ad@virtoway.com>
2023-10-04 16:43:48 +0200

    Avoid double negative in SwaggerPlatformOptions

--- src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
+++ src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
@@ -1,10 +1,27 @@
+using System;
+
 namespace VirtoCommerce.Platform.Core.Swagger
 {
     public class SwaggerPlatformOptions
     {
+        private bool _enable = true;
+
         /// <summary>
         /// Disable swagger at the startup (switch off endpoints for swagger UI and docs)
         /// </summary>
-        public bool Disable { get; set; } = false;
+        [Obsolete("Use Enable", DiagnosticId = "VC0005", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
+        public bool Disable
+        {
+            get => !_enable;
+            set => _enable = !value;
+        }
+
+        // We want the Enable property to have a higher priority than Disable,
+        // so Enable should come after the Disable because of the implementation details of IConfiguration.Bind().
+        public bool Enable
+        {
+            get => _enable;
+            set => _enable = value;
+        }
     }
 }
