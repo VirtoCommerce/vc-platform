deffce400bbfe25ecfa33eb8f87b2e35c270f6d6
Artyom Dudarev <ad@virtoway.com>
2023-10-05 11:39:58 +0300

    PT-13790: Use Enable instead of Disable in SwaggerPlatformOptions (#2703)

--- src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
+++ src/VirtoCommerce.Platform.Core/Swagger/SwaggerPlatformOptions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace VirtoCommerce.Platform.Core.Swagger
 {
     public class SwaggerPlatformOptions
@@ -5,6 +7,15 @@ namespace VirtoCommerce.Platform.Core.Swagger
         /// <summary>
         /// Disable swagger at the startup (switch off endpoints for swagger UI and docs)
         /// </summary>
-        public bool Disable { get; set; } = false;
+        [Obsolete("Use Enable", DiagnosticId = "VC0005", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
+        public bool Disable
+        {
+            get => !Enable;
+            set => Enable = !value;
+        }
+
+        // We want the Enable property to have a higher priority than Disable,
+        // so Enable should come after the Disable because of the implementation details of IConfiguration.Bind().
+        public bool Enable { get; set; } = true;
     }
 }
