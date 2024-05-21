d656d3621e85115e3fa54f043f5276896eb596b8
artem-dudarev <ad@virtoway.com>
2024-05-21 15:03:55 +0200

    Add UnregisterEventHandler()

--- src/VirtoCommerce.Platform.Core/Events/IEventHandlerRegistrar.cs
+++ src/VirtoCommerce.Platform.Core/Events/IEventHandlerRegistrar.cs
@@ -6,6 +6,15 @@ namespace VirtoCommerce.Platform.Core.Events;
 
 public interface IEventHandlerRegistrar
 {
+    [Obsolete("Use IApplicationBuilder.RegisterEventHandler<TEvent, THandler>()", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions")]
     void RegisterEventHandler<T>(Func<T, Task> handler) where T : IEvent;
+
+    [Obsolete("Use IApplicationBuilder.RegisterEventHandler<TEvent, THandler>()", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions")]
     void RegisterEventHandler<T>(Func<T, CancellationToken, Task> handler) where T : IEvent;
+
+    void RegisterEventHandler<T>(IEventHandler<T> handler) where T : IEvent;
+    void RegisterEventHandler<T>(ICancellableEventHandler<T> handler) where T : IEvent;
+
+    void UnregisterEventHandler<T>(Type handlerType = null) where T : IEvent;
+    void UnregisterAllEventHandlers();
 }
