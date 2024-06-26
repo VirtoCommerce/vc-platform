8bd30fbdc11ec8370115f42c0d935f8992a81081
Basil Kotov <basilkot@gmail.com>
2024-06-26 19:37:20 +0100

    remove comments

--- src/VirtoCommerce.Platform.Core/Modularity/ICopyFilePolicy.cs
+++ src/VirtoCommerce.Platform.Core/Modularity/ICopyFilePolicy.cs
@@ -6,11 +6,6 @@ namespace VirtoCommerce.Platform.Core.Modularity;
 public interface ICopyFilePolicy
 {
     public RequireCopyFileReason RequireCopy(Architecture architecture, string sourceFilePath, string targetFilePath);
-
-    //public void NoTarget(RequireCopyFileReason reasons, string targetFilePath);
-    //public void Version(RequireCopyFileReason reasons, string sourceFilePath, string targetFilePath);
-    //public void LaterDate(RequireCopyFileReason reasons, string sourceFilePath, string targetFilePath);
-    //public void Bitwise(RequireCopyFileReason reasons, Architecture architecture, string sourceFilePath, string targetFilePath);
 }
 
 public interface ILibraryVersionProvider
