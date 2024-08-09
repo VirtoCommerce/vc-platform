540eb8f3d88b5b98cc7aee350dfcade7c10cf709
Artem Dudarev <ad@virtoway.com>
2024-08-09 15:40:37 +0200

    VCST-1431: Add external sign in for front end (#2817)

--- src/VirtoCommerce.Platform.Security/Extensions/ClaimsPrincipalExtensions.cs
+++ src/VirtoCommerce.Platform.Security/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using OpenIddict.Abstractions;
@@ -6,14 +7,25 @@ namespace VirtoCommerce.Platform.Security.Extensions
 {
     public static class ClaimsPrincipalExtensions
     {
+        public static bool IsExternalSignIn(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal?.GetAuthenticationMethod() != null;
+        }
+
         public static string GetAuthenticationMethod(this ClaimsPrincipal claimsPrincipal)
         {
             return claimsPrincipal?.GetClaim(ClaimTypes.AuthenticationMethod);
         }
 
+        public static ClaimsPrincipal SetAuthenticationMethod(this ClaimsPrincipal claimsPrincipal, string value, IList<string> destinations)
+        {
+            return claimsPrincipal?.SetClaimWithDestinations(ClaimTypes.AuthenticationMethod, value, destinations);
+        }
+
+        [Obsolete("Use IsExternalSignIn()", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public static bool IsSsoAuthenticationMethod(this ClaimsPrincipal claimsPrincipal)
         {
-            return claimsPrincipal?.GetClaim(ClaimTypes.AuthenticationMethod) != null;
+            return claimsPrincipal.IsExternalSignIn();
         }
 
         public static ClaimsPrincipal SetClaimWithDestinations(this ClaimsPrincipal claimsPrincipal, string type, string value, IList<string> destinations)
