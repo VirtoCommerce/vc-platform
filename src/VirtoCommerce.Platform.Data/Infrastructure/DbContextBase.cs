9bfa659c5bb6e4344e9e1351ad8c492cd330610b
Artem Dudarev <artem@virtoworks.com>
2025-03-06 15:45:58 +0200

    feat: Add ToEntityTable() and ToAuditableEntityTable() extension methods

--- src/VirtoCommerce.Platform.Data/Infrastructure/DbContextBase.cs
+++ src/VirtoCommerce.Platform.Data/Infrastructure/DbContextBase.cs
@@ -9,7 +9,19 @@ namespace VirtoCommerce.Platform.Data.Infrastructure
     /// </summary>
     public class DbContextBase : DbContextWithTriggers
     {
-        public const int IdLength = 128;
+        public const int CultureNameLength = Length16;
+        public const int IdLength = Length128;
+        public const int UrlLength = Length2048;
+        public const int UserNameLength = Length64;
+
+        public const int Length16 = 16;
+        public const int Length32 = 32;
+        public const int Length64 = 64;
+        public const int Length128 = 128;
+        public const int Length256 = 256;
+        public const int Length512 = 512;
+        public const int Length1024 = 1024;
+        public const int Length2048 = 2048;
 
         public DbContextBase()
         {
