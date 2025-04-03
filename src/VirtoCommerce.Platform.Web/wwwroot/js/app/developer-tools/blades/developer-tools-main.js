1c609e72f3685e66dc564af14f6716b214ca98a2
Artem Dudarev <artem@virtoworks.com>
2025-04-03 15:49:35 +0200

    Refactoring

---
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/app/developer-tools/blades/developer-tools-main.js
@@ -0,0 +1,27 @@
+angular.module('platformWebApp')
+    .controller('platformWebApp.developerToolsMainController', ['$scope', 'platformWebApp.developerTools', function ($scope, developerTools) {
+        var blade = $scope.blade;
+        blade.title = 'platform.blades.developer-tools.title';
+        blade.headIcon = 'fab fa-dev';
+        blade.hideToolbar = true;
+        blade.isMaximized = true;
+        blade.isLoading = false;
+
+        $scope.currentUrl = null;
+        $scope.currentName = null;
+        $scope.items = [];
+
+        $scope.items = developerTools.getAll().map(function (tool) {
+            return {
+                name: tool.name,
+                url: document.location.origin + tool.url,
+                executeMethod: function (event) {
+                    $scope.currentUrl = this.url;
+                    $scope.currentName = tool.name;
+                    event.preventDefault();
+                    event.stopPropagation();
+                    return false;
+                },
+            };
+        });
+    }]);
--- src/VirtoCommerce.Platform.Web/wwwroot/js/app/developer-tools/developer-tools.js
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/app/developer-tools/developer-tools.js
@@ -1,21 +1,46 @@
 angular.module('platformWebApp')
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider.state('workspace.developer-tools', {
-            url: '/devtools',
+            url: '/developer-tools',
             templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
             controller: ['platformWebApp.bladeNavigationService',
                 function (bladeNavigationService) {
                     var newBlade = {
                         id: 'developer-tools',
-                        controller: 'platformWebApp.developerToolsController',
-                        template: '$(Platform)/Scripts/app/developer-tools/blades/developer-tools.html',
-                        hideToolbar: true
+                        controller: 'platformWebApp.developerToolsMainController',
+                        template: '$(Platform)/Scripts/app/developer-tools/blades/developer-tools-main.html',
                     };
-
                     bladeNavigationService.showBlade(newBlade);
                 }]
         });
     }])
+    .factory('platformWebApp.developerTools',
+        [function () {
+            var _tools = [
+                {
+                    name: 'Swagger',
+                    url: '/docs/index.html'
+                },
+                {
+                    name: 'Hangfire',
+                    url: '/hangfire'
+                },
+                {
+                    name: 'Health',
+                    url: '/health'
+                },
+            ];
+
+            return {
+                add: function (tool) {
+                    _tools.push(tool);
+                },
+                getAll: function () {
+                    return _tools;
+                }
+            };
+        }]
+    )
     .run(
         ['$state', 'platformWebApp.mainMenuService',
             function ($state, mainMenuService,) {
@@ -23,7 +48,7 @@ angular.module('platformWebApp')
                     path: 'configuration/developer-tools',
                     icon: 'fab fa-dev',
                     title: 'platform.menu.developer-tools',
-                    priority: 10,
+                    priority: Number.MAX_SAFE_INTEGER - 1,
                     action: function () { $state.go('workspace.developer-tools'); },
                 };
                 mainMenuService.addMenuItem(menuItem);
