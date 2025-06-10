bf01891bfca38ea92ea868877931d36fa4bb01d9
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-06-10 21:36:28 +0200

    for tokens

--- src/VirtoCommerce.Platform.Data.SqlServer/SecurityConfiguraion.cs
+++ src/VirtoCommerce.Platform.Data.SqlServer/SecurityConfiguraion.cs
@@ -5,12 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenIddict.EntityFrameworkCore.Models;
 using VirtoCommerce.Platform.Core.Security;
 
 namespace VirtoCommerce.Platform.Data.SqlServer;
 public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
 {
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable(tb => tb.UseSqlOutputClause(false));
+    }
+}
+
+public class OpenIddictTokenConfiguration : IEntityTypeConfiguration<OpenIddictEntityFrameworkCoreToken>
+{
+    public void Configure(EntityTypeBuilder<OpenIddictEntityFrameworkCoreToken> builder)
     {
         builder.ToTable(tb => tb.UseSqlOutputClause(false));
     }
