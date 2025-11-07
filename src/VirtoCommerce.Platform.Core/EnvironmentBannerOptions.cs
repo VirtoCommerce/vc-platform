a03cda90c66339c007add848e9039323c7b13986
Oleg Zhuk <zhukoo@gmail.com>
2025-11-07 20:37:59 +0200

    VCST-4234: Environment Badge to Distinguish Admin Environments.
    feat: Environment Badge to Distinguish Admin Environments.

---
+++ src/VirtoCommerce.Platform.Core/EnvironmentBannerOptions.cs
@@ -0,0 +1,11 @@
+namespace VirtoCommerce.Platform.Core;
+
+public class EnvironmentBannerOptions
+{
+    public bool Enable { get; set; } = false;
+    public string Name { get; set; }
+    public string ThemePreset { get; set; }
+}
+
+
+
--- src/VirtoCommerce.Platform.Core/PlatformUIOptions.cs
+++ src/VirtoCommerce.Platform.Core/PlatformUIOptions.cs
@@ -3,5 +3,7 @@ namespace VirtoCommerce.Platform.Core
     public class PlatformUIOptions
     {
         public bool Enable { get; set; } = true;
+
+        public EnvironmentBannerOptions EnvironmentBanner { get; set; } = new EnvironmentBannerOptions();
     }
 }
