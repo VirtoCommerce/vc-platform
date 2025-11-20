9c75e235ae8330e68d59cdb30dc9ec2bcc70dbf8
Oleg Zhuk <zhukoo@gmail.com>
2025-11-20 17:57:11 +0200

    Change default value of Enable to true

--- src/VirtoCommerce.Platform.Core/EnvironmentBannerOptions.cs
+++ src/VirtoCommerce.Platform.Core/EnvironmentBannerOptions.cs
@@ -2,7 +2,7 @@ namespace VirtoCommerce.Platform.Core;
 
 public class EnvironmentBannerOptions
 {
-    public bool Enable { get; set; } = false;
+    public bool Enable { get; set; } = true;
     public string ThemePreset { get; set; }
     public string Name { get; set; }
 }
