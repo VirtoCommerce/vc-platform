9a945efa028b8b727e77d3f5fa0fd72cf8aa5c61
Aleksandr Vishniakov <av@virtoway.com>
2025-12-02 20:04:41 +0200

    Fix sonar issue

--- src/VirtoCommerce.Platform.Web/Swagger/SwaggerServiceCollectionExtensions.cs
+++ src/VirtoCommerce.Platform.Web/Swagger/SwaggerServiceCollectionExtensions.cs
@@ -93,58 +93,8 @@ namespace VirtoCommerce.Platform.Web.Swagger
                 c.AddModulesXmlComments(provider);
                 c.CustomOperationIds(apiDesc =>
                     apiDesc.TryGetMethodInfo(out var methodInfo) ? $"{((ControllerActionDescriptor)apiDesc.ActionDescriptor).ControllerName}_{methodInfo.Name}" : null);
-                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.OAuth2,
-                    Description = "OAuth2 Resource Owner Password Grant flow",
-                    Flows = new OpenApiOAuthFlows
-                    {
-                        ClientCredentials = new OpenApiOAuthFlow
-                        {
-                            TokenUrl = new Uri("/connect/token", UriKind.Relative)
-                        },
-                        Password = new OpenApiOAuthFlow
-                        {
-                            TokenUrl = new Uri("/connect/token", UriKind.Relative)
-                        }
-                    },
-                });
-                c.AddSecurityDefinition("api_key", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.ApiKey,
-                    Description = "API Key authentication",
-                    In = ParameterLocation.Query,
-                    Name = "api_key",
-                });
-                c.AddSecurityDefinition("api_key_header", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.ApiKey,
-                    Description = "API Key authentication (alternative via header)",
-                    In = ParameterLocation.Header,
-                    Name = "api_key",
-                });
-                c.AddSecurityDefinition("http-signature", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "signature",
-                    Description = "HTTP Signature authentication using Authorization header",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                });
-                c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "basic",
-                    Description = "Basic authentication using username and password",
-                });
 
-                // Register SecurityRequirementsOperationFilter for each security scheme
-                // This allows API clients to use any of the supported authentication methods
-                c.OperationFilter<SecurityRequirementsOperationFilter>(true, "oauth2");
-                c.OperationFilter<SecurityRequirementsOperationFilter>(true, "api_key");
-                c.OperationFilter<SecurityRequirementsOperationFilter>(true, "api_key_header");
-                c.OperationFilter<SecurityRequirementsOperationFilter>(true, "http-signature");
-                c.OperationFilter<SecurityRequirementsOperationFilter>(true, "basic");
+                c.AddSecuritySchemes();
 
                 c.DocInclusionPredicate((docName, apiDesc) => DocInclusionPredicateCustomStrategy(modules, docName, apiDesc));
                 c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
@@ -288,6 +238,63 @@ namespace VirtoCommerce.Platform.Web.Swagger
                     options.IncludeXmlComments(xmlComment);
                 }
             }
+        }
+
+        /// <summary>
+        /// Add security schemes definitions and operation filters for authentication
+        /// </summary>
+        private static void AddSecuritySchemes(this SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Description = "OAuth2 Resource Owner Password Grant flow",
+                Flows = new OpenApiOAuthFlows
+                {
+                    ClientCredentials = new OpenApiOAuthFlow
+                    {
+                        TokenUrl = new Uri("/connect/token", UriKind.Relative)
+                    },
+                    Password = new OpenApiOAuthFlow
+                    {
+                        TokenUrl = new Uri("/connect/token", UriKind.Relative)
+                    }
+                },
+            });
+            options.AddSecurityDefinition("api_key", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.ApiKey,
+                Description = "API Key authentication",
+                In = ParameterLocation.Query,
+                Name = "api_key",
+            });
+            options.AddSecurityDefinition("api_key_header", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.ApiKey,
+                Description = "API Key authentication (alternative via header)",
+                In = ParameterLocation.Header,
+                Name = "api_key",
+            });
+            options.AddSecurityDefinition("http-signature", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "signature",
+                Description = "HTTP Signature authentication using Authorization header",
+            });
+            options.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "basic",
+                Description = "Basic authentication using username and password",
+            });
+
+            // Register SecurityRequirementsOperationFilter for each security scheme
+            // This allows API clients to use any of the supported authentication methods
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, "oauth2");
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, "api_key");
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, "api_key_header");
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, "http-signature");
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, "basic");
         }
     }
 }
