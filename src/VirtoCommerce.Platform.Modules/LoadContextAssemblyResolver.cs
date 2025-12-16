ae8df3f81b9754dfb23255d6e26f432df568ba90
Oleg Zhuk <zhukoo@gmail.com>
2025-12-16 08:31:46 +0200

    fix The call is ambiguous between the following methods or properties

--- src/VirtoCommerce.Platform.Modules/LoadContextAssemblyResolver.cs
+++ src/VirtoCommerce.Platform.Modules/LoadContextAssemblyResolver.cs
@@ -24,6 +24,7 @@ public class LoadContextAssemblyResolver : IAssemblyResolver
         "OpenIddict.Mvc",
         "CryptoHelper",
         "Microsoft.EntityFrameworkCore.Design",
+        "System.Linq.Async",
     ];
 
     public LoadContextAssemblyResolver(ILogger<LoadContextAssemblyResolver> logger, bool isDevelopmentEnvironment)
