5ce3c55e468943fe8723a897b97a9389fade035c
Basil Kotov <basilkot@gmail.com>
2026-04-15 14:55:52 +0100

    VCST-4834: Add reusable delete confirmation dialog with "type yes" requirement` (#2995)
    Co-authored-by: Oleg Zhuk <zhukoo@gmail.com>
    Co-authored-by: Elena Kutasina <kutasina.elena@gmail.com>

--- src/VirtoCommerce.Platform.Web/wwwroot/js/app/security/blades/account-list.js
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/app/security/blades/account-list.js
@@ -41,7 +41,9 @@ function ($scope, accounts, dialogService, uiGridHelper, bladeNavigationService,
         var dialog = {
             id: "confirmDeleteItem",
             title: "platform.dialogs.account-delete.title",
-            message: "platform.dialogs.account-delete.message",
+            items: [
+                { key: 'platform.dialogs.account-delete.account', count: selection.length }
+            ],
             callback: function (remove) {
                 if (remove) {
                     bladeNavigationService.closeChildrenBlades(blade, function () {
@@ -51,7 +53,7 @@ function ($scope, accounts, dialogService, uiGridHelper, bladeNavigationService,
                 }
             }
         };
-        dialogService.showConfirmationDialog(dialog);
+        dialogService.showDeleteConfirmationDialog(dialog);
     };
 
     blade.headIcon = 'fas fa-key';
