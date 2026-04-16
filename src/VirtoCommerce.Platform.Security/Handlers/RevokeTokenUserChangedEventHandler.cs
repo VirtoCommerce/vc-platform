721b3c9b60dad06886ca727cefa2d783aabb7897
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2026-04-16 19:02:36 +0200

    fix: cursor

--- src/VirtoCommerce.Platform.Security/Handlers/RevokeTokenUserChangedEventHandler.cs
+++ src/VirtoCommerce.Platform.Security/Handlers/RevokeTokenUserChangedEventHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using OpenIddict.Abstractions;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.Platform.Core.Security;
@@ -19,20 +20,29 @@ public class RevokeUserTokenEventHandler(Func<(IUserSessionsService SessionServi
             {
                 if (!changedEntry.NewEntry.LockoutEnd.IsEmpty())
                 {
-                    await TerminateAllUserSessions(changedEntry.NewEntry.Id);
+                    await RevokeUserTokensAsync(changedEntry.NewEntry.Id);
                 }
             }
             else if (changedEntry.EntryState == EntryState.Deleted)
             {
-                await TerminateAllUserSessions(changedEntry.NewEntry.Id);
+                await RevokeUserTokensAsync(changedEntry.NewEntry.Id);
             }
         }
     }
 
-    private async Task TerminateAllUserSessions(string userId)
+    protected virtual async Task RevokeUserTokensAsync(string userId)
     {
         var (SessionService, Scope) = userSessionsServiceFactory();
         using var scope = Scope;
         await SessionService.TerminateAllUserSessions(userId);
+    }
+
+    [Obsolete("Use RevokeUserTokensAsync(string userId) class instead.", DiagnosticId = "VC0014", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions")]
+    protected virtual async Task RevokeUserTokensAsync(IOpenIddictTokenManager tokenManager, string userId)
+    {
+        await foreach (var token in tokenManager.FindBySubjectAsync(userId))
+        {
+            await tokenManager.TryRevokeAsync(token);
+        }
     }
 }
