b3eea6c8db7bc096387e6ed82ff8d9a805bd3a8c
Oleg Zhuk <zhukoo@gmail.com>
2026-05-28 12:22:19 +0200

    fix: Filter panel

---
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/common/directives/filterPanel.js
@@ -0,0 +1,46 @@
+angular.module('platformWebApp')
+    .directive('vaFilterPanel', ['$document', function ($document) {
+        return {
+            restrict: 'E',
+            templateUrl: '$(Platform)/Scripts/common/directives/filterPanel.tpl.html',
+            transclude: true,
+            scope: {
+                hasActiveFilters: '&',
+                onClearFilters: '&',
+                searchText: '=?',
+                hideSearch: '=?',
+                searchPlaceholder: '@?',
+                filterTitle: '@?',
+                hideFilter: '=?'
+            },
+            link: function (scope, element) {
+                scope.showPanel = false;
+
+                scope.togglePanel = function ($event) {
+                    if ($event) {
+                        $event.stopPropagation();
+                    }
+                    scope.showPanel = !scope.showPanel;
+                };
+
+                scope.clearFilters = function () {
+                    scope.onClearFilters();
+                    scope.showPanel = false;
+                };
+
+                // Close panel on outside click
+                function onDocumentClick(e) {
+                    if (scope.showPanel && !element[0].contains(e.target)) {
+                        scope.$apply(function () {
+                            scope.showPanel = false;
+                        });
+                    }
+                }
+
+                $document.on('click', onDocumentClick);
+                scope.$on('$destroy', function () {
+                    $document.off('click', onDocumentClick);
+                });
+            }
+        };
+    }]);
