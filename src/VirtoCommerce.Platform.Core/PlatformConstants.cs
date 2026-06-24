f4758afe550049df78c81fb1453113b39907f3e8
Oleg Zhuk <zhukoo@gmail.com>
2026-06-24 17:36:20 +0200

    VCST-5232: Improve dashboard style
    feat: Improve dashboard style

--- src/VirtoCommerce.Platform.Core/PlatformConstants.cs
+++ src/VirtoCommerce.Platform.Core/PlatformConstants.cs
@@ -462,6 +462,7 @@ namespace VirtoCommerce.Platform.Core
                     ValueType = SettingValueType.Json,
                     DefaultValue = "{\n" +
                                                "  \"title\": \"Virto Commerce\",\n" +
+                                               "  \"subtitle\": \"Your store at a glance\",\n" +
                                                "  \"logo\": \"/images/logo.png\",\n" +
                                                "  \"contrast_logo\": \"/images/logo-small.svg\"\n" +
                                                "}"
